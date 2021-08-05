319fdbace88893b8ed6b6fd7c2a35e529b97bd41
James McMahon <james.mcmahon@phs.scot>
2021-08-05 09:49:49 +0100

    Simplify and tidy up R code Created a function for writing to the 'log' file Dropped `stringr` in favour of `glue`

--- Make_R_files/Make SLFs.R
+++ Make_R_files/Make SLFs.R
@@ -1,141 +1,107 @@
-# install.packages("fst")
-
 library(haven)
 library(janitor)
 library(dplyr)
 library(fst)
-library(stringr)
+library(glue)
 library(readr)
 library(furrr)
 library(fs)
-plan("multisession")
+future::plan("multisession")
 
-create_fst_files <- function(year) {
-  start_time <- Sys.time()
+write_to_log <- function(message) {
+  write_lines(message,
+    file = "Make_R_files/log.txt",
+    append = TRUE
+  )
+}
 
-  slf_sourcedev_dir <- path("/conf/sourcedev/Source Linkage File Updates")
-
-  indiv_path <-
-    path(slf_sourcedev_dir, year, str_glue("source-individual-file-20{year}"))
-  episode_path <-
-    path(slf_sourcedev_dir, year, str_glue("/source-episode-file-20{year}"))
-
-
-  # Create individual file.
-  if (file_exists(path_ext_set(indiv_path, ".zsav"))) {
-    write_lines(
-      str_glue("Starting individual file for 20{year}.\nIt is now: {start_time}"),
-      file = "log",
-      append = TRUE
-    )
-
-    if (!file_exists(path_ext_set(indiv_path, ".fst"))) {
-      read_sav(path_ext_set(indiv_path, ".zsav")) %>%
-        rename_all(tolower) %>%
-        as_tibble() %>%
-        write_fst(path_ext_set(indiv_path, ".fst"), compress = 100)
-    } else {
-      write_lines(
-        str_glue("Skipping 20{year} as fst file already exists in sourcedev"),
-        file = "log",
-        append = TRUE
-      )
-    }
-  } else {
-    write_lines(
-      str_glue("Skipping 20{year} as zsav file doesn't exist"),
-      file = "log",
-      append = TRUE
-    )
-  }
-
-
-  # Create episode file.
-  if (file_exists(path_ext_set(episode_path, ".zsav"))) {
-    write_lines(
-      str_glue("Starting episode file for 20{year}.\nIt is now: {Sys.time()}"),
-      file = "log",
-      append = TRUE
-    )
-
-    if (!file_exists(path_ext_set(episode_path, ".fst"))) {
-      read_sav(path_ext_set(episode_path, ".zsav")) %>%
-        rename_all(tolower) %>%
-        as_tibble() %>%
-        write_fst(path_ext_set(episode_path, ".fst"), compress = 100)
-    } else {
-      write_lines(
-        str_glue("Skipping 20{year} as fst file already exists in sourcedev"),
-        file = "log",
-        append = TRUE
-      )
-    }
-  } else {
-    write_lines(str_glue("Skipping 20{year}, as zsav file doesn't exist"),
-      file = "log",
-      append = TRUE
-    )
-  }
-
-  end_time <- Sys.time()
+pretty_time_diff <- function(start_time, end_time) {
   time_diff <- difftime(end_time, start_time, units = "mins") %>%
     as.double() %>%
     round_half_up(digits = 1)
 
-  write_lines(str_glue("Done with {year} at {end_time}\nIt took:{time_diff} minutes"),
-    file = "log",
-    append = TRUE
-  )
+  return(time_diff)
+}
+
+zsav_to_fst <- function(file, compress = 100) {
+  read_sav(file) %>%
+    rename_all(tolower) %>%
+    as_tibble() %>%
+    write_fst(path_ext_set(file, ".fst"), compress = compress)
+}
+
+create_fst_files <- function(year) {
+  start_time <- Sys.time()
+
+  slf_sourcedev_dir <- path("/conf", "sourcedev", "Source_Linkage_File_Updates", year)
+
+  indiv_file <-
+    path(slf_sourcedev_dir, glue("source-individual-file-20{year}.zsav"))
+  ep_file <-
+    path(slf_sourcedev_dir, glue("source-episode-file-20{year}.zsav"))
+
+
+  if (file_exists(indiv_file)) {
+    if (!file_exists(path_ext_set(indiv_file, ".fst"))) {
+      write_to_log(glue("Starting individual file for 20{year}.\nIt is now: {start_time}"))
+
+      # Create individual file
+      zsav_to_fst(indiv_file)
+
+    } else {
+      write_to_log(glue("Skipping 20{year} individual, as the fst file already exists in sourcedev"))
+    }
+  } else {
+    write_to_log(glue("Skipping 20{year} individual, as the zsav file doesn't exist in sourcedev"))
+  }
+
+
+  if (file_exists(ep_file)) {
+    if (!file_exists(path_ext_set(ep_file, ".fst"))) {
+      write_to_log(glue("Starting episode file for 20{year}.\nIt is now: {Sys.time()}"))
+
+      # Create episode file
+      zsav_to_fst(ep_file)
+
+    } else {
+      write_to_log(glue("Skipping 20{year} episode, as the fst file already exists in sourcedev"))
+    }
+  } else {
+    write_to_log(glue("Skipping 20{year} episode, as the zsav file doesn't exist in sourcedev"))
+  }
+
+  end_time <- Sys.time()
+  time_diff <- pretty_time_diff(start_time, end_time)
+
+  write_to_log(glue("Done with {year} at {end_time}\nIt took: {time_diff} minutes"))
 }
 
 create_fst_lookups <- function() {
   start_time <- Sys.time()
 
+  hscdiip_slf_dir <- path("/conf", "hscdiip", "01-Source-linkage-files")
+
   anon_chi_lookup <-
-    path("/conf/hscdiip/01-Source-linkage-files/Anon-to-CHI-lookup")
+    path(hscdiip_slf_dir, "Anon-to-CHI-lookup.zsav")
   chi_lookup <-
-    path("/conf/hscdiip/01-Source-linkage-files/CHI-to-Anon-lookup")
+    path(hscdiip_slf_dir, "CHI-to-Anon-lookup.zsav")
 
-  write_lines(
-    str_glue("Starting Anon CHI lookup it is now: {start_time}"),
-    file = "log",
-    append = TRUE
-  )
+  write_to_log(glue("Starting Anon CHI lookup it is now: {start_time}"))
 
-  read_sav(path_ext_set(anon_chi_lookup, ".zsav")) %>%
-    rename_all(tolower) %>%
-    as_tibble() %>%
-    write_fst(path_ext_set(anon_chi_lookup, ".fst"), compress = 50)
+  zsav_to_fst(anon_chi_lookup, compress = 50)
 
-  write_lines(
-    str_glue("Starting CHI lookup it is now: {Sys.time()}"),
-    file = "log",
-    append = TRUE
-  )
+  write_to_log(glue("Starting CHI lookup it is now: {Sys.time()}"))
 
-  read_sav(path_ext_set(chi_lookup, ".zsav")) %>%
-    rename_all(tolower) %>%
-    as_tibble() %>%
-    write_fst(path_ext_set(chi_lookup, ".fst"), compress = 50)
+  zsav_to_fst(chi_lookup, compress = 50)
 
   end_time <- Sys.time()
-  time_diff <- difftime(end_time, start_time, units = "mins") %>%
-    as.double() %>%
-    round_half_up(digits = 1)
+  time_diff <- pretty_time_diff(start_time, end_time)
 
-  write_lines(
-    str_glue("Done with lookups at {end_time}.\nIt took: {time_diff} minutes"),
-    file = "log",
-    append = TRUE
-  )
+  write_to_log(glue("Done with lookups at {end_time}.\nIt took: {time_diff} minutes"))
 }
 
 years <- list("1819", "1920", "2021")
 
-future_map(
-  years,
-  ~ create_fst_files(.x),
-  .progress = TRUE
-)
+future_map(years, ~create_fst_files(.x))
 
 create_fst_lookups()
