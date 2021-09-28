b88ad61a904ed8324122184ee04dfbc83feeb187
Moohan <james.mcmahon@phs.scot>
2021-09-28 12:01:28 +0100

    add locality lookup read function

---
+++ R_functions/read_lookups.R
@@ -0,0 +1,27 @@
+get_lookups_dir <- function() {
+  fs::path("/conf/linkage/output/lookups/Unicode/")
+}
+
+
+#' Read the locality file
+#'
+#' @param file - the file name of the locality file
+#'
+#' @return The data read using `readr::read_rds``
+#' @export
+#'
+#' @examples
+#' locality_file <- read_locality_file("HSCP Localities_DZ11_Lookup_20200825.rds")
+read_locality_file <- function(file) {
+  locality_path <- fs::path(get_lookups_dir, "Geography", "HSCP Locality", file)
+
+  # If given a sav extension (or other), swap it for rds
+  fs::path_ext_set(locality_path, "rds")
+
+  # Check if the file exists and we can read it
+  if (!fs::file_access(locality_path, "read")) {
+    rlang::abort(message = "Couldn't read the locality file")
+  }
+
+  return(readr::read_rds(locality_path))
+}
