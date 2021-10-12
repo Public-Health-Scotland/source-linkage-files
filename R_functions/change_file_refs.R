bb1ca9b8ea4ca843370eb6fc1c3332e7820a5922
jennit07 <jennit07@nssrstudio05.csa.scot.nhs.uk>
2021-10-12 14:51:24 +0100

    Add function for file refs

---
+++ R_functions/change_file_refs.R
@@ -0,0 +1,22 @@
+#' Change file references
+#'
+#' @param file - the file name will need updating each update
+#'
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#'
+IT_extract_ref <- function() {
+  "SCTASK0247528"
+}
+
+latest_update <- function() {
+  "Dec_2021"
+}
+
+delayed_discharges_period <- function() {
+  "Jul16_Jun21"
+}
