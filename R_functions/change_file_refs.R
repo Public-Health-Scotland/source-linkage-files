156bc266e1b137b6ccd5e1a7480fe0178324275b
Jennifer Thom <jennifer.thom2@phs.scot>
2021-10-26 15:47:05 +0100

    Create functions which need updating
    - These functions are usually updated when files change or during a new update month

---
+++ R_functions/change_file_refs.R
@@ -0,0 +1,22 @@
+#' Change file references
+#'
+#' @param file - the file name will need updating each update
+#'
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#'
+IT_extract_ref <- function() {
+  "SCTASK0247528"
+}
+
+latest_update <- function() {
+  "Sep_2021"
+}
+
+dd_period <- function() {
+  "Jul16_Jun21"
+}
