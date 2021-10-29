4e2741bf6ef28be8553b5f74374f18911c174ee2
James McMahon <james.mcmahon@phs.scot>
2021-10-29 10:17:49 +0100

    Update functions to read lookups (instead of returning the path)

--- R_functions/read_anon_chi_lookups.R
+++ R_functions/read_anon_chi_lookups.R
@@ -8,10 +8,18 @@
 #'
 #' @examples
 #' chi_to_anon <- chi_to_anon()
-chi_to_anon <- function() {
-  "/conf/hscdiip/01-Source-linkage-files/CHI-to-Anon-lookup.zsav"
+read_chi_to_anon <- function() {
+  chi_to_anon_path <- fs::path("/conf/hscdiip/01-Source-linkage-files/CHI-to-Anon-lookup.zsav")
+
+  chi_to_anon <- haven::read_sav(chi_to_anon_path)
+
+  return(chi_to_anon)
 }
 
-anon_to_chi <- function() {
-  "/conf/hscdiip/01-Source-linkage-files/Anon-to-CHI-lookup.zsav"
+read_anon_to_chi <- function() {
+  anon_to_chi_path <- fs::path("/conf/hscdiip/01-Source-linkage-files/Anon-to-CHI-lookup.zsav")
+
+  anon_to_chi <- haven::read_sav(anon_to_chi_path)
+
+  return(anon_to_chi)
 }
