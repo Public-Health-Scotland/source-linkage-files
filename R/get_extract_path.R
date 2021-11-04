fc46b7179ef1e44ac1e9da1c2f09b1b0e38ddf0e
Jennifer Thom <jennifer.thom@phs.scot>
2021-11-04 14:46:39 +0000

    Tidy Extract path function

--- R/get_extract_path.R
+++ R/get_extract_path.R
@@ -3,23 +3,22 @@
 #' @param year
 #' @param type
 #'
-#' @return
+#' @return BOXI extracts containing data for each dataset
 #' @export
 #'
-#' @examples
 extract_path <- function(year, type = c("Acute", "Mental")) {
-  year_dir <- path("/conf/sourcedev/Source_Linkage_File_Updates", year, "Extracts")
+  year_dir <- fs::path("/conf/sourcedev/Source_Linkage_File_Updates", year, "Extracts")
 
-  file_name <- case_when(
+  file_name <- dplyr::case_when(
     type == "Acute" ~ "Acute-episode-level-extract",
     type == "Mental" ~ "Mental-Health-episode-level-extract"
   )
 
-  file_path <- path(year_dir, glue::glue("{file_name}-20{year}.csv.gz"))
+  file_path <- fs::path(year_dir, glue::glue("{file_name}-20{year}.csv.gz"))
 
-  if (file_exists(path_ext_remove(file_path))) {
-    file_path <- path_ext_remove(file_path)
-  } else if (!file_exists(file_path)) {
+  if (fs::file_exists(fs::path_ext_remove(file_path))) {
+    file_path <- fs::path_ext_remove(file_path)
+  } else if (! fs::file_exists(file_path)) {
     rlang::abort(glue::glue("{type} Extract not found"))
   }
 
