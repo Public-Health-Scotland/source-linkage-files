6539238eacca789a4fd96d68c5bcef5b752b3543
James McMahon <james.mcmahon@phs.scot>
2021-11-04 12:46:54 +0000

    Document `is_integer_like`

--- All_years/04-Social_Care/02a-hc_functions.R
+++ All_years/04-Social_Care/02a-hc_functions.R
@@ -60,6 +60,28 @@ read_demog_file <- function(social_care_dir, latest_update) {
 }
 
 
+#' Check if an object looks like an integer
+#'
+#' @description Take a vector and check to see if
+#' it looks like it contains only integers.
+#'
+#' @param x a vector of values to check
+#'
+#' @return TRUE or FALSE indicating if all values
+#' look like integers - if the vector is not of type
+#' character or numeric it will always return FALSE
+#' @export
+#'
+#' @examples
+#' library(dplyr)
+#'data <-
+#'  tibble(
+#'    x = c(1, 2, 3),
+#'    y = c("4", "5", "6"),
+#'    z = c("a", "b", "c")
+#'  )
+#'data %>%
+#'  mutate(across(where(is_integer_like), as.integer))
 is_integer_like <- function(x) {
   values <- unique(x)
 
