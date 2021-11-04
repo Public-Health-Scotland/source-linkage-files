53419966e6f4f3f65938271e23876cc1ab53ed06
Jennifer Thom <jennifer.thom@phs.scot>
2021-11-04 13:18:15 +0000

    Move R_functions to R folder

---
+++ R/change_file_refs.R
@@ -0,0 +1,24 @@
+#' Get the IT extract ref number
+#'
+#' @return the IT extract ref number
+#' @export
+IT_extract_ref <- function() {
+  "SCTASK0247528"
+}
+
+#' Get the latest update
+#'
+#' @return Latest update as MMM_YYYY
+#' @export
+latest_update <- function() {
+  "Sep_2021"
+}
+
+#' Get the Delayed Discharge period
+#'
+#' @return The period for the Delayed Discharge file
+#' as MMMYY_MMMYY
+#' @export
+dd_period <- function() {
+  "Jul16_Jun21"
+}
