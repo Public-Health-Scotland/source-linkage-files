d4cdc0762047d38ab9b5a9d36fd8c309c35ba441
James McMahon <james.mcmahon@phs.scot>
2022-01-07 13:54:27 +0000

    Improve documentation

--- R/read_lookups.R
+++ R/read_lookups.R
@@ -1,3 +1,7 @@
+#' Get the path to the lookups directory
+#'
+#' @return the Lookups directory path as a [fs::path]
+#' @export
 get_lookups_dir <- function() {
   fs::path("/conf/linkage/output/lookups/Unicode")
 }
@@ -7,11 +11,8 @@ get_lookups_dir <- function() {
 #'
 #' @param file - the file name of the locality file
 #'
-#' @return The data read using `readr::read_rds``
+#' @return The data read using `readr::read_rds`
 #' @export
-#'
-#' @examples
-#' locality_file <- read_locality_file("HSCP Localities_DZ11_Lookup_20200825.rds")
 read_locality_file <- function(file) {
   locality_path <- fs::path(get_lookups_dir(), "Geography", "HSCP Locality", file)
 
@@ -27,16 +28,12 @@ read_locality_file <- function(file) {
 }
 
 
-
 #' Read the Scottish Postcode Directory file
 #'
 #' @param file - the file name of the spd file
 #'
-#' @return The data read using `readr::read_rds``
+#' @return The data read using `readr::read_rds`
 #' @export
-#'
-#' @examples
-#' spd_file <- read_spd_file("Scottish_Postcode_Directory_2021_1.rds")
 read_spd_file <- function(file) {
   spd_path <- fs::path(get_lookups_dir(), "Geography", "Scottish Postcode Directory", file)
 
@@ -52,15 +49,11 @@ read_spd_file <- function(file) {
 }
 
 
-
 #' Read the Scottish Index for Multiple Deprivation (SIMD)
 #' @param file - the file name of the simd file
 #'
-#' @return The data read using `readr::read_rds``
+#' @return The data read using `readr::read_rds`
 #' @export
-#'
-#' @examples
-#' simd_file <- read_simd_file("postcode_2021_1_simd2020v2.rds")
 read_simd_file <- function(file) {
   simd_path <- fs::path(get_lookups_dir(), "Deprivation", file)
 
@@ -76,15 +69,11 @@ read_simd_file <- function(file) {
 }
 
 
-
 #' Read the Datazone populations
 #' @param file - the file name of the datazone populations file
 #'
-#' @return The data read using `readr::read_rds``
+#' @return The data read using `readr::read_rds`
 #' @export
-#'
-#' @examples
-#' datazone_pop_file <- read_datazone_pop_file("DataZone2011_pop_est_2011_2019.rds")
 read_datazone_pop_file <- function(file) {
   datazone_pop_path <- fs::path(get_lookups_dir(), "Populations", "Estimates", file)
 
