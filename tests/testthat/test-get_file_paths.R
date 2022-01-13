9ccb4c9248aca4b4b13cf2ec8dd14ba6c9b11852
James McMahon <james.mcmahon@phs.scot>
2022-01-13 13:12:25 +0000

    Refactor SPARRA and HHG functions to their own script

--- tests/testthat/test-get_file_paths.R
+++ tests/testthat/test-get_file_paths.R
@@ -85,25 +85,3 @@ test_that("Deaths file returns data", {
   expect_s3_class(deaths_file, "tbl_df")
   expect_length(deaths_file, 4)
 })
-
-
-test_that("HHG file path works", {
-  expect_s3_class(get_hhg_path("1920"), "fs_path")
-
-  expect_error(get_hhg_path(), "\"year\" is missing, with no default")
-  expect_error(
-    get_hhg_path("1920", ext = "rds"),
-    "The file HHG-201920.rds does not exist in /conf/hscdiip/SLF_Extracts/HHG"
-  )
-})
-
-
-test_that("SPARRA file path works", {
-  expect_s3_class(get_sparra_path("1920"), "fs_path")
-
-  expect_error(get_sparra_path(), "\"year\" is missing, with no default")
-  expect_error(
-    get_sparra_path("1920", ext = "rds"),
-    "The file SPARRA-201920.rds does not exist in /conf/hscdiip/SLF_Extracts/SPARRA"
-  )
-})
