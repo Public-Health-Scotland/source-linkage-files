c0e61059871dc31877520ef28af4c324c3571d74
Jennifer Thom <jennifer.thom@phs.scot>
2022-01-14 15:17:46 +0000

    Create demographic lookup tests script

---
+++ All_years/02-Lookups/Lookup-05-Social_Care_Demographics-tests.R
@@ -0,0 +1,29 @@
+#Demographic Lookup tests
+#Required functions:
+# get_slf_dir
+# latest_update
+# previous_update
+# read_lookups_dir
+#sum_flags
+#demog_lookup_tests
+#compare_tests
+
+library(tidyselect)
+library(dplyr)
+
+####################################################
+#Get demographic lookup data
+new_demog_lookup <- read_demog_lookup(file)
+old_demog_lookup <- read_demog_lookup(file, update = previous_update())
+
+####################################################
+#Create new and old dataframes with measures for testing
+new_tests <- demog_lookup_tests(new_demog_lookup)
+old_tests <- demog_lookup_tests(old_demog_lookup)
+
+####################################################
+#create tests
+comparison <- compare_tests(old_tests, new_tests)
+
+#END OF SCRIPT
+####################################################
