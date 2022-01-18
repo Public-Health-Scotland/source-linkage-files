d612e7790552f1ce9e2b630eb88dda90c281d21e
James McMahon <james.mcmahon@phs.scot>
2022-01-18 13:50:52 +0000

    Refactor Costs path functions to their own scripts

---
+++ tests/testthat/test-get_costs_paths.R
@@ -0,0 +1,5 @@
+test_that("Costs paths work", {
+  expect_s3_class(get_ch_costs_path(), "fs_path")
+  expect_s3_class(get_dn_costs_path(), "fs_path")
+  expect_s3_class(get_gp_ooh_costs_path(), "fs_path")
+})
