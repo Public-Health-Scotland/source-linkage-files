e9b8c5796121443cad6961cfa14aff995f6df180
James McMahon <james.mcmahon@phs.scot>
2022-01-18 13:53:23 +0000

    Refactor slf deaths lookup path function to its own script

---
+++ R/get_slf_deaths_path.R
@@ -0,0 +1,16 @@
+#' Get the full path to the SLF deaths lookup file
+#'
+#' @param update The update month to use, defaults to \code{\link{latest_update}}
+#' @param ... additional arguments passed to \code{\link{get_file_path}}
+#'
+#' @return the path to the costs lookup as an \code{\link[fs]{path}}
+#' @export
+get_slf_deaths_path <- function(update = latest_update(), ...) {
+  slf_deaths_file_path <- get_file_path(
+    directory = fs::path(get_slf_dir(), "Deaths"),
+    file_name = glue::glue("all_deaths_{update}.zsav"),
+    ...
+  )
+
+  return(slf_deaths_file_path)
+}
--- tests/testthat/test-get_file_paths.R
+++ tests/testthat/test-get_file_paths.R
@@ -3,9 +3,3 @@ test_that("main SLF directory exists", {
 
   expect_true(fs::dir_exists(slf_dir_path))
 })
-
-
-test_that("SLF Deaths lookup path works", {
-  expect_s3_class(get_slf_deaths_path(), "fs_path")
-  expect_s3_class(get_slf_deaths_path(previous_update()), "fs_path")
-})
