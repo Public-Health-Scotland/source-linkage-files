4bba3aae66ea180c513c74ebab426c6c1ef801ed
Jennifer Thom <jennifer.thom@phs.scot>
2022-01-19 12:34:10 +0000

    Split produce_test_comparison Rename compare_tests to produce_test_comparison, remove sum_flags from script

---
+++ R/produce_test_comparison.R
@@ -0,0 +1,13 @@
+#' Compare test files
+#'
+#' @param old_data dataframe containing old file
+#' @param new_data dataframe containing new file
+#'
+#' @return a dataframe with a comparison of new and old data
+#' @importFrom dplyr mutate full_join
+produce_test_comparison <- function(old_data, new_data){
+    dplyr::full_join(old_data, new_data, by = "measure", suffix = c("_old", "_new")) %>%
+    dplyr::mutate(diff = .data$value_new - .data$value_old,
+           pctChange = .data$diff/.data$value_old*100,
+           issue = if_else(.data$pctChange >= 5, 1, 0))
+}
