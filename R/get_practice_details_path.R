26687b347160085065c7614997b963a726e25d2c
James McMahon <james.mcmahon@phs.scot>
2022-01-20 12:41:34 +0000

    Update function to use the latest file path / name

--- R/get_practice_details_path.R
+++ R/get_practice_details_path.R
@@ -1,15 +1,16 @@
 #' Get the path to the Practice Details file
 #'
+#' @param update the update month (defaults to use \code{\link{latest_update}})
 #' @param ... additional arguments passed to \code{\link{get_file_path}}
 #'
 #' @return The path to the Practice Details file as an \code{\link[fs]{path}}
 #' @export
 #' @family file path functions
 #' @seealso \code{\link{get_file_path}} for the generic function.
-get_practice_details_path <- function(...) {
+get_practice_details_path <- function(update = latest_update(), ...) {
   practice_details_path <- get_file_path(
     directory = fs::path(get_slf_dir(), "Lookups"),
-    file_name = "Practice Details.sav",
+    file_name = glue::glue("practice_details_{update}.zsav"),
     ...
   )
 
