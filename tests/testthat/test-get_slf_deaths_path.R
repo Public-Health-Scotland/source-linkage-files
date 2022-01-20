2a217460f281af9c173ee2eaa9cdf4c03e064b99
James McMahon <james.mcmahon@phs.scot>
2022-01-20 12:43:01 +0000

    Move deaths lookup function to script with other lookups

--- tests/testthat/test-get_slf_deaths_path.R
+++
@@ -1,4 +0,0 @@
-test_that("SLF Deaths lookup path works", {
-  expect_s3_class(get_slf_deaths_path(), "fs_path")
-  expect_s3_class(get_slf_deaths_path(previous_update()), "fs_path")
-})
--- tests/testthat/test-get_slf_lookup_paths.R
+++ tests/testthat/test-get_slf_lookup_paths.R
@@ -7,3 +7,8 @@ test_that("SLF GP practice lookup file paths work", {
   expect_s3_class(get_slf_gpprac_path(), "fs_path")
   expect_s3_class(get_slf_gpprac_path(update = previous_update()), "fs_path")
 })
+
+test_that("SLF Deaths lookup path works", {
+  expect_s3_class(get_slf_deaths_path(), "fs_path")
+  expect_s3_class(get_slf_deaths_path(previous_update()), "fs_path")
+})
