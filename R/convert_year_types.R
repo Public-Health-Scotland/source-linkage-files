5ceba35407a9874774ee5a814ffb668568fa572a
Catherine Holland <catheh03@nssrstudio05.csa.scot.nhs.uk>
2022-01-26 10:26:11 +0000

    fyyear functions written

---
+++ R/convert_year_types.R
@@ -0,0 +1,28 @@
+#' Convert a year type from normal '1718' to alternate '2017'
+#'
+#' @param fyyear vector of financial year in the form '1718'
+#'
+#' @return a vector of years in the alternate form '2017'
+#' @export
+#'
+#' @examples
+#' convert_fyyear_to_year(c("2017", "2018"))
+convert_fyyear_to_year <- function(year) {
+  year <- paste0("20", substr(year, 1, 2))
+  return (year)
+}
+
+
+#' Convert a year type from alternate '2017' to normal '1718'
+#'
+#' @param year vector of years in the form '2017'
+#'
+#' @return a vector of years in the normal form '1718'
+#' @export
+#'
+#' @examples
+#' convert_year_to_fyyear(c("1718", "1819"))
+convert_year_to_fyyear <- function(year) {
+  year <- paste0(substr(year, 3, 4), as.numeric(substr(year, 3, 4)) + 1)
+  return (year)
+}
