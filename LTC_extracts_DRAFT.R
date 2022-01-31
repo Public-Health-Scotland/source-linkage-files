8582e8cd64c4a66fc6888d67362385fcd6f14ac0
James McMahon <james.mcmahon@phs.scot>
2022-01-31 10:46:49 +0000

    First draft / comments

---
+++ LTC_extracts_DRAFT.R
@@ -0,0 +1,9 @@
+# Read data
+readr::read_csv(get_it_ltc_path())
+
+# Check types / rename
+
+# Create LTC flags 1/0
+
+# Save out to Year folder - /conf/hscdiip/SLF_Extracts/LTCs
+# fs::path(get_slf_dir(), "LTCs", "<filename>")
