526939647dc9925f7517c2bd419b2d133b3b6fb7
Catherine Holland <catheh03@nssrstudio06.csa.scot.nhs.uk>
2022-01-31 16:07:42 +0000

    read data in rename variables date types create flag variables

--- LTC_extracts_DRAFT.R
+++ LTC_extracts_DRAFT.R
@@ -1,17 +1,5 @@
-
-## financial year in question ##
-FY = 1718
-year = 2017
-
-
-# output1718 <- haven::read_sav("/conf/hscdiip/SLF_Extracts/LTCs/LTCs_patient_reference_file-201718.zsav")
-
-
-
 ## Read data ##
-it_extract_ref()
 data <- readr::read_csv(get_it_ltc_path())
-
 
 ## Check types / rename ##
 
@@ -40,88 +28,53 @@ colnames(data) = c("chi",
 
 # change date types
 data <- data %>%
-  dplyr::mutate(arth_date = as.Date(arth_date),
-         asthma_date = as.Date(asthma_date),
-         atrialfib_date = as.Date(atrialfib_date),
-         cancer_date = as.Date(cancer_date),
-         cvd_date = as.Date(cvd_date),
-         liver_date = as.Date(liver_date),
-         copd_date = as.Date(copd_date),
-         dementia_date = as.Date(dementia_date),
-         diabetes_date = as.Date(diabetes_date),
-         epilepsy_date = as.Date(epilepsy_date),
-         chd_date = as.Date(chd_date),
-         hefailure_date = as.Date(hefailure_date),
-         ms_date = as.Date(ms_date),
-         parkinsons_date = as.Date(parkinsons_date),
-         refailure_date = as.Date(refailure_date),
-         congen_date = as.Date(congen_date),
-         bloodbfo_date = as.Date(bloodbfo_date),
-         endomet_date = as.Date(endomet_date),
-         digestive_date = as.Date(digestive_date))
-
+  mutate(arth_date = dmy(arth_date),
+         asthma_date = dmy(asthma_date),
+         atrialfib_date = dmy(atrialfib_date),
+         cancer_date = dmy(cancer_date),
+         cvd_date = dmy(cvd_date),
+         liver_date = dmy(liver_date),
+         copd_date = dmy(copd_date),
+         dementia_date = dmy(dementia_date),
+         diabetes_date = dmy(diabetes_date),
+         epilepsy_date = dmy(epilepsy_date),
+         chd_date = dmy(chd_date),
+         hefailure_date = dmy(hefailure_date),
+         ms_date = dmy(ms_date),
+         parkinsons_date = dmy(parkinsons_date),
+         refailure_date = dmy(refailure_date),
+         congen_date = dmy(congen_date),
+         bloodbfo_date = dmy(bloodbfo_date),
+         endomet_date = dmy(endomet_date),
+         digestive_date = dmy(digestive_date))
 
 
 ## Create LTC flags 1/0 ##
 
 # Create new variables
 data <- data %>%
-  tibble::add_column(arth = NA,
-             asthma = NA,
-             atrialfib = NA,
-             cancer = NA,
-             cvd = NA,
-             liver = NA,
-             copd = NA,
-             dementia = NA,
-             diabetes = NA,
-             epilepsy = NA,
-             chd = NA,
-             hefailure = NA,
-             ms = NA,
-             parkinsons = NA,
-             refailure = NA,
-             congen = NA,
-             bloodbfo = NA,
-             endomet = NA,
-             digestive = NA)
+  add_column(arth = 0,
+             asthma = 0,
+             atrialfib = 0,
+             cancer = 0,
+             cvd = 0,
+             liver = 0 ,
+             copd = 0,
+             dementia = 0,
+             diabetes = 0,
+             epilepsy = 0,
+             chd = 0,
+             hefailure = 0,
+             ms = 0,
+             parkinsons = 0,
+             refailure = 0,
+             congen = 0,
+             bloodbfo = 0,
+             endomet = 0,
+             digestive = 0)
 
 
-
-# Set flags to 1 or 0 based on FY
-# also clear the date if outside of FY
-
-# create ltc and ltc_data
-ltc = data[1:10, 22:40]
-ltc_date = data[1:10, 3:21]
-
-# fy date to compare to
-new_fyyear <- as.numeric(substr(year, 3, 4)) + 1
-fyyear_date <- paste0("01-04-", new_fyyear)
-
-
-for (i in 1:nrow(ltc)) {
-  for (j in 1:ncol(ltc)) {
-
-    ifelse(is.na(ltc_date[i,j] == TRUE | ltc_date[i,j] > fyyear_date | ltc_date[i,j] == fyyear_date),
-           ltc[i,j] <- 0, ltc[i,j] <- 1)
-  }
-}
-
-
-# combine ltc and ltc_date
-LTC <- cbind(data[1:10, 1:2], ltc, ltc_date)
-
-
-
-## sort data by chi ##
-LTC <- LTC %>%
-  dplyr::arrange(chi)
-
-
-
-
-## Save out to Year folder - /conf/hscdiip/SLF_Extracts/LTCs ##
+# Save out to Year folder - /conf/hscdiip/SLF_Extracts/LTCs
 # fs::path(get_slf_dir(), "LTCs", "<filename>")
 
 # haven
