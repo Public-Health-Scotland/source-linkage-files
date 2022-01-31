97d3c53a340424a96a0ec514d39baac1f4c2dd16
Jennifer Thom <jennifer.thom@phs.scot>
2022-01-31 14:55:53 +0000

    Apply create_demog_test_flags function to scripts Use this in produce___tests functions

--- R/produce_sc_demog_lookup_tests.R
+++ R/produce_sc_demog_lookup_tests.R
@@ -9,15 +9,11 @@
 produce_sc_demog_lookup_tests <- function(data) {
   data %>%
     # create test flags
+    create_demog_test_flags %>%
     dplyr::mutate(
-      n_sending_loc = if_else(is.na(.data$sending_location) | .data$sending_location == "", 0, 1),
-      n_sc_id = if_else(is.na(.data$social_care_id) | .data$social_care_id == "", 0, 1),
-      n_chi = if_else(is.na(.data$chi) | .data$chi == "", 0, 1),
-      n_postcode = if_else(is.na(.data$postcode) | .data$postcode == "", 0, 1),
-      n_males = if_else(.data$gender == 1, 1, 0),
-      n_females = if_else(.data$gender == 2, 1, 0),
-      missing_dob = if_else(is.na(.data$dob) | .data$dob == "", 1, 0)
-    ) %>%
+      n_missing_sending_loc = if_else(is.na(.data$sending_location) | .data$sending_location == "", 1, 0),
+      n_missing_sc_id = if_else(is.na(.data$social_care_id) | .data$social_care_id == "", 1, 0)
+    )%>%
     # remove variables that won't be summed
     dplyr::select(-c(.data$sending_location:.data$postcode)) %>%
     # use function to sum new test flags
