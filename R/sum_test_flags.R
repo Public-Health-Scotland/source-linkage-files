fa1aa5701f15af88719ec2be2c8a9e9f7fa7501b
Jennifer Thom <jennifer.thom@phs.scot>
2022-02-01 12:56:39 +0000

    Produce generic test functions

---
+++ R/sum_test_flags.R
@@ -0,0 +1,16 @@
+#' Summarise flags for testing
+#'
+#' @param data the data should be in a format for summarising test flags
+#'
+#' @return a dataframe with a count of each flag
+#' @importFrom dplyr across
+sum_test_flags <- function(data) {
+  data <- data %>%
+    dplyr::summarise_all(sum, na.rm = TRUE) %>%
+    tidyr::pivot_longer(
+      cols = tidyselect::everything(),
+      names_to = "measure",
+      values_to = "value"
+    )
+  return(data)
+}
