ac5a1fdb919fc669d693db5b74db5f4335354771
James McMahon <james.mcmahon@phs.scot>
2022-02-07 17:37:00 +0000

    Remove import that isn't needed

--- R/produce_test_comparison.R
+++ R/produce_test_comparison.R
@@ -4,7 +4,6 @@
 #' @param new_data dataframe containing new file
 #'
 #' @return a dataframe with a comparison of new and old data
-#' @importFrom dplyr mutate full_join
 #' @family produce tests functions
 produce_test_comparison <- function(old_data, new_data) {
   dplyr::full_join(old_data, new_data, by = "measure", suffix = c("_old", "_new")) %>%
