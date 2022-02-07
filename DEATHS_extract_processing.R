99ce2f6cfee3485b79e332ad077cefca22c21569
Catherine Holland <catheh03@nssrstudio03.csa.scot.nhs.uk>
2022-02-07 14:29:02 +0000

    review edits complete

--- DEATHS_extract_processing.R
+++ DEATHS_extract_processing.R
@@ -1,48 +1,63 @@
 
+#####################################################
+# Draft DEATHS Extract Processing Code
+# Author: Catherine Holland
+# Date: February 2022
+# Written on RStudio Server
+# Version of R - 3.6.1
+# Input -
+# Description - Takes IT extract of Deaths data.
+#               Creates new death date variable,
+#               uses NRS death date as death date if avaliable.
+#               Saves outputted flagged dataset.
+#####################################################
+
+
+## packages ##
+
+library(dplyr)
+library(stringr)
+library(readr)
+
 
 
 ## Read data in ##
 data <- readr::read_csv(get_it_deaths_path())
 
+deaths_file <- read_csv(
+  file = get_it_deaths_path(),
+  col_type = cols(
+    `PATIENT_UPI [C]` = col_character(),
+    `PATIENT DoD DATE (NRS)` = col_date(format = "%d-%m-%Y"),
+    `PATIENT DoD DATE (CHI)` = col_date(format = "%d-%m-%Y")
+  )
+)
+
+
+names(deaths_file) <- str_replace_all(names(deaths_file), " ", "_")
+
 
 # rename
-colnames(data) = c("chi",
-                   "death_date_NRS",
-                   "death_date_CHI")
+deaths_file <- deaths_file %>%
+  rename(
+    chi = "PATIENT_UPI_[C]",
+    death_date_NRS = "PATIENT_DoD_DATE_(NRS)",
+    death_date_CHI = "PATIENT_DoD_DATE_(CHI)")
+
 
 
 
 ## one record per chi ##
-data <- data %>%
-  dplyr::distinct(chi, .keep_all = TRUE)
+deaths_file <- deaths_file %>%
+  dplyr::distinct(chi, .keep_all = TRUE) %>%
+  # Use the NRS deathdate unless it isn't there
+  mutate(death_date = if_else(is.na(death_date_NRS), death_date_CHI, death_date_NRS))
 
 
+## Save file - //stats/hscdiip/SLF_extracts/Deaths/ ##
+# .zsav file
+haven::write_sav(deaths_file, get_slf_deaths_path(), compress = TRUE)
 
-## numeric death date ##
-data <- data %>%
-  tibble::add_column(death_date = as.numeric(NA))
-
-
-
-
-## NRS death date if avaliable ##
-for (i in 1:nrow(data)) {
-  data$death_date[i] <- ifelse(is.na(data$death_date_NRS[i]) == FALSE,
-                     data$death_date_NRS[i],
-                     data$death_date_CHI[i])
-}
-
-
-
-## Save file ##
-# //stats/hscdiip/SLF_extracts/Deaths/ #
-# get_slf_deaths_path()
-saveRDS(data, get_slf_deaths_path())
-
-# haven
-haven::write_sav(LTC, get_slf_deaths_path(), compress = TRUE
-)
-
-#readr
-readr::write_rds(LTC, get_slf_deaths_path(), compress = "gz")
+# .rds file
+readr::write_rds(deaths_file, get_slf_deaths_path(), compress = "gz")
 
