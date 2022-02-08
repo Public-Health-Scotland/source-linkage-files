a458111048b1c8e7a43ee0e5519f81bb9fe1623c
Catherine Holland <catheh03@nssrstudio06.csa.scot.nhs.uk>
2022-02-08 09:50:45 +0000

    include new function for endFY computation

--- LTC_extracts_DRAFT.R
+++ LTC_extracts_DRAFT.R
@@ -20,8 +20,8 @@ library(readr)
 
 ## financial year in question ##
 FY <- 1718
-end_fy <- as.Date(paste0((as.numeric(convert_fyyear_to_year(FY)) + 1), "-03-31"))
-
+# end_fy <- as.Date(paste0((as.numeric(convert_fyyear_to_year(FY)) + 1), "-03-31"))
+end_fy <- endFY(FY)
 
 ## Read data ##
 ltc_file <- read_csv(
