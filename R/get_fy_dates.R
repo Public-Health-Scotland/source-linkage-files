3c9bfd60b1b920ce1858c84752482bd4fd5c3396
Catherine Holland <catheh03@nssrstudio07.csa.scot.nhs.uk>
2022-02-08 11:17:06 +0000

    edits: - edit function name - re-run document() updated fn name

--- R/get_fy_dates.R
+++ R/get_fy_dates.R
@@ -1,18 +1,18 @@
 #' Return the start date of FY year
 #'
-#' @param year a vector of year
+#' @param year a vector of years
 #'
 #' @return a vector of dates of the start date of the FY year
 #' @export
 #'
 #' @examples
 #' startFY("1718")
-startFY <- function(FY) {
-  as.Date(paste0(as.numeric(convert_fyyear_to_year(FY)), "-04-01"))
+start_fy <- function(year) {
+  as.Date(paste0(as.numeric(convert_fyyear_to_year(year)), "-04-01"))
 }
 
 
-#' Return the end date of FY year
+#' Return the end date of FY years
 #'
 #' @param year a vector of year
 #'
@@ -21,22 +21,22 @@ startFY <- function(FY) {
 #'
 #' @examples
 #' endFY("1718")
-endFY <- function(FY) {
-  as.Date(paste0((as.numeric(convert_fyyear_to_year(FY)) + 1), "-03-31"))
+end_fy <- function(year) {
+  as.Date(paste0((as.numeric(convert_fyyear_to_year(year)) + 1), "-03-31"))
 }
 
 
 #' Return the mid date of FY year
 #'
-#' @param year a vector of year
+#' @param year a vector of years
 #'
 #' @return a vector of dates of the mid date of the FY year
 #' @export
 #'
 #' @examples
 #' midpointFY("1718")
-midpointFY <- function(FY) {
+midpoint_fy <- function(year) {
   #as.Date(startFY(FY) + floor((endFY(FY)-startFY(FY))/2))
-  as.Date(paste0(as.numeric(convert_fyyear_to_year(FY)), "-09-30"))
+  as.Date(paste0(as.numeric(convert_fyyear_to_year(year)), "-09-30"))
 }
 
