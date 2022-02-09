ca6a0f8ffd79f2b30a01a9c87c54a495b407a2a7
Jennifer Thom <jennifer.thom@phs.scot>
2022-02-09 09:02:27 +0000

    Use tidyverse style - snake_case

--- R/produce_test_comparison.R
+++ R/produce_test_comparison.R
@@ -9,7 +9,7 @@ produce_test_comparison <- function(old_data, new_data) {
   dplyr::full_join(old_data, new_data, by = "measure", suffix = c("_old", "_new")) %>%
     dplyr::mutate(
       diff = .data$value_new - .data$value_old,
-      pctChange = .data$diff / .data$value_old * 100,
-      issue = if_else(.data$pctChange >= 5, 1, 0)
+      pct_change = .data$diff / .data$value_old * 100,
+      issue = if_else(.data$pct_change >= 5, 1, 0)
     )
 }
