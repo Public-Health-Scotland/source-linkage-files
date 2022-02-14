374c99a7b5d7f155550c448a93c2dcb27e052416
Jennifer Thom <jennifer.thom@phs.scot>
2022-02-14 08:35:36 +0000

    Update documentation

--- tests/testthat/test-produce_test_comparison.R
+++ tests/testthat/test-produce_test_comparison.R
@@ -1,21 +1,21 @@
 test_that("produce test comparison function returns data", {
   old_data <- tibble::tibble(
     measure = c("NHS_Ayrshire_and_Arran", "NHS_Borders"),
-    value = c(1234L, 5678L)
+    value = c(10L, 20L)
   )
 
   new_data <- tibble::tibble(
     measure = c("NHS_Ayrshire_and_Arran", "NHS_Borders"),
-    value = c(2345L, 6789L)
+    value = c(15L, 25L)
   )
 
   outcome <- tibble::tibble(
     measure = c("NHS_Ayrshire_and_Arran", "NHS_Borders"),
-    value_old = c(1234L, 5678L),
-    value_new = c(2345L, 6789L),
-    diff = c(1111L, 1111L),
-    pctChange = c(90.032415, 19.566749),
-    issue = c(1L, 1L)
+    value_old = c(10L, 20L),
+    value_new = c(15L, 25L),
+    diff = c(5L, 5L),
+    pct_change = c(50L, 25L),
+    issue = c(TRUE, TRUE)
   )
 
   # Check type etc.
