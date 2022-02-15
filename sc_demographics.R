db92da50a3e3449ad3da2135ceae78a75f784d78
Catherine Holland <catheh03@nssrstudio06.csa.scot.nhs.uk>
2022-02-15 17:25:00 +0000

    changes compelete - re-review

--- sc_demographics.R
+++ sc_demographics.R
@@ -27,7 +27,6 @@ library(phsmethods)
 
 ## data ##
 
-
 ######################################################
 # set-up conection to platform
 db_connection <- odbc::dbConnect(
@@ -90,120 +89,46 @@ sc_demog <- sc_demog %>%
     chi_postcode = na_if(chi_postcode, str_length(chi_postcode) > 8 | str_length(chi_postcode) == 8)
   )
 
+
+# format postcodes using `phsmethods`
 sc_demog <-
   sc_demog %>%
-  mutate(
-    # add space to create length 7 postcode
-    # length 5 postcode
-    # submitted postcode
-    submitted_postcode = if_else(str_length(submitted_postcode) == 5 & !is.na(submitted_postcode),
-      paste0(substr(submitted_postcode, 1, 2), " ", substr(submitted_postcode, 3, 5)),
-      submitted_postcode
-    ),
-    # chi postcode
-    chi_postcode = if_else(str_length(chi_postcode) == 5 & !is.na(chi_postcode),
-      paste0(substr(chi_postcode, 1, 2), " ", substr(chi_postcode, 3, 5)),
-      chi_postcode
-    )
-  ) %>%
-  mutate(
-    # length 6 postcode
-    # submitted postcode
-    submitted_postcode = if_else(str_length(submitted_postcode) == 6 & !is.na(submitted_postcode),
-      paste0(substr(submitted_postcode, 1, 3), " ", substr(submitted_postcode, 4, 6)),
-      submitted_postcode
-    ),
-    # chi postcode
-    chi_postcode = if_else(str_length(chi_postcode) == 6 & !is.na(chi_postcode),
-      paste0(substr(chi_postcode, 1, 3), " ", substr(chi_postcode, 4, 6)),
-      chi_postcode
-    )
-  )
+  mutate(across(contains("postcode"), ~postcode(.x, format = "pc7")))
 
 
-# invalid postcodes
-invalid_postcodes <- c(
-  "DY103DJ",
-  "EH191TR",
-  "EH292EZ",
-  "EH33TNZ",
-  "G46 2NF",
-  "G46 6FY",
-  "G69 2YB",
-  "G73 8NZ",
-  "G74 7SN",
-  "G75 1ZZ",
-  "G75 6DF",
-  "G77 3GT",
-  "G78 6BU",
-  "G78 ITE",
-  "G79 8AJ",
-  "IV178ED",
-  "KA113FP",
-  "KA28OBE",
-  "KA5 1LQ",
-  "KA71JUJ",
-  "KA9 9FG",
-  "KW152SE",
-  "KY1 3DO",
-  "KY13OPX",
-  "L15 0PR",
-  "M16 0GS",
-  "ML3 0GS",
-  "ML7 6AQ",
-  "NK1 1AA",
-  "NK1 1AB",
-  "PA438JP",
-  "PA494JS",
-  "PR2 5AL",
-  "TD8 8JD"
-)
+# UK postcode regex
+uk_pc_regexp <- "([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\\s?[0-9][A-Za-z]{2})"
 
 sc_demog <-
   sc_demog %>%
   mutate(
     # remove dummy postcodes "NK1 0AA" and "NK1 1AB"
-    submitted_postcode = na_if(
-      submitted_postcode,
-      submitted_postcode == "NK1 0AA" | submitted_postcode == "NF1 1AB"
-    ),
-    chi_postcode = na_if(
-      chi_postcode,
-      chi_postcode == "NK1 0AA" | chi_postcode == "NF1 1AB"
-    )
+    across(ends_with("_postcode"),
+           ~na_if(.x, .x== "NK1 0AA" | .x== "NF1 1AB" |
+                    # remove invalid postcodes missed by regex check
+                    .x == "PR2 5AL" | .x ==  "M16 0GS" | .x == "DY103DJ"))
   ) %>%
   mutate(
     # comparing with regex UK postcode
-    submitted_postcode = na_if(submitted_postcode, !str_detect(
-      submitted_postcode,
-      "([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\\s?[0-9][A-Za-z]{2})"
-    )),
-    chi_postcode = na_if(chi_postcode, !str_detect(
-      chi_postcode,
-      "([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\\s?[0-9][A-Za-z]{2})"
-    ))
-  ) %>%
-  mutate(
-    # na if postcode is in the invalid postcode vector
-    submitted_postcode = na_if(submitted_postcode, submitted_postcode %in% invalid_postcodes),
-    chi_postcode = na_if(chi_postcode, chi_postcode %in% chi_postcode)
-  ) %>%
-  # sort by submitted_postcode
-  arrange(submitted_postcode)
+    across(ends_with("_postcode"),
+           ~na_if(.x, !str_detect(.x, uk_pc_regexp)))
+  )
 
 
 
 ## postcode type ##
-postcode_lookup <- readr::read_rds(file = "/conf/linkage/output/lookups/Unicode/Geography/Scottish Postcode Directory/Scottish_Postcode_Directory_2021_2.rds")
+pc_lookup <- read_spd_file()
+
 
 sc_demog <-
   sc_demog %>%
   select(
-    latest_record_flag, extract_date, sending_location, social_care_id, upi, gender, dob, postcode,
+    latest_record_flag, extract_date, sending_location, social_care_id, upi, gender, dob,
     submitted_postcode, chi_postcode
   ) %>%
   # check if submitted_postcode matches with postcode lookup
-  mutate(valid_pc = if_else(submitted_postcode %in% postcode_lookup$pc7, 1, 0))
+  mutate(valid_pc = if_else(submitted_postcode %in% pc_lookup$pc7, 1, 0))
+
 
 # use submitted_postcode if valid, otherwise use chi_postcode
 sc_demog <-
@@ -227,26 +152,20 @@ sc_demog %>%
 ## outfile ##
 outfile <-
   sc_demog %>%
-  # sort so latest submissions are last
-  arrange(sending_location, social_care_id, latest_record_flag, extract_date) %>%
-  ## Aggregate to create one row per sending_location / ID ##
-  distinct(social_care_id, .keep_all = TRUE) %>%
-  mutate(
-    sending_location = coalesce(sending_location),
-    chi = coalesce(upi),
-    gender = coalesce(gender),
-    dob = coalesce(dob),
-    postcode = coalesce(postcode)
-  ) %>%
-  # select variables for outfile
-  select(
-    sending_location,
-    social_care_id,
-    chi,
-    gender,
-    dob,
-    postcode
-  )
+  # group by sending location and ID
+  group_by(sending_location, social_care_id) %>%
+  # arranage so lastest submissions are last
+  arrange(sending_location,
+          social_care_id,
+          latest_record_flag,
+          extract_date
+          ) %>%
+  # summarise so select last submission
+  summarise(chi = last(upi),
+            gender = last(gender),
+            dob = last(dob),
+            postcode = last(postcode)
+            )
 
 
 ## save file ##
