a27b8052d09e386581117fd8d10ce4fab6b75c97
James McMahon <james.mcmahon@phs.scot>
2022-02-17 15:35:40 +0000

    Style, lint and re-document

--- R/produce_slf_deaths_tests.R
+++ R/produce_slf_deaths_tests.R
@@ -1,8 +1,10 @@
 #' Produce the SLF deaths lookup tests
 #'
-#' @param data new or old data for testing summary flags (data is from \code{\link{get_slf_deaths_path}})
+#' @param data new or old data for testing summary
+#' flags (data is from \code{\link{get_slf_deaths_path}})
 #
-#' @return a dataframe with a count of each flag from \code{\link{sum_test_flags}}
+#' @return a dataframe with a count of each flag
+#' from \code{\link{sum_test_flags}}
 #' @export
 #' @importFrom dplyr mutate select
 #' @family produce tests functions
--- tests/testthat/test-get_sc_demog_path.R
+++ tests/testthat/test-get_sc_demog_path.R
@@ -1,4 +1,9 @@
 test_that("SC demographic file path works", {
-  expect_s3_class(get_sc_demog_lookup_path (), "fs_path")
-  expect_s3_class(get_sc_demog_lookup_path (update = previous_update()), "fs_path")
+  expect_s3_class(get_sc_demog_lookup_path(), "fs_path")
+  expect_s3_class(
+    get_sc_demog_lookup_path(
+      update = previous_update()
+    ),
+    "fs_path"
+  )
 })
