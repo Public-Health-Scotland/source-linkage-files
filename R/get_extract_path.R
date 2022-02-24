f80d9892fba6eb7afe0772102925acef37929609
James McMahon <james.mcmahon@phs.scot>
2022-02-24 12:38:23 +0000

    Simplify `get_extracts` function using other functions.
    document extract path changes

--- R/get_extract_path.R
+++ R/get_extract_path.R
@@ -7,21 +7,16 @@
 #' @export
 #'
 extract_path <- function(year, type = c("Acute", "Mental", "Homelessness")) {
-  year_dir <- fs::path("/conf/sourcedev/Source_Linkage_File_Updates", year, "Extracts")
 
   file_name <- dplyr::case_when(
     type == "Acute" ~ "Acute-episode-level-extract",
     type == "Mental" ~ "Mental-Health-episode-level-extract",
     type == "Homelessness" ~ "Homelessness-extract"
-  )
+  ) %>%
+    glue::glue("-20{year}.csv.gz")
 
-  file_path <- fs::path(year_dir, glue::glue("{file_name}-20{year}.csv.gz"))
-
-  if (fs::file_exists(fs::path_ext_remove(file_path))) {
-    file_path <- fs::path_ext_remove(file_path)
-  } else if (!fs::file_exists(file_path)) {
-    rlang::abort(glue::glue("{type} Extract not found"))
-  }
+  file_path <- get_file_path(directory = get_year_dir(year = year, extracts_dir = TRUE),
+                             file_name = file_name)
 
   return(file_path)
 }
