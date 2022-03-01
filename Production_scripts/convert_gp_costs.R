ef1e46964146ef4527a159be5c8a7cfd8196a05a
Catherine Holland <catheh03@nssrstudio07.csa.scot.nhs.uk>
2022-03-01 14:50:38 +0000

    updated script:
    includes archived file
    map
    full_join

--- Production_scripts/convert_gp_costs.R
+++ Production_scripts/convert_gp_costs.R
@@ -9,6 +9,24 @@
 #####################################################
 
 
+library(dplyr)
+library(ggplot2)
+
+
+## Make a copy of the existing file ##
+# read data in
+current_file <- haven::read_sav(get_gp_ooh_costs_path())
+
+
+# write data to folder
+# .zsav
+haven::write_sav(current_file,
+                 paste0(get_slf_dir(), "/Costs/Cost_GPOoH_Lookup_pre", latest_update(), ".zsav"),
+                 compress = TRUE
+)
+
+
+
 ## update file ##
 
 ## Attendances taken from 2018 Primary Care Out of Hours Report ##
@@ -19,9 +37,6 @@
 
 ## The above should be checked / added to the Excel file 'OOH_Costs.xlsx' before running this syntax.
 
-
-library(dplyr)
-library(ggplot2)
 
 
 ## data ##
@@ -45,25 +60,21 @@ gp_out_of_hours <-
 
 
 ## add in years by copying the most recent year ##
-year <- 1920
-
-tempyear1 <- paste0(as.numeric(substr(year, 1, 2)) + 1, as.numeric(substr(year, 3, 4)) + 1)
-tempyear2 <- paste0(as.numeric(substr(year, 1, 2)) + 2, as.numeric(substr(year, 3, 4)) + 2)
-
-
+latest_year <- 1920
 
 ## increase by 1% for every year after the latest ##
 gp_out_of_hours <-
-  gp_out_of_hours %>%
-  mutate(
-    cost_per_consultation =
-      if_else(Year > year,
-        cost_per_consultation * 1.01,
-        cost_per_consultation
-      )
-  ) %>%
-  # arrange by HB2019 and year
-  arrange(HB2019, Year)
+  map_df(1:5, ~
+          gp_out_of_hours %>%
+            group_by(HB2019, year) %>%
+            arrange(HB2019, year) %>%
+            mutate(
+              cost_per_consultation = cost_per_consultation * (1.01)^.x,
+              year = convert_year_to_fyyear(as.numeric(convert_fyyear_to_year(year)) + .x)
+            )
+) %>%
+  arrange(HB2019, year)
+
 
 
 ## match files - to make sure costs haven't changed radically ##
@@ -71,18 +82,19 @@ lookup <- haven::read_sav(
   find_latest_file(get_slf_dir(), regexp = "Cost_GPOoH_Lookup_pre.+?\\.sav")
 )
 
-# rename lookup varialbles to match
+# rename lookup variables to match
 lookup <-
   lookup %>%
   rename(
     cost_old = "Cost_per_consultation",
-    HB2019 = "TreatmentNHSBoardCode"
+    HB2019 = "TreatmentNHSBoardCode",
+    year = "Year"
   )
 
 # match
 data <-
   gp_out_of_hours %>%
-  left_join(lookup, by = c("HB2019", "Year"))
+  full_join(lookup, by = c("HB2019", "year"))
 
 
 # compute difference
