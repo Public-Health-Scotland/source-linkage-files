10d464d56512397c978a181c6ea0ab9f47869cf3
Catherine Holland <catheh03@nssrstudio06.csa.scot.nhs.uk>
2022-03-08 11:42:06 +0000

    B03 written

---
+++ Production_scripts/B03_Process_Outpatients_(SMR00)_extract.R
@@ -0,0 +1,276 @@
+#####################################################
+# Outpatient Extract
+# Author: Catherine Holland
+# Date: February 2022
+# Written on RStudio Server
+# Version of R - 3.6.1
+# Input -
+# Description - Process Outpatients extract
+#####################################################
+
+
+library(dplyr)
+library(purrr)
+library(vroom)
+
+
+## Read in CSV output file ##
+
+# latest year
+latest_year <- 1920
+
+# function here till PR pushed
+get_year_dir <- function(year, extracts_dir = FALSE) {
+  year_dir <- fs::path("/conf/sourcedev/Source_Linkage_File_Updates", year)
+
+  year_extracts_dir <- fs::path(year_dir, "Extracts")
+
+  return(dplyr::if_else(extracts_dir, year_extracts_dir, year_dir))
+}
+
+
+outpatient_episode_extract <- readr::read_csv(
+  paste0(
+  get_year_dir(year = latest_year),
+  "/Extracts/Outpatients-episode-level-extract-",
+  paste0(convert_fyyear_to_year(latest_year), substr(latest_year, 3, 4)),
+  ".csv")
+  )  %>%
+  # rename
+  rename(
+    clinic_date_fy = "Clinic Date Fin Year",
+    record_keydate1 = "Clinic Date (00)",
+    dob = "Pat Date Of Birth [C]",
+    age = "Age at Midpoint of Financial Year",
+    alcohol_adm = "Alcohol Related Admission",
+    attendance_status = "Clinic Attendance (Status) Code",
+    clinic_type = "Clinic Type Code",
+    commhosp = "Community Hospital Flag",
+    conc = "Consultant/HCP Code",
+    uri = "Episode Record Key (SMR00) [C]",
+    falls_adm = "Falls Related Admission",
+    lca = "Geo Council Area Code",
+    postcode = "Geo Postcode [C]",
+    hbrescode = "NHS Board of Residence Code - current",
+    nhshosp = "NHS Hospital Flag",
+    op1a = "Operation 1A Code (4 char)",
+    op1b = "Operation 1B Code (4 char)",
+    dateop1 = "Date of Main Operation(00)",
+    op2a = "Operation 2A Code (4 char)",
+    op2b = "Operation 2B Code (4 char)",
+    dateop2 = "Date of Operation 2 (00)",
+    gender = "Pat Gender Code",
+    chi = "Pat UPI",
+    cat = "Patient Category Code",
+    gpprac = "Practice Location Code",
+    hbpraccode = "Practice NHS Board Code - current",
+    refsource = "Referral Source Code",
+    reftype = "Referral Type Code",
+    selfharm_adm = "Self Harm Related Admission",
+    sigfac = "Significant Facility Code",
+    spec = "Specialty Classificat. 1/4/97 Code",
+    submis_adm = "Substance Misuse Related Admission",
+    cost_total_net = "Total Net Costs",
+    location = "Treatment Location Code",
+    hbtreatcode = "Treatment NHS Board Code - current"
+  ) %>%
+  # date types
+  mutate(
+    dob = as.Date(dob),
+    dateop1 = as.Date(dateop1),
+    dateop2 = as.Date(dateop2),
+    record_keydate1 = as.Date(record_keydate1)
+  )
+
+
+# year variable
+outpatient_episode_extract <-
+  outpatient_episode_extract %>%
+  mutate(
+    year = latest_year,
+    recid = "00B")
+
+
+## Recode GP Practice into a 5 digit number ##
+# assume that if it starts with a letter it's an English practice and so recode to 99995
+outpatient_episode_extract <-
+  outpatient_episode_extract %>%
+  mutate(gpprac = replace(gpprac, substr(gpprac, 1, 1) %in% c("A", "Z"), "99995"))
+
+
+## compute record key date ##
+outpatient_episode_extract <-
+  outpatient_episode_extract %>%
+  mutate(record_keydate2 = record_keydate1)
+
+
+## Allocate the costs to the correct month ##
+
+# month and month_cost variable
+outpatient_episode_extract <-
+  outpatient_episode_extract %>%
+  mutate(month = strftime(record_keydate1, "%m"))
+
+
+# cost in correct month
+outpatient_episode_extract <-
+           outpatient_episode_extract %>%
+           mutate(
+             apr_cost = if_else(month == "04", cost_total_net, 0),
+             may_cost = if_else(month == "05", cost_total_net, 0),
+             jun_cost = if_else(month == "06", cost_total_net, 0),
+             jul_cost = if_else(month == "07", cost_total_net, 0),
+             aug_cost = if_else(month == "08", cost_total_net, 0),
+             sep_cost = if_else(month == "09", cost_total_net, 0),
+             oct_cost = if_else(month == "10", cost_total_net, 0),
+             nov_cost = if_else(month == "11", cost_total_net, 0),
+             dec_cost = if_else(month == "12", cost_total_net, 0),
+             jan_cost = if_else(month == "01", cost_total_net, 0),
+             feb_cost = if_else(month == "02", cost_total_net, 0),
+             mar_cost = if_else(month == "03", cost_total_net, 0)
+           )
+
+
+# sort by chi record_keydate1
+outpatient_episode_extract <-
+  outpatient_episode_extract %>%
+  arrange(chi, record_keydate1)
+
+
+## outpatient labels ##
+outpatient_episode_extract <-
+  outpatient_episode_extract %>%
+  mutate(
+    reftype = factor(reftype,
+                     levels = c(1:3),
+                     labels = c(
+                       "New Outpatient: Consultation and Management",
+                       "New Outpatient: Consultation only",
+                       "Follow-up/Return Outpatient"
+                       )),
+    clinic_type = factor(clinic_type,
+                         levels = c(1:4),
+                         labels = c(
+                           "Consultant",
+                           "Dentist",
+                           "Nurse PIN",
+                           "AHP"
+                           ))
+  )
+
+
+
+## save outfile ##
+outfile <-
+  outpatient_episode_extract %>%
+  select(
+    year,
+    recid,
+    record_keydate1,
+    record_keydate2,
+    chi,
+    gender,
+    dob,
+    gpprac,
+    hbpraccode,
+    postcode,
+    hbrescode,
+    lca,
+    location,
+    hbtreatcode,
+    op1a,
+    op1b,
+    dateop1,
+    op2a,
+    op2b,
+    dateop2,
+    spec,
+    sigfac,
+    conc,
+    cat,
+    age,
+    refsource,
+    reftype,
+    attendance_status,
+    clinic_type,
+    alcohol_adm,
+    submis_adm,
+    falls_adm,
+    selfharm_adm,
+    commhosp,
+    nhshosp,
+    cost_total_net,
+    apr_cost,
+    may_cost,
+    jun_cost,
+    jul_cost,
+    aug_cost,
+    sep_cost,
+    oct_cost,
+    nov_cost,
+    dec_cost,
+    jan_cost,
+    feb_cost,
+    mar_cost,
+    uri
+  )
+
+# .zsav
+haven::write_sav(outfile,
+                 paste0(
+                   get_year_dir(year = latest_year),
+                   "/outpatient_for_source-20",
+                   latest_year, ".zsav"
+                 ),
+                 compress = TRUE
+)
+
+# .rds file
+readr::write_rds(outfile,
+                 paste0(
+                   get_year_dir(year = latest_year),
+                   "/outpatient_for_source-20",
+                   latest_year, ".zsav"
+                 ),
+                 compress = "gz"
+)
+
+
+# -------------------------------------------------------------------------------------------
+
+## tests ##
+
+
+## Flags ##
+outfile <-
+  outfile %>%
+  mutate(
+    # count CHI
+    has_chi = if_else(is.na(chi), 0, 1),
+    # count DNA
+    dna = if_else(attendance_status == 8, 1, 0),
+    # count M/F
+    male = if_else(gender == 1, 1, 0),
+    female = if_else(gender == 2, 1, 0),
+    # count missing values
+    no_dob = if_else(is.na(dob), 1, 0),
+    # count how many episodes in each HB by treatment code
+    nhs_ayrshire_and_arran = if_else(hbtreatcode == 'S08000015', 1, 0),
+    nhs_borders = if_else(hbtreatcode == 'S08000016', 1, 0),
+    nhs_dumfries_and_galloway = if_else(hbtreatcode == 'S08000017', 1, 0),
+    nhs_forth_valley = if_else(hbtreatcode == 'S08000019', 1, 0),
+    nhs_grampian = if_else(hbtreatcode == 'S08000020', 1, 0),
+    nhs_greater_glasgow_and_clyde == if_else(hbtreatcode %in% c('S08000021', 'S08000031'), 1, 0),
+    nhs_highland = if_else(hbtreatcode == 'S08000022', 1, 0),
+    nhs_lanarkshire = if_else(hbtreatcode %in% c('S08000023', 'S08000032'), 1, 0),
+    nhs_lothian = if_else(hbtreatcode == 'S08000024', 1, 0),
+    nhs_orkney = if_else(hbtreatcode == 'S08000025', 1, 0),
+    nhs_shetland = if_else(hbtreatcode == 'S08000026', 1, 0),
+    nhs_western_isles = if_else(hbtreatcode == 'S08000028', 1, 0),
+    nhs_fife = if_else(hbtreatcode %in% c('S08000018', 'S08000029'), 1, 0),
+    nhs_tayside = if_else(hbtreatcode %in% c('S08000027', 'S08000030'), 1, 0)
+  )
+
+
+
+
