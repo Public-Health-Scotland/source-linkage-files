c2d9a0a6a8a3d6a2e361d18787cf481a7e33a57a
Jennifer Thom <jennifer.thom@phs.scot>
2022-03-09 11:33:41 +0000

    Rename sc CH episodes test script

--- Production_scripts/Social_Care-01_ch_episodes_tests.R
+++ Production_scripts/Social_Care-01_ch_episodes_tests.R
@@ -10,13 +10,34 @@
 # Description - Produce tests for social care - care home episodes file.
 
 ####################################################
+
+# Packages
+library(createslf)
+
+
+# Read in Data---------------------------------------
+
 # Create new and old dataframes with measures for testing
 new_tests <- produce_sc_ch_episodes_tests(haven::read_sav(get_sc_ch_episodes_path()))
 old_tests <- produce_sc_ch_episodes_tests(haven::read_sav(get_sc_ch_episodes_path(update = previous_update())))
 
-####################################################
-# create tests
+# Create tests-------------------------------------------
+
+# Compare new and old outputs
 comparison <- produce_test_comparison(old_tests, new_tests)
 
-# END OF SCRIPT
-####################################################
+
+# Produce Outfile----------------------------------------
+
+# Save output as zsav for now
+# Eventually change this to rds when we have more R scripts
+haven::write_sav(comparison,
+                 path(get_slf_dir(), "Social_care",
+                      paste0("all_ch_episodes_", latest_update(), "_tests",
+                             ext = ".zsav")
+                 ),
+                 compress = TRUE
+)
+
+
+## END OF SCRIPT ##
