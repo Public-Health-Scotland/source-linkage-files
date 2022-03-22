31370eb88caff77167971f6cdf1d55331928015f
Jennifer Thom <jennifer.thom@phs.scot>
2022-03-22 10:48:26 +0000

    Add maternity back into get_boxi_extract_path

--- R/get_boxi_extract_path.R
+++ R/get_boxi_extract_path.R
@@ -6,12 +6,13 @@
 #' @return BOXI extracts containing data for each dataset
 #' @export
 #'
-get_boxi_extract_path <- function(year, type = c("Acute", "Mental", "Outpatient", "AE", "DN", "GPOoH", "Deaths", "CMH", "Homelessness")) {
+get_boxi_extract_path <- function(year, type = c("Acute", "Mental", "Maternity", "Outpatient", "AE", "DN", "GPOoH", "Deaths", "CMH", "Homelessness")) {
   year_dir <- fs::path("/conf/sourcedev/Source_Linkage_File_Updates", year, "Extracts")
 
   file_name <- dplyr::case_when(
     type == "Acute" ~ "Acute-episode-level-extract",
     type == "Mental" ~ "Mental-Health-episode-level-extract",
+    type == "Maternity" ~ "Maternity-episode-level-extract",
     type == "Outpatient" ~ "Outpatients-episode-level-extract",
     type == "AE" ~ "A&E-episode-level-extract",
     type == "DN" ~ "District-Nursing-contact-level-extract",
