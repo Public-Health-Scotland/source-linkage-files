ffbf36d1b64c71635ef39c6d83f86c67244d4632
James McMahon <james.mcmahon@phs.scot>
2022-03-23 17:19:47 +0000

    Some fixes for lint issues.
    Mostly line length > 80 characters.

--- R/get_boxi_extract_path.R
+++ R/get_boxi_extract_path.R
@@ -6,26 +6,36 @@
 #' @return BOXI extracts containing data for each dataset
 #' @export
 #'
-get_boxi_extract_path <- function(year, type = c("Acute", "Mental", "Outpatient", "AE", "DN", "GPOoH", "Deaths", "CMH", "Homelessness")) {
-  year_dir <- fs::path("/conf/sourcedev/Source_Linkage_File_Updates", year, "Extracts")
+get_boxi_extract_path <-
+  function(year,
+           type = c(
+             "Acute",
+             "Mental",
+             "Outpatient",
+             "AE",
+             "DN",
+             "GPOoH",
+             "Deaths",
+             "CMH",
+             "Homelessness"
+           )) {
+    file_name <- dplyr::case_when(
+      type == "Acute" ~ "Acute-episode-level-extract",
+      type == "Mental" ~ "Mental-Health-episode-level-extract",
+      type == "Outpatient" ~ "Outpatients-episode-level-extract",
+      type == "AE" ~ "A&E-episode-level-extract",
+      type == "DN" ~ "District-Nursing-contact-level-extract",
+      type == "GPOoH" ~ "GP-OoH-diagnosis-extract",
+      type == "Deaths" ~ "NRS-death-registrations-extract",
+      type == "CMH" ~ "Community-MH-contact-level-extract",
+      type == "Homelessness" ~ "Homelessness-extract"
+    ) %>%
+      glue::glue("-20{year}.csv.gz")
 
-  file_name <- dplyr::case_when(
-    type == "Acute" ~ "Acute-episode-level-extract",
-    type == "Mental" ~ "Mental-Health-episode-level-extract",
-    type == "Outpatient" ~ "Outpatients-episode-level-extract",
-    type == "AE" ~ "A&E-episode-level-extract",
-    type == "DN" ~ "District-Nursing-contact-level-extract",
-    type == "GPOoH" ~ "GP-OoH-diagnosis-extract",
-    type == "Deaths" ~ "NRS-death-registrations-extract",
-    type == "CMH" ~ "Community-MH-contact-level-extract",
-    type == "Homelessness" ~ "Homelessness-extract"
-  ) %>%
-    glue::glue("-20{year}.csv.gz")
+    file_path <- get_file_path(
+      directory = get_year_dir(year = year, extracts_dir = TRUE),
+      file_name = file_name
+    )
 
-  file_path <- get_file_path(
-    directory = get_year_dir(year = year, extracts_dir = TRUE),
-    file_name = file_name
-  )
-
-  return(file_path)
-}
+    return(file_path)
+  }
--- R/produce_test_comparison.R
+++ R/produce_test_comparison.R
@@ -6,7 +6,11 @@
 #' @return a dataframe with a comparison of new and old data
 #' @family produce tests functions
 produce_test_comparison <- function(old_data, new_data) {
-  dplyr::full_join(old_data, new_data, by = "measure", suffix = c("_old", "_new")) %>%
+  dplyr::full_join(old_data,
+    new_data,
+    by = "measure",
+    suffix = c("_old", "_new")
+  ) %>%
     dplyr::mutate(
       diff = .data$value_new - .data$value_old,
       pct_change = .data$diff / .data$value_old * 100,
