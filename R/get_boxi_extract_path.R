9e14f44f99019cd4bbe0d2a216bd0bef3a58167e
Catherine Holland <75622719+catherineholland1@users.noreply.github.com>
2022-03-28 12:03:11 +0100

    Source Extract fn (#166)
    * Split out get boxi/source extract functions
    Rename get_extract_path as get_boxi_extract path. Create get_source_extract_path for saving out extracts in year dir
    
    * Split out get boxi/source extract functions
    Rename get_extract_path as get_boxi_extract path. Create get_source_extract_path for saving out extracts in year dir
    
    * rename extract function, inc additions of other extracts in function
    
    * edit to source function to allow for different file extensions
    
    * include PIS
    
    * additions to source extract file paths
    
    * Add checking on the type param.
    
    This will now allow partial matches e.g. Ac for Acute but importantly, will error when a match isn't found in the list.
    
    * re-style
    
    * Make csv vs. csv.gz selection make more sense.
    
    * Rename and document extension param
    
    * Use `get_file_path`
    
    * Add maternity into list of BOXI extracts
    
    * update documentation
    
    Co-authored-by: Jennifer Thom <jennifer.thom@phs.scot>
    Co-authored-by: Catherine Holland <catheh03@nssrstudio07.csa.scot.nhs.uk>
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>
    Co-authored-by: Jennifer Thom <jennifer.thom2@phs.scot>

--- R/get_boxi_extract_path.R
+++ R/get_boxi_extract_path.R
@@ -11,6 +11,7 @@ get_boxi_extract_path <-
            type = c(
              "Acute",
              "Mental",
+             "Maternity",
              "Outpatient",
              "AE",
              "DN",
@@ -19,9 +20,18 @@ get_boxi_extract_path <-
              "CMH",
              "Homelessness"
            )) {
+    type <- match.arg(type)
+
+    year_dir <- fs::path(
+      "/conf/sourcedev/Source_Linkage_File_Updates",
+      year,
+      "Extracts"
+    )
+
     file_name <- dplyr::case_when(
       type == "Acute" ~ "Acute-episode-level-extract",
       type == "Mental" ~ "Mental-Health-episode-level-extract",
+      type == "Maternity" ~ "Maternity-episode-level-extract",
       type == "Outpatient" ~ "Outpatients-episode-level-extract",
       type == "AE" ~ "A&E-episode-level-extract",
       type == "DN" ~ "District-Nursing-contact-level-extract",
@@ -29,13 +39,19 @@ get_boxi_extract_path <-
       type == "Deaths" ~ "NRS-death-registrations-extract",
       type == "CMH" ~ "Community-MH-contact-level-extract",
       type == "Homelessness" ~ "Homelessness-extract"
-    ) %>%
-      glue::glue("-20{year}.csv.gz")
-
-    file_path <- get_file_path(
-      directory = get_year_dir(year = year, extracts_dir = TRUE),
-      file_name = file_name
     )
 
-    return(file_path)
+    boxi_extract_path_csv_gz <- fs::path(year_dir, glue::glue("{file_name}-20{year}.csv.gz"))
+    boxi_extract_path_csv <- fs::path(year_dir, glue::glue("{file_name}-20{year}.csv"))
+
+    # If the csv.gz file doesn't exist look for the unzipped csv.
+    if (fs::file_exists(boxi_extract_path_csv_gz)) {
+      boxi_extract_path <- boxi_extract_path_csv_gz
+    } else if (fs::file_exists(boxi_extract_path_csv)) {
+      boxi_extract_path <- boxi_extract_path_csv
+    } else {
+      rlang::abort(glue::glue("{type} Extract not found"))
+    }
+
+    return(boxi_extract_path)
   }
