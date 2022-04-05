a62ea83e6476c21314b2ce91f9ddbd05756a85e0
Catherine Holland <catheh03@nssrstudio06.csa.scot.nhs.uk>
2022-04-05 08:34:22 +0100

    remove factor labels

--- Production_scripts/B05a_Process_A&E_(AE2)_extract.R
+++ Production_scripts/B05a_Process_A&E_(AE2)_extract.R
@@ -139,6 +139,7 @@ ae_file <- readr::read_csv(
     case_ref_number = "Case Reference Number"
   )
 
+
 # Data Cleaning -----------------------------------------
 
 # Data Cleaning -----------------------------------------
