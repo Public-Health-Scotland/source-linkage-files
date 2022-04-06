90a01d205e152e8f9f246fd70e1db29ea36e7069
Jennifer Thom <jennifer.thom@phs.scot>
2022-04-06 09:53:13 +0100

    Update convert_monthly_rows_to_vars fn Styling and sorts monthly variables into FY month order

--- R/convert_monthly_rows_to_vars.R
+++ R/convert_monthly_rows_to_vars.R
@@ -8,16 +8,20 @@
 #' @return A dataframe with monthly cost and bed day variables
 #'
 convert_monthly_rows_to_vars <- function(data, month_num_var, cost_var, beddays_var) {
+  month_order <- tolower(month.abb[c(4:12, 1:3)])
 
-    data %>%
+  data %>%
     dplyr::mutate(month_name = tolower(month.abb[{{ month_num_var }}])) %>%
     select(-{{ month_num_var }}) %>%
-    dplyr::rename(cost = {{cost_var}},
-           beddays = {{beddays_var}}) %>%
+    dplyr::rename(
+      cost = {{ cost_var }},
+      beddays = {{ beddays_var }}
+    ) %>%
     tidyr::pivot_wider(
       names_from = .data$month_name,
       names_glue = "{month_name}_{.value}",
       values_from = c(.data$cost, .data$beddays),
       values_fill = 0.00
-    )
+    ) %>%
+    select(!ends_with(c("_beddays", "_cost")), glue::glue("{month_order}_beddays"), glue::glue("{month_order}_cost"))
 }
