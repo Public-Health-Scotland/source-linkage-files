d471c0d0a48676bfe52e72404ae433a15cb2f91c
Jennifer Thom <jennifer.thom@phs.scot>
2022-04-08 11:46:16 +0100

    Update create measures fn specify variables you want to calculate sum/mean

--- R/calculate_measures.R
+++ R/calculate_measures.R
@@ -1,20 +1,28 @@
 #' Calculate measures for testing
 #'
-#' @param data A processed dataframe containing cost_total_net and monthly costs
+#' @param data A processed dataframe containing a summary of the mean and sum of variables
+#' @param vars Specify variables you want to test.
+#' This will 'match' this e.g c("beddays", "cost", "yearstay)
 #'
-#' @return a tibble with the total sum of costs
+#' @return a tibble with a summary
 #' @export
-calculate_measures <- function(data, type = c("cost", "beddays", "yearstay")) {
-  data %>%
+#'
+calculate_measures <- function(data, vars) {
+
+  data<- data %>%
+    select(matches({{vars}})) %>%
     summarise(
-      across(matches(type), ~ sum(.x, na.rm = TRUE), .names = "total_{col}"),
-      across(matches(type), ~ mean(.x, na.rm = TRUE), .names = "mean_{col}")
-    )
-  tidyr::pivot_longer(
-    cols = tidyselect::everything(),
-    names_to = "measure",
-    values_to = "value"
-  )
+      across(everything(), ~ sum(.x, na.rm = TRUE), .names = "total_{col}"),
+      across(everything(vars = !starts_with("total_")), ~ mean(.x, na.rm = TRUE), .names = "mean_{col}")
+    )  %>%
+    tidyr::pivot_longer(
+      cols = tidyselect::everything(),
+      names_to = "measure",
+      values_to = "value"
+    ) %>%
+    mutate(value = format(round(value, digits = 2), scientific = F))
+
+  return(data)
 }
 
 
