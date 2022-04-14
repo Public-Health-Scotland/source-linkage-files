453f82a58132af9c5eff91a9d96bd12c495846b4
James McMahon <james.mcmahon@phs.scot>
2022-04-14 13:46:17 +0100

    Add some variables to the GP cluster file
    These are needed for the Source Platform Matrix dashboard which uses the same lookup. I also trimmed some of the code.

--- All_years/02-Lookups/00_get_gp_cluster_data.R
+++ All_years/02-Lookups/00_get_gp_cluster_data.R
@@ -1,12 +1,6 @@
 # Load phsopendata
 # If it's not installed, install it from GitHub
-if (!require(phsopendata)) {
-  # Check that we have remotes, if not install it first
-  if (!("remotes" %in% installed.packages())) install.packages("remotes")
-
-  remotes::install_github("Public-Health-Scotland/phsopendata")
-  library(phsopendata)
-}
+library(phsopendata)
 
 library(dplyr)
 library(janitor)
@@ -17,7 +11,7 @@ latest_update <- "Mar_2022"
 lookup_dir <- path("/conf/hscdiip/SLF_Extracts/Lookups")
 
 # Retrieve the latest resource from the dataset
-get_dataset("gp-practice-contact-details-and-list-sizes",
+gp_clusters <- get_dataset("gp-practice-contact-details-and-list-sizes",
   max_resources = 1
 ) %>%
   clean_names() %>%
@@ -26,15 +20,19 @@ get_dataset("gp-practice-contact-details-and-list-sizes",
     gpprac = practice_code,
     practice_name = gp_practice_name,
     postcode,
-    cluster = gp_cluster
+    cluster = gp_cluster,
+    partnership = hscp,
+    health_board = hb
   ) %>%
   # Sort for SPSS matching
-  arrange(gpprac) %>%
-  # Write as an SPSS file
-  write_sav(
-    path(lookup_dir,
-      paste0("practice_details_", latest_update),
-      ext = "zsav"
-    ),
-    compress = TRUE
-  )
+  arrange(gpprac)
+
+# Write as an SPSS file
+write_sav(
+  gp_clusters,
+  path(lookup_dir,
+    paste0("practice_details_", latest_update),
+    ext = "zsav"
+  ),
+  compress = TRUE
+)
