72ccffc10e61846aa361fec8f8c27492b5484def
Catherine Holland <catheh03@nssrstudio02.csa.scot.nhs.uk>
2022-04-25 11:36:47 +0100

    combine cup marker into a&e script

--- Production_scripts/Process_extracts/extract_a&e.R
+++ Production_scripts/Process_extracts/extract_a&e.R
@@ -507,12 +507,112 @@ outfile <-
     case_ref_number
   )
 
-# Save a temp file for matching (.zsav)
+
+# ------------------------------------------------------------------------------------------------------------
+
+## CUP Marker ##
+
+# Read in data---------------------------------------
+
+ae_cup_file <- readr::read_csv(
+  file = get_boxi_extract_path(year, "AE_CUP"),
+  col_type = cols(
+    `ED Arrival Date` = col_date(format = "%Y/%m/%d %T"),
+    `ED Arrival Time` = col_time(""),
+    `ED Case Reference Number [C]` = col_character(),
+    `CUP Marker` = col_double(),
+    `CUP Pathway Name` = col_character()
+  )
+) %>%
+  # rename variables
+  rename(
+    record_keydate1 = "ED Arrival Date",
+    keyTime1 = "ED Arrival Time",
+    case_ref_number = "ED Case Reference Number [C]",
+    cup_marker = "CUP Marker",
+    cup_pathway = "CUP Pathway Name"
+  )
+
+
+# Data Cleaning---------------------------------------
+
+ae_cup_clean <- ae_cup_file %>%
+  # Sort for linking and remove any duplicates
+  arrange(record_keydate1, keyTime1, case_ref_number) %>%
+  group_by(record_keydate1, keyTime1, case_ref_number) %>%
+  summarise(
+    cup_marker = first(cup_marker),
+    cup_pathway = first(cup_pathway)
+  ) %>%
+  ungroup()
+
+
+# Join data--------------------------------------------
+
+matched_ae_data <- outfile %>%
+  full_join(ae_cup_clean, by = c("record_keydate1", "keyTime1", "case_ref_number")) %>%
+  arrange(chi, record_keydate1, keyTime1, record_keydate2, keyTime2)
+
+
+# Save outfile----------------------------------------
+outfile <- matched_ae_data %>%
+  select(
+    year,
+    recid,
+    record_keydate1,
+    record_keydate2,
+    keyTime1,
+    keyTime2,
+    chi,
+    gender,
+    dob,
+    gpprac,
+    postcode,
+    lca,
+    hscp,
+    location,
+    hbrescode,
+    hbtreatcode,
+    diag1,
+    diag2,
+    diag3,
+    ae_arrivalmode,
+    refsource,
+    sigfac,
+    ae_attendcat,
+    ae_disdest,
+    ae_patflow,
+    ae_placeinc,
+    ae_reasonwait,
+    ae_bodyloc,
+    ae_alcohol,
+    alcohol_adm,
+    submis_adm,
+    falls_adm,
+    selfharm_adm,
+    cost_total_net,
+    age,
+    apr_cost,
+    may_cost,
+    jun_cost,
+    jul_cost,
+    aug_cost,
+    sep_cost,
+    oct_cost,
+    nov_cost,
+    dec_cost,
+    jan_cost,
+    feb_cost,
+    mar_cost,
+    cup_marker,
+    cup_pathway
+  )
+
+# Save as zsav file
 outfile %>%
-  haven::write_sav(get_source_extract_path(year, "AE", ext = "zsav"))
+  readr::write_rds(get_source_extract_path(year, "AE", ext = "zsav"))
 
-
-# Save a temp file for matching (.rds)
+# Save as rds file
 outfile %>%
   readr::write_rds(get_source_extract_path(year, "AE", ext = "rds"))
 
