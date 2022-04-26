6cd7ad45ab8cb1386898fa2b1dfbc4fba60bc598
Catherine Holland <catheh03@nssrstudio03.csa.scot.nhs.uk>
2022-04-26 14:21:23 +0100

    update to ca_to_lca function

--- R/convert_ca_to_lca.R
+++ R/convert_ca_to_lca.R
@@ -1,6 +1,6 @@
 #' Convert Council Areas into Local Council Authority Codes
 #'
-#' @param ca vector of council area codes
+#' @param ca vector of council area codes or names
 #'
 #' @return a vector of local council codes
 #' @export
@@ -10,38 +10,38 @@
 #' ca_to_lca(ca)
 ca_to_lca <- function(ca) {
   lca <- dplyr::case_when(
-    ca == "S12000033" ~ "01", # Aberdeen City
-    ca == "S12000034" ~ "02", # Aberdeenshire
-    ca == "S12000041" ~ "03", # Angus
-    ca == "S12000035" ~ "04", # Argyll and Bute
-    ca == "S12000026" ~ "05", # Scottish Borders
-    ca == "S12000005" ~ "06", # Clackmannanshire
-    ca == "S12000039" ~ "07", # West Dun
-    ca == "S12000006" ~ "08", # Dumfies and Galloway
-    ca == "S12000042" ~ "09", # Dundee City
-    ca == "S12000008" ~ "10", # East Ayrshire
-    ca == "S12000045" ~ "11", # East Dun
-    ca == "S12000010" ~ "12", # East Lothian
-    ca == "S12000011" ~ "13", # East Ren
-    ca == "S12000036" ~ "14", # City of Edinburgh
-    ca == "S12000014" ~ "15", # Falkirk
-    ca %in% c("S12000015", "S12000047") ~ "16", # Fife
-    ca %in% c("S12000046", "S12000049") ~ "17", # Glasgow City
-    ca == "S12000017" ~ "18", # Highland
-    ca == "S12000018" ~ "19", # Inverclyde
-    ca == "S12000019" ~ "20", # Midlothian
-    ca == "S12000020" ~ "21", # Moray
-    ca == "S12000021" ~ "22", # North Ayrshire
-    ca %in% c("S12000044", "S12000050") ~ "23", # North Lan
-    ca == "S12000023" ~ "24", # Orkney
-    ca %in% c("S12000024", "S12000048") ~ "25", # P and K
-    ca == "S12000038" ~ "26", # Renfrewshire
-    ca == "S12000027" ~ "27", # Shetland
-    ca == "S12000028" ~ "28", # South Ayrshire
-    ca == "S12000029" ~ "29", # South Lan
-    ca == "S12000030" ~ "30", # Stirling
-    ca == "S12000040" ~ "31", # West Lothian
-    ca == "S12000013" ~ "32" # Na h-Eileanan Siar
+    ca == "S12000033" | ca == "Aberdeen City"                  ~ "01", # Aberdeen City
+    ca == "S12000034" | ca == "Aberdeenshire"                  ~ "02", # Aberdeenshire
+    ca == "S12000041" | ca == "Angus"                          ~ "03", # Angus
+    ca == "S12000035" | ca == "Argyll and Bute"                ~ "04", # Argyll and Bute
+    ca == "S12000026" | ca == "Scottish Borders"               ~ "05", # Scottish Borders
+    ca == "S12000005" | ca == "Clackmannanshire"               ~ "06", # Clackmannanshire
+    ca == "S12000039" | ca == "West Dunbartonshire"            ~ "07", # West Dun
+    ca == "S12000006" | ca == "Dumfries and Galloway"          ~ "08", # Dumfries and Galloway
+    ca == "S12000042" | ca == "Dundee City"                    ~ "09", # Dundee City
+    ca == "S12000008" | ca == "East Ayrshire"                  ~ "10", # East Ayrshire
+    ca == "S12000045" | ca == "East Dunbartonshire"            ~ "11", # East Dun
+    ca == "S12000010" | ca == "East Lothian"                   ~ "12", # East Lothian
+    ca == "S12000011" | ca == "East Renfrewshire"              ~ "13", # East Ren
+    ca == "S12000036" | ca == "City of Edinburgh"              ~ "14", # City of Edinburgh
+    ca == "S12000014" | ca == "Falkirk"                        ~ "15", # Falkirk
+    ca %in% c("S12000015", "S12000047") | ca == "Fife"         ~ "16", # Fife
+    ca %in% c("S12000046", "S12000049") | ca == "Glasgow City" ~ "17", # Glasgow City
+    ca == "S12000017" | ca == "Highland"                       ~ "18", # Highland
+    ca == "S12000018" | ca == "Inverclyde"                     ~ "19", # Inverclyde
+    ca == "S12000019" | ca == "Midlothian"                     ~ "20", # Midlothian
+    ca == "S12000020" | ca == "Moray"                          ~ "21", # Moray
+    ca == "S12000021" | ca == "North Ayrshire"                 ~ "22", # North Ayrshire
+    ca %in% c("S12000044", "S12000050") | ca == "North Lanarkshire" ~ "23", # North Lan
+    ca == "S12000023" | ca == "Orkney"                         ~ "24", # Orkney
+    ca %in% c("S12000024", "S12000048") | ca == "Perth and Kinross" ~ "25", # P and K
+    ca == "S12000038" | ca == "Renfrewshire"                   ~ "26", # Renfrewshire
+    ca == "S12000027" | ca == "Shetland Islands"               ~ "27", # Shetland
+    ca == "S12000028" | ca == "South Ayrshire"                 ~ "28", # South Ayrshire
+    ca == "S12000029" | ca == "South Lanarkshire"              ~ "29", # South Lan
+    ca == "S12000030" | ca == "Stirling"                       ~ "30", # Stirling
+    ca == "S12000040" | ca == "West Lothian"                   ~ "31", # West Lothian
+    ca == "S12000013" | ca == "Na h-Eileanan Siar"             ~ "32" # Na h-Eileanan Siar
   )
   return(lca)
 }
