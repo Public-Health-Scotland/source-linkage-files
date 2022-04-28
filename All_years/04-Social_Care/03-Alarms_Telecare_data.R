8be410774536bdbb3c605243765162036a235dc1
Jennifer Thom <jennifer.thom@phs.scot>
2022-04-28 09:08:52 +0100

    recode sc_send_lca

--- All_years/04-Social_Care/03-Alarms_Telecare_data.R
+++ All_years/04-Social_Care/03-Alarms_Telecare_data.R
@@ -114,8 +114,42 @@ at_clean <- replaced_start_dates %>%
     ),
     mid_fy = as.Date(paste0(year, "-09-30")),
     age = difftime(mid_fy, dob, "years"),
-    person_id = glue::glue("{sending_location}-{social_care_id}")
+    person_id = glue::glue("{sending_location}-{social_care_id}"),
+    sc_send_lca = case_when(sending_location == "100" ~ 01,
+                            sending_location == "110" ~ 02,
+                            sending_location == "120" ~ 03,
+                            sending_location == "130" ~ 04,
+                            sending_location == "150" ~ 06,
+                            sending_location == "170" ~ 08,
+                            sending_location == "180" ~ 09,
+                            sending_location == "190" ~ 10,
+                            sending_location == "200" ~ 11,
+                            sending_location == "210" ~ 12,
+                            sending_location == "220" ~ 13,
+                            sending_location == "230" ~ 14,
+                            sending_location == "235" ~ 32,
+                            sending_location == "240" ~ 15,
+                            sending_location == "250" ~ 16,
+                            sending_location == "260" ~ 17,
+                            sending_location == "270" ~ 18,
+                            sending_location == "280" ~ 19,
+                            sending_location == "290" ~ 20,
+                            sending_location == "300" ~ 21,
+                            sending_location == "310" ~ 22,
+                            sending_location == "330" ~ 24,
+                            sending_location == "340" ~ 25,
+                            sending_location == "350" ~ 26,
+                            sending_location == "355" ~ 05,
+                            sending_location == "360" ~ 27,
+                            sending_location == "370" ~ 28,
+                            sending_location == "380" ~ 29,
+                            sending_location == "390" ~ 30,
+                            sending_location == "395" ~ 07,
+                            sending_location == "400" ~ 31,
+                            TRUE ~ NA
+    )
   )
+
 
 # Save outfile--------------------------------------------
 outfile <- at_clean %>%
