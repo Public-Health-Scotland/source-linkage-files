84aa5768b091caf8a8704ccfdb3b04d350edae6c
Jennifer Thom <jennifer.thom@phs.scot>
2022-05-04 11:09:25 +0100

    produce maternity tests

---
+++ Production_scripts/Process_extracts/Produce_extract_tests/extract_maternity_tests.R
@@ -0,0 +1,43 @@
+####################################################
+# Name of file - produce-maternity-tests.R
+# Original Authors - Jennifer Thom
+# Original Date - March 2022
+# Written/run on - RStudio Server
+# Version of R - 3.6.1
+# Description - Produce tests for source linkage files:
+#               acute processed file.
+#####################################################
+
+# Load packages
+library(createslf)
+library(openxlsx)
+library(slfhelper)
+
+
+# Read in Data-----------------------------------------
+
+year <- "1920"
+
+# Read new data file
+new_data <- readr::read_rds(get_source_extract_path(year, "Maternity", ext = "rds"))
+
+# Read current SLF episode file and filter for 01B and GLS records
+existing_data <- read_slf_episode(year, recid = "02B") %>%
+  rename(chi = anon_chi)
+
+
+# Produce comparison-------------------------------------
+# Compare new file with existing slf data
+comparison <- produce_test_comparison(
+  produce_source_maternity_tests(existing_data),
+  produce_source_maternity_tests(new_data)
+)
+
+
+# Produce Outfile----------------------------------------
+
+# Save test comparisons as an excel workbook
+write_tests_xlsx(comparison, "maternity_extract")
+
+
+## END OF SCRIPT ##
