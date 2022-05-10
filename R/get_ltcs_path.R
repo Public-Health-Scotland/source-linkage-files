5e859d2e7eaf2f6f89264855af7c2c0a4ce46ec3
Jennifer Thom <jennifer.thom@phs.scot>
2022-05-10 12:12:45 +0100

    New LTCs path function

---
+++ R/get_ltcs_path.R
@@ -0,0 +1,20 @@
+#' Get the full path to the SLF deaths lookup file
+#'
+#' @param update The update month to use,
+#' defaults to \code{\link{latest_update}}
+#' @param ... additional arguments passed to \code{\link{get_file_path}}
+#' @param year financial year e.g. "1920"
+#'
+#' @return The path to the costs lookup as an \code{\link[fs]{path}}
+#' @export
+#' @family file path functions
+#' @seealso \code{\link{get_file_path}} for the generic function.
+get_ltcs_path <- function(year, ...) {
+  ltcs_file_path <- get_file_path(
+    directory = fs::path(get_slf_dir(), "LTCs"),
+    file_name = glue::glue("LTC_patient_reference_file-20{year}.rds"),
+    ...
+  )
+
+  return(ltcs_file_path)
+}
