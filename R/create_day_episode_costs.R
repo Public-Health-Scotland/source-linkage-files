5323c967fc20ec09df50e337ae185b3c7bfaee20
James McMahon <james.mcmahon@phs.scot>
2022-05-12 14:15:10 +0100

    Remove imports from `{dplyr}`

--- R/create_day_episode_costs.R
+++ R/create_day_episode_costs.R
@@ -12,19 +12,19 @@
 create_day_episode_costs <- function(data, date_var, cost_var) {
   data <- data %>%
     # month and month_cost variable
-    mutate(month = strftime({{ date_var }}, "%m")) %>%
-    mutate(
-      apr_cost = if_else(.data$month == "04", {{ cost_var }}, 0),
-      may_cost = if_else(.data$month == "05", {{ cost_var }}, 0),
-      jun_cost = if_else(.data$month == "06", {{ cost_var }}, 0),
-      jul_cost = if_else(.data$month == "07", {{ cost_var }}, 0),
-      aug_cost = if_else(.data$month == "08", {{ cost_var }}, 0),
-      sep_cost = if_else(.data$month == "09", {{ cost_var }}, 0),
-      oct_cost = if_else(.data$month == "10", {{ cost_var }}, 0),
-      nov_cost = if_else(.data$month == "11", {{ cost_var }}, 0),
-      dec_cost = if_else(.data$month == "12", {{ cost_var }}, 0),
-      jan_cost = if_else(.data$month == "01", {{ cost_var }}, 0),
-      feb_cost = if_else(.data$month == "02", {{ cost_var }}, 0),
-      mar_cost = if_else(.data$month == "03", {{ cost_var }}, 0)
+    dplyr::mutate(month = strftime({{ date_var }}, "%m")) %>%
+    dplyr::mutate(
+      apr_cost = dplyr::if_else(.data$month == "04", {{ cost_var }}, 0),
+      may_cost = dplyr::if_else(.data$month == "05", {{ cost_var }}, 0),
+      jun_cost = dplyr::if_else(.data$month == "06", {{ cost_var }}, 0),
+      jul_cost = dplyr::if_else(.data$month == "07", {{ cost_var }}, 0),
+      aug_cost = dplyr::if_else(.data$month == "08", {{ cost_var }}, 0),
+      sep_cost = dplyr::if_else(.data$month == "09", {{ cost_var }}, 0),
+      oct_cost = dplyr::if_else(.data$month == "10", {{ cost_var }}, 0),
+      nov_cost = dplyr::if_else(.data$month == "11", {{ cost_var }}, 0),
+      dec_cost = dplyr::if_else(.data$month == "12", {{ cost_var }}, 0),
+      jan_cost = dplyr::if_else(.data$month == "01", {{ cost_var }}, 0),
+      feb_cost = dplyr::if_else(.data$month == "02", {{ cost_var }}, 0),
+      mar_cost = dplyr::if_else(.data$month == "03", {{ cost_var }}, 0)
     )
 }
