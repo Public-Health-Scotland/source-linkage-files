2e71af6b38f71fbc3d3f19785648a094774724ed
James McMahon <james.mcmahon@phs.scot>
2022-05-16 11:51:10 +0100

    Make the LTC flags an integer

--- Production_scripts/Create_slf_lookups/lookup_ltc.R
+++ Production_scripts/Create_slf_lookups/lookup_ltc.R
@@ -80,7 +80,7 @@ ltc_file <- read_csv(
 # then sort by chi
 
 ltc_flags <- ltc_file %>%
-  mutate(across(ends_with("date"), list(flag = ~ if_else(is.na(.x) | .x > end_fy, 0, 1)))) %>%
+  mutate(across(ends_with("date"), list(flag = ~ if_else(is.na(.x) | .x > end_fy, 0L, 1L)))) %>%
   rename_with(.cols = ends_with("flag"), .fn = ~ stringr::str_remove(.x, "_date_flag"))
 
 
