7980fb67369f7d6766204f55820342d1499ba125
Catherine Holland <75622719+catherineholland1@users.noreply.github.com>
2022-05-16 14:22:20 +0100

    Update All_years/04-Social_Care/Care_Home_data.R
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>

--- All_years/04-Social_Care/Care_Home_data.R
+++ All_years/04-Social_Care/Care_Home_data.R
@@ -246,7 +246,7 @@ ch_markers <- matched_deaths_data %>%
   # uses the social care id and sending location so can be used for episodes that are not attached to a CHI number
   # This will restrict continuous stays to each Local Authority
   group_by(social_care_id, sending_location) %>%
-  mutate(continuous_stay_sc = pmax(ch_admission_date != lag(ch_discharge_date) + days(1), FALSE, na.rm = TRUE)) %>%
+  mutate(continuous_stay_sc = pmax(ch_admission_date >= lag(ch_discharge_date) + days(1), FALSE, na.rm = TRUE)) %>%
   mutate(ch_sc_id_cis = cumsum(continuous_stay_sc) + 1) %>%
   ungroup()
 
