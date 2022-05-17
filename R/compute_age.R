0d30d96ccca7eedd20fa19b67ed737fed9e87b34
Catherine Holland <catheh03@nssrstudio04.csa.scot.nhs.uk>
2022-05-17 11:25:11 +0100

    age function

--- R/compute_age.R
+++ R/compute_age.R
@@ -1,4 +1,4 @@
-#' Compute Age at Midpoint of Clients
+#' Compute Age of Clients at Midpoint of Year
 #'
 #' @param data data to assign age to
 #' @param fyyear financial year
@@ -9,9 +9,10 @@
 #'
 #' @examples
 #' dob <- c("01-01-1990", "10-05-1960")
-#' compute_age(ca)
-compute_age <- function(data, year, dob) {
+#' fyyear <- "1920"
+#' compute_age(fyyear, dob)
+compute_age <- function(data, fyyear, dob) {
   data <- data %>%
     # age
-    mutate(age = lubridate::as.period(lubridate::interval(start = dob, end = midpoint_fy(fyyear)))$year)
+    dplyr::mutate(age = lubridate::as.period(lubridate::interval(start = dob, end = midpoint_fy(fyyear)))$year)
 }
