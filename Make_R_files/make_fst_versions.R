e51b226d626907e05c6723a28f855a76e38bfe5c
James McMahon <james.mcmahon@phs.scot>
2022-05-18 16:07:34 +0100

    Use `set_names` so that any returned list will be named too

--- Make_R_files/make_fst_versions.R
+++ Make_R_files/make_fst_versions.R
@@ -2,7 +2,7 @@ library(furrr)
 
 source("Make_R_files/make_fst_version_functions.R")
 
-years <- list("1718", "1819", "1920", "2021", "2122")
+years <- purrr::set_names(c("1718", "1819", "1920", "2021", "2122"))
 
 plan(multisession, workers = length(years))
 future_map(years, ~ create_fst_files(.x))
