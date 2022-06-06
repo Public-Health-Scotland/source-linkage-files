a0a0c96dadecd98ac26402b00048d013f2c56870
James McMahon <james.mcmahon@phs.scot>
2022-06-06 09:07:20 +0100

    Fix some build errors/warnings. (#247)
    The `surpressWarnings` is needed as `{slfhelper}` loads `{fst}` which produces the warning:
    ```
    fst package v0.9.8
    Warning message:
    package ‘fst’ was built under R version 3.6.3
    ```
    I could/should probably suppress this more cleverly in slfhelper itself!
    
    The `get_practice_details` test failure is just because we now also produce an rds version. These errors will become more common as we make rds versions and eventually stop producing zsav versions, so we just need to stay on top of them.

--- tests/testthat/test-get_practice_details_path.R
+++ tests/testthat/test-get_practice_details_path.R
@@ -1,4 +1,8 @@
 test_that("GP clusters file (Practice Details) path works", {
   expect_s3_class(get_practice_details_path(ext = "zsav"), "fs_path")
-  expect_error(get_practice_details_path(ext = "rds"))
+  expect_s3_class(get_practice_details_path(), "fs_path")
+
+  expect_equal(fs::path_ext(get_practice_details_path()), "rds")
+
+  expect_match(get_practice_details_path(), glue::glue("practice_details_{latest_update()}"))
 })
