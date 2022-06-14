1e167410c167c3e9740723a62391c118b2f7621f
Catherine Holland <catherinefholland@gmail.com>
2022-06-14 12:23:30 +0100

    gp clusters

--- R/get_slf_lookup_paths.R
+++ R/get_slf_lookup_paths.R
@@ -51,3 +51,25 @@ get_slf_deaths_path <- function(update = latest_update(), ...) {
 
   return(slf_deaths_file_path)
 }
+
+
+
+#' Get the full path to the SLF GP Cluster lookup file
+#'
+#' @param update The update month to use,
+#' defaults to [latest_update()]
+#' @param ... additional arguments passed to [get_file_path()]
+#'
+#' @return The path to the costs lookup as an [fs::path()]
+#' @export
+#' @family file path functions
+#' @seealso [get_file_path()] for the generic function.
+get_slf_gp_cluster_path <- function(update = latest_update(), ...) {
+  slf_gp_cluster_file_path <- get_file_path(
+    directory = fs::path(get_slf_dir(), "Lookups"),
+    file_name = glue::glue("practice_details_{update}.rds"),
+    ...
+  )
+
+  return(slf_gp_cluster_file_path)
+}
