771a3ccefe7eb5e330a50cf863051a6f639eb435
James McMahon <james.mcmahon@phs.scot>
2022-06-20 14:42:55 +0100

    Add a function to check if a date is within a given FY

---
+++ tests/testthat/test-is_date_in_fyyear.R
@@ -0,0 +1,17 @@
+test_that("is_date_in_year errors as expected", {
+  expect_error(is_date_in_year(Sys.time(), "2017"),
+               "The year has been entered in the wrong format")
+
+  expect_error(is_date_in_year("2017-04-01", "1718"),
+               "not a recognized date-time")
+})
+
+test_that("is_date_in_year works as expected", {
+  expect_type(is_date_in_year(Sys.time(), "1718"), "logical")
+
+  expect_true(is_date_in_year(as.Date("2017-04-01"), "1718"))
+  expect_true(is_date_in_year(as.Date("2018-03-31"), "1718"))
+
+  expect_false(is_date_in_year(as.Date("2017-03-31"), "1718"))
+  expect_false(is_date_in_year(as.Date("2018-04-01"), "1718"))
+})
