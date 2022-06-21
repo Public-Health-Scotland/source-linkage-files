66bf2b6f0a513b5feedda1a77443788519092914
Catherine Holland <75622719+catherineholland1@users.noreply.github.com>
2022-06-21 08:16:08 +0100

    Update R/compute_mid_year_age.R
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>

--- R/compute_mid_year_age.R
+++ R/compute_mid_year_age.R
@@ -1,18 +1,20 @@
-#' Compute Age of Clients at Midpoint of Year
+#' Compute Age at Midpoint of Year
 #'
-#' @param data data to assign age to
 #' @param fyyear financial year
-#' @param dob date of birth of the client
+#' @param dob date of birth
 #'
-#' @return a vector of age at midpoint
+#' @return a vector of ages at the financial year midpoint
 #' @export
 #'
 #' @examples
 #' dob <- as.Date(c("01-01-1990", "10-05-1960"))
 #' fyyear <- "1920"
 #' compute_mid_year_age(fyyear, dob)
-compute_mid_year_age <- function(data, fyyear, dob) {
-  data <- data %>%
-    # age
-    dplyr::mutate(age = lubridate::as.period(lubridate::interval(start = dob, end = midpoint_fy(fyyear)))$year)
+compute_mid_year_age <- function(fyyear, dob) {
+
+ age_intervals <- lubridate::interval(start = dob, end = midpoint_fy(fyyear))
+
+ ages <- lubridate::as.period(age_intervals$year)
+ 
+ return(ages)
 }
