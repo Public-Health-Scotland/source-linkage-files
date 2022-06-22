0252ae17cd43de6f005206e6eac632837b0bd48c
Catherine Holland <75622719+catherineholland1@users.noreply.github.com>
2022-06-22 07:48:16 +0100

    Update R/get_costs_paths.R
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>

--- R/get_costs_paths.R
+++ R/get_costs_paths.R
@@ -94,7 +94,7 @@ get_hc_costs_path <- function(..., update = NULL) {
 #'
 #' @return The path to the costs lookup as an [fs::path()]
 #' @export
-#' @family costs lookup
+#' @family costs lookup file paths
 #' @seealso [get_file_path()] for the generic function.
 get_hc_raw_costs_path <- function(..., update = NULL) {
   hc_raw_costs_path <- get_file_path(
