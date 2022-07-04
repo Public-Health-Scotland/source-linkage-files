650b54fec4212b243c42b2a64a2f61176693f9ea
Jennifer Thom <jennifer.thom@phs.scot>
2022-07-04 15:59:43 +0100

    Specify test variables - no beddays in A&E data

--- Production_scripts/Process_extracts/Produce_extract_tests/extract_ae_tests.R
+++ Production_scripts/Process_extracts/Produce_extract_tests/extract_ae_tests.R
@@ -23,16 +23,22 @@ new_data <- readr::read_rds(
   get_source_extract_path(year, "AE", ext = "rds")
 )
 
-
 # Read current SLF episode file
 existing_data <- get_existing_data_for_tests(new_data = new_data)
 
 
 # Produce comparison-------------------------------------
+
 # Compare new file with existing slf data
 comparison <- produce_test_comparison(
-  produce_source_extract_tests(existing_data),
-  produce_source_extract_tests(new_data)
+  produce_source_extract_tests(existing_data,
+    sum_mean_vars = "cost",
+    max_min_vars = c("record_keydate1", "record_keydate2", "cost_total_net")
+  ),
+  produce_source_extract_tests(new_data,
+    sum_mean_vars = "cost",
+    max_min_vars = c("record_keydate1", "record_keydate2", "cost_total_net")
+  )
 )
 
 
