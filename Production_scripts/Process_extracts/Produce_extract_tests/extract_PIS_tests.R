6a84eec7d9d7447887d3404f04d36020636142f1
Jennifer Thom <jennifer.thom@phs.scot>
2022-07-05 14:45:39 +0100

    Produce PIS test script

---
+++ Production_scripts/Process_extracts/Produce_extract_tests/extract_PIS_tests.R
@@ -0,0 +1,42 @@
+####################################################
+# Name of file - PIS-tests.R
+# Original Authors - Jennifer Thom
+# Original Date - July 2022
+# Written/run on - RStudio Server
+# Version of R - 3.6.1
+# Description - Produce tests for source linkage files:
+#               PIS processed file.
+#####################################################
+
+# Load packages
+library(createslf)
+library(openxlsx)
+library(slfhelper)
+
+
+# Read in Data-----------------------------------------
+
+year <- "1920"
+
+# Read new data file
+new_data <- readr::read_rds(get_source_extract_path(year, "PIS", ext = "rds"))
+
+# Read current SLF episode file and filter for 01B and GLS records
+existing_data <- get_existing_data_for_tests(new_data = new_data)
+
+
+# Produce comparison-------------------------------------
+# Compare new file with existing slf data
+comparison <- produce_test_comparison(
+  produce_source_extract_tests(existing_data),
+  produce_source_extract_tests(new_data)
+)
+
+
+# Produce Outfile----------------------------------------
+
+# Save test comparisons as an excel workbook
+write_tests_xlsx(comparison, "PIS_extract")
+
+
+## END OF SCRIPT ##
