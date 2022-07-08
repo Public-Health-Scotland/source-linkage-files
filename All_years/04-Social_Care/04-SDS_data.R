a8d112d55387448d72c30df45996cf0df5a73d88
Jennifer Thom <jennifer.thom@phs.scot>
2022-07-08 13:06:44 +0100

    SDS data cleaning/save outfile

--- All_years/04-Social_Care/04-SDS_data.R
+++ All_years/04-Social_Care/04-SDS_data.R
@@ -51,7 +51,7 @@ sds_full_data <- tbl(db_connection, in_schema("social_care_2", "sds_snapshot")) 
     sds_end_date,
     sds_option_1,
     sds_option_2,
-    sds_option_3,
+    sds_option_3
   ) %>%
   collect()
 
@@ -61,16 +61,16 @@ sds_full_clean <- sds_full_data %>%
   # Match on demographics data (chi, gender, dob and postcode)
   left_join(sc_demographics, by = c("sending_location", "social_care_id")) %>%
   # If sds start date is missing, assign start of FY
-  mutate(start_fy = as.Date(paste0(period, "-04-01")),
-         sds_start_date = if_else(is.na(sds_start_date), start_fy, sds_start_date)
-         ) %>%
+  mutate(
+    start_fy = as.Date(paste0(period, "-04-01")),
+    sds_start_date = if_else(is.na(sds_start_date), start_fy, sds_start_date)
+  ) %>%
   # rename for matching source variables
   rename(
     record_keydate1 = sds_start_date,
     record_keydate2 = sds_end_date
   ) %>%
   # Pivot longer on sds option variables
-  # tried cols = contains("sds_option_") here but not working
   pivot_longer(
     cols = contains("sds_option_"),
     names_to = "sds_option",
@@ -79,17 +79,50 @@ sds_full_clean <- sds_full_data %>%
   filter(received == "1") %>%
   # Include source variables
   mutate(
-    year = period,
     recid = "SDS",
+    sds_option = case_when(
+      sds_option == "sds_option_1" ~ 1,
+      sds_option == "sds_option_2" ~ 2,
+      sds_option == "sds_option_3" ~ 3
+    ),
     smrtype = case_when(
-      sds_option == "1" ~ "SDS-1",
-      sds_option == "2" ~ "SDS-2",
-      sds_option == "3" ~ "SDS-3"),
-    mid_fy = as.Date(paste0(year, "-09-30")),
-    # age not working
-    age = lubridate::difftime(mid_fy, dob, units = "years"),
+      sds_option == 1 ~ "SDS-1",
+      sds_option == 2 ~ "SDS-2",
+      sds_option == 3 ~ "SDS-3"
+    ),
+    mid_fy = as.Date(paste0(period, "-09-30")),
+    # create age variable
+    age = floor(time_length(interval(dob, mid_fy), "years")),
     # Create person id variable
     person_id = glue::glue("{sending_location}-{social_care_id}"),
     # Use function for creating sc send lca variables
     sc_send_lca = convert_sc_sl_to_lca(sending_location)
   )
+
+# work out sds_option_4
+sds_option_4 <- sds_full_clean %>%
+  group_by(social_care_id) %>%
+  summarise(n_packages = n())
+
+# Match back onto data
+outfile <- sds_full_clean %>%
+  left_join(sds_option_4, by = "social_care_id") %>%
+  mutate(
+    sds_option_4 = if_else(n_packages >= 2, 1, 0)
+  )
+
+
+# Save outfile------------------------------------------------
+
+outfile %>%
+  # save rds file
+  readr::write_rds(path(social_care_dir, str_glue("all_sds_episodes_{latest_update}.rds")),
+    compress = "gz"
+  ) %>%
+  # save sav file
+  haven::write_sav(path(social_care_dir, str_glue("all_sds_episodes_{latest_update}.zsav")),
+    compress = TRUE
+  )
+
+
+# End of Script #
