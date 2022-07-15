194b5d54f008f3e13c4c11ce35b56d4fe78ad02d
James McMahon <james.mcmahon@phs.scot>
2022-07-15 14:51:33 +0100

    remove unnessecary rds parameter as rds is the default

--- Production_scripts/Process_extracts/Produce_extract_tests/extract_acute_incl_gls_tests.R
+++ Production_scripts/Process_extracts/Produce_extract_tests/extract_acute_incl_gls_tests.R
@@ -17,7 +17,7 @@ library(createslf)
 year <- check_year_format("1920")
 
 # Read new data file
-new_data <- readr::read_rds(get_source_extract_path(year, "Acute", ext = "rds"))
+new_data <- readr::read_rds(get_source_extract_path(year, "Acute"))
 
 # Read current SLF episode file and filter for 01B and GLS records
 existing_data <- get_existing_data_for_tests(new_data = new_data)
--- Production_scripts/Process_extracts/Produce_extract_tests/extract_mental_health_tests.R
+++ Production_scripts/Process_extracts/Produce_extract_tests/extract_mental_health_tests.R
@@ -17,7 +17,7 @@ library(createslf)
 year <- check_year_format("1920")
 
 # Read new data file
-new_data <- readr::read_rds(get_source_extract_path(year, "MH", ext = "rds"))
+new_data <- readr::read_rds(get_source_extract_path(year, "MH"))
 
 # Read current SLF episode file and filter for 01B and GLS records
 existing_data <- get_existing_data_for_tests(new_data = new_data)
@@ -35,5 +35,6 @@ comparison <- produce_test_comparison(
 
 # Save test comparisons as an excel workbook
 write_tests_xlsx(comparison, "mental_health_extract")
+
 
 ## END OF SCRIPT ##
