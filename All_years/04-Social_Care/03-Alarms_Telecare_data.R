f57d42c6b58f38b89c9acf5e0be4dde3edd276ba
Jennifer Thom <jennifer.thom@phs.scot>
2022-07-19 13:51:34 +0100

    Alarms Telecare tweaks - WIP

--- All_years/04-Social_Care/03-Alarms_Telecare_data.R
+++ All_years/04-Social_Care/03-Alarms_Telecare_data.R
@@ -60,26 +60,54 @@ at_full_data <- tbl(db_connection, in_schema("social_care_2", "equipment_snapsho
 
 # Data Cleaning-----------------------------------------------------
 
+
+----------------------------------------------------------------
+# assign start of FY
+
 # Work out the start fY for each period
 # CHECK - do we need 2017 as the start of FY?
 # CHECK - do we need start of Quarter?
 # CHECK - do we remove these records? - 11,575 are missing
 # CHECK - period = 2020 what do we change this to?
+#pre_compute_record_dates <- at_full_data %>%
+#  distinct(period) %>%
+#  mutate(
+ #   year = substr(period, 1, 4),
+ #   start_fy = as.Date(paste0(year, "-04-01"))
+#  )
+
+#replaced_start_dates <- at_full_data %>%
+  # Replace missing start dates with the start of the fy
+  #left_join(pre_compute_record_dates, by = "period") %>%
+  #tidylog::mutate(
+   # start_date_missing = is.na(service_start_date),
+   # at_service_start_date = if_else(
+   #   start_date_missing,
+   #  start_fy,
+   #   service_start_date
+   #   )
+#)
+
+------------------------------------------------------------
+
+# Work out the dates for each period
+# Record date is the last day of the quarter
+# qtr_start is the first day of the quarter
 pre_compute_record_dates <- at_full_data %>%
   distinct(period) %>%
   mutate(
-    year = substr(period, 1, 4),
-    start_fy = as.Date(paste0(year, "-04-01"))
+    record_date = yq(period) %m+% period(6, "months") %m-% days(1),
+    qtr_start = yq(period) %m+% period(3, "months")
   )
 
 replaced_start_dates <- at_full_data %>%
-  # Replace missing start dates with the start of the fy
+  # Replace missing start dates with the start of the quarter
   left_join(pre_compute_record_dates, by = "period") %>%
   tidylog::mutate(
     start_date_missing = is.na(service_start_date),
-    at_service_start_date = if_else(
+    service_start_date = if_else(
       start_date_missing,
-      start_fy,
+      qtr_start,
       service_start_date
     )
   )
@@ -89,7 +117,7 @@ at_full_clean <- replaced_start_dates %>%
   left_join(sc_demographics, by = c("sending_location", "social_care_id")) %>%
   # rename for matching source variables
   rename(
-    record_keydate1 = at_service_start_date,
+    record_keydate1 = service_start_date,
     record_keydate2 = service_end_date
   ) %>%
   # Include source variables
@@ -104,9 +132,36 @@ at_full_clean <- replaced_start_dates %>%
     # Use function for creating sc send lca variables
     sc_send_lca = convert_sc_sl_to_lca(sending_location)
   ) %>%
+  # rename period as sc_latest_submission variable
   rename(
     sc_latest_submission = period
+  ) %>%
+  # Assign period with no quarter to yearQ4 - TODO - ask SC team as last
+  # meeting said look at extract date - these dont relate.
+  # e.g. extract date later than period
+  mutate(
+    sc_latest_submission = case_when(sc_latest_submission == "2020" ~ "2020Q4",
+                                     sc_latest_submission == "2021" ~ "2021Q4",
+                                     TRUE ~ sc_latest_submission)
+      )
+
+# WIP from here onwards
+
+check <- at_full_clean %>%
+  group_by(sending_location, social_care_id, smrtype) %>%
+  arrange(sc_latest_submission, record_keydate1, .by_group = TRUE) %>%
+  mutate(flag = (record_keydate1 > lag(record_keydate2)) %>%
+           replace_na(TRUE),
+         episode_counter = cumsum(flag)
   )
+
+last <- check %>%
+  # possibly do an ungroup and then group by(sending_location, social_care_id, sds_option)
+  group_by(episode_counter, .add = TRUE) %>%
+  summarise(record_keydate1 = min(record_keydate1),
+            across(everything(), last)
+
+
 
 
 # Save outfile------------------------------------------------
