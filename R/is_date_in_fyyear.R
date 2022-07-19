3048d245141fb591b6db280ef635a1e3686a7a29
James McMahon <james.mcmahon@phs.scot>
2022-07-19 17:00:29 +0100

    Expand `is_date_in_year` to check intervals
    The original function checked a single date was in the financial year, it can now also check if a date range falls in a financial year.

--- R/is_date_in_fyyear.R
+++ R/is_date_in_fyyear.R
@@ -1,17 +1,37 @@
-#' Check if a date in the financial year
+#' Check if a date, or interval is in the financial year
 #'
-#' @description Test to check if a date is within the financial year
+#' @description Test to check if a date, or intervals is within a given
+#' financial year. If given just one date it will check to see if it is in the
+#' given financial year. When supplied with two dates it will check to see if
+#' any part of that date range falls in the given financial year.
 #'
-#' @param date The date to be checked
+#' @param date The main / start date to check
 #' @param year The financial year in the format '1718' as a character
+#' @param date_end (optional) The end date
 #'
-#' @return logical T/F
+#' @return a logical T/F
+#'
 #' @export
 #'
 #' @examples
 #' is_date_in_year(Sys.time(), "2223")
+#' is_date_in_year(
+#'   date = as.Date("2020-01-01"),
+#'   date_end = as.Date("2023-01-01"),
+#'   year = "2122"
+#' )
 #'
 #' @family date functions
-is_date_in_year <- function(date, year) {
-  lubridate::`%within%`(date, fy_interval(year))
+is_date_in_year <- function(date, year, date_end = NULL) {
+  if (is.null(date_end)) {
+    lubridate::`%within%`(date, fy_interval(year))
+  } else {
+    if (date > date_end) {
+      cli::cli_abort("The start date cannot come after the end date.")
+    }
+
+    date_interval <- lubridate::interval(date, date_end)
+
+    lubridate::int_overlaps(date_interval, fy_interval(year))
+  }
 }
