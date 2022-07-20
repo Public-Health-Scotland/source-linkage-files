22b3f1791c8ff5eb67315777e0a9e43025f7cac6
Jennifer Thom <jennifer.thom@phs.scot>
2022-07-20 10:26:06 +0100

    Remove typo

--- Production_scripts/Process_extracts/extract_acute_incl_gls.R
+++ Production_scripts/Process_extracts/extract_acute_incl_gls.R
@@ -270,6 +270,6 @@ outfile %>%
   # Save as zsav file
   write_sav(get_source_extract_path(year, "Acute", ext = "zsav", check_mode = "write")) %>%
   # Save as rds file
-  write_rds(get_source_extract_path(year, "Acute", , check_mode = "write"))
+  write_rds(get_source_extract_path(year, "Acute", check_mode = "write"))
 
 ## End of Script ##
