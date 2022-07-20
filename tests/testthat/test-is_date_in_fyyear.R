a03757b28dcc2c2b31fc94e74741929714061f5b
Moohan <Moohan@users.noreply.github.com>
2022-07-20 12:04:42 +0000

    Style code

--- tests/testthat/test-is_date_in_fyyear.R
+++ tests/testthat/test-is_date_in_fyyear.R
@@ -23,12 +23,12 @@ test_that("is_date_in_fyyear works for a single year", {
 test_that("is_date_in_fyyear works for a year range (interval)", {
   expect_type(is_date_in_fyyear("1718", Sys.Date(), Sys.Date() + 1), "logical")
 
-  expect_true(is_date_in_fyyear("1718",as.Date("2017-04-01"),  as.Date("2018-04-01")))
-  expect_true(is_date_in_fyyear("1718",as.Date("2018-03-31"),  as.Date("2021-04-01")))
+  expect_true(is_date_in_fyyear("1718", as.Date("2017-04-01"), as.Date("2018-04-01")))
+  expect_true(is_date_in_fyyear("1718", as.Date("2018-03-31"), as.Date("2021-04-01")))
 
   # Starts before, ends after
-  expect_true(is_date_in_fyyear("1718",as.Date("2016-03-31"),  as.Date("2019-04-01")))
+  expect_true(is_date_in_fyyear("1718", as.Date("2016-03-31"), as.Date("2019-04-01")))
 
-  expect_false(is_date_in_fyyear("1718",as.Date("2017-01-01"),  as.Date("2017-03-31")))
-  expect_false(is_date_in_fyyear("1718",as.Date("2018-04-01"),  as.Date("2018-12-31")))
+  expect_false(is_date_in_fyyear("1718", as.Date("2017-01-01"), as.Date("2017-03-31")))
+  expect_false(is_date_in_fyyear("1718", as.Date("2018-04-01"), as.Date("2018-12-31")))
 })
