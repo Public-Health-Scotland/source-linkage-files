62d87e1404e15fc02943fb84b3b4861c245ac608
James McMahon <james.mcmahon@phs.scot>
2022-07-20 14:00:00 +0100

    Expand to deal with missing end dates
    The full check that we need is now encapsulated in the single function.

--- R/is_date_in_fyyear.R
+++ R/is_date_in_fyyear.R
@@ -1,15 +1,19 @@
 #' Check if a date, or interval is in the financial year
 #'
 #' @description Test to check if a date, or intervals is within a given
-#' financial year. If given just one date it will check to see if it is in the
-#' given financial year. When supplied with two dates it will check to see if
-#' any part of that date range falls in the given financial year.
+#' financial year.
+#' If given just one date it will check to see if it is in the
+#' given financial year.
+#' When supplied with two dates it will check to see if
+#' any part of that date range falls in the given financial year. If the
+#' `date_end` is `NA` it checks that `date` was before or during the financial
+#' year.
 #'
 #' @param fyyear The financial year in the format '1718' as a character
 #' @param date The main/start date to check
 #' @param date_end (optional) The end date
 #'
-#' @return a logical T/F or `NA` if either of the dates are `NA`
+#' @return a logical, TRUE/FALSE
 #'
 #' @export
 #'
@@ -24,10 +28,16 @@
 #' @family date functions
 is_date_in_fyyear <- function(fyyear, date, date_end = NULL) {
   if (is.null(date_end)) {
-    return(lubridate::`%within%`(date, fy_interval(fyyear)))
+    is_date_in_fyyear <- lubridate::`%within%`(date, fy_interval(fyyear))
+
+    return(is_date_in_fyyear)
   } else {
     date_interval <- lubridate::interval(date, date_end)
 
-    return(lubridate::int_overlaps(date_interval, fy_interval(fyyear)))
+    is_date_in_fyyear <- dplyr::if_else(!is.na(date_end),
+                   lubridate::int_overlaps(date_interval, fy_interval(fyyear)),
+                   (date <= end_fy(fyyear)))
+
+    return(is_date_in_fyyear)
   }
 }
