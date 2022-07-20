318a1a7b8eaa19cabfb3a5913c12ba4f3c1539bb
James McMahon <james.mcmahon@phs.scot>
2022-07-20 13:01:44 +0100

    update tests

--- tests/testthat/test-is_date_in_fyyear.R
+++ tests/testthat/test-is_date_in_fyyear.R
@@ -1,34 +1,34 @@
-test_that("is_date_in_year errors as expected", {
+test_that("is_date_in_fyyear errors as expected", {
   expect_error(
-    is_date_in_year(Sys.time(), "2017"),
+    is_date_in_fyyear("2017", Sys.time()),
     "The year has been entered in the wrong format"
   )
 
   expect_error(
-    is_date_in_year("2017-04-01", "1718"),
+    is_date_in_fyyear("1718", "2017-04-01"),
     "not a recognized date-time"
   )
 })
 
-test_that("is_date_in_year works for a single year", {
-  expect_type(is_date_in_year(Sys.time(), "1718"), "logical")
+test_that("is_date_in_fyyear works for a single year", {
+  expect_type(is_date_in_fyyear("1718", Sys.time()), "logical")
 
-  expect_true(is_date_in_year(as.Date("2017-04-01"), "1718"))
-  expect_true(is_date_in_year(as.Date("2018-03-31"), "1718"))
+  expect_true(is_date_in_fyyear("1718", as.Date("2017-04-01")))
+  expect_true(is_date_in_fyyear("1718", as.Date("2018-03-31")))
 
-  expect_false(is_date_in_year(as.Date("2017-03-31"), "1718"))
-  expect_false(is_date_in_year(as.Date("2018-04-01"), "1718"))
+  expect_false(is_date_in_fyyear("1718", as.Date("2017-03-31")))
+  expect_false(is_date_in_fyyear("1718", as.Date("2018-04-01")))
 })
 
-test_that("is_date_in_year works for a year range (interval)", {
-  expect_type(is_date_in_year(Sys.Date(), "1718", Sys.Date() + 1), "logical")
+test_that("is_date_in_fyyear works for a year range (interval)", {
+  expect_type(is_date_in_fyyear("1718", Sys.Date(), Sys.Date() + 1), "logical")
 
-  expect_true(is_date_in_year(as.Date("2017-04-01"), "1718", as.Date("2018-04-01")))
-  expect_true(is_date_in_year(as.Date("2018-03-31"), "1718", as.Date("2021-04-01")))
+  expect_true(is_date_in_fyyear("1718",as.Date("2017-04-01"),  as.Date("2018-04-01")))
+  expect_true(is_date_in_fyyear("1718",as.Date("2018-03-31"),  as.Date("2021-04-01")))
 
   # Starts before, ends after
-  expect_true(is_date_in_year(as.Date("2016-03-31"), "1718", as.Date("2019-04-01")))
+  expect_true(is_date_in_fyyear("1718",as.Date("2016-03-31"),  as.Date("2019-04-01")))
 
-  expect_false(is_date_in_year(as.Date("2017-01-01"), "1718", as.Date("2017-03-31")))
-  expect_false(is_date_in_year(as.Date("2018-04-01"), "1718", as.Date("2018-12-31")))
+  expect_false(is_date_in_fyyear("1718",as.Date("2017-01-01"),  as.Date("2017-03-31")))
+  expect_false(is_date_in_fyyear("1718",as.Date("2018-04-01"),  as.Date("2018-12-31")))
 })
