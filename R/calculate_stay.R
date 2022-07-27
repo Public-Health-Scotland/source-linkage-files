5c89a9ba1b1c7997624ef31ae618c0b759421253
Jennifer Thom <jennifer.thom@phs.scot>
2022-07-27 13:10:10 +0100

    update documentation/parameter info

--- R/calculate_stay.R
+++ R/calculate_stay.R
@@ -3,10 +3,10 @@
 #' @description Calculate the total length of stay between start_date and end_date.
 #' If the end_date is missing then use the dummy discharge date.
 #'
-#' @param data Data to calculate the total length of stay
 #' @param year The financial year in '1920' format
 #' @param start_date The admission/start date variable. e.g. record_keydate1
 #' @param end_date The discharge/end date variable. e.g record_keydate2
+#' @param sc_qtr The latest submitted quarter. e.g. sc_latest_submission
 #'
 #' @return a [tibble][tibble::tibble-package] with additional variable `stay`.
 #' If there is no end date use dummy discharge to calculate the total length of stay.
