15143f8fa8f521a1ba13ba73d1c81304c9277faa
shintoLamp <bateman.mcbride@phs.scot>
2022-07-27 14:19:21 +0100

    Used create_demog_test_flags instead, as per suggestion

--- R/produce_slf_homelessness_tests.R
+++ R/produce_slf_homelessness_tests.R
@@ -12,20 +12,12 @@
 produce_slf_homelessness_tests <- function(data,
                                            max_min_vars = c("record_keydate1", "record_keydate2")) {
   test_flags <- data %>%
-    # Would usually use create_demog_test_flags() here but needs to be different for Homelessness
     dplyr::arrange(.data$chi) %>%
     # create test flags
-    dplyr::mutate(
-      has_chi = dplyr::if_else(!is_missing(.data$chi), 1, 0),
-      n_males = dplyr::if_else(.data$gender == 1, 1, 0),
-      n_females = dplyr::if_else(.data$gender == 2, 1, 0),
-      missing_dob = dplyr::if_else(is.na(.data$dob), 1, 0),
-      hl1_main = dplyr::if_else(.data$smrtype == "HL1-Main", 1, 0),
-      hl1_other = dplyr::if_else(.data$smrtype == "HL1-Other", 1, 0)
-    ) %>%
+    create_demog_test_flags() %>%
     create_lca_test_flags(.data$hl1_sending_lca) %>%
     # keep variables for comparison
-    dplyr::select(c(.data$has_chi:.data$West_Lothian)) %>%
+    dplyr::select(c(.data$valid_chi:.data$West_Lothian)) %>%
     # use function to sum new test flags
     calculate_measures(measure = "sum")
 
