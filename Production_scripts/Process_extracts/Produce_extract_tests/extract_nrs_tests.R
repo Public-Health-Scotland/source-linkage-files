64db41e9cc6810780400985e1567bb2e60858a08
shintoLamp <bateman.mcbride@phs.scot>
2022-08-02 10:26:21 +0100

    Renamed function, corrected comment

--- Production_scripts/Process_extracts/Produce_extract_tests/extract_nrs_tests.R
+++ Production_scripts/Process_extracts/Produce_extract_tests/extract_nrs_tests.R
@@ -18,15 +18,15 @@ year <- check_year_format("1920")
 # Read new data file
 new_data <- readr::read_rds(get_source_extract_path(year, "Deaths"))
 
-# Read current SLF episode file and filter for 01B and GLS records
+# Read current SLF episode file and filter for Death records
 existing_data <- get_existing_data_for_tests(new_data = new_data)
 
 
 # Produce comparison-------------------------------------
 # Compare new file with existing slf data
 comparison <- produce_test_comparison(
-  produce_nrs_extract_tests(existing_data),
-  produce_nrs_extract_tests(new_data)
+  produce_source_nrs_tests(existing_data),
+  produce_source_nrs_tests(new_data)
 )
 
 # Produce Outfile----------------------------------------
