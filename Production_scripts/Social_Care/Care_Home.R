b39a7e3bad0dc8d5b3e06a8c68c361c7b665dd46
James McMahon <james.mcmahon@phs.scot>
2022-08-03 17:25:11 +0100

    WIP - Care Home name matching basically done but with some issues

--- Production_scripts/Social_Care/Care_Home.R
+++ Production_scripts/Social_Care/Care_Home.R
@@ -96,62 +96,138 @@ ch_clean <- ch_data %>%
 # read in demographic data
 sc_demog <- readr::read_rds(get_sc_demog_lookup_path())
 
-matched_ch_data <- ch_clean %>%
-  left_join(sc_demog, by = c("sending_location", "social_care_id")) %>%
-  # correct postcode formatting
-  mutate(across(contains("postcode"), phsmethods::format_postcode))
-
 # Get a list of confirmed valid Scottish postcodes from the SPD
 valid_spd_postcodes <- readr::read_rds(get_spd_path()) %>%
   pull(pc7)
 
+matched_ch_data <- ch_clean %>%
+  left_join(sc_demog, by = c("sending_location", "social_care_id")) %>%
+  # Make the care home name more uniform
+  mutate(ch_name = clean_up_free_text(ch_name)) %>%
+  # correct postcode formatting
+  mutate(across(contains("postcode"), phsmethods::format_postcode),
+    # replace invalid postcode with NA
+    ch_postcode = na_if(ch_postcode, ch_postcode %in% valid_spd_postcodes)
+  )
+
+
 # Care Home Name lookup
 ch_name_lookup <-
+  # Care Home name lookup from the Care Inspectorate
+  # Previous contact Al Scrougal - Al.Scougal@careinspectorate.gov.scot
   readxl::read_xlsx(get_slf_ch_name_lookup_path()) %>%
-  filter(is.na(DateCanx) | DateCanx > start_fy("1718")) %>%
+  # Drop any Care Homes that were closed before 2017/18
+  filter(is.na(DateCanx) | DateCanx >= start_fy("1718")) %>%
   rename(
     ch_postcode = "AccomPostCodeNo",
-    ch_name_lookup = "ServiceName"
+    ch_name_validated = "ServiceName"
   ) %>%
   select(
     ch_postcode,
-    ch_name_lookup,
+    ch_name_validated,
     DateReg,
     DateCanx
   ) %>%
-  # format postcode and CH name
+  # Standardise the postcode and CH name
   mutate(
     ch_postcode = phsmethods::format_postcode(ch_postcode),
-    ch_name_lookup = clean_up_free_text(ch_name_lookup)
+    ch_name_validated = clean_up_free_text(ch_name_validated)
   ) %>%
-  group_by(ch_postcode, ch_name_lookup) %>%
+  # Merge any duplicates, and get the interval each CH name was active
+  group_by(ch_postcode, ch_name_validated) %>%
   summarise(open_interval = interval(min(DateReg), replace_na(max(DateCanx), Sys.Date()))) %>%
-  arrange(open_interval) %>%
+  # Find the latest date for each CH name
+  mutate(latest_close_date = max(int_end(open_interval))) %>%
+  ungroup() %>%
+  arrange(open_interval)
+
+# Generate some metrics for how the submitted names connect to the valid names
+ch_name_match_metrics <- matched_ch_data %>%
+  distinct(ch_postcode, ch_name) %>%
+  left_join(ch_name_lookup, by = c("ch_postcode")) %>%
+  drop_na() %>%
+  # Work out string distances between names for each postcode
+  mutate(
+    match_distance_jaccard = stringdist::stringdist(ch_name, ch_name_validated, method = "jaccard"),
+    match_distance_cosine = stringdist::stringdist(ch_name, ch_name_validated, method = "cosine"),
+    match_mean = (match_distance_jaccard + match_distance_cosine) / 2
+  ) %>%
+  # Drop any name matches which aren't very close
+  filter(match_distance_jaccard <= 0.25 | match_distance_cosine <= 0.3) %>%
+  group_by(ch_postcode, ch_name) %>%
+  # Identify the closest match in case there are multiple close matches
+  mutate(
+    min_jaccard = min(match_distance_jaccard, na.rm = TRUE),
+    min_cosine = min(match_distance_cosine, na.rm = TRUE),
+    min_match_mean = min(match_mean, na.rm = TRUE)
+  ) %>%
   ungroup()
 
+no_postcode_match <- matched_ch_data %>%
+  anti_join(ch_name_lookup, by = "ch_postcode")
 
 name_postcode_clean <- matched_ch_data %>%
-  # Make the care home name more uniform
-  mutate(ch_name = clean_up_free_text(ch_name)) %>%
-  # replace invalid postcode with NA
-  mutate(ch_postcode = na_if(ch_postcode, ch_postcode %in% valid_spd_postcodes)) %>%
-  # where there is only a single Care Home at the Postcode, use that name
-  group_by(ch_postcode) %>%
-  # fill in names where NA but has a name in another record
-  fill(ch_name, .direction = "downup") %>%
-  # change name if CH name does not match previous name
-  mutate(ch_name_change_counter = pmax(ch_name != lag(ch_name) & !is.na(ch_name), FALSE, na.rm = TRUE)) %>%
-  mutate(ch_name = if_else(ch_name_change_counter == 1, last(ch_name), ch_name)) %>%
+  # Remove records with no matching postcode, we'll add them back later
+  semi_join(ch_name_lookup, by = "ch_postcode") %>%
+  # Create a unique ID per row so we can get rid of duplicates later
+  mutate(ch_record_id = row_number()) %>%
+  # Match CH names with the generated metrics and the lookup. This will create
+  # duplicates which should be filtered out as we identify matches
+  left_join(ch_name_match_metrics, by = c("ch_postcode", "ch_name")) %>%
+  mutate(
+    # Work out the duration of the stay
+    # If the end date is missing set this to the end of the quarter
+    stay_interval = interval(
+      ch_admission_date,
+      min(ch_discharge_date, record_date, na.rm = TRUE)
+    ),
+    # Highlight which stays overlap with an open care home name
+    stay_overlaps_open = int_overlaps(stay_interval, open_interval) & (int_start(stay_interval) >= int_start(open_interval)),
+    # Hightlight which names seem to be good matches
+    name_match = case_when(
+      # Exact match
+      ch_name == ch_name_validated ~ TRUE,
+      # Submitted name is missing and stay dates are valid for the CH
+      is.na(ch_name) & stay_overlaps_open ~ TRUE,
+      # This name had the closest 'jaccard' distance of all possibilities
+      (min_jaccard == match_distance_jaccard) & match_distance_jaccard <= 0.25 ~ TRUE,
+      # This name had the closest 'cosine' distance of all possibilities
+      (min_cosine == match_distance_cosine) & match_distance_cosine <= 0.3 ~ TRUE,
+      # This name had the closest 'mean' distance (used when the above disagree)
+      (min_match_mean == match_mean) & match_mean <= 0.25 ~ TRUE,
+      # No good match
+      TRUE ~ FALSE
+    )
+  ) %>%
+  # Group by record (There will be duplicate rows per record if there multiple 'options' for the possible CH name)
+  group_by(ch_record_id) %>%
+  mutate(
+    # Highlight where the record has no matches out of any of the options
+    no_name_matches = all(!name_match),
+    # Highlight where the record has no overlaps (in dates) with any of the options
+    no_overlaps = all(!stay_overlaps_open)
+  ) %>%
+  # Keep a record if:
+  # 1) It's name matches `name_match`
+  # Or either 2)a) None of the option's names match AND this option overlaps in dates (e.g. the submitted name is missing but the dates match)
+  # or 2)b) None of the option's names match AND none of the dates overlap (i.e. we don't have any idea what name to use)
+  filter(n() == 1 | sum(name_match) == 1 | all(!name_match)) %>%
+  # For the records which still have multiple options (usually multiple names matched)
+  filter(n() == 1 | int_end(open_interval) == latest_close_date) %>%
+  filter(n() == 1 | match_mean == min_match_mean) %>%
   ungroup() %>%
-  # match CH names with CH lookup
-  left_join(ch_name_lookup, by = c("ch_postcode")) %>%
-  # check CH name matches the lookup name
-  mutate(name_match = if_else(ch_name != ch_name_lookup | is.na(ch_name_lookup), 0, 1)) %>%
-  # replace ch name with lookup name if not a match
-  mutate(ch_name = if_else(name_match == 0 & !is.na(ch_name_lookup), ch_name_lookup, ch_name)) %>%
-  # check admission date within CH dates
-  mutate(date_check = if_else(is.na(DateReg) | is.na(DateCanx) | ch_admission_date %in% range(DateReg, DateCanx), TRUE, FALSE))
+  # Bring back to single record with no duplicates introduce by the lookup
+  distinct(ch_record_id, .keep_all = TRUE) %>%
+  # Replace the ch name with our best guess at the proper name from the lookup
+  mutate(
+    ch_name_old = ch_name,
+    ch_name = dplyr::if_else(is.na(ch_name_validated), ch_name, ch_name_validated)
+  ) %>%
+  # Bring back the records which had no postcode match
+  bind_rows(no_postcode_match)
 
+(check_names <- name_postcode_clean %>%
+  count(ch_name_old, ch_name, sort = TRUE))
 
 # Data Cleaning Care Home Data ---------------------------------------
 
@@ -337,4 +413,5 @@ outfile %>%
   write_rds(get_sc_ch_episodes_path(check_mode = "write"))
 
 
+# End of Script #
 # End of Script #
