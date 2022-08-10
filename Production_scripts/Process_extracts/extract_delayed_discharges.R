12858322d86292f163f1a0819cec40d22ca02765
Jennifer Thom <jennifer.thom@phs.scot>
2022-08-10 15:40:31 +0100

    WIP - date logic not quite working correctly

--- Production_scripts/Process_extracts/extract_delayed_discharges.R
+++ Production_scripts/Process_extracts/extract_delayed_discharges.R
@@ -36,7 +36,7 @@ dd_file <- haven::read_sav(get_dd_path(ext = "zsav")) %>%
 dd_clean <- dd_file %>%
   # Drop any records with obviously bad dates
   filter(
-    keydate1_dateformat <= keydate2_dateformat | keydate2_dateformat == ymd("1900, 1, 1")
+    (keydate1_dateformat <= keydate2_dateformat) | keydate2_dateformat == ymd("1900, 1, 1")
   ) %>%
   # set up variables
   mutate(
@@ -44,8 +44,41 @@ dd_clean <- dd_file %>%
     smrtype = "DelayedDis",
     year = year
   ) %>%
-  # recode the local authority to match source coding
+  # recode blanks to NA
   mutate(
     primary_delay_reason = na_if(primary_delay_reason, ""),
     secondary_delay_reason = na_if(secondary_delay_reason, "")
-  )
+  ) %>%
+  # Use end of the month date for records with no end date (but we think have ended)
+  # Create a flag for these records
+  mutate(
+    month_end = lubridate::ceiling_date(keydate1_dateformat, "month") - 1,
+    ammended_dates = case_when(
+      keydate2_dateformat == ymd("1900,1,1") ~ TRUE,
+      TRUE ~ FALSE
+    ),
+    keydate2_dateformat = if_else(ammended_dates == TRUE, month_end, keydate2_dateformat),
+    no_end_date = case_when(
+      is.na(keydate2_dateformat) & spec !=
+        c("CC", "G1", "G2", "G21", "G22", "G3", "G4", "G5", "G6", "G61", "G62", "G63") ~ TRUE,
+      TRUE ~ FALSE
+    ),
+    correct_dates = case_when(
+      is_date_in_fyyear(year, keydate1_dateformat) | is_date_in_fyyear(year, keydate2_dateformat) |
+        is.na(keydate2_dateformat) &
+          spec %in% c("CC", "G1", "G2", "G21", "G22", "G3", "G4", "G5", "G6", "G61", "G62", "G63") ~ TRUE,
+      TRUE ~ FALSE
+    )
+  ) %>%
+  filter(correct_dates == TRUE)
+
+
+## save outfile ---------------------------------------
+
+dd_clean %>%
+  # Save as zsav file
+  write_sav(get_source_extract_path(year, "DD", ext = "zsav", check_mode = "write")) %>%
+  # Save as rds file
+  write_rds(get_source_extract_path(year, "DD", check_mode = "write"))
+
+## End of Script ##
