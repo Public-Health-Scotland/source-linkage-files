d56332e7c34fc321f9960ae57a4f7f09aadd62ac
shintoLamp <shintoLamp@users.noreply.github.com>
2022-08-10 13:34:41 +0000

    Style code

--- tests/testthat/test-add_smr_type.R
+++ tests/testthat/test-add_smr_type.R
@@ -1,5 +1,4 @@
 test_that("SMR type works", {
-
   expect_equal(add_smr_type(recid = "02B", mpat = "0"), "Matern-HB")
   expect_equal(add_smr_type(recid = "02B", mpat = "1"), "Matern-IP")
   expect_equal(add_smr_type(recid = "02B", mpat = "4"), "Matern-DC")
@@ -8,5 +7,4 @@ test_that("SMR type works", {
   expect_equal(add_smr_type(recid = "AE2"), "A & E")
   expect_equal(add_smr_type(recid = "PIS"), "PIS")
   expect_equal(add_smr_type(recid = "NRS"), "NRS Deaths")
-
 })
