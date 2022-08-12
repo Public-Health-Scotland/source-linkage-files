b9514da9304c0b0ff1f48223f812b1f0a04cc8db
Jennifer Thom <jennifer.thom@phs.scot>
2022-08-12 13:51:33 +0100

    Apply create_dd_flags fn and select vars

--- Production_scripts/Process_extracts/extract_delayed_discharges.R
+++ Production_scripts/Process_extracts/extract_delayed_discharges.R
@@ -58,33 +58,35 @@ dd_clean <- dd_file %>%
     primary_delay_reason = na_if(primary_delay_reason, ""),
     secondary_delay_reason = na_if(secondary_delay_reason, "")
   ) %>%
-  # Use end of the month date for records with no end date (but we think have ended)
-  # Create a flag for these records
-  mutate(
-    month_end = lubridate::ceiling_date(keydate1_dateformat, "month") - 1,
-    ammended_dates = case_when(
-      keydate2_dateformat == ymd("1900,1,1") ~ TRUE,
-      TRUE ~ FALSE
-    ),
-    keydate2_dateformat = if_else(ammended_dates == TRUE, month_end, keydate2_dateformat),
-    no_end_date = case_when(
-      is.na(keydate2_dateformat) & spec !=
-        c("CC", "G1", "G2", "G21", "G22", "G3", "G4", "G5", "G6", "G61", "G62", "G63") ~ TRUE,
-      TRUE ~ FALSE
-    ),
-    correct_dates = case_when(
-      is_date_in_fyyear(year, keydate1_dateformat) | is_date_in_fyyear(year, keydate2_dateformat) |
-        is.na(keydate2_dateformat) &
-          spec %in% c("CC", "G1", "G2", "G21", "G22", "G3", "G4", "G5", "G6", "G61", "G62", "G63") ~ TRUE,
-      TRUE ~ FALSE
-    )
-  ) %>%
-  filter(correct_dates == TRUE)
+  # create flags for no_end_date and correct_dates
+  create_dd_flags(year) %>%
+  # Keep only records which have an end date (except Mental Health) and fall within our dates.
+  filter(correct_dates == TRUE & no_end_date == FALSE)
 
 
 ## save outfile ---------------------------------------
+outfile <- dd_clean %>%
+  select(
+    year,
+    recid,
+    original_admission_date,
+    keydate1_dateformat,
+    keydate2_dateformat,
+    SMRType,
+    chi,
+    postcode,
+    delay_end_reason,
+    primary_delay_reason,
+    secondary_delay_reason,
+    spec,
+    location,
+    hbtreatcode,
+    dd_responsible_lca,
+    monthflag,
+    cennum
+  )
 
-dd_clean %>%
+outfile %>%
   # Save as zsav file
   write_sav(get_source_extract_path(year, "DD", ext = "zsav", check_mode = "write")) %>%
   # Save as rds file
