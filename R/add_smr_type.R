4b7e159c63104393e3dbabcc3ff62508f691b0db
shintoLamp <bateman.mcbride@phs.scot>
2022-08-31 15:07:13 +0100

    Added acute, CMH, and DN. Home care not working currently

--- R/add_smr_type.R
+++ R/add_smr_type.R
@@ -2,6 +2,8 @@
 #'
 #' @param recid A vector of record IDs
 #' @param mpat A vector of mpat values
+#' @param ipdc A vector of inpatient/day case markers
+#' @param hc_service A vector of Home Care service markers
 #'
 #' @return A vector of SMR types
 #' @export
@@ -9,30 +11,38 @@
 #' @family Codes
 #'
 #' @examples
-#' x <- tibble::tibble(
-#'   recid = c("02B", "02B", "02B", "04B", "00B", "AE2", "PIS", "NRS"),
-#'   mpat = c("1", "2", "0", "", "", "", "", "")
-#' )
-#' add_smr_type(x)
-add_smr_type <- function(recid, mpat = NULL) {
+#' add_smr_type(recid = c("04B", "00B", "AE2", "PIS", "NRS"))
+#' add_smr_type(recid = c("02B", "02B"), mpat = c("1", "4"))
+#' add_smr_type(recid = c("01B", "01B", "GLS"), ipdc = c("I", "D", "I"))
+add_smr_type <- function(recid,
+                         mpat = NULL,
+                         ipdc = NULL,
+                         hc_service = NULL) {
 
-  # Situation where some recids are missing
-  if (any(is.na(recid)) & !all(is.na(recid))) {
-    cli::cli_inform(c("i" = "Some values of {.var recid} are {.val NA},
-                    please check this is populated throughout the data"))
+  # Situation where mpat is provided when not neccessary
+  if (!all(recid == "02B") & !is.null(mpat)) {
+    cli::cli_abort("You have specified an {.var mpat} value outside of Maternity records.
+                   This is not necessary; please re-run the function with {.var mpat} == {.val NULL},
+                   which is the default.")
   }
 
   # Situation where some recids are not in the accepted values
-  if (any(!(recid %in% c("02B", "04B", "00B", "AE2", "PIS", "NRS"))) &
+  if (any(!(recid %in% c("02B", "04B", "00B", "AE2", "PIS", "NRS", "01B", "GLS", "CMH", "DN", "HC"))) &
     !any(is.na(recid))) {
-    cli::cli_inform(c("i" = "One or more values of {.var recid} do not have an
+    cli::cli_warn(c("i" = "One or more values of {.var recid} do not have an
                    assignable {.var smrtype}"))
   }
 
+  # Situation where some recids are missing
+  if (any(is.na(recid))) {
+    cli::cli_abort("One or more values of {.var recid} are {.val NA}. Please check
+                   the data before continuing.")
+  }
+
   # Situation where maternity records are present without an mpat
-  if (any(recid == "02B") & any(is.na(mpat)) & !all(is.na(mpat))) {
-    cli::cli_inform(c("i" = "In maternity records, {.var mpat} is required to assign
-                      an smrtype, and there are some {.val NA} values. Please check the data."))
+  if (any(recid == "02B") & any(is.na(mpat))) {
+    cli::cli_abort("In maternity records, {.var mpat} is required to assign an smrtype,
+                    and there are some {.val NA} values. Please check the data.")
   }
 
   # Situation where there are no recid values
@@ -46,27 +56,46 @@ add_smr_type <- function(recid, mpat = NULL) {
     cli::cli_abort("Cannot assign Maternity smrtype with no valid {.var mpat} values")
   }
 
+  # Situation where an Acute/GLS recid is given but no ipdc marker
+  if (any(recid %in% c("01B", "GLS")) & is.null(ipdc)) {
+    cli::cli_abort("An {.var ipdc} vector has not been supplied, and therefore Acute/GLS
+                   records cannot be given an {.var smrtype}")
+  }
 
-  # Recode non-maternity recids
-  if (missing(mpat)) {
+  # Recids that can be recoded with no identifier
+  if (is.null(mpat) & is.null(ipdc)) {
     smrtype <- dplyr::case_when(
       recid == "04B" ~ "Psych-IP",
       recid == "00B" ~ "Outpatient",
       recid == "AE2" ~ "A & E",
       recid == "PIS" ~ "PIS",
-      recid == "NRS" ~ "NRS Deaths"
+      recid == "NRS" ~ "NRS Deaths",
+      recid == "CMH" ~ "Comm-MH",
+      recid == "DN" ~ "DN"
     )
-  } else {
-    # Recode all recids
+  }
+  # Maternity recids, identifier is `mpat`
+  else if (all(recid == "02B") & !is.null(mpat) & is.null(ipdc)) {
     smrtype <- dplyr::case_when(
       recid == "02B" & mpat %in% c("1", "3", "5", "7", "A") ~ "Matern-IP",
       recid == "02B" & mpat %in% c("2", "4", "6") ~ "Matern-DC",
-      recid == "02B" & mpat == "0" ~ "Matern-HB",
-      recid == "04B" ~ "Psych-IP",
-      recid == "00B" ~ "Outpatient",
-      recid == "AE2" ~ "A & E",
-      recid == "PIS" ~ "PIS",
-      recid == "NRS" ~ "NRS Deaths"
+      recid == "02B" & mpat == "0" ~ "Matern-HB"
+    )
+  }
+  # Acute recids, identifier is `ipdc`
+  else if (is.null(mpat) & !is.null(ipdc)) {
+    smrtype <- dplyr::case_when(
+      recid == "01B" & ipdc == "I" ~ "Acute-IP",
+      recid == "01B" & ipdc == "D" ~ "Acute-DC",
+      recid == "GLS" & ipdc == "I" ~ "GLS-IP"
+    )
+  }
+  # Home care
+  else if (recid == "HC" & !is.null(hc_service)) {
+    smrtype <- dplyr::case_when(
+      recid == "HC" & hc_service == "1" ~ "HC-Non-Per",
+      recid == "HC" & hc_service == "2" ~ "HC-Per",
+      recid == "HC" & !(hc_service %in% c("1", "2")) ~ "HC-Unknown"
     )
   }
 
--- tests/testthat/test-add_smr_type.R
+++ tests/testthat/test-add_smr_type.R
@@ -8,38 +8,35 @@ test_that("SMR type works for single input", {
   expect_equal(add_smr_type(recid = "AE2"), "A & E")
   expect_equal(add_smr_type(recid = "PIS"), "PIS")
   expect_equal(add_smr_type(recid = "NRS"), "NRS Deaths")
+  expect_equal(add_smr_type(recid = "CMH"), "Comm-MH")
+  expect_equal(add_smr_type(recid = "DN"), "DN")
+  expect_equal(add_smr_type(recid = "01B", ipdc = "I"), "Acute-IP")
+  expect_equal(add_smr_type(recid = "01B", ipdc = "D"), "Acute-DC")
+  expect_equal(add_smr_type(recid = "GLS", ipdc = "I"), "GLS-IP")
+  expect_equal(add_smr_type(recid = "HC", hc_service = 1), "HC-Non-Per")
 })
 
 # Vector input
 test_that("SMR type works for vector input", {
   expect_equal(
-    add_smr_type(recid = c("04B", "00B", "PIS", "AE2", "NRS")),
-    c("Psych-IP", "Outpatient", "PIS", "A & E", "NRS Deaths")
+    add_smr_type(recid = c("04B", "00B", "PIS", "AE2", "NRS", "CMH")),
+    c("Psych-IP", "Outpatient", "PIS", "A & E", "NRS Deaths", "Comm-MH")
   )
   expect_equal(
     add_smr_type(recid = c("02B", "02B", "02B"), mpat = c("5", "6", "A")),
     c("Matern-IP", "Matern-DC", "Matern-IP")
   )
+  expect_equal(
+    add_smr_type(recid = c("01B", "01B", "GLS"), ipdc = c("I", "D", "I")),
+    c("Acute-IP", "Acute-DC", "GLS-IP")
+  )
 })
 
 # Informational messages
-test_that("Error messages return as expected", {
-  expect_message(
-    add_smr_type(recid = c(NA, NA, "04B")),
-    suppressMessages(cli::cli_inform(c("i" = "Some values of {.var recid} are {.val NA},
-                    please check this is populated throughout the data")))
-  )
-  expect_message(
-    add_smr_type(recid = c("02B", "02B"), mpat = c(NA, "1")),
-    suppressMessages(cli::cli_inform(c("i" = "In maternity records, {.var mpat} is required to assign
-                      an smrtype, and there are some {.val NA} values. Please check the data.")))
-  )
-
-  expect_message(
-    add_smr_type(recid = c("00B", "AE2", "Bum", "PIS")),
-    suppressMessages(cli::cli_inform(c("i" = "One or more values of {.var recid} do not have an
-                   assignable {.var smrtype}")))
-  )
+test_that("Warnings return as expected", {
+  expect_warning(
+    add_smr_type(recid = c("00B", "AE2", "Bum", "PIS"))
+    )
 })
 
 # Errors that abort the function
@@ -49,5 +46,17 @@ test_that("Error escapes functions as expected", {
   )
   expect_error(
     add_smr_type(recid = c("02B", "02B", "02B"), mpat = c(NA, NA, NA))
+  )
+  expect_error(
+    add_smr_type(recid = c(NA, NA, "04B"))
+  )
+  expect_error(
+    add_smr_type(recid = c("02B", "02B"), mpat = c(NA, "1"))
+  )
+  expect_error(
+    add_smr_type(recid = c("01B", "CMH"), mpat = c("1", "2"))
+  )
+  expect_error(
+    add_smr_type(recid = c("01B", "GLS"))
   )
 })
