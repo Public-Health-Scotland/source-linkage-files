cd04e37df216c84a0556554a2e12b5efe6b60065
shintoLamp <bateman.mcbride@phs.scot>
2022-08-31 09:58:20 +0100

    Added optional argument to all functions, it_reference

--- R/get_it_extract_paths.R
+++ R/get_it_extract_paths.R
@@ -2,18 +2,25 @@
 #'
 #' @description Get the full path to the IT Long Term Conditions extract
 #'
+#' @param it_reference Optional argument for the seven-digit code in the IT extract file name
 #' @param ... additional arguments passed to [get_file_path()]
 #'
 #' @return The path to the LTC extract as an [fs::path()]
 #' @export
 #' @family extract file paths
 #' @seealso [get_file_path()] for the generic function.
-get_it_ltc_path <- function(...) {
+get_it_ltc_path <- function(it_reference = NULL, ...) {
+  if (is.null(it_reference)) {
   it_ltc_path <- get_file_path(
     directory = fs::path(get_slf_dir(), "IT_extracts"),
     file_name_regexp = "SCTASK[0-9]{7}_LTCs\\.csv(?:\\.gz)?",
     ...
-  )
+  )} else {
+    it_ltc_path <- get_file_path(
+      directory = fs::path(get_slf_dir(), "IT_extracts"),
+      file_name = glue::glue("SCTASK{it_reference}_LTCs.csv.gz")
+    )
+  }
 
   return(it_ltc_path)
 }
@@ -22,6 +29,7 @@ get_it_ltc_path <- function(...) {
 #'
 #' @description Get the full path to the IT Deaths extract
 #'
+#' @param it_reference Optional argument for the seven-digit code in the IT extract file name
 #' @param ... additional arguments passed to [get_file_path()]
 #'
 #' @return The path to the IT Deaths extract as an [fs::path()]
@@ -29,12 +37,18 @@ get_it_ltc_path <- function(...) {
 #' @family extract file paths
 #' @seealso [get_file_path()] for the generic function.
 get_it_deaths_path <-
-  function(...) {
+  function(it_reference = NULL, ...) {
+    if (is.null(it_reference)) {
     it_deaths_path <- get_file_path(
       directory = fs::path(get_slf_dir(), "IT_extracts"),
       file_name_regexp = "SCTASK[0-9]{7}_Deaths\\.csv(?:\\.gz)?",
       ...
-    )
+    )} else {
+      it_deaths_path <- get_file_path(
+        directory = fs::path(get_slf_dir(), "IT_extracts"),
+        file_name = glue::glue("SCTASK{it_reference}_Deaths.csv.gz")
+      )
+    }
 
     return(it_deaths_path)
   }
@@ -44,6 +58,7 @@ get_it_deaths_path <-
 #' @description Get the full path to the IT PIS extract
 #'
 #' @param year the year for the required extract
+#' @param it_reference Optional argument for the seven-digit code in the IT extract file name
 #' @param ... additional arguments passed to [get_file_path()]
 #'
 #' @return The path to the PIS extract as an [fs::path()]
@@ -51,11 +66,17 @@ get_it_deaths_path <-
 #' @family extract file paths
 #' @seealso [get_file_path()] for the generic function.
 get_it_prescribing_path <-
-  function(year, ...) {
+  function(year, it_reference = NULL, ...) {
+    if (is.null(it_reference)) {
     it_pis_path <- get_file_path(
       directory = fs::path(get_slf_dir(), "IT_extracts"),
       file_name_regexp = glue::glue("SCTASK[0-9]{{7}}_PIS_{convert_fyyear_to_year(year)}.csv(?:\\.gz)?")
-    )
+    )} else {
+      it_pis_path <- get_file_path(
+        directory = fs::path(get_slf_dir(), "IT_extracts"),
+        file_name = glue::glue("SCTASK{it_reference}_PIS_{convert_fyyear_to_year(year)}.csv.gz")
+      )
+    }
 
     return(it_pis_path)
   }
