d0da2b5e3c28f8a0f8df9bbb43d120f22eccdde8
Jennifer Thom <jennifer.thom@phs.scot>
2022-09-06 07:22:17 +0100

    WIP - Notes for adding new function for tests

--- Production_scripts/Phase_2_production/phase_2_production.R
+++ Production_scripts/Phase_2_production/phase_2_production.R
@@ -12,13 +12,13 @@
 # Load packages
 library(createslf)
 
-year <- "1920"
+year <- "2021"
 
 # Below is from Homelessness processing
 # Create a list of the years to run
 # Use set_names so that any returned list will be named.
-#years_to_run <- convert_year_to_fyyear(as.character(2017:2021)) %>%
-#  purrr::set_names()
+years_to_run <- convert_year_to_fyyear(as.character(2020:2021)) %>%
+  purrr::set_names()
 
 ------------------------------------------------------
 # PHASE 1 - process extracts
@@ -33,21 +33,48 @@ year <- "1920"
 #  process_data_extracts
 #)
 
+# Keep the data but don't write to disk (for testing)
+test_data <- purrr::map(
+  years_to_run,
+  process_data_extracts
+)
+
 # Test with homelessness and mental health extracts - working
 # This reads in the data and processes ready for SLF episode file
-test <- process_data_extracts(year)
+process_data_extracts(year)
 
 ------------------------------------------------------
 # PHASE 2 - Create Episode file
 ------------------------------------------------------
 
-
+read_cs
 
 ------------------------------------------------------
 # PHASE 3 - Create Individual file
 ------------------------------------------------------
 
-
+  data <- tibble(x = 1:10, y = 11:20)
+test_data <- list(
+  "1920" = list("homelessness" = data,
+                "mental_health" = data),
+  "2021" = list("homelessness" = data,
+                "mental_health" = data)
+)
+run_homelessness_tests <- function(data, year) {
+  old_data <- createslf::get_existing_data_for_tests(data)
+  createslf::produce_test_comparison(
+    old_data = createslf::produce_slf_homelessness_tests(data),
+    new_data = createslf::produce_slf_homelessness_tests(old_data)
+  ) %>%
+    createslf::write_tests_xlsx(sheet_name = "homelessness", year = year)
+}
+run_tests <- function(data_list, year) {
+  if (year > "2016") {
+    run_homelessness_tests(data_list[["homelessness"]], year)
+  }
+  run_mental_health_tests(data_list[["mental_health"]], year)
+}
+purrr::iwalk(test_data, run_tests)
 ------------------------------------------------------
 # PHASE 1 - Calculate Anon CHI
 ------------------------------------------------------
