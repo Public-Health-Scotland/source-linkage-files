6bf56eaf8e0a7a0460c9bc94034343440cdd5f74
Jennifer Thom <jennifer.thom@phs.scot>
2022-09-06 07:21:26 +0100

    update format and add names to list The reason this was not working before was due to problems with the homelessness extract where this was zipped twice

--- R/process_data_extracts.R
+++ R/process_data_extracts.R
@@ -7,10 +7,9 @@
 #'
 process_data_extracts <- function(year) {
 
-  # Add extracts to this list for processing
   process_extracts <- list(
-    process_extract_homelessness(year, read_extract_homelessness(year)),
-    process_extract_mental_health(year, read_extract_mental_health(year))
+    'homelessness' = process_extract_homelessness(year, read_extract_homelessness(year)),
+    'mental_health' = process_extract_mental_health(year, read_extract_mental_health(year))
   )
 
   return(process_extracts)
