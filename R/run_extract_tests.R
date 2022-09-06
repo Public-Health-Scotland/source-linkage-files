187a9da7c8ca2731003b7358b5eb9806bbc9a317
Jennit07 <Jennit07@users.noreply.github.com>
2022-09-06 11:17:12 +0000

    Style code

--- R/run_extract_tests.R
+++ R/run_extract_tests.R
@@ -8,10 +8,9 @@
 #'
 #' @examples
 run_extract_tests <- function(data_list, year) {
-
   run_extract_tests <- list(
-    run_homelessness_tests(data_list[[year]][['homelessness']], year),
-    run_mental_health_tests(data_list[[year]][['homelessness']], year)
+    run_homelessness_tests(data_list[[year]][["homelessness"]], year),
+    run_mental_health_tests(data_list[[year]][["homelessness"]], year)
   )
 
   return(run_extract_tests)
