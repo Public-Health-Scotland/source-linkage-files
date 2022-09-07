062b05628dfe3d6f0a1d6ba02ce7cdaa32042b71
Jennit07 <Jennit07@users.noreply.github.com>
2022-09-07 14:58:19 +0000

    Style code

--- R/01_run_phase_1_processing.R
+++ R/01_run_phase_1_processing.R
@@ -6,16 +6,14 @@
 #' @export
 #'
 #' @examples
-run_phase_1_processing <- function(select_years_to_run){
+run_phase_1_processing <- function(select_years_to_run) {
+  extract_data <- purrr::map(
+    years_to_run,
+    process_data_extracts
+  )
 
-extract_data <- purrr::map(
-  years_to_run,
-  process_data_extracts
-)
-
-extract_tests <- purrr::map(
-  years_to_run,
-  run_extract_tests
-)
-
+  extract_tests <- purrr::map(
+    years_to_run,
+    run_extract_tests
+  )
 }
--- R/run_extract_tests.R
+++ R/run_extract_tests.R
@@ -8,9 +8,8 @@
 #'
 #' @examples
 run_extract_tests <- function(year) {
-
-    run_homelessness_tests(extract_data[[year]][["homelessness"]], year)
-    run_mental_health_tests(extract_data[[year]][["mental_health"]], year)
+  run_homelessness_tests(extract_data[[year]][["homelessness"]], year)
+  run_mental_health_tests(extract_data[[year]][["mental_health"]], year)
 
 
   return(run_extract_tests)
