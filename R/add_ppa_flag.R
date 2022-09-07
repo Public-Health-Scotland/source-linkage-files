225648fb0bd5645085a5fd600032368d869e7c21
shintoLamp <bateman.mcbride@phs.scot>
2022-09-07 12:05:00 +0100

    Creation of function add_ppa_flag

---
+++ R/add_ppa_flag.R
@@ -0,0 +1,109 @@
+#' Add Potentially Preventable Admissions Marker
+#'
+#' @description This function takes a data frame input and determines, based on
+#' a combination of diagnostic codes and operation codes, whether an admission was
+#' preventable or not
+#' @param data A data frame
+#'
+#' @return A data frame to use as a lookup of PPAs
+#' @export
+add_ppa_flag <- function(data) {
+  matching_data <- data %>%
+    # Select out only the columns we need
+    dplyr::select(.data$anon_chi, .data$cij_marker, .data$cij_pattype, .data$recid,
+                  .data$op1a, .data$diag1, .data$diag2, .data$diag3, .data$diag4,
+                  .data$diag5, .data$diag6) %>%
+    # Filter only recids and patient type where admission was preventable
+    dplyr::filter(.data$recid %in% c("01B", "02B", "04B", "GLS") & .data$cij_pattype == "Non-Elective") %>%
+    # We only want the first record in each cij, and we want to exclude empty cij and empty anon_chi
+    dplyr::group_by(.data$anon_chi, .data$cij_marker) %>%
+    dplyr::filter(dplyr::row_number() == 1 & !is.na(.data$cij_marker) & !is.na(.data$anon_chi)) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(
+
+      # Excluding operations are op1a codes from K01 to K50, K56, K60, and K61
+      excluding_operation = stringr::str_sub(.data$op1a, 1, 3) %in%
+        purrr::reduce(list(
+          glue::glue("K{stringr::str_c(\"0\", c(1:9))}"),
+          glue::glue("K{as.character(c(10:50))}"),
+          c("K56", "K60", "K61")
+        ), union),
+      ppa = dplyr::case_when(
+        # Just reliant on diag1, first 3 characters
+        stringr::str_sub(.data$diag1, 1, 3) %in%
+          c(
+            "H66", "J06", "K02", "K06", "K08",
+            "G40", "G41", "R56", "O15", "E40",
+            "E41", "E43", "E86", "N10", "N11",
+            "N12", "N70", "N73", "J45", "J46",
+            "J41", "J42", "J43", "J44", "J47"
+          ) ~ TRUE,
+
+        # Just reliant on diag1, first four characters
+        stringr::str_sub(.data$diag1, 1, 4) %in%
+          c(
+            "J028", "J029", "J038", "J039", "J321",
+            "E550", "E643", "M833", "K522", "K528",
+            "K529", "K250", "K251", "K252", "K254",
+            "K255", "K256", "K260", "K261", "K262",
+            "K264", "K265", "K266", "K270", "K271",
+            "K272", "K274", "K275", "K276", "K280",
+            "K281", "K282", "K284", "K285", "K286",
+            "D501", "D508", "D509"
+          ) ~ TRUE,
+
+        # Reliant on any of the six diagnosis codes, first three characters
+        rowSums(dplyr::across(
+          c(.data$diag1, .data$diag2, .data$diag3, .data$diag4, .data$diag5, .data$diag6),
+          ~ stringr::str_sub(.x, 1, 3) %in%
+            c(
+              "R02", "J10", "J11", "J13",
+              "A35", "A36", "A80", "B05", "B06", "B26"
+            )
+        )) > 0 ~ TRUE,
+
+        # Reliant on any of the six diagnosis codes, first four characters
+        rowSums(dplyr::across(
+          c(.data$diag1, .data$diag2, .data$diag3, .data$diag4, .data$diag5, .data$diag6),
+          ~ stringr::str_sub(.x, 1, 4) %in%
+            c(
+              "A370", "A379", "B161", "B169", "E100",
+              "E101", "E102", "E103", "E104", "E105",
+              "E106", "E107", "E108", "E110", "E111",
+              "E112", "E113", "E114", "E115", "E116",
+              "E117", "E118", "E120", "E121", "E122",
+              "E123", "E124", "E125", "E126", "E127",
+              "E128", "E130", "E131", "E132", "E133",
+              "E134", "E135", "E136", "E137", "E138",
+              "E140", "E141", "E142", "E143", "E144",
+              "E145", "E146", "E147", "E148", "J181"
+            )
+        )) > 0 ~ TRUE,
+
+        # Reliant on op1a and diag1
+        stringr::str_sub(.data$diag1, 1, 3) == "I20" &
+          !(stringr::str_sub(.data$op1a, 1, 3) %in% c("K40", "K45", "K49", "K60", "K65", "K66")) ~ TRUE,
+        stringr::str_sub(.data$diag1, 1, 3) %in% c("L03", "L04") &
+          !(stringr::str_sub(.data$op1a, 1, 3) %in% c("S06", "S57", "S68", "S70", "W90", "X11")) ~ TRUE,
+        stringr::str_sub(.data$diag1, 1, 4) %in% c("L080", "L088", "L089", "L980") &
+          !(stringr::str_sub(.data$op1a, 1, 3) %in% c("S06", "S57", "S68", "S70", "W90", "X11")) ~ TRUE,
+
+        # Reliant on diag1 and excluding_operation
+        stringr::str_sub(.data$diag1, 1, 3) %in% c("I10", "I50", "J81") &
+          excluding_operation == FALSE ~ TRUE,
+        stringr::str_sub(.data$diag1, 1, 4) %in% c("I119", "I110") &
+          excluding_operation == FALSE ~ TRUE,
+
+        # Reliant on diag1 and diag2
+        stringr::str_sub(.data$diag1, 1, 3) == "J40" &
+          stringr::str_sub(.data$diag2, 1, 3) %in% c("J41", "J42", "J43", "J44") ~ TRUE,
+
+        # Reliant pn diag2 only
+        stringr::str_sub(.data$diag2, 1, 3) == "J47" ~ TRUE,
+
+        # All other values
+        TRUE ~ FALSE
+      )
+    )
+  return(matching_data)
+}
