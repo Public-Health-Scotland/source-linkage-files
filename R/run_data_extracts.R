59d65c46bdd4ebf3a2dbef1ffa790e9bdcfb7511
Jennifer Thom <jennifer.thom@phs.scot>
2022-09-08 14:53:33 +0100

    Fix typo

--- R/run_data_extracts.R
+++ R/run_data_extracts.R
@@ -13,7 +13,7 @@ run_data_extracts <- function(year) {
   process_extracts <- list(
     "homelessness" = process_extract_homelessness(year, read_extract_homelessness(year)),
     "mental_health" = process_extract_mental_health(year, read_extract_mental_health(year)),
-    "maternity" = process_extract_mental_health(year, read_extract_maternity(year))
+    "maternity" = process_extract_maternity(year, read_extract_maternity(year))
   )
 
   return(process_extracts)
