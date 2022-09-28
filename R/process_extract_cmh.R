d934e6b67290800e92ff961c2d9e4364510cc616
Jennifer Thom <jennifer.thom@phs.scot>
2022-09-28 11:46:42 +0100

    Include CMH extract as function

---
+++ R/process_extract_cmh.R
@@ -0,0 +1,77 @@
+#' Process the CMH extract
+#'
+#' @description This will read and process the
+#' cmh extract, it will return the final data
+#' but also write this out as a zsav and rds.
+#'
+#' @param year The year to process, in FY format.
+#' @param data The extract to process
+#' @param write_to_disk (optional) Should the data be written to disk default is
+#' `TRUE` i.e. write the data to disk.
+#'
+#' @return the final data as a [tibble][tibble::tibble-package].
+#' @export
+#' @family process extracts
+process_extract_cmh <- function(year, data, write_to_disk = TRUE) {
+  # Only run for a single year
+  stopifnot(length(year) == 1)
+
+  # Check that the supplied year is in the correct format
+  year <- check_year_format(year)
+
+  # Data Cleaning  ---------------------------------------
+
+  cmh_clean <- data %>%
+    # create recid, year, SMRType variables
+    dplyr::mutate(
+      recid = "CMH",
+      smrtype = add_smr_type(recid = .data$recid),
+      year = year
+    ) %>%
+    # contact end time
+    dplyr::mutate(
+      keyTime1 = lubridate::hms(keyTime1),
+      duration = lubridate::hms(duration)
+    ) %>%
+    dplyr::mutate(keyTime2 = .data$keyTime1 + .data$duration) %>%
+    # record key date 2
+    dplyr::mutate(record_keydate2 = .data$record_keydate1) %>%
+    # create blank diag 6
+    dplyr::mutate(diag6 = NA)
+
+  # Outfile --------------------------------------------
+
+  outfile <- cmh_clean %>%
+    dplyr::select(
+      .data$year,
+      .data$recid,
+      .data$record_keydate1,
+      .data$record_keydate2,
+      .data$keyTime1,
+      .data$keyTime2,
+      .data$smrtype,
+      .data$chi,
+      .data$gender,
+      .data$dob,
+      .data$gpprac,
+      .data$postcode,
+      .data$hbrescode,
+      .data$hscp,
+      .data$location,
+      .data$hbtreatcode,
+      .data$diag1,
+      .data$diag2,
+      .data$diag3,
+      .data$diag4,
+      .data$diag5,
+      .data$diag6
+    )
+
+  if (write_to_disk) {
+    # Save as rds file
+    outfile %>%
+      write_rds(get_source_extract_path(year, "CMH", check_mode = "write"))
+  }
+
+  return(outfile)
+}
