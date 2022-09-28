17e5f17d959caef3cfdadd6a1be044366b4c985a
James McMahon <james.mcmahon@phs.scot>
2022-09-28 11:47:48 +0100

    Change `convert_eng_gp_prac...` to take and return a vector

--- tests/testthat/test-convert_eng_gpprac_to_dummy.R
+++ tests/testthat/test-convert_eng_gpprac_to_dummy.R
@@ -1,12 +1,10 @@
 test_that("GP English dummy code function returns data", {
-  data <- tibble::tibble(gpprac = c("1234", "A1235"))
-
-  # Check type etc.
-  expect_s3_class(convert_eng_gpprac_to_dummy(data, gpprac), "tbl_df")
-  expect_length(convert_eng_gpprac_to_dummy(data, gpprac), length(data))
-  expect_equal(nrow(convert_eng_gpprac_to_dummy(data, gpprac)), nrow(data))
+  # Check types
+  expect_type(convert_eng_gpprac_to_dummy(c("1234", "A1235")), "integer")
+  expect_length(convert_eng_gpprac_to_dummy(c("1234", "A1235")), 2)
 
   # Check contents
-  expect_type(convert_eng_gpprac_to_dummy(data, gpprac)$gpprac, "integer")
-  expect_equal(convert_eng_gpprac_to_dummy(data, gpprac)$gpprac, c(1234, 9995))
+  expect_equal(convert_eng_gpprac_to_dummy(c("1234", "A1235")), c(1234, 9995))
+  expect_equal(convert_eng_gpprac_to_dummy(c("1234", "A1235", NA_character_)),
+               c(1234, 9995, NA_integer_))
 })
