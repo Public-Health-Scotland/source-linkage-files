8686e67de1ab59993db9a14c3e533b22438a4882
Jennifer Thom <jennifer.thom@phs.scot>
2022-09-30 13:44:33 +0100

    Test new set up `run_data_extracts`

--- R/run_data_extracts.R
+++ R/run_data_extracts.R
@@ -10,46 +10,69 @@
 #' @export
 #'
 run_data_extracts <- function(year, write_to_disk = FALSE) {
-  process_extracts <- list(
-    "homelessness" = process_extract_homelessness(year, read_extract_homelessness(year), write_to_disk = write_to_disk),
-    "mental_health" = process_extract_mental_health(year, read_extract_mental_health(year), write_to_disk = write_to_disk),
-    "maternity" = process_extract_maternity(year, read_extract_maternity(year), write_to_disk = write_to_disk),
-    "ae" = process_extract_ae(year, read_extract_ae(year), write_to_disk = write_to_disk),
-    "acute" = process_extract_acute(year, read_extract_acute(year), write_to_disk = write_to_disk),
-    "outpatients" = process_extract_outpatients(year, read_extract_outpatients(year), write_to_disk = write_to_disk),
-    "nrs_deaths" = process_extract_nrs_deaths(year, read_extract_nrs_deaths(year), write_to_disk = write_to_disk),
-    "cmh" = process_extract_cmh(year, read_extract_cmh(year), write_to_disk = write_to_disk),
-    "district_nursing" = process_extract_district_nursing(year, read_extract_district_nursing(year), write_to_disk = write_to_disk),
-    "pis" = process_extract_pis(year, read_extract_pis(year), write_to_disk = write_to_disk),
-    "dd" = process_extract_delayed_discharges(year, read_extract_delayed_discharges(year), write_to_disk = write_to_disk)
-  )
-
-
   # process_extracts <- list(
-  #   "mental_health" = process_extract_mental_health(year, read_extract_mental_health(year)),
-  #   "maternity" = process_extract_maternity(year, read_extract_maternity(year))
-  #   #"a&e" = process_extract_ae(year, read_extract_ae(year))
+  # "homelessness" = process_extract_homelessness(year, read_extract_homelessness(year), write_to_disk = write_to_disk),
+  # "mental_health" = process_extract_mental_health(year, read_extract_mental_health(year), write_to_disk = write_to_disk),
+  # "maternity" = process_extract_maternity(year, read_extract_maternity(year), write_to_disk = write_to_disk),
+  # "ae" = process_extract_ae(year, read_extract_ae(year), write_to_disk = write_to_disk),
+  # "acute" = process_extract_acute(year, read_extract_acute(year), write_to_disk = write_to_disk),
+  # "outpatients" = process_extract_outpatients(year, read_extract_outpatients(year), write_to_disk = write_to_disk),
+  # "nrs_deaths" = process_extract_nrs_deaths(year, read_extract_nrs_deaths(year), write_to_disk = write_to_disk),
+  # "cmh" = process_extract_cmh(year, read_extract_cmh(year), write_to_disk = write_to_disk),
+  # "district_nursing" = process_extract_district_nursing(year, read_extract_district_nursing(year), write_to_disk = write_to_disk),
+  # "pis" = process_extract_pis(year, read_extract_pis(year), write_to_disk = write_to_disk),
+  # "dd" = process_extract_delayed_discharges(year, read_extract_delayed_discharges(year), write_to_disk = write_to_disk)
   # )
 
-  #  if (year > 2016) {
-  #    process_extracts <- append(process_extracts,
-  #                               list(
-  #                                 "homelessness" = process_extract_homelessness(year, read_extract_homelessness(year)),
-  #                                 DD
-  #                                ))
+  process_extracts <- list(
+    "acute" = process_extract_acute(year, read_extract_acute(year), write_to_disk = write_to_disk),
+    "ae" = process_extract_ae(year, read_extract_ae(year), write_to_disk = write_to_disk),
+    "mental_health" = process_extract_mental_health(year, read_extract_mental_health(year), write_to_disk = write_to_disk),
+    "maternity" = process_extract_maternity(year, read_extract_maternity(year), write_to_disk = write_to_disk),
+    "nrs_deaths" = process_extract_nrs_deaths(year, read_extract_nrs_deaths(year), write_to_disk = write_to_disk),
+    "outpatients" = process_extract_outpatients(year, read_extract_outpatients(year), write_to_disk = write_to_disk),
+    "pis" = process_extract_pis(year, read_extract_pis(year), write_to_disk = write_to_disk)
+  )
 
-  # }
+  if (year > 2015 & year < 2020) {
+    process_extracts <- append(
+      process_extracts,
+      list(
+        "district_nursing" = process_extract_district_nursing(year, read_extract_district_nursing(year), write_to_disk = write_to_disk)
+      )
+    )
+  }
 
-  # if (year > 2017) {
-  #   process_extracts <- append(process_extracts,
-  #                              list(
-  #                                "Alarms telecare" = process_extract_homelessness(year, read_extract_homelessness(year)),
-  #                                "HC",
-  #                                "CH",
-  #                                "SDS"
-  #                              ))
-  #
-  # }
+  if (year > 2016 & year < 2020) {
+    process_extracts <- append(
+      process_extracts,
+      list(
+        "cmh" = process_extract_cmh(year, read_extract_cmh(year), write_to_disk = write_to_disk)
+      )
+    )
+  }
+
+  if (year > 2016) {
+    process_extracts <- append(
+      process_extracts,
+      list(
+        "homelessness" = process_extract_homelessness(year, read_extract_homelessness(year)),
+        "dd" = process_extract_delayed_discharges(year, read_extract_delayed_discharges(year), write_to_disk = write_to_disk)
+      )
+    )
+  }
+
+  #  if (year > 2017) {
+  #   process_extracts <- append(
+  #    process_extracts,
+  #   list(
+  #    "Alarms telecare" = process_extract_alarms_telecare(year, read_extract_alarms_telecare(year)),
+  #   "CH" = process_extract_care_homes(year, read_extract_care_homes(year)),
+  #  "HC" = process_extract_home_care(year, read_extract_home_care(year)),
+  # "SDS" = process_extract_sds(year, read_extract_sds(year))
+  #)
+  #)
+  #}
 
   return(process_extracts)
 }
