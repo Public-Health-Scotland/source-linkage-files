2d58e57576b16f07e2f60bad6e3299d3bd83b4a0
Jennifer Thom <jennifer.thom@phs.scot>
2022-10-11 11:28:20 +0100

    Include PIS extract/tests as a function

--- R/process_extract_prescribing.R
+++ R/process_extract_prescribing.R
@@ -1,26 +1,25 @@
-#' Process the PIS extract
+#' Process the prescribing extract
 #'
 #' @description This will read and process the
-#' PIS extract, it will return the final data
+#' prescribing extract, it will return the final data
 #' but also write this out as a zsav and rds.
 #'
-#' @param year The year to process, in FY format.
 #' @param data The extract to process
+#' @param year The year to process, in FY format.
 #' @param write_to_disk (optional) Should the data be written to disk default is
 #' `TRUE` i.e. write the data to disk.
 #'
 #' @return the final data as a [tibble][tibble::tibble-package].
 #' @export
 #' @family process extracts
-process_extract_pis <- function(year, data, write_to_disk = TRUE) {
+process_extract_prescribing <- function(data, year, write_to_disk = TRUE) {
   # Only run for a single year
   stopifnot(length(year) == 1)
 
   # Check that the supplied year is in the correct format
   year <- check_year_format(year)
 
-  # Data Cleaning--------------------------------------------------
-
+  # Data Cleaning--------------------------------------------
   pis_clean <- data %>%
     # filter for chi NA
     dplyr::filter(phsmethods::chi_check(.data$chi) == "Valid CHI") %>%
@@ -32,10 +31,10 @@ process_extract_pis <- function(year, data, write_to_disk = TRUE) {
     # Recode GP Practice into a 5 digit number
     # assume that if it starts with a letter it's an English practice
     # and so recode to 99995
-    dplyr::mutate(gpprac = convert_eng_gpprac_to_dummy(.data$gpprac)) %>%
+    convert_eng_gpprac_to_dummy(.data$gpprac) %>%
     # Set date to the end of the FY
     dplyr::mutate(
-      record_keydate1 = end_fy(.data$year),
+      record_keydate1 = end_fy(year),
       record_keydate2 = .data$record_keydate1,
       # Add SMR type variable
       smrtype = add_smr_type(.data$recid)
@@ -51,7 +50,7 @@ process_extract_pis <- function(year, data, write_to_disk = TRUE) {
   }
 
 
-  # Save out ---------------------------------------
+  # Save outfile ---------------------------------------
 
   if (write_to_disk) {
     # Save as rds file
