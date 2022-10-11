0e8adb37afa185bf66d23043bc902cd34c3775ff
Jennifer Thom <jennifer.thom@phs.scot>
2022-10-11 11:32:28 +0100

    fix conflict

--- R/read_extract_prescribing.R
+++ R/read_extract_prescribing.R
@@ -1,38 +1,3 @@
-#' Read Prescribing extract
-#'
-#' @param year Year of extract
-#'
-#' @return csv data file for prescribing
-#' @export
-#'
-read_extract_prescribing <- function(year) {
-    get_it_prescribing_path(year),
-    col_type = cols_only(
-      `Pat UPI [C]` = col_character(),
-      `Pat DoB [C]` = col_date(format = "%d-%m-%Y"),
-      `Pat Gender` = col_double(),
-      `Pat Postcode [C]` = col_character(),
-      `Practice Code` = col_character(),
-      `Number of Dispensed Items` = col_double(),
-      `DI Paid NIC excl. BB` = col_double()
-    )
-  ) %>%
-    # Rename variables
-    dplyr::rename(
-      chi = "Pat UPI [C]",
-      dob = "Pat DoB [C]",
-      gender = "Pat Gender",
-      postcode = "Pat Postcode [C]",
-      gpprac = "Practice Code",
-      no_dispensed_items = "Number of Dispensed Items",
-      cost_total_net = "DI Paid NIC excl. BB"
-    )
-
-  return(pis_file)
-
-}
-
-  pis_file <- readr::read_csv(
 #' Read PIS extract
 #'
 #' @param year Year of BOXI extract
@@ -40,7 +5,7 @@ read_extract_prescribing <- function(year) {
 #' @return csv data file for PIS
 #' @export
 #'
-read_extract_pis <- function(year) {
+read_extract_prescribing <- function(year) {
   extract_pis_path <- get_it_prescribing_path(year)
 
   pis_file <- readr::read_csv(extract_pis_path,
