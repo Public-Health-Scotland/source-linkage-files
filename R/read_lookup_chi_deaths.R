ec69d1893038fc727a74c3d4ce339943fe55fede
Jennit07 <Jennit07@users.noreply.github.com>
2022-10-11 14:51:00 +0000

    Style code

--- R/read_lookup_chi_deaths.R
+++ R/read_lookup_chi_deaths.R
@@ -13,22 +13,21 @@
 
 read_lookup_chi_deaths <- function(write_to_disk = TRUE) {
 
-# Read data -------------------------------------------------------
-deaths_data <- readr:: read_csv(
-  file = get_it_deaths_path(),
-  col_type = cols(
-    'PATIENT_UPI [C]' = col_character(),
-    'PATIENT DoD DATE (NRS)' = col_date(format = "%d-%m-%Y"),
-    'PATIENT DoD DATE (CHI)' = col_date(format = "%d-%m-%Y")
-  )
-) %>%
-  # rename variables
-  dplyr::rename(
-    chi = 'PATIENT_UPI [C]',
-    death_date_nrs = 'PATIENT DoD DATE (NRS)',
-    death_date_chi = 'PATIENT DoD DATE (CHI)'
-  )
+  # Read data -------------------------------------------------------
+  deaths_data <- readr::read_csv(
+    file = get_it_deaths_path(),
+    col_type = cols(
+      "PATIENT_UPI [C]" = col_character(),
+      "PATIENT DoD DATE (NRS)" = col_date(format = "%d-%m-%Y"),
+      "PATIENT DoD DATE (CHI)" = col_date(format = "%d-%m-%Y")
+    )
+  ) %>%
+    # rename variables
+    dplyr::rename(
+      chi = "PATIENT_UPI [C]",
+      death_date_nrs = "PATIENT DoD DATE (NRS)",
+      death_date_chi = "PATIENT DoD DATE (CHI)"
+    )
 
-return(deaths_data)
-
+  return(deaths_data)
 }
