ebc2210b216f0ebda1a7cfeb3f1738895519702f
shintoLamp <bateman.mcbride@phs.scot>
2022-10-12 15:56:22 +0100

    Function to create flags based on specific instances in records + tests

---
+++ R/create_service_instances.R
@@ -0,0 +1,26 @@
+#' Create counters for elective and non-elective instances
+#'
+#' @param data A data frame
+#'
+#' @return A data frame with four counters, a death flag, and a cost variable for
+#' elective inpatients
+#' @export
+#'
+#' @family Demographic and Service Use Cohort functions
+create_service_instances <- function(data){
+
+  check_variables_exist(data, variables = c("cij_marker", "cij_pattype", "cij_ipdc", "cost_total_net"))
+
+  return_data <- data %>%
+    dplyr::mutate(
+      emergency_instances = .data$cij_pattype == "Non-Elective",
+      elective_instances = .data$cij_pattype == "Elective" | .data$cij_ipdc == "D",
+      elective_inpatient_instances = .data$cij_pattype == "Elective" & .data$cij_ipdc == "I",
+      elective_daycase_instances = .data$cij_pattype == "Elective" & .data$cij_ipdc == "D",
+      death_flag = .data$cij_marker == "NRS",
+      elective_inpatient_cost = dplyr::if_else(elective_inpatient_instances, cost_total_net, 0)
+    )
+
+  return(return_data)
+
+}
