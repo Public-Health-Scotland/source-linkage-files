7d9072d417a2b63802358269027b9a07301291fb
Jennifer Thom <jennifer.thom@phs.scot>
2022-10-17 13:00:45 +0100

    Set up costs in `run_data_lookups`

--- R/run_data_lookups.R
+++ R/run_data_lookups.R
@@ -18,7 +18,10 @@ run_data_lookups <- function(year, write_to_disk = FALSE) {
     ## Social Care lookups
     "sc_demographics" = process_lookup_sc_demographics(read_lookup_sc_demographics(), write_to_disk = write_to_disk),
     ## Cost lookups
-
+    "costs_gp_ooh" = process_costs_gp_ooh(read_costs_gp_ooh(), write_to_disk = write_to_disk),
+    "costs_district_nursing" = process_costs_district_nursing(read_costs_district_nursing(), write_to_disk = write_to_disk),
+    "costs_care_home" = process_costs_care_home(read_costs_care_home(), write_to_disk = write_to_disk),
+    "costs_home_care" = process_costs_home_care(read_costs_home_care(), write_to_disk = write_to_disk)
   )
 
   return(process_lookups)
