fd2de6bcf19c6e08038b5e7decd7da2dac1be015
Jennifer Thom <jennifer.thom@phs.scot>
2022-10-17 12:44:20 +0100

    Remove numbering system in top level scripts This makes it easier for any future changes

--- Production_scripts/Phase_2_production/phase_2_production.R
+++ Production_scripts/Phase_2_production/phase_2_production.R
@@ -16,50 +16,42 @@ library(createslf)
 year <- "2021"
 
 # Create a list of the years to run
-# Use set_names so that any returned list will be named.
-select_years_to_run <- convert_year_to_fyyear(as.character(2020:2021)) %>%
+years <- convert_year_to_fyyear(as.character(2020:2021)) %>%
   purrr::set_names()
 
 
 ------------------------------------------------------
-# PHASE 1a - Process Lookups
+# Process Lookups
 ------------------------------------------------------
 # Lookups/costs
-
-run_process_1a_lookups(select_years_to_run)
-
-
-------------------------------------------------------
-  # PHASE 1b - Process Costs
-------------------------------------------------------
-run_process_1b_costs(select_years_to_run)
+run_process_lookups(years)
 
 
 ------------------------------------------------------
-# PHASE 1c - Process Social Care data
+# Process Social Care data
 ------------------------------------------------------
 # Social Care
   # sc lookups
   # all files
-run_process_1c_social_care(select_years_to_run)
+run_process_social_care(years)
 
 ------------------------------------------------------
-# PHASE 2 - process extracts
+# Process extracts
 ------------------------------------------------------
 # Process data---------------------------------------
 
 # Process data extracts and tests.
 # Currently, this is set up to write to disk (we may not need this)
-run_process_2_extracts(select_years_to_run)
+run_process_extracts(years)
 
 ------------------------------------------------------
-# PHASE 3 - Create Episode file
+# Create Episode file
 ------------------------------------------------------
 
-run_phase_2_processing(select_years_to_run)
+run_process_ep_file(years)
 
 ------------------------------------------------------
-# PHASE 4 - Create Individual file
+# Create Individual file
 ------------------------------------------------------
 
 
