3616811ba87033ada95142c66c26cfd19e7fa8d6
Jennit07 <Jennit07@users.noreply.github.com>
2022-10-19 12:44:31 +0000

    Style code

--- R/run_data_lookups.R
+++ R/run_data_lookups.R
@@ -16,11 +16,11 @@ run_data_lookups <- function(write_to_disk = FALSE) {
     ## Social Care lookups
     # "sc_demographics" = process_lookup_sc_demographics(read_lookup_sc_demographics(), write_to_disk = write_to_disk),
     ## Cost lookups
-    #"costs_gp_ooh" = process_costs_gp_ooh(read_costs_gp_ooh(), write_to_disk = write_to_disk)
-     "costs_gp_ooh" = process_costs_gp_ooh_rmd(),
+    # "costs_gp_ooh" = process_costs_gp_ooh(read_costs_gp_ooh(), write_to_disk = write_to_disk)
+    "costs_gp_ooh" = process_costs_gp_ooh_rmd(),
     "costs_district_nursing" = process_costs_dn_rmd()
-    #"costs_care_home" = process_costs_care_home(read_costs_care_home(), write_to_disk = write_to_disk),
-    #"costs_home_care" = process_costs_home_care(read_costs_home_care(), write_to_disk = write_to_disk)
+    # "costs_care_home" = process_costs_care_home(read_costs_care_home(), write_to_disk = write_to_disk),
+    # "costs_home_care" = process_costs_home_care(read_costs_home_care(), write_to_disk = write_to_disk)
   )
 
   return(process_lookups)
