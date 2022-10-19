fdb1dcc22eea2277acce691073b4c3d5c9b10e28
Jennifer Thom <jennifer.thom@phs.scot>
2022-10-19 12:56:37 +0100

    Create DN cost lookup function

---
+++ R/process_costs_dn_rmd.R
@@ -0,0 +1,19 @@
+#' Process District Nursing cost lookup RMD file
+#'
+#' @description This will read and process the
+#' District Nursing cost lookup, it will return the final data
+#' but also write this out as a rds.
+#'
+#' @return the final data as a html document.
+#'
+process_costs_dn_rmd <- function() {
+  rmarkdown::render(
+    input = "costs_district_nursing.Rmd",
+    output_file = "costs_district_nursing.html"
+  )
+
+  dn_lookup <- readr::read_rds(get_dn_costs_path())
+
+  return(dn_lookup)
+
+}
