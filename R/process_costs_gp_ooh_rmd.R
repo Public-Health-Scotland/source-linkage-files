286e0c5ec20ec62de2f76d4cea2244a60aef7d6e
Jennifer Thom <jennifer.thom@phs.scot>
2022-10-19 08:51:16 +0100

    Add option for rmarkdown within function

---
+++ R/process_costs_gp_ooh_rmd.R
@@ -0,0 +1,18 @@
+#' Process GP ooh cost lookup RMD file
+#'
+#' @description This will read and process the
+#' GP ooh cost lookup, it will return the final data
+#' but also write this out as a rds.
+#'
+#' @return the final data as a html document.
+#'
+process_costs_gp_ooh_rmd <- function(){
+
+rmarkdown::render(
+  input = "gp_ooh_costs.Rmd",
+  output_file = "gp_ooh_costs.html")
+
+}
+
+
+
