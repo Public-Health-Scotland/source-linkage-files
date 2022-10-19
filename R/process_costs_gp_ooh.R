18361c31e16280cccf5aa3e81fb814ab5f331285
Jennit07 <Jennit07@users.noreply.github.com>
2022-10-19 08:02:00 +0000

    Style code

--- R/process_costs_gp_ooh.R
+++ R/process_costs_gp_ooh.R
@@ -10,36 +10,36 @@
 #'
 #' @return the final data as a [tibble][tibble::tibble-package].
 #' @family process extracts
-process_costs_gp_ooh <- function(data, write_to_disk = TRUE){
+process_costs_gp_ooh <- function(data, write_to_disk = TRUE) {
 
-# Data Cleaning ---------------------------------------
+  # Data Cleaning ---------------------------------------
 
-## data - wide to long ##
-gp_ooh_costs <- data %>%
-  tidyr::pivot_longer(c(tidyselect::ends_with("_Consultations"), tidyselect::ends_with("_Cost")),
-               names_to = c("year", ".value"),
-               names_pattern = "(\\d{4})_(.+)"
-  ) %>%
-  ## create cost per consultation ##
-  dplyr::mutate(
-    cost_per_consultation = .data$Cost * 1000 / .data$Consultations
-  ) %>%
-  dplyr::select(
-    .data$year,
-    .data$HB2019,
-    .data$Board_Name,
-    .data$cost_per_consultation
-  )
+  ## data - wide to long ##
+  gp_ooh_costs <- data %>%
+    tidyr::pivot_longer(c(tidyselect::ends_with("_Consultations"), tidyselect::ends_with("_Cost")),
+      names_to = c("year", ".value"),
+      names_pattern = "(\\d{4})_(.+)"
+    ) %>%
+    ## create cost per consultation ##
+    dplyr::mutate(
+      cost_per_consultation = .data$Cost * 1000 / .data$Consultations
+    ) %>%
+    dplyr::select(
+      .data$year,
+      .data$HB2019,
+      .data$Board_Name,
+      .data$cost_per_consultation
+    )
 
-## add in years by copying the most recent year ##
-latest_cost_year <- max(gp_ooh_costs$.data$year)
+  ## add in years by copying the most recent year ##
+  latest_cost_year <- max(gp_ooh_costs$.data$year)
 
-## increase by 1% for every year after the latest ##
-gp_ooh_costs_uplifted <-
-  dplyr::bind_rows(
-    gp_ooh_costs,
-    purrr:map(1:5, ~
-          gp_ooh_costs %>%
+  ## increase by 1% for every year after the latest ##
+  gp_ooh_costs_uplifted <-
+    dplyr::bind_rows(
+      gp_ooh_costs,
+      purrr:map(1:5, ~
+        gp_ooh_costs %>%
           dplyr::filter(year == .data$latest_cost_year) %>%
           dplyr::group_by(.data$year, .data$HB2019, .data$Board_Name) %>%
           dplyr::summarise(
@@ -47,41 +47,39 @@ gp_ooh_costs_uplifted <-
             .groups = "drop"
           ) %>%
           dplyr::mutate(year = (as.numeric(convert_fyyear_to_year(.data$year)) + .x) %>%
-                   convert_year_to_fyyear()))
-  ) %>%
-  dplyr::arrange(.data$year, .data$HB2019, .data$Board_Name)
+            convert_year_to_fyyear()))
+    ) %>%
+    dplyr::arrange(.data$year, .data$HB2019, .data$Board_Name)
 
-## match files - to make sure costs haven't changed radically ##
-old_costs <- readr::read_rds(get_gp_ooh_costs_path(update = latest_update())) %>%
-  # rename lookup variables to match
-  dplyr::rename(
-    cost_old = "cost_per_consultation",
-    HB2019 = "TreatmentNHSBoardCode",
-    year = "Year"
-  )
+  ## match files - to make sure costs haven't changed radically ##
+  old_costs <- readr::read_rds(get_gp_ooh_costs_path(update = latest_update())) %>%
+    # rename lookup variables to match
+    dplyr::rename(
+      cost_old = "cost_per_consultation",
+      HB2019 = "TreatmentNHSBoardCode",
+      year = "Year"
+    )
 
-# match files
-matched_costs_data <- gp_ooh_costs_uplifted %>%
-  dplyr::full_join(old_costs, by = c("HB2019", "year")) %>%
-  # compute difference
-  dplyr::mutate(
-    difference = .data$cost_per_consultation - .data$cost_old,
-    pct_diff = .data$difference / .data$cost_old * 100
-  )
+  # match files
+  matched_costs_data <- gp_ooh_costs_uplifted %>%
+    dplyr::full_join(old_costs, by = c("HB2019", "year")) %>%
+    # compute difference
+    dplyr::mutate(
+      difference = .data$cost_per_consultation - .data$cost_old,
+      pct_diff = .data$difference / .data$cost_old * 100
+    )
 
-if (write_to_disk){
-
-  gp_ooh_costs_uplifted %>%
-    dplyr::rename(TreatmentNHSBoardCode = "HB2019") %>%
-    # Save .rds file
-    write_rds(get_gp_ooh_costs_path(check_mode = "write"))
-}
-
-
-# Create charts ---------------------------------------
+  if (write_to_disk) {
+    gp_ooh_costs_uplifted %>%
+      dplyr::rename(TreatmentNHSBoardCode = "HB2019") %>%
+      # Save .rds file
+      write_rds(get_gp_ooh_costs_path(check_mode = "write"))
+  }
 
 
+  # Create charts ---------------------------------------
 
-return(gp_ooh_costs_uplifted)
 
+
+  return(gp_ooh_costs_uplifted)
 }
