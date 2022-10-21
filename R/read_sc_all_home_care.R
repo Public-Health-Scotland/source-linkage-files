11f3850d8f2ec14fb7ddf83a851b6986e09ee4e5
Jennifer Thom <jennifer.thom@phs.scot>
2022-10-21 13:55:22 +0100

    Apply changes due to `tidyselect 1.2.0`

--- R/read_sc_all_home_care.R
+++ R/read_sc_all_home_care.R
@@ -12,21 +12,21 @@ read_sc_all_home_care <- function() {
   # read in data - social care 2 home care
   home_care_data <- dplyr::tbl(db_connection, dbplyr::in_schema("social_care_2", "homecare_snapshot")) %>%
     dplyr::select(
-      .data$sending_location,
-      .data$sending_location_name,
-      .data$social_care_id,
-      .data$hc_service_start_date,
-      .data$hc_service_end_date,
-      .data$period,
-      .data$financial_year,
-      .data$financial_quarter,
-      .data$hc_service,
-      .data$hc_service_provider,
-      .data$reablement,
-      .data$hc_hours_derived,
-      .data$total_staff_home_care_hours,
-      .data$multistaff_input,
-      .data$hc_start_date_after_end_date
+      "sending_location",
+      "sending_location_name",
+      "social_care_id",
+      "hc_service_start_date",
+      "hc_service_end_date",
+      "period",
+      "financial_year",
+      "financial_quarter",
+      "hc_service",
+      "hc_service_provider",
+      "reablement",
+      "hc_hours_derived",
+      "total_staff_home_care_hours",
+      "multistaff_input",
+      "hc_start_date_after_end_date"
     ) %>%
     # fix 2017
     dplyr::mutate(financial_quarter = dplyr::if_else(.data$financial_year == 2017 & is.na(.data$financial_quarter), 4, .data$financial_quarter)) %>%
