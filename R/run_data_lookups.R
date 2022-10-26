cf4f38a132501824f664d76bcf89036090917532
Jennifer Thom <jennifer.thom@phs.scot>
2022-10-26 10:59:27 +0100

    Reorder list

--- R/run_data_lookups.R
+++ R/run_data_lookups.R
@@ -13,14 +13,13 @@ run_data_lookups <- function(write_to_disk = FALSE) {
     "postcode" = process_lookup_postcode(write_to_disk = write_to_disk),
     "gpprac" = process_lookup_gpprac(write_to_disk = write_to_disk),
     "chi_deaths" = process_lookup_chi_deaths(read_lookup_chi_deaths(), write_to_disk = write_to_disk),
-    ## Social Care lookups
-    # "sc_demographics" = process_lookup_sc_demographics(read_lookup_sc_demographics(), write_to_disk = write_to_disk),
     ## Cost lookups
-    # "costs_gp_ooh" = process_costs_gp_ooh(read_costs_gp_ooh(), write_to_disk = write_to_disk)
     "costs_gp_ooh" = process_costs_gp_ooh_rmd(),
-    "costs_district_nursing" = process_costs_dn_rmd()
-    # "costs_care_home" = process_costs_care_home(read_costs_care_home(), write_to_disk = write_to_disk),
-    # "costs_home_care" = process_costs_home_care(read_costs_home_care(), write_to_disk = write_to_disk)
+    "costs_district_nursing" = process_costs_dn_rmd(),
+    "costs_care_home" = process_costs_ch_rmd(),
+    "costs_home_care" = process_costs_hc_rmd(),
+    ## Social Care lookups
+    "sc_demographics" = process_lookup_sc_demographics(read_lookup_sc_demographics(), write_to_disk = write_to_disk)
   )
 
   return(process_lookups)
