17e588738aa4db536568c8e5cde96cc373867925
shintoLamp <shintoLamp@users.noreply.github.com>
2022-10-27 17:04:34 +0000

    Style code

--- R/match_pc7_gpprac.R
+++ R/match_pc7_gpprac.R
@@ -3,27 +3,28 @@ test <- haven::read_sav("/conf/sourcedev/Source_Linkage_File_Updates/1920/temp-s
 )
 
 # Recoding hb codes to 2018 standard ----
-test2 <- test %>% tidylog::mutate(dplyr::across(
-  c(hbrescode, hbpraccode, hbtreatcode),
-  ~ dplyr::case_when(
-    # HB2014 to HB2018
-    . == "S08000018" ~ "S08000029",
-    . == "S08000027" ~ "S08000030",
-    # HB2019 to HB2018
-    . == "S08000031" ~ "S08000021",
-    . == "S08000032" ~ "S08000023",
-    TRUE ~ .
+test2 <- test %>% tidylog::mutate(
+  dplyr::across(
+    c(hbrescode, hbpraccode, hbtreatcode),
+    ~ dplyr::case_when(
+      # HB2014 to HB2018
+      . == "S08000018" ~ "S08000029",
+      . == "S08000027" ~ "S08000030",
+      # HB2019 to HB2018
+      . == "S08000031" ~ "S08000021",
+      . == "S08000032" ~ "S08000023",
+      TRUE ~ .
+    )
+  ),
+  HSCP = dplyr::case_when(
+    # HSCP2016 to HSCP2018
+    HSCP == "S37000014" ~ "S37000032",
+    HSCP == "S37000023" ~ "S37000033",
+    # HSCP2019 to HSCP2018
+    HSCP == "S37000034" ~ "S37000015",
+    HSCP == "S37000035" ~ "S37000021",
+    TRUE ~ HSCP
   )
-),
-HSCP = dplyr::case_when(
-  # HSCP2016 to HSCP2018
-  HSCP == "S37000014" ~ "S37000032",
-  HSCP == "S37000023" ~ "S37000033",
-  # HSCP2019 to HSCP2018
-  HSCP == "S37000034" ~ "S37000015",
-  HSCP == "S37000035" ~ "S37000021",
-  TRUE ~ HSCP
-)
 )
 
 # Making postcodes into 7-character format ----
@@ -290,21 +291,21 @@ test12 <- test11 %>%
   dplyr::rename(hbpraccode_old = hbpraccode)
 
 test13 <- dplyr::bind_rows(
-    # First, get all the rows that do match, and give the variable gpprac_match = 1
-    dplyr::inner_join(
-      test12,
-      readr::read_rds(fs::path(get_slf_dir(), "Lookups/source_GPprac_lookup_Sep_2022.rds")) %>%
-        dplyr::select(-(2:dplyr::last_col())),
-      by = "gpprac"
-    ) %>% dplyr::mutate(gpprac_match = 1),
-    # For the rows that do not match, give value of gpprac_match = 0
-    dplyr::anti_join(
-      test12,
-      readr::read_rds(fs::path(get_slf_dir(), "Lookups/source_GPprac_lookup_Sep_2022.rds")) %>%
-        dplyr::select(-(2:dplyr::last_col())),
-      by = "gpprac"
-    ) %>% dplyr::mutate(gpprac_match = 0)
-  )
+  # First, get all the rows that do match, and give the variable gpprac_match = 1
+  dplyr::inner_join(
+    test12,
+    readr::read_rds(fs::path(get_slf_dir(), "Lookups/source_GPprac_lookup_Sep_2022.rds")) %>%
+      dplyr::select(-(2:dplyr::last_col())),
+    by = "gpprac"
+  ) %>% dplyr::mutate(gpprac_match = 1),
+  # For the rows that do not match, give value of gpprac_match = 0
+  dplyr::anti_join(
+    test12,
+    readr::read_rds(fs::path(get_slf_dir(), "Lookups/source_GPprac_lookup_Sep_2022.rds")) %>%
+      dplyr::select(-(2:dplyr::last_col())),
+    by = "gpprac"
+  ) %>% dplyr::mutate(gpprac_match = 0)
+)
 
 test14 <- test13 %>%
   dplyr::mutate(gpprac_match = dplyr::if_else(
@@ -351,11 +352,11 @@ test17 <- dplyr::bind_rows(
     by = "gpprac"
   ) %>% dplyr::mutate(gpprac_match = 0)
 ) %>%
-  dplyr::mutate(hbpraccode = dplyr::if_else(gpprac_match == 0, hbpraccode_old, hbpraccode),
-                gpprac = dplyr::if_else(gpprac_match == 0 & is_missing(hppraccode), NA_real_, gpprac))
+  dplyr::mutate(
+    hbpraccode = dplyr::if_else(gpprac_match == 0, hbpraccode_old, hbpraccode),
+    gpprac = dplyr::if_else(gpprac_match == 0 & is_missing(hppraccode), NA_real_, gpprac)
+  )
 
 # Here is a section in SPSS for adding value labels, maybe we could do this with factors?
 
 # The end
-
-
