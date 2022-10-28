43c64a014ea43d8aaef8c274b930bed31a4cfeba
Jennifer Thom <jennifer.thom@phs.scot>
2022-10-28 13:06:40 +0100

    Revert `year` changes This is now in the year specific functions

--- R/process_sc_all_alarms_telecare.R
+++ R/process_sc_all_alarms_telecare.R
@@ -59,7 +59,6 @@ process_sc_all_alarms_telecare <- function(data, sc_demographics = NULL, write_t
     ) %>%
     # Include source variables
     dplyr::mutate(
-      year = convert_year_to_fyyear(substr(period, 1, 4)),
       recid = "AT",
       smrtype = dplyr::case_when(
         .data$service_type == 1 ~ "AT-Alarm",
@@ -112,7 +111,6 @@ process_sc_all_alarms_telecare <- function(data, sc_demographics = NULL, write_t
       record_keydate2 = dplyr::last(.data$record_keydate2),
       smrtype = dplyr::last(.data$smrtype),
       pkg_count = dplyr::last(.data$pkg_count),
-      year = last(.data$year),
       chi = dplyr::last(.data$chi),
       gender = dplyr::last(.data$gender),
       dob = dplyr::last(.data$dob),
