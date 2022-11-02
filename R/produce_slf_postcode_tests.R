3f65e856f30f9a1c5e29db822a5f64a7725faf12
Jennit07 <67372904+Jennit07@users.noreply.github.com>
2022-11-02 13:58:46 +0000

    Convert postcode lookup into a function for processing in phase 1b (#408)
    * Include pc lookup as a function
    
    * remove script from production scripts
    lookup_postcode.R
    
    * Update documentation
    
    * Update documentation
    
    * Style code
    
    * Write tests function for pc lookup
    
    * Update documentation
    
    * Style code
    
    * Simplify code
    
    * Use null instead of missing in `write_tests_xlsx`
    
    * Style code
    
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>
    Co-authored-by: Moohan <Moohan@users.noreply.github.com>
    Co-authored-by: Jennit07 <Jennit07@users.noreply.github.com>

--- R/produce_slf_postcode_tests.R
+++ R/produce_slf_postcode_tests.R
@@ -19,15 +19,7 @@ produce_slf_postcode_tests <- function(data) {
     # create other test flags
     dplyr::mutate(n_postcode = 1) %>%
     # remove variables that are not test flags
-    dplyr::select(-c(
-      "postcode", "hb2018", "hscp2018", "ca2018",
-      "lca", "locality", "datazone2011", "hb2019",
-      "ca2019", "hscp2019", "simd2020v2_rank",
-      "simd2020v2_sc_decile", "simd2020v2_sc_quintile",
-      "simd2020v2_hb2019_decile", "simd2020v2_hb2019_quintile",
-      "simd2020v2_hscp2019_decile", "simd2020v2_hscp2019_quintile",
-      "ur8_2016", "ur6_2016", "ur3_2016", "ur2_2016"
-    )) %>%
+    dplyr::select(c(.data$NHS_Ayrshire_and_Arran:.data$n_postcode)) %>%
     # use function to sum new test flags
     calculate_measures(measure = "sum")
 }
