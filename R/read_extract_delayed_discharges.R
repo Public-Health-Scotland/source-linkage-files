a6a205b41f4f951c6b9ee77e7e2d2b68af137934
Jennifer Thom <jennifer.thom@phs.scot>
2022-11-02 10:28:07 +0000

    Remove year parameter

--- R/read_extract_delayed_discharges.R
+++ R/read_extract_delayed_discharges.R
@@ -1,11 +1,9 @@
 #' Read Delayed Discharges extract
 #'
-#' @param year Year of BOXI extract
-#'
 #' @return zsav/rds data file for Delayed discharges
 #' @export
 #'
-read_extract_delayed_discharges <- function(year) {
+read_extract_delayed_discharges <- function() {
   extract_delayed_discharges <- haven::read_sav(get_dd_path(ext = "zsav")) %>%
     janitor::clean_names() %>%
     # rename variables
