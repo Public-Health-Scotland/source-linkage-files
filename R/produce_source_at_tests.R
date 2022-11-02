b158731522eff3c5c62a51720f06d2ea22914489
Jennifer Thom <jennifer.thom@phs.scot>
2022-11-02 11:02:17 +0000

    Update documentation

--- R/produce_source_at_tests.R
+++ R/produce_source_at_tests.R
@@ -4,7 +4,7 @@
 #'
 #' @param data new or old data for testing summary flags
 #' (data is from [get_sc_ch_episodes_path()])
-#'
+#' @param max_min_vars variables used when selecting 'min-max' from [calculate_measures()]
 #' @return a dataframe with a count of each flag.
 #'
 #' @export
@@ -14,10 +14,10 @@ produce_source_at_tests <- function(data, max_min_vars = c("record_keydate1", "r
     # create test flags
     create_demog_test_flags() %>%
     dplyr::mutate(
-      n_at_alarms = dplyr::if_else(smrtype == "AT-Alarm", 1, 0),
-      n_at_telecare = dplyr::if_else(smrtype == "AT-Tele", 1, 0)
+      n_at_alarms = dplyr::if_else(.data$smrtype == "AT-Alarm", 1, 0),
+      n_at_telecare = dplyr::if_else(.data$smrtype == "AT-Tele", 1, 0)
     ) %>%
-    create_lca_test_flags(sc_send_lca) %>%
+    create_lca_test_flags(.data$sc_send_lca) %>%
     # remove variables that won't be summed
     dplyr::select(.data$valid_chi:.data$West_Lothian) %>%
     # use function to sum new test flags
