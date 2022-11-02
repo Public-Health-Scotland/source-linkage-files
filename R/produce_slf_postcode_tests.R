9baf2fc1f607772d64ac66eb871de9eee1692b4f
Jennifer Thom <jennifer.thom@phs.scot>
2022-11-02 13:08:32 +0000

    Simplify code

--- R/produce_slf_postcode_tests.R
+++ R/produce_slf_postcode_tests.R
@@ -19,15 +19,8 @@ produce_slf_postcode_tests <- function(data) {
     # create other test flags
     dplyr::mutate(n_postcode = 1) %>%
     # remove variables that are not test flags
-    dplyr::select(-c(
-      .data$postcode, .data$hb2018, .data$hscp2018, .data$ca2018,
-      .data$lca, .data$locality, .data$datazone2011, .data$hb2019,
-      .data$ca2019, .data$hscp2019, .data$simd2020v2_rank,
-      .data$simd2020v2_sc_decile, .data$simd2020v2_sc_quintile,
-      .data$simd2020v2_hb2019_decile, .data$simd2020v2_hb2019_quintile,
-      .data$simd2020v2_hscp2019_decile, .data$simd2020v2_hscp2019_quintile,
-      .data$ur8_2016, .data$ur6_2016, .data$ur3_2016, .data$ur2_2016
-    )) %>%
+    dplyr::select(c(.data$NHS_Ayrshire_and_Arran:.data$n_postcode)
+    ) %>%
     # use function to sum new test flags
     calculate_measures(measure = "sum")
 }
