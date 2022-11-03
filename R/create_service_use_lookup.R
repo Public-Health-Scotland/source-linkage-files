f901c563de4009c0db22f56ad2526fb92f03dcdf
shintoLamp <bateman.mcbride@phs.scot>
2022-11-03 13:16:56 +0000

    Started re-factoring, still WIP

--- R/create_service_use_lookup.R
+++ R/create_service_use_lookup.R
@@ -84,79 +84,209 @@ create_service_use_lookup <- function(data, year, write_to_disk = TRUE) {
   }
 }
 
-#' Create seperate costs for each service type
+#' Calculate cost for Geriatric Care records
+#' @description A record is considered to have a Geriatric Care cost if
+#' \itemize{\item It has a recid of 50B or GLS
+#'          \item It has a specialty of AB (Geriatric Medicine)
+#'                or G4 (Psychiatry of Old Age)}
 #'
-#' @param data A data frame
+#' @param recid A vector of record IDs
+#' @param spec A vector of specialty codes
+#' @param cost_total_net A vector of total net costs
 #'
-#' @return A data frame with 14 additional variables, 13 costs and one flag for operations
-#'
-#' @family Demographic and Service Use Cohort functions
-create_service_costs <- function(data) {
-  # Check data has required variables
-  check_variables_exist(data,
-    variables =
-      c("chi", "recid", "cij_pattype", "cij_ipdc", "spec", "op1a", "cost_total_net")
-  )
-
-  return_data <- data %>%
-    # Create cost variables based on service use
-    # Specialty AB = Geriatric Medicine, G4 = Psychiatry of Old Age
-    dplyr::mutate(
-      # Geriatric medicine
-      geriatric_cost = dplyr::if_else(.data$spec %in% c("AB", "G4") |
-        .data$recid %in% c("50B", "GLS"),
-      .data$cost_total_net, 0
-      ),
-      # Maternity
-      maternity_cost = dplyr::if_else(.data$recid == "02B" |
-        .data$cij_pattype == "Maternity",
-      .data$cost_total_net, 0
-      ),
-      # Psychiatry
-      psychiatry_cost = dplyr::if_else(.data$recid == "04B" &
-        .data$spec != "G4", .data$cost_total_net, 0),
-      # Acute Elective
-      acute_elective_cost = dplyr::if_else(.data$recid == "01B" &
-        (.data$cij_pattype == "Elective" | .data$cij_ipdc == "D") &
-        !(.data$spec %in% c("AB")), .data$cost_total_net, 0),
-      # Acute Emergency
-      acute_emergency_cost = dplyr::if_else(.data$recid == "01B" &
-        .data$cij_pattype == "Non-Elective" & !(.data$spec %in% c("AB")),
-      .data$cost_total_net, 0
-      ),
-      # Outpatient
-      outpatient_cost = dplyr::if_else(.data$recid == "00B", .data$cost_total_net - .data$geriatric_cost, 0),
-      total_outpatient_cost = dplyr::if_else(.data$recid == "00B", .data$cost_total_net, 0),
-      # Home Care is not added yet, here is the code for future
-      # home_care_cost = dplyr::if_else(.data$recid %in% c("HC-", "HC + ", "INS", "RSP", "MLS", "DC", "CPL"),
-      #                                 .data$cost_total_net, 0),
-      # Care home
-      care_home_cost = dplyr::if_else(.data$recid == "CH", .data$cost_total_net, 0),
-      # Hospital elective
-      hospital_elective_cost = dplyr::if_else(.data$recid %in% c("01B", "04B", "50B", "GLS") &
-        .data$cij_pattype == "Elective",
-      .data$cost_total_net, 0
-      ),
-      # Hospital Emergency
-      hospital_emergency_cost = dplyr::if_else(.data$recid %in% c("01B", "04B", "50B", "GLS") &
-        .data$cij_pattype == "Non-Elective",
-      .data$cost_total_net, 0
-      ),
-      # Prescribing
-      prescribing_cost = dplyr::if_else(.data$recid == "PIS", .data$cost_total_net, 0),
-      # A&E
-      ae2_cost = dplyr::if_else(.data$recid %in% c("AE2", "OoH", "SAS", "N24"), .data$cost_total_net, 0),
-      # Future: Include CMH here
-      # Community
-      community_health_cost = dplyr::if_else(.data$recid == "DN", .data$cost_total_net, 0),
-      # Add a flag if person has had an operation
-      operation_flag = !is_missing(.data$op1a),
-      # Replace any NA values with 0
-      dplyr::across(c(.data$geriatric_cost:.data$community_health_cost), ~ tidyr::replace_na(., 0))
-    )
-
-  return(return_data)
+#' @return A vector of Geriatric Care costs
+calculate_geriatric_cost <- function(recid, spec, cost_total_net) {
+  geriatric_cost <- dplyr::if_else(
+    recid %in% c("50B", "GLS") | spec %in% c("AB", "G4"), cost_total_net, 0)
+  return(geriatric_cost)
 }
+
+#' Calculate cost for Maternity records
+#' @description A record is considered to have a Maternity cost if the recid is
+#' 02B or the cij_pattype is "Maternity"
+#'
+#' @param recid A vector of record IDs
+#' @param cij_pattype A vector of CIJ patient types
+#' @param cost_total_net A vector of total net costs
+#'
+#' @return A vector of Maternity costs
+calculate_maternity_cost <- function(recid, cij_pattype, cost_total_net) {
+  maternity_cost <- dplyr::if_else(
+    recid %in% c("02B") | cij_pattype %in% c("Maternity"), cost_total_net, 0)
+  return(maternity_cost)
+}
+
+#' Calculate cost for Psychiatry records
+#' @description A record is considered to have a Psychiatry cost if the recid is 04B
+#' and the specialty is not G4
+#'
+#' @param recid A vector of record IDs
+#' @param spec A vector of specialty codes
+#' @param cost_total_net A vector of total net costs
+#'
+#' @return A vector of Psychiatry costs
+calculate_psychiatry_cost <- function(recid, spec, cost_total_net) {
+  psychiatry_cost <- dplyr::if_else(
+    recid %in% c("04B") & !(spec %in% c("G4")), cost_total_net, 0)
+  return(psychiatry_cost)
+}
+
+#' Calculate cost for Acute Elective records
+#' @description A record is considered to have an Acute Elective cost if
+#' the recid is 01B, the CIJ patient type is Elective or the IPDC shows it is a day case,
+#' and the specialty is not AB
+#'
+#' @param recid A vector of record IDs
+#' @param cij_pattype A vector of CIJ patient types
+#' @param cij_ipdc A vector of CIJ inpatient/daycase markers
+#' @param spec A vector of specialty codes
+#' @param cost_total_net A vector of total net costs
+#'
+#' @return A vector of Acute Elective costs
+calculate_acute_elective_cost <- function(recid, cij_pattype, cij_ipdc, spec, cost_total_net) {
+  acute_elective_cost <- dplyr::if_else(recid %in% c("01B") &
+                                          (cij_pattype %in% c("Elective") | cij_ipdc %in% c("D")) &
+                                          !(spec %in% c("AB")), cost_total_net, 0)
+  return(acute_elective_cost)
+}
+
+#' Calculate cost for Acute Emergency records
+#' @description A record is considered to have an Acute Emergency cost if
+#' the recid is 01B, the CIJ patient type is Non-Elective, and the specialty is not AB
+#'
+#' @param recid A vector of record IDs
+#' @param cij_pattype A vector of CIJ patient types
+#' @param spec A vector of specialty codes
+#' @param cost_total_net A vector of total net costs
+#'
+#' @return A vector of Acute Emergency costs
+calculate_acute_emergency_cost <- function(recid, cij_pattype, spec, cost_total_net) {
+  acute_emergency_cost <- dplyr::if_else(recid %in% c("01B") & cij_pattype %in% c("Non-Elective") &
+                                          !(spec %in% c("AB")), cost_total_net, 0
+  )
+  return(acute_emergency_cost)
+}
+
+#' Calculate cost for Outpatient records
+#' @description A record is considered to have an Outpatient cost if the recid is 00B. The
+#' Outpatient Cost is defined as (total net cost - geriatric cost), and the Total Outpatient Cost
+#' is the total net cost
+#'
+#' @param recid A vector of record IDs
+#' @param cost_total_net A vector of total net costs
+#' @param geriatric_cost A vector of geriatric care costs
+#'
+#' @return A list with outpatient costs in index 1 and total outpatient costs in index 2
+calculate_outpatient_costs <- function(recid, cost_total_net, geriatric_cost) {
+  outpatient_cost <- dplyr::if_else(
+    recid %in% c("00B"), cost_total_net - geriatric_cost, 0)
+  total_outpatient_cost <- dplyr::if_else(
+    recid  %in% c("00B"), cost_total_net, 0)
+  return(list(outpatient_cost = outpatient_cost,
+              total_outpatient_cost = total_outpatient_cost))
+}
+
+#' Calculate cost for Home Care records
+#' @description Please note: this function is currently not in use.
+#' A record is considered to have a home care cost if the recid is in
+#' c("HC-", "HC + ", "INS", "RSP", "MLS", "DC", "CPL")
+#'
+#' @param recid A vector of record IDs
+#'
+#' @return A vector of home care costs
+calculate_home_care_cost <- function(recid, cost_total_net) {
+  home_care_cost <- dplyr::if_else(
+    recid %in% c("HC-", "HC + ", "INS", "RSP", "MLS", "DC", "CPL"), cost_total_net, 0)
+  return(home_care_cost)
+}
+
+#' Calculate cost for Care Home records
+#' @description A record is considered to have a home care cost if the recid is CH
+#'
+#' @param recid A vector of record IDs
+#'
+#' @return A vector of care home costs
+calculate_care_home_cost <- function(recid, cost_total_net) {
+  care_home_cost <- dplyr::if_else(recid %in% c("CH"), cost_total_net, 0)
+  return(care_home_cost)
+}
+
+#' Calculate cost for Hospital Elective records
+#' @description A record is considered to have a Hospital Elective cost if the recid is
+#' in c("01B", "04B", "50B", "GLS") and the CIJ patient type is Elective
+#'
+#' @param recid A vector of record IDs
+#' @param cij_pattype A vector of CIJ patient types
+#' @param cost_total_net A vector of total net costs
+#'
+#' @return A vector of Hospital Elective costs
+calculate_hospital_elective_cost <- function(recid, cij_pattype, cost_total_net) {
+  hospital_elective_cost <- dplyr::if_else(
+    recid %in% c("01B", "04B", "50B", "GLS") & cij_pattype %in% c("Elective"), cost_total_net, 0)
+  return(hospital_elective_cost)
+}
+
+#' Calculate cost for Hospital Emergency records
+#' @description A record is considered to have a Hospital Emergency cost if the recid is
+#' in c("01B", "04B", "50B", "GLS") and the CIJ patient type is Non-Elective
+#'
+#' @param recid A vector of record IDs
+#' @param cij_pattype A vector of CIJ patient types
+#' @param cost_total_net A vector of total net costs
+#'
+#' @return A vector of Hospital Emergency costs
+calculate_hospital_emergency_cost <- function(recid, cij_pattype, cost_total_net) {
+  hospital_emergency_cost <- dplyr::if_else(
+    recid %in% c("01B", "04B", "50B", "GLS") & cij_pattype %in% c("Non-Elective"), cost_total_net, 0)
+  return(hospital_emergency_cost)
+}
+
+#' Calculate cost for Prescribing records
+#' @description A record is considered to have a Prescribing cost if the recid is PIS
+#'
+#' @param recid A vector of record IDs
+#' @param cost_total_net A vector of total net costs
+#'
+#' @return A vector of Prescribing costs
+calculate_prescribing_cost <- function(recid, cost_total_net) {
+  prescribing_cost <- dplyr::if_else(recid %in% c("PIS"), cost_total_net, 0)
+  return(prescribing_cost)
+}
+
+#' Calculate cost for Accident & Emergency records
+#' @description A record is considered to have an A&E cost if the recid is one of
+#' "AE2", "OoH", "SAS", or "N24"
+#'
+#' @param recid A vector of record IDs
+#' @param cost_total_net A vector of total net costs
+#'
+#' @return A vector of Prescribing costs
+calculate_ae2_cost <- function(recid, cost_total_net) {
+  ae2_cost <- dplyr::if_else(recid %in% c("AE2", "OoH", "SAS", "N24"), cost_total_net, 0)
+  return(ae2_cost)
+}
+
+#' Calculate cost for Community Health records
+#' @description A record is considered to have a Community Health cost if the recid is DN
+#'
+#' @param recid A vector of record IDs
+#' @param cost_total_net A vector of total net costs
+#'
+#' @return A vector of Community Health costs
+calculate_community_health_cost <- function(recid, cost_total_net) {
+  community_health_cost = dplyr::if_else(recid %in% c("DN"), cost_total_net, 0)
+  return(community_health_cost)
+}
+
+#' Add operation flag
+#'
+#' @param op1a A vector of operation codes
+#'
+#' @return A boolean vector showing whether a record contains an operation or not
+add_operation_flag <- function(op1a) {
+  operation_flag <- !is_missing(op1a)
+}
+
 
 #' Create counters for elective and non-elective instances
 #'
