c21fd90cb67e5461df51b090ca89ea5baf8f431a
Jennifer Thom <jennifer.thom@phs.scot>
2022-11-07 15:03:28 +0000

    Update `replace_sc_id_with_latest` function

--- R/replace_sc_id_with_latest.R
+++ R/replace_sc_id_with_latest.R
@@ -1,28 +1,39 @@
-
+#' Replace sc id with the latest sc id
+#'
+#' @param data a dataframe containing the required variables
+#'
+#' @return a dataframe overwritten with the latest sc id.
+#'
+#' @export
+#'
 replace_sc_id_with_latest <- function(data) {
+
   # Check for required variables
   check_variables_exist(data, c("sending_location", "social_care_id", "chi", "period"))
 
   # select variables we need
-  filter_data <- matched_hc_data %>%
+  filter_data <- data %>%
     dplyr::select(
       "sending_location", "social_care_id", "chi", "period"
-    ) %>%
-    # Filter to CHIs with multiple sc_id per sending loc
-    dplyr::group_by(.data$sending_location, .data$social_care_id) %>%
-    dplyr::mutate(latest_sc_id = dplyr::last(.data$social_care_id)) %>%
-    # count changed social_care_id
-    dplyr::mutate(
-      changed_sc_id = dplyr::if_else(!is.na(.data$chi) & .data$social_care_id != .data$latest_sc_id, 1, 0),
-      social_care_id = dplyr::if_else(!is.na(.data$chi) & .data$social_care_id != .data$latest_sc_id,
-        .data$latest_sc_id, .data$social_care_id
-      )
-    ) %>%
-    dplyr::ungroup()
+    )
 
-  # Sort (by period?) to find the latest sc_id for each CHI / sending_loc
+  change_sc_id <- filter_data %>%
+    # Sort (by sending_location, chi and period) for unique chi/sending location
+    dplyr::arrange(sending_location, chi, desc(period)) %>%
+    # Find the latest sc_id for each chi/sending location by keeping latest period
+    dplyr::distinct(sending_location, chi, .keep_all = TRUE) %>%
+    # Rename for latest sc id
+    dplyr::rename(latest_sc_id = "social_care_id") %>%
+    # drop period for matching
+    dplyr::select(-"period")
 
-  # match that back to the data overwriting the older one.
+  return_data <- change_sc_id %>%
+    # Match back onto data
+    dplyr::right_join(data, by = c("sending_location", "chi")) %>%
+    # Overwrite sc id with the latest
+    dplyr::mutate(social_care_id = dplyr::if_else(!is.na(.data$chi) & .data$social_care_id != .data$latest_sc_id,
+      .data$latest_sc_id, .data$social_care_id
+    ))
 
-  # Return the data (no new variables added, just overwrite the sc_id)
+  return(return_data)
 }
