c5c521aa444eca20048ecc1eef9ba6eda7fffb0a
Jennifer Thom <jennifer.thom@phs.scot>
2022-11-08 13:27:30 +0000

    Deal with NA's and update logic

--- All_years/04-Social_Care/04-SDS_data.R
+++ All_years/04-Social_Care/04-SDS_data.R
@@ -56,12 +56,14 @@ sds_full_clean <- sds_full_data %>%
   # Deal with SDS option 4
   # First turn the option flags into a logical T/F
   mutate(across(starts_with("sds_option_"), ~ .x == "1")) %>%
+  # Deal with NA's
+  mutate(across(starts_with("sds_option_"), ~ if_else(is.na(.x), FALSE,.x))) %>%
   # SDS option 4 is derived when a person receives more than one option.
   # e.g. if a person has options 1 and 2 then option 4 will be derived
   mutate(
     sds_option_4 = rowSums(across(starts_with("sds_option_"))) > 1, .after = sds_option_3,
     # Fix sds option 4 cases where all 3 options are missing
-    sds_option_4 = if_else(sending_location == "120" & !(sds_option_1 & sds_option_2 & sds_option_3), TRUE, sds_option_4)
+    sds_option_4 = if_else(sending_location == "120" & all(!sds_option_1, !sds_option_2, !sds_option_3), TRUE, sds_option_4)
   ) %>%
   # Match on demographics data (chi, gender, dob and postcode)
   left_join(sc_demographics, by = c("sending_location", "social_care_id")) %>%
