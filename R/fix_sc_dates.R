21a4f604f488e00d5f19b08d409dfc7a1d924d9e
Jennifer Thom <jennifer.thom@phs.scot>
2022-11-15 12:02:54 +0000

    Update comment

--- R/fix_sc_dates.R
+++ R/fix_sc_dates.R
@@ -9,7 +9,7 @@
 #' @return A date vector with replaced end dates
 #' @export
 fix_sc_start_dates <- function(start_date, period) {
-  # Fix sds_end_date is earlier than sds_start_date by setting end_date to be the end of fyear
+  # Fix sds_start_date is missing by setting start_date to be the start of fyear
   start_date <- dplyr::if_else(
     is.na(start_date),
     start_fy(year = substr(period, 1, 4), "alternate"),
