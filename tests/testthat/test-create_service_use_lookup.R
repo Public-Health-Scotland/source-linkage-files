ae7219b30d2f202addc22687027db2bfcf38f0b6
James McMahon <james.mcmahon@phs.scot>
2022-11-15 09:07:56 +0000

    Delete spurious test

--- tests/testthat/test-create_service_use_lookup.R
+++ tests/testthat/test-create_service_use_lookup.R
@@ -443,9 +443,3 @@ test_that("Cohort names are assigned correctly", {
     )
   )
 })
-
-# test_that("Errors handled as expected", {
-#   expect_error(create_service_instances(tibble::tribble(~cij_marker, ~test_bad)),
-#     regexp = "Variables .+ are required, but are missing from `data`"
-#   )
-# })
