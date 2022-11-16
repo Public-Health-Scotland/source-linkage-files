05f07385bf1ddfd2da67297696e60fac663c2d5d
lizihao-anu <lizihao-anu@users.noreply.github.com>
2022-11-16 13:17:11 +0000

    Style code

--- R/cost_uplift.R
+++ R/cost_uplift.R
@@ -9,7 +9,6 @@ cost_uplift <- function(data) {
   data <- data %>%
     # attach a uplift scale as the last column
     uplift_set() %>%
-
     dplyr::mutate(
       cost_total_net = cost_total_net * uplift,
       cost_total_net_incdnas = cost_total_net_incdnas * uplift,
@@ -38,42 +37,42 @@ cost_uplift <- function(data) {
 #'
 #' @return episode data with a uplift scale
 #' @export
-uplift_set <- function(data, latest_cost_year = '2223'){
-  #We have set uplifts to use for 2020/21, 2021/22 and 2022/23, provided by Paul Leak.
-  #For older years, don't uplift.
-  #For years after 2022/23 uplift by an additional 1% per year after the latest cost year (2022/23)
-  #For non plics recids use uplift of 1 so we won't change anything.
+uplift_set <- function(data, latest_cost_year = "2223") {
+  # We have set uplifts to use for 2020/21, 2021/22 and 2022/23, provided by Paul Leak.
+  # For older years, don't uplift.
+  # For years after 2022/23 uplift by an additional 1% per year after the latest cost year (2022/23)
+  # For non plics recids use uplift of 1 so we won't change anything.
 
   # to accelerate, create a data frame of year and uplift for match-joining
   start_year <- 10
   end_year <- 70
-  year <- paste0(start_year:end_year, (start_year+1) : (end_year+1)) %>% as.numeric()
+  year <- paste0(start_year:end_year, (start_year + 1):(end_year + 1)) %>% as.numeric()
   uplift_df <- data.frame(year, uplift = 1) %>%
     dplyr::mutate(row_no = dplyr::row_number())
-  latest_cost_year_row <- uplift_df[year == as.numeric(latest_cost_year),]$row_no
+  latest_cost_year_row <- uplift_df[year == as.numeric(latest_cost_year), ]$row_no
 
   uplift_df <- uplift_df %>%
     dplyr::mutate(uplift = dplyr::case_when(
-      #We have set uplifts to use for 2020/21, 2021/22 and 2022/23, provided by Paul Leak.
+      # We have set uplifts to use for 2020/21, 2021/22 and 2022/23, provided by Paul Leak.
       year == 2021L ~ 1.015,
-      year == 2122L ~ 1.015*1.041,
-      year == 2223L ~ 1.015*1.041*1.062,
-      #For years after 2022/23 uplift by an additional 1% per year after the latest cost year (2022/23)
+      year == 2122L ~ 1.015 * 1.041,
+      year == 2223L ~ 1.015 * 1.041 * 1.062,
+      # For years after 2022/23 uplift by an additional 1% per year after the latest cost year (2022/23)
       year > as.numeric(latest_cost_year) ~ (1.015 * 1.041 * 1.062) * (1.01^(row_no - latest_cost_year_row)),
-      #For older years, don't uplift.
+      # For older years, don't uplift.
       TRUE ~ 1
     )) %>%
     dplyr::mutate(year = as.character(year)) %>%
     dplyr::select(-row_no)
 
- data = data %>%
-   dplyr::left_join(uplift_df, by = "year") %>%
-   #For non plics recids use uplift of 1 so we won't change anything.
-   dplyr::mutate(uplift = dplyr::if_else(
-     recid %in%  c("00B", "01B", "GLS", "02B", "04B", "AE2"),
-     1,
-     uplift
-   ))
+  data <- data %>%
+    dplyr::left_join(uplift_df, by = "year") %>%
+    # For non plics recids use uplift of 1 so we won't change anything.
+    dplyr::mutate(uplift = dplyr::if_else(
+      recid %in% c("00B", "01B", "GLS", "02B", "04B", "AE2"),
+      1,
+      uplift
+    ))
 
- return(data)
+  return(data)
 }
