62dd2f619c1ea21c45db4934b7b6bd55a4c4aea5
James McMahon <james.mcmahon@phs.scot>
2022-11-17 11:51:17 +0000

    tweak comments and documentation

--- R/fix_sc_dates.R
+++ R/fix_sc_dates.R
@@ -9,7 +9,8 @@
 #' @return A date vector with replaced end dates
 #' @export
 fix_sc_start_dates <- function(start_date, period) {
-  # Fix sds_start_date is missing by setting start_date to be the start of fyear
+  # Fix sds_start_date is missing by setting start_date to be the start of
+  # financial year
   start_date <- dplyr::if_else(
     is.na(start_date),
     start_fy(year = substr(period, 1, 4), "alternate"),
@@ -22,7 +23,8 @@ fix_sc_start_dates <- function(start_date, period) {
 
 #' Fix sc end dates
 #'
-#' @description Fix social care end dates when the end date is earlier than the start date.
+#' @description Fix social care end dates when the end date is earlier than the
+#' start date.
 #' Set this to the end of the fyear
 #'
 #' @param start_date A vector containing dates.
@@ -32,7 +34,8 @@ fix_sc_start_dates <- function(start_date, period) {
 #' @return A date vector with replaced end dates
 #' @export
 fix_sc_end_dates <- function(start_date, end_date, period) {
-  # Fix sds_end_date is earlier than sds_start_date by setting end_date to be the end of fyear
+  # Fix sds_end_date is earlier than sds_start_date by setting end_date to be
+  # the end of financial year
   end_date <- dplyr::if_else(
     start_date > end_date,
     end_fy(year = substr(period, 1, 4), "alternate"),
