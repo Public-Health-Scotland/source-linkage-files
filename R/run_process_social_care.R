60232cf07aa8cfa75ae9ab00dc1826b9c8a439be
Jennifer Thom <jennifer.thom@phs.scot>
2022-11-18 14:36:41 +0000

    Add connection to sc platform as a parameter

--- R/run_process_social_care.R
+++ R/run_process_social_care.R
@@ -2,6 +2,8 @@
 #'
 #' @description Process and social care data so they are ready for processing
 #' social care extracts
+#'
+#' @param sc_dvprod_connection Connection to the SC platform
 #' @param sc_demographics The demographic file. Set to NULL for passing through
 #' a data list
 #' @param write_to_disk (optional) Should the data be written to disk default is
@@ -11,22 +13,23 @@
 #'
 #' @export
 #'
-run_process_social_care <- function(sc_demographics = NULL, write_to_disk = FALSE) {
+run_process_social_care <- function(sc_dvprod_connection = phs_db_connection(dsn = "DVPROD"), sc_demographics = NULL, write_to_disk = FALSE) {
   process_social_care <- list(
-    "home_care" = process_sc_all_home_care(read_sc_all_home_care(),
+    "home_care" = process_sc_all_home_care(read_sc_all_home_care(sc_dvprod_connection),
       sc_demographics = sc_demographics,
       write_to_disk = write_to_disk
     ),
-    "alarms_telecare" = process_sc_all_alarms_telecare(read_sc_all_alarms_telecare(),
+    "alarms_telecare" = process_sc_all_alarms_telecare(read_sc_all_alarms_telecare(sc_dvprod_connection),
       sc_demographics = sc_demographics,
       write_to_disk = write_to_disk
     ),
-    "sds" = process_sc_all_sds(read_sc_all_sds(),
+    "sds" = process_sc_all_sds(read_sc_all_sds(sc_dvprod_connection),
       sc_demographics = sc_demographics,
       write_to_disk = write_to_disk
-      # "care_home" = process_sc_all_care_home(write_to_disk = write_to_disk),
     )
   )
 
   return(process_social_care)
 }
+
+# "care_home" = process_sc_all_care_home(write_to_disk = write_to_disk),
