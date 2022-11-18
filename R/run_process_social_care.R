cd2f7969f61d3147ee08330b6d23a1cfbc9d9c9a
Jennifer Thom <jennifer.thom@phs.scot>
2022-11-18 11:16:08 +0000

    Rename function to `run_process_social_care` This removes one level of function not needed

--- R/run_process_social_care.R
+++ R/run_process_social_care.R
@@ -1,14 +1,32 @@
-#' Run Social Care processing.
-#' @description This script is the top level processing script for processing
-#' all social care files
+#' Run Social Care data
 #'
-#' @param years Specify years which need to run.
+#' @description Process and social care data so they are ready for processing
+#' social care extracts
+#' @param sc_demographics The demographic file. Set to NULL for passing through
+#' a data list
+#' @param write_to_disk (optional) Should the data be written to disk default is
+#' `TRUE` i.e. write the data to disk.
 #'
-#' @return A list of data with the data lookups as a tibble along with the years.
+#' @return A list of data containing processed extracts.
+#'
 #' @export
 #'
-run_process_social_care <- function(years) {
-  social_care_data <- run_data_social_care()
+run_process_social_care <- function(sc_demographics = NULL, write_to_disk = FALSE) {
+  process_social_care <- list(
+    "home_care" = process_sc_all_home_care(read_sc_all_home_care(),
+      sc_demographics = sc_demographics,
+      write_to_disk = write_to_disk
+    ),
+    "alarms_telecare" = process_sc_all_alarms_telecare(read_sc_all_alarms_telecare(),
+      sc_demographics = sc_demographics,
+      write_to_disk = write_to_disk
+    ),
+    "sds" = process_sc_all_sds(read_sc_all_sds(),
+      sc_demographics = sc_demographics,
+      write_to_disk = write_to_disk
+      # "care_home" = process_sc_all_care_home(write_to_disk = write_to_disk),
+    )
+  )
 
-  return(social_care_data)
+  return(process_social_care)
 }
