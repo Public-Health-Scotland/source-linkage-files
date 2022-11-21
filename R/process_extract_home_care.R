1dbf815305cb569efb93a6b082051c6fae7ce203
Jennifer Thom <jennifer.thom@phs.scot>
2022-11-21 09:08:14 +0000

    Remove TODO comment - not needed

--- R/process_extract_home_care.R
+++ R/process_extract_home_care.R
@@ -38,10 +38,7 @@ process_extract_home_care <- function(data = NULL, year, client_lookup = NULL, w
     # select episodes for FY
     dplyr::filter(is_date_in_fyyear(year, .data$record_keydate1, .data$record_keydate2)) %>%
     # remove any episodes where the latest submission was before the current year
-    dplyr::filter(substr(.data$sc_latest_submission, 1L, 4L) >= convert_fyyear_to_year(year)) %>%
-    # alter sending location type to allow match
-    # TODO change the client script to use sending location as an integer
-    dplyr::mutate(sending_location = as.character(.data$sending_location)) %>%
+    dplyr::filter(substr(.data$sc_latest_submission, 1, 4) >= convert_fyyear_to_year(year)) %>%
     # Match to client data
     dplyr::left_join(client_lookup, by = c("sending_location", "social_care_id")) %>%
     dplyr::mutate(year = year)
