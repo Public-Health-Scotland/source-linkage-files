f5f42b3cf0df185704a4ead5307db27f54a800c7
Zihao Li <zihao.li@phs.scot>
2022-11-25 12:38:59 +0000

    cost uplift from C10 to C01 (#451)
    * cost uplift from C10 to C01
    
    * Style code
    
    * update documentation
    
    * Update documentation
    
    * create a uplift_set function to decide the uplift rate for each episode
    
    * Style code
    
    * Update documentation
    
    * fix the bug of non plics recids
    
    * Replace `data.frame` with `tibble::tibble`
    
    * Update R/cost_uplift.R
    
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>
    
    * Update R/cost_uplift.R
    
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>
    
    * Update R/cost_uplift.R
    
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>
    
    * Update documentation
    
    * rename the uplift_set function as lookup_uplift
    
    * Style code
    
    Co-authored-by: Zihao Li <zihaol01@nssrstudio03.csa.scot.nhs.uk>
    Co-authored-by: lizihao-anu <lizihao-anu@users.noreply.github.com>
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>

---
+++ R/cost_uplift.R
@@ -0,0 +1,78 @@
+#' Uplift costs
+#'
+#' @param data episode data
+#'
+#' @return episode data with uplifted costs
+#' @export
+cost_uplift <- function(data) {
+  data <- data %>%
+    # attach a uplift scale as the last column
+    lookup_uplift() %>%
+    dplyr::mutate(
+      cost_total_net = .data$cost_total_net * .data$uplift,
+      cost_total_net_incdnas = .data$cost_total_net_incdnas * .data$uplift,
+      apr_cost = .data$apr_cost * .data$uplift,
+      may_cost = .data$may_cost * .data$uplift,
+      jun_cost = .data$jun_cost * .data$uplift,
+      jul_cost = .data$jul_cost * .data$uplift,
+      aug_cost = .data$aug_cost * .data$uplift,
+      sep_cost = .data$sep_cost * .data$uplift,
+      oct_cost = .data$oct_cost * .data$uplift,
+      nov_cost = .data$nov_cost * .data$uplift,
+      dec_cost = .data$dec_cost * .data$uplift,
+      jan_cost = .data$jan_cost * .data$uplift,
+      feb_cost = .data$feb_cost * .data$uplift,
+      mar_cost = .data$mar_cost * .data$uplift
+    ) %>%
+    # remove the last uplift column
+    dplyr::select(-"uplift")
+
+  return(data)
+}
+
+#' Set uplift scale
+#'
+#' @param data episode data
+#'
+#' @return episode data with a uplift scale
+lookup_uplift <- function(data) {
+  # We have set uplifts to use for 2020/21, 2021/22 and 2022/23, provided by Paul Leak.
+  # For older years, don't uplift.
+  # For years after 2022/23 uplift by an additional 1% per year after the latest cost year (2022/23)
+  # For non plics recids use uplift of 1 so we won't change anything.
+
+  # to accelerate, create a data frame of year and uplift for match-joining
+  start_year <- 10L
+  end_year <- as.integer(format(Sys.Date(), "%y"))
+  year <- paste0(start_year:end_year, (start_year + 1):(end_year + 1)) %>% as.integer()
+  uplift_df <- tibble::tibble(year,
+    uplift = 1.0
+  ) %>%
+    dplyr::mutate(row_no = dplyr::row_number())
+  latest_cost_year_row <- uplift_df[year == as.integer(latest_cost_year()), ][["row_no"]]
+
+  uplift_df <- uplift_df %>%
+    dplyr::mutate(uplift = dplyr::case_when(
+      # We have set uplifts to use for 2020/21, 2021/22 and 2022/23, provided by Paul Leak.
+      year == 2021L ~ 1.015,
+      year == 2122L ~ 1.015 * 1.041,
+      year == 2223L ~ 1.015 * 1.041 * 1.062,
+      # For years after 2022/23 uplift by an additional 1% per year after the latest cost year (2022/23)
+      year > as.integer(latest_cost_year()) ~ (1.015 * 1.041 * 1.062) * (1.01^(.data$row_no - latest_cost_year_row)),
+      # For older years, don't uplift.
+      TRUE ~ 1
+    )) %>%
+    dplyr::mutate(year = as.character(.data$year)) %>%
+    dplyr::select(-"row_no")
+
+  data <- data %>%
+    dplyr::left_join(uplift_df, by = "year") %>%
+    # For non plics recids use uplift of 1 so we won't change anything.
+    dplyr::mutate(uplift = dplyr::if_else(
+      .data$recid %in% c("00B", "01B", "GLS", "02B", "04B", "AE2"),
+      .data$uplift,
+      1
+    ))
+
+  return(data)
+}
