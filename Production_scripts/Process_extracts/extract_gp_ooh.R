81c82a56e50c00f1bd090609551b6dba899c3f9e
James McMahon <james.mcmahon@phs.scot>
2022-12-08 14:38:09 +0000

    Final changes
    Have skipped some minor sections and left a TODO comment

--- Production_scripts/Process_extracts/extract_gp_ooh.R
+++ Production_scripts/Process_extracts/extract_gp_ooh.R
@@ -26,7 +26,7 @@ diagnosis_extract <- readr::read_csv(
 ) %>%
   # rename variables
   dplyr::rename(
-    guid = "GUID",
+    ooh_case_id = "GUID",
     readcode = "Diagnosis Code",
     description = "Diagnosis Description"
   ) %>%
@@ -39,9 +39,9 @@ diagnosis_extract <- readr::read_csv(
 diagnosis_readcodes <- diagnosis_extract %>%
   dplyr::mutate(
     # Replace question marks with dot
-    readcode = stringr::str_replace_all(readcode, "\\?", "\\."),
-    # Pad with dots up to 5 charaters
-    readcode = stringr::str_pad(readcode, 5, "right", ".")
+    readcode = stringr::str_replace_all(readcode, "\\?", "\\.") %>%
+      # Pad with dots up to 5 charaters
+      stringr::str_pad(5L, "right", ".")
   ) %>%
   # Join diagnosis to readcode lookup
   # Identify diagnosis descriptions which match the readcode lookup
@@ -53,27 +53,33 @@ diagnosis_readcodes <- diagnosis_extract %>%
   # match on true description from readcode lookup
   dplyr::left_join(
     readcode_lookup %>%
-      dplyr::rename(true_description = description),
-    by = c("readcode")
+      dplyr::rename(true_description = "description"),
+    by = "readcode"
   ) %>%
-  # replace description with true description from readcode lookup if this is different
-  dplyr::mutate(description = dplyr::if_else(is.na(full_match_1) & !is.na(true_description),
-    true_description, description
-  )) %>%
+  # Replace  the description with the true description from the Readcode Lookup.
+  dplyr::mutate(
+    description = dplyr::if_else(is.na(full_match_1) & !is.na(true_description),
+      true_description, description
+    )
+  ) %>%
   # Join to readcode lookup again to check
   dplyr::left_join(
     readcode_lookup %>%
       dplyr::mutate(full_match_2 = 1L),
     by = c("readcode", "description")
   ) %>%
-  # Check the output for any dodgy Read codes and try and fix by adding exceptions
-  dplyr::mutate(readcode = dplyr::if_else(is.na(full_match_2), dplyr::case_when(
-    readcode == "Xa1m." ~ "S349",
-    readcode == "Xa1mz" ~ "S349",
-    readcode == "HO6.." ~ "H06..",
-    readcode == "zV6.." ~ "ZV6..",
-    TRUE ~ readcode
-  ), readcode)) %>%
+  # Check the output for any bad Read codes and try and fix by adding exceptions
+  dplyr::mutate(
+    readcode = dplyr::if_else(is.na(full_match_2),
+      dplyr::case_when(
+        readcode == "Xa1m." ~ "S349",
+        readcode == "Xa1mz" ~ "S349",
+        readcode == "HO6.." ~ "H06..",
+        readcode == "zV6.." ~ "ZV6..",
+        TRUE ~ readcode
+      ), readcode
+    )
+  ) %>%
   # Join to readcode lookup again to check
   dplyr::left_join(
     readcode_lookup %>%
@@ -98,7 +104,7 @@ unrecognised_but_ok_codes <- c("@1JX.", "@1JXz", "@43jS", "@65PW", "@8CA.", "@8C
 new_bad_codes <- readcodes_not_matched %>%
   dplyr::filter(!(readcode %in% unrecognised_but_ok_codes))
 
-if (nrow(new_bad_codes) != 0) {
+if (nrow(new_bad_codes) != 0L) {
   cli::cli_abort(c("New unrecognised readcodes",
     "i" = "There {?is/are} {nrow(new_bad_codes)} new unrecognised readcode{?s} in the data.",
     " " = "Check the {cli::qty(nrow(new_bad_codes))} code{?s} then either fix, or add {?it/them} to the {.var unrecognised_but_ok_codes} vector",
@@ -107,31 +113,36 @@ if (nrow(new_bad_codes) != 0) {
   ))
 }
 
-rm(readcode_lookup, readcodes_not_matched, unrecognised_but_ok_codes, new_bad_codes)
+rm(
+  readcode_lookup,
+  readcodes_not_matched,
+  unrecognised_but_ok_codes,
+  new_bad_codes
+)
 
 ## Data Cleaning
 
 diagnosis_clean <- diagnosis_readcodes %>%
-  dplyr::select(guid, readcode, description) %>%
+  dplyr::select("ooh_case_id", "readcode", "description") %>%
   dplyr::mutate(
-    readcode_level = stringr::str_locate(readcode, "\\.")[, "start"],
-    readcode_level = tidyr::replace_na(readcode_level, 6)
+    readcode_level = stringr::str_locate(readcode, "\\.")[, "start"] %>%
+      tidyr::replace_na(6L)
   ) %>%
-  dtplyr::lazy_dt() %>%
-  dplyr::group_by(guid) %>%
+  dplyr::group_by(ooh_case_id) %>%
   # Sort so that the 'more specific' readcodes are preferred
-  dplyr::arrange(desc(readcode_level)) %>%
+  dplyr::arrange(dplyr::desc(readcode_level)) %>%
   dplyr::mutate(diag_n = dplyr::row_number()) %>%
   dplyr::ungroup() %>%
-  dplyr::select(-readcode_level) %>%
+  dplyr::select(-"readcode_level") %>%
+  dplyr::rename(diag = "readcode") %>%
   # restructure data
   tidyr::pivot_wider(
     names_from = diag_n,
-    values_from = c(readcode, description),
+    values_from = c(diag, description),
     names_glue = "{.value}_{diag_n}"
   ) %>%
   dplyr::select(
-    "guid",
+    "ooh_case_id",
     # Use any of in case we have fewer than 6 diagnoses
     tidyselect::any_of(c(
       "diag_1",
@@ -141,8 +152,7 @@ diagnosis_clean <- diagnosis_readcodes %>%
       "diag_5",
       "diag_6"
     ))
-  ) %>%
-  dplyr::as_tibble()
+  )
 
 rm(diagnosis_extract, diagnosis_readcodes)
 
@@ -158,7 +168,7 @@ outcomes_extract <- readr::read_csv(
 ) %>%
   # rename variables
   dplyr::rename(
-    guid = "GUID",
+    ooh_case_id = "GUID",
     outcome = "Case Outcome"
   ) %>%
   # Remove blank outcomes
@@ -185,7 +195,7 @@ outcomes_clean <- outcomes_extract %>%
     )
   ) %>%
   # Sort so we prefer 'lower' outcomes e.g. Death, over things like 'Other'
-  dplyr::group_by(guid) %>%
+  dplyr::group_by(ooh_case_id) %>%
   dplyr::arrange(outcome) %>%
   dplyr::mutate(outcome_n = dplyr::row_number()) %>%
   dplyr::ungroup() %>%
@@ -196,7 +206,7 @@ outcomes_clean <- outcomes_extract %>%
     values_from = outcome
   ) %>%
   dplyr::select(
-    "guid",
+    "ooh_case_id",
     tidyselect::any_of(c(
       "outcome_1",
       "outcome_2",
@@ -214,26 +224,14 @@ rm(outcomes_extract)
 consultations_file <- readr::read_csv(
   file = get_boxi_extract_path(year, "GP_OoH-c"),
   col_types = readr::cols(
-    "UPI Number [C]" = readr::col_character(),
     "Patient DoB Date [C]" = readr::col_date(format = "%Y/%m/%d %T"),
     "Gender" = readr::col_integer(),
-    "Patient Postcode [C]" = readr::col_character(),
-    "Patient NHS Board Code 9 - current" = readr::col_character(),
-    "HSCP of Residence Code Current" = readr::col_character(),
-    "Patient Data Zone 2011" = readr::col_character(),
-    "Practice Code" = readr::col_character(),
-    "Practice NHS Board Code 9 - current" = readr::col_character(),
-    "GUID" = readr::col_character(),
     "Consultation Recorded" = readr::col_factor(levels = c("Y", "N")),
-    "Consultation Start Date Time" = readr::col_datetime(format = "%Y/%m/%d %H:%M:%S"),
-    "Consultation End Date Time" = readr::col_datetime(format = "%Y/%m/%d %H:%M:%S"),
-    "Treatment Location Code" = readr::col_character(),
-    "Treatment Location Description" = readr::col_character(),
-    "Treatment NHS Board Code 9" = readr::col_character(),
+    "Consultation Start Date Time" = readr::col_datetime(format = "%Y/%m/%d %T"),
+    "Consultation End Date Time" = readr::col_datetime(format = "%Y/%m/%d %T"),
     "KIS Accessed" = readr::col_factor(levels = c("Y", "N")),
-    "Referral Source" = readr::col_character(),
-    "Consultation Type" = readr::col_character(),
-    "Consultation Type Unmapped" = readr::col_character()
+    # All other columns are character type
+    .default = readr::col_character()
   )
 ) %>%
   # rename variables
@@ -246,7 +244,7 @@ consultations_file <- readr::read_csv(
     hscp = "HSCP of Residence Code Current",
     datazone = "Patient Data Zone 2011",
     gpprac = "Practice Code",
-    guid = "GUID",
+    ooh_case_id = "GUID",
     attendance_status = "Consultation Recorded",
     record_keydate1 = "Consultation Start Date Time",
     record_keydate2 = "Consultation End Date Time",
@@ -283,7 +281,7 @@ consultations_filtered <- consultations_file %>%
   # Filter missing / bad CHI numbers
   dplyr::filter(phsmethods::chi_check(chi) == "Valid CHI") %>%
   # Fix some times - if end before start, remove the time portion
-  tidylog::mutate(
+  dplyr::mutate(
     bad_dates = record_keydate1 > record_keydate2,
     record_keydate1 = dplyr::if_else(bad_dates,
       lubridate::floor_date(record_keydate1, "day"),
@@ -308,7 +306,12 @@ consultations_covid <- consultations_filtered %>%
     dplyr::case_when(
       consultation_type_unmapped == "COVID19 ASSESSMENT" ~ consultation_type_unmapped,
       consultation_type_unmapped == "COVID19 ADVICE" ~ consultation_type_unmapped,
-      consultation_type_unmapped %in% c("COVID19 HOME VISIT", "COVID19 OBSERVATION", "COVID19 VIDEO CALL", "COVID19 TEST") ~ "COVID19 OTHER"
+      consultation_type_unmapped %in% c(
+        "COVID19 HOME VISIT",
+        "COVID19 OBSERVATION",
+        "COVID19 VIDEO CALL",
+        "COVID19 TEST"
+      ) ~ "COVID19 OTHER"
     ),
     consultation_type
   ))
@@ -316,31 +319,33 @@ consultations_covid <- consultations_filtered %>%
 # Clean up some overlapping episodes
 # Only merge if they look like duplicates other than the time,
 # In which case take the earliest start and latest end.
-consultations_clean <- consultations_covid %>%
-  dtplyr::lazy_dt() %>%
-  # Sort in reverse order so we can use coalesce which takes the first non-missing value
-  dplyr::arrange(chi, guid, dplyr::desc(record_keydate1), dplyr::desc(record_keydate2)) %>%
-  # This seems to be enough to identify a unique episode
-  dplyr::group_by(chi, guid, consultation_type, location) %>%
-  # Records will be merged if they don't look unique and there is overlap or no time between them
-  dplyr::mutate(episode_counter = replace_na(record_keydate1 > lag(record_keydate2), TRUE) %>%
-    cumsum()) %>%
-  dplyr::group_by(chi, guid, consultation_type, location, episode_counter) %>%
-  dplyr::summarise(
-    record_keydate1 = min(record_keydate1),
-    record_keydate2 = max(record_keydate2),
-    dplyr::across(c(dplyr::everything(), -record_keydate1, -record_keydate2), dplyr::coalesce)
-  ) %>%
-  dplyr::ungroup() %>%
-  dplyr::as_tibble()
+consultations_clean <- consultations_covid
+
+# TODO Remove / merge overlapping records in GP OoHs
+# dtplyr::lazy_dt() %>%
+# # Sort in reverse order so we can use coalesce which takes the first non-missing value
+# dplyr::arrange(chi, ooh_case_id, dplyr::desc(record_keydate1), dplyr::desc(record_keydate2)) %>%
+# # This seems to be enough to identify a unique episode
+# dplyr::group_by(chi, ooh_case_id, consultation_type, location) %>%
+# # Records will be merged if they don't look unique and there is overlap or no time between them
+# dplyr::mutate(episode_counter = replace_na(record_keydate1 > lag(record_keydate2), TRUE) %>%
+#   cumsum()) %>%
+# dplyr::group_by(chi, ooh_case_id, consultation_type, location, episode_counter) %>%
+# dplyr::summarise(
+#   record_keydate1 = min(record_keydate1),
+#   record_keydate2 = max(record_keydate2),
+#   dplyr::across(c(dplyr::everything(), -"record_keydate1", -"record_keydate2"), dplyr::coalesce)
+# ) %>%
+# dplyr::ungroup() %>%
+# dplyr::as_tibble()
 
 rm(consultations_filtered, consultations_covid)
 
 # Join data ---------------------------------
 
 matched_data <- consultations_clean %>%
-  dplyr::left_join(diagnosis_clean, by = "guid") %>%
-  dplyr::left_join(outcomes_clean, by = "guid")
+  dplyr::left_join(diagnosis_clean, by = "ooh_case_id") %>%
+  dplyr::left_join(outcomes_clean, by = "ooh_case_id")
 
 rm(consultations_clean, diagnosis_clean, outcomes_clean)
 
@@ -349,7 +354,7 @@ rm(consultations_clean, diagnosis_clean, outcomes_clean)
 # OOH cost lookup
 ooh_cost_lookup <- readr::read_rds(get_gp_ooh_costs_path()) %>%
   dplyr::rename(
-    hbtreatcode = TreatmentNHSBoardCode
+    hbtreatcode = "TreatmentNHSBoardCode"
   )
 
 ooh_costs <- matched_data %>%
@@ -359,60 +364,49 @@ ooh_costs <- matched_data %>%
       hbtreatcode == "S08000018" ~ "S08000029",
       hbtreatcode == "S08000027" ~ "S08000030",
       # Recode Greater Glasgow & Clyde and Lanarkshire so they
-      # match the costs lookup (2018 > 2019 HB codes).
+      # match the costs lookup (2018 -> 2019 HB codes).
       hbtreatcode == "S08000021" ~ "S08000031",
       hbtreatcode == "S08000023" ~ "S08000032",
       TRUE ~ hbtreatcode
     ),
     year = year
   ) %>%
-  dplyr::arrange(hbtreatcode, year) %>%
   # Match to cost lookup
   dplyr::left_join(ooh_cost_lookup, by = c("hbtreatcode", "year")) %>%
   dplyr::rename(
-    cost_total_net = cost_per_consultation
+    cost_total_net = "cost_per_consultation"
   ) %>%
   create_day_episode_costs(record_keydate1, cost_total_net)
 
+rm(matched_data, ooh_cost_lookup)
 
 # Final cleaning  ---------------------------------
 
 ooh_clean <- ooh_costs %>%
-  # rename outcomes
-  dplyr::rename(
-    ooh_outcome.1 = outcome.1,
-    ooh_outcome.2 = outcome.2,
-    ooh_outcome.3 = outcome.3,
-    ooh_outcome.4 = outcome.4
-  ) %>%
   dplyr::mutate(
-    # Replace location unknown with blank. Should this be NA?
-    location = dplyr::if_else(location == "UNKNOWN", "", location),
+    # Replace location unknown with NA
+    location = dplyr::na_if(location, "UNKNOWN"),
     recid = "OoH",
-    smrtype = dplyr::case_when(
-      smrtype == "DISTRICT NURSE" ~ "OOH-DN",
-      smrtype == "DOCTOR ADVICE/NURSE ADVICE" ~ "OOH-Advice",
-      smrtype == "HOME VISIT" ~ "OOH-HomeV",
-      smrtype == "NHS 24 NURSE ADVICE" ~ "OOH-NHS24",
-      smrtype == "PCEC/PCC" ~ "OOH-PCC",
-      TRUE ~ "OOH-Other"
+    smrtype = add_smr_type("OoH", consultation_type = consultation_type),
+    kis_accessed = factor(
+      dplyr::case_when(
+        kis_accessed == "Y" ~ 1L,
+        kis_accessed == "N" ~ 0L,
+        TRUE ~ 9L
+      ),
+      levels = c(0L, 1L, 9L),
+      labels = c("Y", "N", "Unknown")
     ),
-    kis_accessed = dplyr::case_when(
-      kis_accessed == "Y" ~ 1,
-      kis_accessed == "N" ~ 0,
-      TRUE ~ 9
-    ),
-    gpprac = convert_eng_gpprac_to_dummy(gpprac)
-  ) %>%
-  # split time from date
-  dplyr::mutate(
-    key_time1 = hms::as_hms(stringr::str_sub(record_keydate1, 12L, 19L)),
-    key_time2 = hms::as_hms(stringr::str_sub(record_keydate2, 12L, 19L)),
-    record_keydate1 = stringr::str_sub(record_keydate1, 1L, 10L),
-    record_keydate2 = stringr::str_sub(record_keydate2, 1L, 10L)
+    gpprac = convert_eng_gpprac_to_dummy(gpprac),
+    # Split the time from the date
+    key_time1 = hms::as_hms(record_keydate1),
+    key_time2 = hms::as_hms(record_keydate2),
+    record_keydate1 = trunc(record_keydate1, "days"),
+    record_keydate2 = trunc(record_keydate2, "days")
   )
 
 # Keep the location descriptions as a lookup.
+# TODO write the GP OoH lookup out using some functions
 location_lookup <- ooh_clean %>%
   dplyr::group_by(location) %>%
   dplyr::summarise(
@@ -420,29 +414,10 @@ location_lookup <- ooh_clean %>%
   ) %>%
   dplyr::ungroup()
 
-ooh_clean <- ooh_clean %>%
-  dplyr::arrange(guid, chi) %>%
-  # group for getting row order
-  dplyr::group_by(guid, chi) %>%
-  dplyr::mutate(
-    row_order = dplyr::row_number(),
-    ooh_cc = 0,
-    ooh_cc = dplyr::case_when(
-      ooh_cc == 0 & row_order == 1 | chi != lag(chi, default = dplyr::first(chi)) ~ 1,
-      ooh_cc == 0 & guid != lag(guid) ~ lag(ooh_cc, default = dplyr::first(ooh_cc)) + 1,
-      ooh_cc == 0 ~ lag(ooh_cc, default = dplyr::first(ooh_cc))
-    )
-  )
-
 
 ## Save Outfile -------------------------------------
 
-outfile <- ooh_clean %>%
-  dplyr::arrange(
-    chi,
-    record_keydate1,
-    key_time1
-  ) %>%
+final_data <- ooh_clean %>%
   dplyr::select(
     "year",
     "recid",
@@ -454,7 +429,6 @@ outfile <- ooh_clean %>%
     "chi",
     "gender",
     "dob",
-    "age",
     "gpprac",
     "postcode",
     "hbrescode",
@@ -463,7 +437,7 @@ outfile <- ooh_clean %>%
     "hbtreatcode",
     "location",
     "attendance_status",
-    "kis_Accessed",
+    "kis_accessed",
     "refsource",
     tidyselect::contains("diag"),
     tidyselect::contains("ooh_outcome"),
@@ -480,7 +454,7 @@ outfile <- ooh_clean %>%
     "jan_cost",
     "feb_cost",
     "mar_cost",
-    "ooh_CC"
+    "ooh_case_id"
   )
 
 # End of Script #
