2065e336b35de986d32c92ee9b40f92d1aa594ad
Moohan <Moohan@users.noreply.github.com>
2022-12-12 16:30:21 +0000

    Style code

--- R/match_on_ltcs.R
+++ R/match_on_ltcs.R
@@ -18,13 +18,14 @@ match_on_ltcs <- function(data, year) {
     suffix = c("", "_ltc")
   ) %>%
     dplyr::mutate(
-    # Replace any NA values with 0 for the LTC flags
+      # Replace any NA values with 0 for the LTC flags
       dplyr::across("arth":"digestive", ~ tidyr::replace_na(., 0)),
-    # Use the postcode from the LTC file if it's otherwise missing
-    postcode = dplyr::if_else(is.na(.data$postcode),
-                              .data$postcode_ltc,
-                              .data$postcode)
-      ) %>%
+      # Use the postcode from the LTC file if it's otherwise missing
+      postcode = dplyr::if_else(is.na(.data$postcode),
+        .data$postcode_ltc,
+        .data$postcode
+      )
+    ) %>%
     dplyr::select(-tidyselect::ends_with("_ltc"))
 
 
