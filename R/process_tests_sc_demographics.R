a8ebeefa1e0e63e9101c1fb6b712d56065276af1
Jennifer Thom <jennifer.thom@phs.scot>
2022-12-13 16:46:12 +0000

    convert sc demographics tests into a function

---
+++ R/process_tests_sc_demographics.R
@@ -0,0 +1,18 @@
+#' Process sc demographics tests
+#'
+#' @description This script takes the processed demographic extract and produces
+#' a test comparison with the previous data. This is written to disk as a csv.
+#'
+#' @return a csv document containing tests
+#' @export
+#'
+process_tests_sc_demographics <- function() {
+
+  comparison <- produce_test_comparison(
+    old_data = produce_sc_demog_lookup_tests(readr::read_rds(get_sc_demog_lookup_path(update = previous_update()))),
+    new_data = produce_sc_demog_lookup_tests(readr::read_rds(get_sc_demog_lookup_path()))
+  ) %>%
+    write_tests_xlsx(sheet_name = "sc_demographics")
+
+  return(comparison)
+}
