19e1c2c6bd48008cda333a20bb151540ee9a9d0d
Jennifer Thom <jennifer.thom@phs.scot>
2022-12-13 15:37:28 +0000

    Convert CH tests into a function

---
+++ R/process_tests_care_home.R
@@ -0,0 +1,22 @@
+#' Process Care Home tests
+#'
+#' @description This script takes the processed Care home extract and produces
+#' a test comparison with the previous data. This is written to disk as a csv.
+#'
+#' @param data The processed data extract
+#' @param year year of extract
+#'
+#' @return a csv document containing tests for extracts
+#' @export
+#'
+process_tests_care_home <- function(data, year) {
+  old_data <- get_existing_data_for_tests(data)
+
+  comparison <- produce_test_comparison(
+    old_data = produce_source_ch_tests(old_data),
+    new_data = produce_source_ch_tests(data)
+  ) %>%
+    write_tests_xlsx(sheet_name = "CH", year)
+
+  return(comparison)
+}
