ceae11f2147c215d38b6f26fad0eaaa6892ab4a9
Jennifer Thom <jennifer.thom@phs.scot>
2022-12-14 13:43:10 +0000

    Convert sds tests script into a function

---
+++ R/process_tests_sds.R
@@ -0,0 +1,22 @@
+#' Process SDS tests
+#'
+#' @description This script takes the processed SDS extract and produces
+#' a test comparison with the previous data. This is written to disk as a csv.
+#'
+#' @param data The processed data extract
+#' @param year year of extract
+#'
+#' @return a csv document containing tests for extracts
+#' @export
+#'
+process_tests_sds <- function(data, year) {
+  old_data <- get_existing_data_for_tests(data)
+
+  comparison <- produce_test_comparison(
+    old_data = produce_source_sds_tests(old_data),
+    new_data = produce_source_sds_tests(data)
+  ) %>%
+    write_tests_xlsx(sheet_name = "SDS", year)
+
+  return(comparison)
+}
