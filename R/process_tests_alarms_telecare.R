2a55247f7582d5e67c1aeb34b75c040ee19d0d33
Jennifer Thom <jennifer.thom@phs.scot>
2022-12-14 11:26:30 +0000

    Convert A-T tests into a function

---
+++ R/process_tests_alarms_telecare.R
@@ -0,0 +1,22 @@
+#' Process Alarms Telecare tests
+#'
+#' @description This script takes the processed Alarms Telecare extract and produces
+#' a test comparison with the previous data. This is written to disk as a csv.
+#'
+#' @param data The processed data extract
+#' @param year year of extract
+#'
+#' @return a csv document containing tests for extracts
+#' @export
+#'
+process_tests_alarms_telecare <- function(data, year) {
+  old_data <- get_existing_data_for_tests(data)
+
+  comparison <- produce_test_comparison(
+    old_data = produce_source_at_tests(old_data),
+    new_data = produce_source_at_tests(data)
+  ) %>%
+    write_tests_xlsx(sheet_name = "AT", year)
+
+  return(comparison)
+}
