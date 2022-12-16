f9ee62e2cc7638a9b07be2514d04e3440a7ca8af
James McMahon <james.mcmahon@phs.scot>
2022-12-16 18:19:14 +0000

    Reduce line lengths

--- R/process_tests_sc_ch_episodes.R
+++ R/process_tests_sc_ch_episodes.R
@@ -8,8 +8,12 @@
 #'
 process_tests_sc_ch_episodes <- function() {
   comparison <- produce_test_comparison(
-    old_data = produce_sc_ch_episodes_tests(readr::read_rds(get_sc_ch_episodes_path(update = previous_update()))),
-    new_data = produce_sc_ch_episodes_tests(readr::read_rds(get_sc_ch_episodes_path()))
+    old_data = produce_sc_ch_episodes_tests(
+    readr::read_rds(get_sc_ch_episodes_path(update = previous_update()))
+    ),
+    new_data = produce_sc_ch_episodes_tests(
+    readr::read_rds(get_sc_ch_episodes_path())
+    )
   )
 
   comparison %>%
