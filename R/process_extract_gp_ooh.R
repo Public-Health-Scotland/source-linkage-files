db13b62bc84ee5772a023037ed28f35332f9dac0
Jennifer Thom <jennifer.thom@phs.scot>
2023-01-10 13:09:00 +0000

    Use .data$

--- R/process_extract_gp_ooh.R
+++ R/process_extract_gp_ooh.R
@@ -53,7 +53,7 @@ process_extract_gp_ooh <- function(year, data_list, write_to_disk = TRUE) {
       dplyr::rename(
         cost_total_net = "cost_per_consultation"
       ) %>%
-      create_day_episode_costs(record_keydate1, cost_total_net)
+      create_day_episode_costs(.data$record_keydate1, .data$cost_total_net)
 
     rm(matched_data, ooh_cost_lookup)
 
@@ -62,9 +62,9 @@ process_extract_gp_ooh <- function(year, data_list, write_to_disk = TRUE) {
     ooh_clean <- ooh_costs %>%
       dplyr::mutate(
         # Replace location unknown with NA
-        location = dplyr::na_if(location, "UNKNOWN"),
+        location = dplyr::na_if(.data$location, "UNKNOWN"),
         recid = "OoH",
-        smrtype = add_smr_type(recid, consultation_type = consultation_type),
+        smrtype = add_smr_type(.data$recid, consultation_type = .data$consultation_type),
         kis_accessed = factor(
           dplyr::case_when(
             kis_accessed == "Y" ~ 1L,
@@ -74,22 +74,22 @@ process_extract_gp_ooh <- function(year, data_list, write_to_disk = TRUE) {
           levels = c(0L, 1L, 9L),
           labels = c("Y", "N", "Unknown")
         ),
-        gpprac = convert_eng_gpprac_to_dummy(gpprac),
+        gpprac = convert_eng_gpprac_to_dummy(.data$gpprac),
         # Split the time from the date
-        key_time1 = hms::as_hms(record_keydate1),
-        key_time2 = hms::as_hms(record_keydate2),
-        record_keydate1 = trunc(record_keydate1, "days"),
-        record_keydate2 = trunc(record_keydate2, "days")
+        key_time1 = hms::as_hms(.data$record_keydate1),
+        key_time2 = hms::as_hms(.data$record_keydate2),
+        record_keydate1 = trunc(.data$record_keydate1, "days"),
+        record_keydate2 = trunc(.data$record_keydate2, "days")
       ) %>%
       dplyr::mutate(
-        record_keydate1 = as.Date(record_keydate1),
-        record_keydate2 = as.Date(record_keydate2)
+        record_keydate1 = as.Date(.data$record_keydate1),
+        record_keydate2 = as.Date(.data$record_keydate2)
       )
 
     # Keep the location descriptions as a lookup.
     # TODO write the GP OoH lookup out using some functions
     location_lookup <- ooh_clean %>%
-      dplyr::group_by(location) %>%
+      dplyr::group_by(.data$location) %>%
       dplyr::summarise(
         location_description = dplyr::first(location_description)
       ) %>%
