b78583c486d4faa56b4566c1405ce5b23df230ca
Jennifer Thom <jennifer.thom@phs.scot>
2023-01-27 10:27:15 +0000

    Deal with year specific extracts - return null

--- R/read_extract_cmh.R
+++ R/read_extract_cmh.R
@@ -5,48 +5,54 @@
 #' @export
 #'
 read_extract_cmh <- function(year, file_path = get_boxi_extract_path(year = year, type = "CMH")) {
-  # Read BOXI extract
-  extract_cmh <- readr::read_csv(file_path,
-    col_types = cols_only(
-      "UPI Number [C]" = col_character(),
-      "Patient DoB Date [C]" = col_date(format = "%Y/%m/%d %T"),
-      "Gender" = col_double(),
-      "Patient Postcode [C]" = col_character(),
-      "NHS Board of Residence Code 9" = col_character(),
-      "Patient HSCP Code - current" = col_character(),
-      "Practice Code" = col_character(),
-      "Treatment NHS Board Code 9" = col_character(),
-      "Contact Date" = col_date(format = "%Y/%m/%d %T"),
-      "Contact Start Time" = col_time(format = "%T"),
-      "Duration of Contact" = col_integer(),
-      "Location of Contact" = col_character(),
-      "Main Aim of Contact" = col_character(),
-      "Other Aim of Contact (1)" = col_character(),
-      "Other Aim of Contact (2)" = col_character(),
-      "Other Aim of Contact (3)" = col_character(),
-      "Other Aim of Contact (4)" = col_character()
-    )
-  ) %>%
-    # rename
-    dplyr::rename(
-      chi = "UPI Number [C]",
-      dob = "Patient DoB Date [C]",
-      gender = "Gender",
-      postcode = "Patient Postcode [C]",
-      hbrescode = "NHS Board of Residence Code 9",
-      hscp = "Patient HSCP Code - current",
-      gpprac = "Practice Code",
-      hbtreatcode = "Treatment NHS Board Code 9",
-      record_keydate1 = "Contact Date",
-      keyTime1 = "Contact Start Time",
-      duration = "Duration of Contact",
-      location = "Location of Contact",
-      diag1 = "Main Aim of Contact",
-      diag2 = "Other Aim of Contact (1)",
-      diag3 = "Other Aim of Contact (2)",
-      diag4 = "Other Aim of Contact (3)",
-      diag5 = "Other Aim of Contact (4)"
-    )
 
+  # Specify years available for running
+  if (year >= 1617 & year <= 2021) {
+
+    # Read BOXI extract
+    extract_cmh <- readr::read_csv(file_path,
+      col_types = cols_only(
+        "UPI Number [C]" = col_character(),
+        "Patient DoB Date [C]" = col_date(format = "%Y/%m/%d %T"),
+        "Gender" = col_double(),
+        "Patient Postcode [C]" = col_character(),
+        "NHS Board of Residence Code 9" = col_character(),
+        "Patient HSCP Code - current" = col_character(),
+        "Practice Code" = col_character(),
+        "Treatment NHS Board Code 9" = col_character(),
+        "Contact Date" = col_date(format = "%Y/%m/%d %T"),
+        "Contact Start Time" = col_time(format = "%T"),
+        "Duration of Contact" = col_integer(),
+        "Location of Contact" = col_character(),
+        "Main Aim of Contact" = col_character(),
+        "Other Aim of Contact (1)" = col_character(),
+        "Other Aim of Contact (2)" = col_character(),
+        "Other Aim of Contact (3)" = col_character(),
+        "Other Aim of Contact (4)" = col_character()
+      )
+    ) %>%
+      # rename
+      dplyr::rename(
+        chi = "UPI Number [C]",
+        dob = "Patient DoB Date [C]",
+        gender = "Gender",
+        postcode = "Patient Postcode [C]",
+        hbrescode = "NHS Board of Residence Code 9",
+        hscp = "Patient HSCP Code - current",
+        gpprac = "Practice Code",
+        hbtreatcode = "Treatment NHS Board Code 9",
+        record_keydate1 = "Contact Date",
+        keyTime1 = "Contact Start Time",
+        duration = "Duration of Contact",
+        location = "Location of Contact",
+        diag1 = "Main Aim of Contact",
+        diag2 = "Other Aim of Contact (1)",
+        diag3 = "Other Aim of Contact (2)",
+        diag4 = "Other Aim of Contact (3)",
+        diag5 = "Other Aim of Contact (4)"
+      )
+  } else {
+    extract_cmh <- NULL
+  }
   return(extract_cmh)
 }
