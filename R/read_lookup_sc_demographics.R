ff9cde8fd6e015fd87dc12d7a2edfc5bfce26ea9
Jennifer Thom <jennifer.thom@phs.scot>
2023-01-30 16:11:33 +0000

    Update `read_lookup_sc_demographics`

--- R/read_lookup_sc_demographics.R
+++ R/read_lookup_sc_demographics.R
@@ -1,21 +1,21 @@
 #' Read SC demographics
 #'
-#' @param file_path Path to sc platform.
+#' @param sc_connection Connection to the sc platform
 #'
 #' @return a [tibble][tibble::tibble-package]
 #' @export
 #'
-read_lookup_sc_demographics <- function(file_path = dplyr::tbl(
-                                          phs_db_connection(dsn = "DVPROD"),
-                                          dbplyr::in_schema(
-                                            "social_care_2",
-                                            "demographic_snapshot"
-                                          )
-                                        )) {
+read_lookup_sc_demographics <- function(sc_connection = phs_db_connection(dsn = "DVPROD")) {
   # Read in data---------------------------------------
 
   # read in data - social care 2 demographic
-  sc_demog <- file_path %>%
+  sc_demog <- dplyr::tbl(
+    sc_connection,
+    dbplyr::in_schema(
+      "social_care_2",
+      "demographic_snapshot"
+    )
+  ) %>%
     dplyr::select(
       "latest_record_flag", "extract_date", "sending_location", "social_care_id", "upi",
       "chi_upi", "submitted_postcode", "chi_postcode", "submitted_date_of_birth",
