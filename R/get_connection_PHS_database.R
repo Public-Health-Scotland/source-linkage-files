1cd39cbe39a76e9d703189a63ef5c00f181dbed2
Jennifer Thom <jennifer.thom@phs.scot>
2023-01-30 15:16:49 +0000

    Style code

--- R/get_connection_PHS_database.R
+++ R/get_connection_PHS_database.R
@@ -17,12 +17,42 @@ phs_db_connection <- function(dsn, username = Sys.getenv("USER")) {
 
   # Check the username is not empty and take input if not
   if (is.na(username) | username == "") {
-    username <- rstudioapi::showPrompt(
-      title = "Username",
-      message = "Username",
-      default = ""
-    )
+    if (interactive()) {
+      username <- rstudioapi::showPrompt(
+        title = "Username",
+        message = "Username",
+        default = ""
+      )
+    } else {
+      cli::cli_abort("no username supplied")
+    }
   }
+
+  # TODO improve error messages and provide instructions for setting up keyring
+  # Add the following code to R profile.
+  # Sys.setenv("CREATESLF_KEYRING_PASS" = "createslf"),
+  # keyring_create("createslf", password = Sys.getenv("CREATESLF_KEYRING_PASS")),
+  # key_set(keyring = "createslf", service = "db_password")
+
+  if (!("createslf" %in% keyring::keyring_list()[["keyring"]])) {
+    cli::cli_abort("The keyring for createslf does not exist. Please set this up by updating your R profile")
+  }
+
+  if (!("db_password" %in% keyring::key_list(keyring = "createslf")[["service"]])) {
+    cli::cli_abort("The keyring password has not been supplied. Please set this up in your R profile")
+  }
+
+  if (Sys.getenv("CREATESLF_KEYRING_PASS") == "") {
+    cli::cli_abort("createslf keyring pw needs to be set in the environment variable
+                   {.env CREATESLF_KEYRING_PASS}")
+  }
+
+  keyring::keyring_unlock(keyring = "createslf", password = Sys.getenv("CREATESLF_KEYRING_PASS"))
+
+  if (keyring::keyring_is_locked(keyring = "createslf")) {
+    cli::cli_abort("Keyring is locked. To unlock createslf keyring, please use {.fun keyring_unlock}")
+  }
+
 
   # Create the connection
   password_text <- glue::glue("{dsn} password for user: {username}")
@@ -30,8 +60,10 @@ phs_db_connection <- function(dsn, username = Sys.getenv("USER")) {
     odbc::odbc(),
     dsn = dsn,
     uid = username,
-    pwd = rstudioapi::askForPassword(password_text)
+    pwd = keyring::key_get(keyring = "createslf", service = "db_password")
   )
+
+  keyring::keyring_lock(keyring = "createslf")
 
   return(db_connection)
 }
