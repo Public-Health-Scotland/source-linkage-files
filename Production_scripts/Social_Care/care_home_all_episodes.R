4e89ad6f3404b18a757aa4cce9c77c6502a51e6e
James McMahon <james.mcmahon@phs.scot>
2023-02-08 15:46:22 +0000

    More changes to make ready for being a function

--- Production_scripts/Social_Care/care_home_all_episodes.R
+++ Production_scripts/Social_Care/care_home_all_episodes.R
@@ -66,34 +66,30 @@ ch_clean <- ch_data %>%
       lubridate::NA_Date_,
       .data[["ch_discharge_date"]]
     )
+  ) %>%
+  dplyr::left_join(readr::read_rds(sc_demog_lookup_path),
+    by = c("sending_location", "social_care_id")
   )
 
-# Read in demographic data
-sc_demog <- readr::read_rds(sc_demog_lookup_path)
 
-matched_ch_data <- ch_clean %>%
-  dplyr::left_join(sc_demog, by = c("sending_location", "social_care_id"))
-
-rm(ch_data, ch_clean, sc_demog)
-
-name_postcode_clean <- fill_ch_names(matched_ch_data)
+name_postcode_clean <- fill_ch_names(ch_clean)
 
 ch_data_clean <- name_postcode_clean %>%
   # sort data
   dplyr::arrange(
-    sending_location,
-    social_care_id,
-    ch_admission_date,
-    period
+    "sending_location",
+    "social_care_id",
+    "ch_admission_date",
+    "period"
   ) %>%
   dplyr::mutate(
-    min_ch_provider = min(ch_provider),
-    max_ch_provider = max(ch_provider)
+    min_ch_provider = min(.data[["ch_provider"]]),
+    max_ch_provider = max(.data[["ch_provider"]])
   ) %>%
   dplyr::mutate(ch_provider = dplyr::if_else(
-    min_ch_provider != max_ch_provider,
+    .data[["min_ch_provider"]] != .data[["max_ch_provider"]],
     6L,
-    ch_provider
+    .data[["ch_provider"]]
   )) %>%
   dplyr::select(
     -"min_ch_provider",
@@ -101,39 +97,50 @@ ch_data_clean <- name_postcode_clean %>%
   ) %>%
   # when multiple social_care_id from sending_location for
   # single CHI replace social_care_id with latest
-  dplyr::group_by(sending_location, chi) %>%
-  dplyr::mutate(latest_sc_id = dplyr::last(social_care_id)) %>%
+  dplyr::group_by(.data[["sending_location"]], .data[["chi"]]) %>%
+  dplyr::mutate(latest_sc_id = dplyr::last(.data[["social_care_id"]])) %>%
   # count changed social_care_id
   dplyr::mutate(
-    changed_sc_id = !is.na(chi) & social_care_id != latest_sc_id,
-    social_care_id = dplyr::if_else(changed_sc_id, latest_sc_id, social_care_id)
+    changed_sc_id = !is.na(.data[["chi"]]) &
+      (.data[["social_care_id"]] != .data[["latest_sc_id"]]),
+    social_care_id = dplyr::if_else(
+      .data[["changed_sc_id"]],
+      .data[["latest_sc_id"]],
+      .data[["social_care_id"]]
+    )
   ) %>%
   dplyr::ungroup() %>%
-  dplyr::group_by(sending_location, social_care_id, ch_admission_date) %>%
+  dplyr::group_by(
+    .data[["sending_location"]],
+    .data[["social_care_id"]],
+    .data[["ch_admission_date"]]
+  ) %>%
   # fill in nursing care provision when missing
   # but present in the following entry
   dplyr::mutate(
-    nursing_care_provision = dplyr::na_if(nursing_care_provision, 9L)
+    nursing_care_provision = dplyr::na_if(.data[["nursing_care_provision"]], 9L)
   ) %>%
-  tidyr::fill(nursing_care_provision, .direction = "downup") %>%
+  tidyr::fill(.data[["nursing_care_provision"]], .direction = "downup") %>%
   # tidy up ch_provider using 6 when disagreeing values
-  tidyr::fill(ch_provider, .direction = "downup") %>%
+  tidyr::fill(.data[["ch_provider"]], .direction = "downup") %>%
   # remove any duplicate records before merging for speed and simplicity
   dplyr::distinct() %>%
   # counter for split episodes
   dplyr::mutate(
     split_episode = tidyr::replace_na(
-      nursing_care_provision != lag(nursing_care_provision),
+      .data[["nursing_care_provision"]] != dplyr::lag(
+        .data[["nursing_care_provision"]]
+      ),
       TRUE
     ),
-    split_episode_counter = cumsum(split_episode)
+    split_episode_counter = cumsum(.data[["split_episode"]])
   ) %>%
   dplyr::ungroup()
 
 
 # count changed social_care_id
 ch_data_clean %>%
-  dplyr::count(changed_sc_id)
+  dplyr::count(.data[["changed_sc_id"]])
 
 # Merge records to a single row per episode
 # where admission is the same
@@ -142,12 +149,12 @@ ch_episode <- ch_data_clean %>%
   # records within the episode, split the episode
   # at this point.
   dplyr::group_by(
-    chi,
-    sending_location,
-    social_care_id,
-    ch_admission_date,
-    nursing_care_provision,
-    split_episode_counter
+    .data[["chi"]],
+    .data[["sending_location"]],
+    .data[["social_care_id"]],
+    .data[["ch_admission_date"]],
+    .data[["nursing_care_provision"]],
+    .data[["split_episode_counter"]]
   ) %>%
   dplyr::summarise(
     sc_latest_submission = dplyr::last("period"),
@@ -169,60 +176,70 @@ ch_episode <- ch_data_clean %>%
   # Amend dates for split episodes
   # Change the start and end date as appropriate when an episode is split,
   # using the start / end date of the submission quarter
-  dplyr::group_by(chi, sending_location, social_care_id, ch_admission_date) %>%
+  dplyr::group_by(
+    .data[["chi"]],
+    .data[["sending_location"]],
+    .data[["social_care_id"]],
+    .data[["ch_admission_date"]]
+  ) %>%
   # counter for latest submission
   # TODO check if this is the same as split_episode_counter?
   dplyr::mutate(
     latest_submission_counter = tidyr::replace_na(
-      sc_latest_submission != dplyr::lag(sc_latest_submission),
+      .data[["sc_latest_submission"]] != dplyr::lag(
+        .data[["sc_latest_submission"]]
+      ),
       TRUE
     ),
-    sum_latest_submission = cumsum(latest_submission_counter)
+    sum_latest_submission = cumsum(.data[["latest_submission_counter"]])
   ) %>%
   # TODO double check this works
   dplyr::mutate(
     # If it's the first episode(s) then keep the admission date(s),
     # otherwise use the start of the quarter
     ch_admission_date = dplyr::if_else(
-      sum_latest_submission == min(sum_latest_submission),
-      ch_admission_date,
-      qtr_start
+      .data[["sum_latest_submission"]] == min(.data[["sum_latest_submission"]]),
+      .data[["ch_admission_date"]],
+      .data[["qtr_start"]]
     ),
     # If it's the last episode(s) then keep the discharge date(s), otherwise
     # use the end of the quarter
     ch_discharge_date = dplyr::if_else(
-      sum_latest_submission == max(sum_latest_submission),
-      ch_discharge_date,
-      record_date
+      .data[["sum_latest_submission"]] == max(.data[["sum_latest_submission"]]),
+      .data[["ch_discharge_date"]],
+      .data[["record_date"]]
     )
   ) %>%
   dplyr::ungroup()
 
-
 # Compare to Deaths Data
-deaths_data <- readr::read_rds(slf_deaths_path)
-
 # match ch_episode data with deaths data
 matched_deaths_data <- ch_episode %>%
-  dplyr::left_join(deaths_data, by = "chi") %>%
+  dplyr::left_join(
+    readr::read_rds(slf_deaths_path),
+    by = "chi"
+  ) %>%
   # compare discharge date with NRS and CHI death date
   # if either of the dates are 5 or fewer days before discharge
   # adjust the discharge date to the date of death
   # corrects most cases of ‘discharge after death’
   dplyr::mutate(dis_after_death = tidyr::replace_na(
-    death_date > (ch_discharge_date - lubridate::days(5L)) &
-      death_date < ch_discharge_date,
+    .data[["death_date"]] > (.data[["ch_discharge_date"]] - lubridate::days(5L)) &
+      .data[["death_date"]] < .data[["ch_discharge_date"]],
     FALSE
   )) %>%
-  dplyr::mutate(ch_discharge_date = dplyr::if_else(dis_after_death,
-    death_date,
-    ch_discharge_date
+  dplyr::mutate(ch_discharge_date = dplyr::if_else(.data[["dis_after_death"]],
+    .data[["death_date"]],
+    .data[["ch_discharge_date"]]
   )) %>%
   dplyr::ungroup() %>%
   # remove any episodes where discharge is now before admission,
   # i.e. death was before admission
   dplyr::filter(
-    !tidyr::replace_na(ch_discharge_date < ch_admission_date, FALSE)
+    !tidyr::replace_na(
+      .data[["ch_discharge_date"]] < .data[["ch_admission_date"]],
+      FALSE
+    )
   )
 
 
@@ -233,13 +250,15 @@ matched_deaths_data <- ch_episode %>%
 
 ch_markers <- matched_deaths_data %>%
   # ch_chi_cis
-  dplyr::group_by(chi) %>%
+  dplyr::group_by(.data[["chi"]]) %>%
   dplyr::mutate(
     continuous_stay_chi = tidyr::replace_na(
-      ch_admission_date <= dplyr::lag(ch_discharge_date) + lubridate::days(1L),
+      .data[["ch_admission_date"]] <= dplyr::lag(
+        .data[["ch_discharge_date"]]
+      ) + lubridate::days(1L),
       TRUE
     ),
-    ch_chi_cis = cumsum(continuous_stay_chi)
+    ch_chi_cis = cumsum(.data[["continuous_stay_chi"]])
   ) %>%
   dplyr::ungroup() %>%
   # ch_sc_id_cis
@@ -249,10 +268,12 @@ ch_markers <- matched_deaths_data %>%
   dplyr::group_by(social_care_id, sending_location) %>%
   dplyr::mutate(
     continuous_stay_sc = tidyr::replace_na(
-      ch_admission_date <= dplyr::lag(ch_discharge_date) + lubridate::days(1L),
+      .data[["ch_admission_date"]] <= dplyr::lag(
+        .data[["ch_discharge_date"]]
+      ) + lubridate::days(1L),
       TRUE
     ),
-    ch_sc_id_cis = cumsum(continuous_stay_sc)
+    ch_sc_id_cis = cumsum(.data[["continuous_stay_sc"]])
   ) %>%
   dplyr::ungroup()
 
