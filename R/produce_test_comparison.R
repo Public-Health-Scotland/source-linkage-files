5ad540b0fc23c31fde9ecbb2df8ea4592e39eccc
Jennifer Thom <jennifer.thom@phs.scot>
2023-03-01 10:41:41 +0000

    Update `produce_test_comparison` for ep tests

--- R/produce_test_comparison.R
+++ R/produce_test_comparison.R
@@ -5,6 +5,7 @@
 #'
 #' @param old_data dataframe containing the old data test flags
 #' @param new_data dataframe containing the new file data test flags
+#' @param recid Logical True/False. Use True when comparing the ep file.
 #'
 #' @return a dataframe with a comparison of new and old data
 #'
@@ -12,15 +13,27 @@
 #'
 #' @family test functions
 #' @seealso write_tests_xlsx
-produce_test_comparison <- function(old_data, new_data) {
-  dplyr::full_join(old_data,
-    new_data,
-    by = "measure",
-    suffix = c("_old", "_new")
-  ) %>%
+produce_test_comparison <- function(old_data, new_data, recid = FALSE) {
+  if (recid) {
+    join <- dplyr::full_join(old_data,
+                             new_data,
+                             by = c("recid", "measure"),
+                             suffix = c("_old", "_new")
+    )
+  } else {
+    join <- dplyr::full_join(old_data,
+                             new_data,
+                             by = "measure",
+                             suffix = c("_old", "_new")
+    )
+  }
+
+  comparison <- join %>%
     dplyr::mutate(
       difference = round(.data$value_new - .data$value_old, digits = 2L),
       pct_change = scales::percent(.data$difference / .data$value_old),
       issue = !dplyr::between(.data$difference / .data$value_old, -0.05, 0.05)
     )
+
+  return(comparison)
 }
