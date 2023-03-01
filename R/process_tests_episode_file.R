4554d06d738fb644048b40a21d703c8a46bc0e75
Jennifer Thom <jennifer.thom@phs.scot>
2023-03-01 14:07:05 +0000

    include recid = TRUE

--- R/process_tests_episode_file.R
+++ R/process_tests_episode_file.R
@@ -14,8 +14,10 @@ process_tests_episode_file <- function(data, year) {
 
   comparison <- produce_test_comparison(
     old_data = produce_episode_file_tests(old_data),
-    new_data = produce_episode_file_tests(data)
+    new_data = produce_episode_file_tests(data),
+    recid = TRUE
   ) %>%
+    dplyr::arrange(recid) %>%
     write_tests_xlsx(sheet_name = "ep_file", year)
 
   return(comparison)
