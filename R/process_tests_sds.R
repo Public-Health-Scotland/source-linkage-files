3e553a7132bc2cd84d8184faf2fba74a628196c3
James McMahon <james.mcmahon@phs.scot>
2023-03-13 16:20:17 +0000

    Refactor SDS tests

--- R/process_tests_sds.R
+++ R/process_tests_sds.R
@@ -17,3 +17,34 @@ process_tests_sds <- function(data, year) {
 
   return(comparison)
 }
+
+#' SDS Episodes Tests
+#'
+#' @description Produce the test for the SDS all episodes
+#'
+#' @param data new or old data for testing summary flags
+#' (data is from [get_sc_sds_episodes_path()])
+#' @param max_min_vars variables used when selecting 'min-max'
+#' from [calculate_measures()]
+#' @return a dataframe with a count of each flag.
+#'
+#' @family social care test functions
+produce_source_sds_tests <- function(data,
+                                     max_min_vars = c("record_keydate1", "record_keydate2")) {
+  test_flags <- data %>%
+    # create test flags
+    create_demog_test_flags() %>%
+    create_lca_test_flags(.data$sc_send_lca) %>%
+    # remove variables that won't be summed
+    dplyr::select("valid_chi":"West_Lothian") %>%
+    # use function to sum new test flags
+    calculate_measures(measure = "sum")
+
+  min_max_measures <- data %>%
+    calculate_measures(vars = max_min_vars, measure = "min-max")
+
+  join_output <- list(test_flags, min_max_measures) %>%
+    purrr::reduce(dplyr::full_join, by = c("measure", "value"))
+
+  return(join_output)
+}
