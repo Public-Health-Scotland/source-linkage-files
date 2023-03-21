7263c77cb22f9a0c50a3aa160ecd83e867207ce6
James McMahon <james.mcmahon@phs.scot>
2023-03-21 17:54:51 +0000

    Draft a function to gzip compress files

---
+++ R/gzip_files.R
@@ -0,0 +1,27 @@
+gzip_files <- function(
+    extract_year = NULL,
+    path = get_year_dir(extract_year, extracts_dir = TRUE)
+) {
+  unzipped_files <- fs::dir_ls(
+    path = path,
+    regexp = "\\.csv$",
+    type = "file",
+    recurse = TRUE
+  )
+
+  cli::cli_inform(c(
+    "i" = "There {?is/are} {length(unzipped_files)} uncompressed
+                   file{?s}, which will be compressed with gzip.",
+    ">" = "{unzipped_files}"
+  ))
+
+  purrr::walk(
+    unzipped_files,
+    function(file_path) {
+      system2(
+        command = "gzip",
+        args = shQuote(file_path)
+      )
+    }
+  )
+}
