c2da6318cc4eb3a7f856fb34144a1726ec391bb0
Zihao Li <lizihao_anu@outlook.com>
2023-03-29 11:18:38 +0100

    Revert "Update R/create_monthly_costs.R"
    This reverts commit bbb05c1c08945c92a0396751bcfa87224d293cf7.

--- R/create_monthly_costs.R
+++ R/create_monthly_costs.R
@@ -41,9 +41,8 @@ create_monthly_costs <- function(data,
       daycase_added = dplyr::if_else(.data$daycase_added, 1, 0)
     ) %>%
     tidyr::pivot_wider(
-      names_from = "whichmonth",
-      names_glue = "{whichmonth}_cost",
-      values_from = "daycase_added",
+      names_from = .data$whichmonth,
+      values_from = .data$daycase_added,
       values_fill = 0
     ) %>%
     dplyr::select(
