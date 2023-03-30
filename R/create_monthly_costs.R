b113be638fff5eeef6849e37fc1d4f4359ac0c22
Zihao Li <zihao.li@phs.scot>
2023-03-30 11:22:11 +0100

    Fix costs 20230328 (#600)
    * fix calculate_measures.R
    
    * fix create_monthly_costs.R for maternity costs
    
    * Shorten some long lines
    
    * Add `.data$` to fix build notes
    
    * use case_when in create_monthly_costs
    
    * minor changes to create_monthly_costs
    
    * Add `daycase` as an expected word
    
    * Fix some issues with deprecated features
    
    * Rename `whichmonth` -> `cost_month`
    
    * Use `check_variables_exist`
    
    * Tidy up `select` statements
    
    * Add todo for tests
    
    ---------
    
    Co-authored-by: lizihao-anu <lizihao-anu@users.noreply.github.com>
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>
    Co-authored-by: Moohan <Moohan@users.noreply.github.com>

--- R/create_monthly_costs.R
+++ R/create_monthly_costs.R
@@ -1,6 +1,7 @@
 #' Create Monthly Costs
 #'
-#' @description Assign monthly costs using a cost variable and vector containing monthly beddays
+#' @description Assign monthly costs using a cost variable
+#' and vector containing monthly beddays.
 #'
 #' @param data Data containing bedday variables,
 #' see [create_monthly_beddays] to create
@@ -16,19 +17,64 @@
 create_monthly_costs <- function(data,
                                  yearstay = yearstay,
                                  cost_total_net = cost_total_net) {
+  check_variables_exist(data, c(
+    "record_keydate1",
+    "record_keydate2",
+    paste0(tolower(month.abb[c(4:12, 1:3)]), "_beddays")
+  ))
+
   costs <- data %>%
     dplyr::select(dplyr::ends_with("_beddays")) %>%
     dplyr::rename_with(~ stringr::str_replace(., "_beddays", "_cost"))
+  # Fix the instances where the episode is a daycase;
+  # these will sometimes have 0.33 for the yearstay,
+  # this should be applied to the relevant month.
+  costs_daycase <- data %>%
+    dplyr::select(!dplyr::ends_with("_beddays")) %>%
+    dplyr::mutate(
+      daycase_added = (.data$record_keydate1 == .data$record_keydate2)
+    ) %>%
+    dplyr::mutate(daycase_check = .data$daycase_added) %>%
+    dplyr::mutate(cost_month = dplyr::if_else(
+      .data$daycase_added,
+      lubridate::month(.data$record_keydate1),
+      NA
+    )) %>%
+    dplyr::mutate(
+      cost_month = month.abb[.data$cost_month] %>%
+        tolower() %>%
+        paste0("_cost"),
+      daycase_added = dplyr::if_else(.data$daycase_added, 1, 0)
+    ) %>%
+    tidyr::pivot_wider(
+      names_from = "cost_month",
+      values_from = "daycase_added",
+      values_fill = 0
+    ) %>%
+    dplyr::select(
+      month.abb[c(4:12, 1:3)] %>%
+        tolower() %>%
+        paste0("_cost"),
+      "daycase_check"
+    )
+
+  costs <- (costs_daycase[1:12] + costs) %>%
+    dplyr::bind_cols(daycase_check = costs_daycase$daycase_check)
 
   data <- dplyr::bind_cols(data, costs) %>%
     dplyr::mutate(dplyr::across(
       dplyr::ends_with("_cost"),
-      ~ dplyr::if_else(
-        .x != 0.0,
-        .x / {{ yearstay }} * {{ cost_total_net }},
-        0.0
+      ~ dplyr::case_when(
+        {{ cost_total_net }} == 0.0 ~ 0.0,
+        .x != 0L ~ dplyr::if_else(
+          daycase_check,
+          {{ cost_total_net }},
+          .x / yearstay * {{ cost_total_net }}
+        ),
+        .default = 0.0
       )
-    ))
+    )) %>%
+    dplyr::select(!"daycase_check")
 
   return(data)
 }
