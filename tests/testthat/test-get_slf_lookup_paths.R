c04b1cc2c14bd0703540cdd516162512df7037df
James McMahon <james.mcmahon@phs.scot>
2023-04-26 10:24:11 +0100

    Remove tests for `sav`/`zsav` files
    Also some other test improvements

--- tests/testthat/test-get_slf_lookup_paths.R
+++ tests/testthat/test-get_slf_lookup_paths.R
@@ -3,7 +3,7 @@ skip_on_ci()
 test_that("SLF postcode lookup file paths work", {
   expect_s3_class(get_slf_postcode_path(), "fs_path")
   expect_s3_class(
-    get_slf_postcode_path(update = previous_update(), ext = "zsav"),
+    get_slf_postcode_path(update = previous_update()),
     "fs_path"
   )
 })
@@ -11,7 +11,7 @@ test_that("SLF postcode lookup file paths work", {
 test_that("SLF GP practice lookup file paths work", {
   expect_s3_class(get_slf_gpprac_path(), "fs_path")
   expect_s3_class(
-    get_slf_gpprac_path(update = previous_update(), ext = "zsav"),
+    get_slf_gpprac_path(update = previous_update()),
     "fs_path"
   )
 })
@@ -19,18 +19,15 @@ test_that("SLF GP practice lookup file paths work", {
 test_that("SLF Deaths lookup path works", {
   expect_s3_class(get_slf_deaths_path(), "fs_path")
   expect_s3_class(
-    get_slf_deaths_path(previous_update(), ext = "zsav"),
+    get_slf_deaths_path(previous_update()),
     "fs_path"
   )
 })
 
 test_that("SLF Care Home names lookup path works", {
+  expect_s3_class(get_slf_ch_name_lookup_path(), "fs_path")
   expect_s3_class(
-    get_slf_ch_name_lookup_path(),
-    "fs_path"
-  )
-  expect_s3_class(
-    get_slf_ch_name_lookup_path(previous_update(), ext = "zsav"),
+    get_slf_ch_name_lookup_path(previous_update()),
     "fs_path"
   )
 })
