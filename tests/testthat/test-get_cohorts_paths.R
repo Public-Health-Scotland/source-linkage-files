0b090fa64ab7e3441fed773ffb3507d18650644a
James McMahon <james.mcmahon@phs.scot>
2023-04-26 10:25:10 +0100

    Remove tests for cohorts paths
    Using targets we might not create these files, if we do it's later in the process so the tests will be prone to breaking for no useful reason.

--- tests/testthat/test-get_cohorts_paths.R
+++
@@ -1,6 +0,0 @@
-skip_on_ci()
-
-test_that("Cohorts paths work", {
-  expect_s3_class(get_demographic_cohorts_path("1920", ext = "zsav"), "fs_path")
-  expect_s3_class(get_service_use_cohorts_path("1920", ext = "zsav"), "fs_path")
-})
