d18f061e218c08a1e73044a8af3d8f3a740995be
Zihao Li <lizihao_anu@outlook.com>
2023-04-26 16:44:37 +0100

    a function of adding delay discharge to episode data

--- R/add_dd.R
+++ R/add_dd.R
@@ -10,99 +10,173 @@
 add_dd <- function(data, year) {
   year_param <- year
 
-  data_chi <- data %>%
-    # Keep records that have a chi, and a cij_marker.
-    dplyr::filter(is.na(chi)) %>%
-    dplyr::filter(recid %in% c("01B", "02B", "04B", "GLS")) %>%
-    # create a copy of the CIJ maker
+  data = data %>%
+    dplyr::arrange(chi, cij_marker) %>%
     dplyr::mutate(
-      temp_cij_maker = cij_maker
-    ) %>%
-    dplyr::full_join(
-      # Not sure which function to use here. Will change it later
-      haven::read_sav("/conf/hscdiip/SLF_Extracts/Delayed_Discharges/Jul16_Sep22DD_LinkageFile.zsav"),
-      by = "chi"
-    ) %>%
-    # Create an order variable to make DD records appear after others.
-    # but might it be better if recid has levels?
-    dplyr::mutate(
-      order = dplyr::case_when(
-        recid %in% c("00B", "01B", "02B", "04B", "GLS") ~ 1L,
-        recid == "DD" ~ 2L,
-        TRUE ~ NA
-      )
-    ) %>%
-    # Remove any DD records which don't match a chi in the file.
-    dplyr::arrange(chi) %>%
-    dplyr::filter(!(recid == "DD" & chi != dplyr::lag(chi))) %>%
-    # sort so that DD is roughly where we expect it to fit
-    dplyr::arrange(chi, keydate1_dateformat) %>%
-    # add row number to restore the order later
-    dplyr::mutate(row_no = dplyr::row_number())
-
-  # Capture the Mental Health delays with no end dates.
-  data_chi_1 <- data_chi %>%
-    dplyr::select(
-      chi,
-      recid,
-      keydate1_dateformat,
-      keydate2_dateformat,
-      CIJ_start_date,
-      CIJ_end_date,
-      temp_cij_marker,
-      row_no
-    ) %>%
-    dplyr::filter(
-      chi == lag(chi) & recid == "DD" &
-        lag(recid) == "04B" &
-        is.na(keydate2_dateformat) &
-        is.na(lag(keydate2_dateformat)) &
-        keydate1_dateformat >= lag(CIJ_start_date) - lubridate::days(1)
-    ) %>%
-    dplyr::mutate(
-      Flag_8 = dplyr::if_else(keydate1_dateformat >= lag(CIJ_start_date), 2, 1),
-      temp_cij_marker = lag(temp_cij_marker),
-      CIJ_start_date = lag(CIJ_start_date),
-      CIJ_end_date = lag(CIJ_end_date)
+      cij_start_date_lower = cij_start_date - lubridate::days(1),
+      cij_end_date_upper = cij_end_date + lubridate::days(1)
     )
 
-  data_chi <- data_chi %>%
-    dplyr::left_join(data_chi_1, suffix = c("", "_redundancy")) %>%
-    dplyr::select(-ends_with("_redundancy"))
-  # As I imagine, this will possibly leave some NA in columns including
-  # CIJ_start_date, CIJ_end_date
-
-  # Use Min and Max CIJ dates to fill in temp_cij_marker -
-  # where possible - DD episodes with no CIJ.
-  ## difficult parts. hard to vectorize it.
-  # data_chi_1 <- data_chi %>%
-  #   dplyr::if_else(
-  #     chi == lag(chi) & is.na(temp_cij_marker),
-  #     Flag_1 = 0,
-  #
-  #   )
-
-  # ## non-vectorized version. for loop
-  # for(ii in 2:max(data_chi$row_no)){
-  #   if(chi[ii] == chi[ii - 1] & is.na(temp_cij_marker[ii])){
-  #
-  #   }
-  # }
-
-
-
-
-
-
-  # Eventually, bind non_chi back
-  data_return <- row_bind(
-    data_chi,
-    data %>%
-      dplyr::filter(is.na(chi)) %>%
-      dplyr::filter(!(recid %in% c("01B", "02B", "04B", "GLS"))),
-    data %>%
-      dplyr::filter(!is.na(chi))
+  ## handling DD ----
+  dd_data = read_file(get_source_extract_path(year_param, "DD"))
+  by_dd = dplyr::join_by(
+    chi,
+    x$keydate1_dateformat >= y$cij_start_date_lower,
+    x$keydate2_dateformat <= y$cij_end_date_upper
   )
+  data = dd_data %>%
+    dplyr::inner_join(data,
+                      by_dd,
+                      suffix = c("_dd", "")) %>%
+    dplyr::arrange(cij_start_date, cij_end_date, cij_marker, postcode) %>%
+    # remove duplicate columns
+    dplyr::distinct(
+      cij_start_date,
+      cij_end_date,
+      cij_marker,
+      keydate1_dateformat_dd,
+      keydate2_dateformat_dd,
+      .keep_all = TRUE
+    ) %>%
+    # determine DD quality
+    dplyr::mutate(dd_type = dplyr::if_else(
+      is.na(cij_marker),
+      "no-cij",
+      dplyr::case_when(
+        # "1"	"Accurate Match - (1)"
+        # "1P"	"Accurate Match (allowing +-1 day) - (1P)"
+        # "1A"	"Accurate Match (has an assumed  end date) - (1A)"
+        # "1AP"	"Accurate Match (allowing +-1 day and has an assumed end date) - (1AP)"
+        # "2"	"Starts in CIJ - (2)"
+        # "2D"	"Starts in CIJ (ends one day after) - (2D)"
+        # "2DP"	"Starts in CIJ (allowing +-1 day and ends one day after) - (2DP)"
+        # "2A"	"Starts in CIJ (Accurate Match after correcting assumed end date) - (2A)"
+        # "2AP"	"Starts in CIJ (Accurate Match (allowing +-1 day) after correcting assumed end date) - (2AP)"
+        # "3"	"Ends in CIJ - (3)"
+        # "3D"	"Ends in CIJ (starts one day before) - (3D)"
+        # "3DP"	"Ends in CIJ (allowing +-1 day and starts one day before) - (3DP)"
+        # "4"	"Matches unended MH record - (4)"
+        # "4P" "Matches unended MH record (allowing -1 day) - (4P)"
+        # "-" "No Match (We don't keep these)".
 
-  return()
+        # "1"	"Accurate Match - (1)"
+        keydate1_dateformat_dd >= cij_start_date &
+          keydate2_dateformat_dd <= cij_end_date &
+          !amended_dates ~ "1",
+
+        # "1P"	"Accurate Match (allowing +-1 day) - (1P)"
+        keydate1_dateformat_dd >= cij_start_date_lower &
+          keydate2_dateformat_dd <= cij_end_date_upper &
+          !amended_dates ~ "1P",
+
+        # "1A"	"Accurate Match (has an assumed end date) - (1A)"
+        keydate1_dateformat_dd >= cij_start_date &
+          keydate2_dateformat_dd <= cij_end_date &
+          amended_dates ~ "1P",
+
+        # "1AP"	"Accurate Match (allowing +-1 day and has an assumed end date) - (1AP)"
+        keydate1_dateformat_dd >= cij_start_date_lower &
+          keydate2_dateformat_dd <= cij_end_date_upper &
+          amended_dates ~ "1AP",
+
+        # "2"	"Starts in CIJ - (2)"
+        keydate1_dateformat_dd >= cij_start_date &
+          keydate1_dateformat_dd <= cij_end_date &
+          keydate2_dateformat_dd >= cij_end_date &
+          !amended_dates ~ "2",
+
+        # "2D"	"Starts in CIJ (ends one day after) - (2D)"
+        keydate1_dateformat_dd >= cij_start_date &
+          keydate1_dateformat_dd <= cij_end_date &
+          keydate2_dateformat_dd >= cij_end_date_upper &
+          !amended_dates ~ "2D",
+
+        # "2DP"	"Starts in CIJ (allowing +-1 day and ends one day after) - (2DP)"
+        keydate1_dateformat_dd >= cij_start_date_lower &
+          keydate1_dateformat_dd <= cij_end_date_upper &
+          keydate2_dateformat_dd >= cij_end_date_upper &
+          !amended_dates ~ "2DP",
+
+        # "2A"	"Starts in CIJ (Accurate Match after correcting assumed end date) - (2A)"
+        keydate1_dateformat_dd >= cij_start_date &
+          keydate1_dateformat_dd <= cij_end_date &
+          keydate2_dateformat_dd >= cij_end_date &
+          amended_dates ~ "2A",
+
+        # "2AP"	"Starts in CIJ (Accurate Match (allowing +-1 day) after correcting assumed end date) - (2AP)"
+        keydate1_dateformat_dd >= cij_start_date_lower &
+          keydate1_dateformat_dd <= cij_end_date_upper &
+          keydate2_dateformat_dd >= cij_end_date_upper &
+          amended_dates ~ "2AP",
+
+        # "3"	"Ends in CIJ - (3)"
+        keydate1_dateformat_dd <= cij_start_date &
+          keydate2_dateformat_dd >= cij_start_date &
+          keydate2_dateformat_dd >= cij_end_date &
+          !amended_dates ~ "3",
+
+        # "3D"	"Ends in CIJ (starts one day before) - (3D)"
+        keydate1_dateformat_dd <= cij_start_date_lower &
+          keydate2_dateformat_dd >= cij_start_date &
+          keydate2_dateformat_dd >= cij_end_date &
+          !amended_dates ~ "3D",
+
+        # "3DP"	"Ends in CIJ (allowing +-1 day and starts one day before) - (3DP)"
+        keydate1_dateformat_dd <= cij_start_date_lower &
+          keydate2_dateformat_dd >= cij_start_date_lower &
+          keydate2_dateformat_dd >= cij_end_date_upper &
+          !amended_dates ~ "3DP",
+
+        # "4"	"Matches unended MH record - (4)"
+        recid == "04B" &
+          keydate1_dateformat_dd >= cij_start_date &
+          amended_dates ~ "4",
+
+        # "4P"	"Matches unended MH record (allowing -1 day) - (4P)"
+        recid == "04B" &
+          keydate1_dateformat_dd >= cij_start_date_lower &
+          amended_dates ~ "4P",
+
+        # "-" "No Match (We don't keep these)"
+        .default = "-"
+      )
+    )) %>%
+    dplyr::filter(dd_type != "-") %>%
+    dplyr::mutate(smrtype_dd = dplyr::case_when(
+      dd_type %in% c(
+        "1",
+        "1P",
+        "1A",
+        "1AP",
+        "2",
+        "2D",
+        "2DP",
+        "2A",
+        "2AP",
+        "3",
+        "3D",
+        "3DP",
+        "4",
+        "4P"
+      ) ~ "DD-CIJ",
+      dd_type %in% c("no-cij") ~ "DD-No CIJ"
+    )) %>%
+    # tidy up and rename columns to match the format of episode files
+    dplyr::select(
+      chi,
+      recid = recid_dd,
+      keydate1_dateformat = keydate1_dateformat_dd,
+      keydate2_dateformat = keydate2_dateformat_dd,
+      smrtype = smrtype_dd,
+      cij_marker,
+      cij_start_date,
+      cij_end_date,
+      postcode = postcode_dd
+    ) %>%
+    # combind DD with episode data
+    dplyr::bind_rows(data %>% dplyr::select(-c(
+      "cij_start_date_lower", "cij_end_date_upper"
+    )))
+
+  return(data)
 }
