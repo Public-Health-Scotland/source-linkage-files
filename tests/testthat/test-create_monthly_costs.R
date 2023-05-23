19f726768e7b4d8ab740f3e8e30bfa80dbfe7a85
lizihao-anu <lizihao-anu@users.noreply.github.com>
2023-05-23 16:10:16 +0000

    Style code

--- tests/testthat/test-create_monthly_costs.R
+++ tests/testthat/test-create_monthly_costs.R
@@ -6,7 +6,7 @@ test_that("can create monthly cost for maternity", {
   # * episodes which span financial years work
   # * episodes in a single month (multiday) work
 
-  dummy_data =  dplyr::bind_cols(
+  dummy_data <- dplyr::bind_cols(
     year = rep("1718", 5),
     yearstay = c(0, 0.33, 0.33, 30, 365),
     record_keydate1 = c(
@@ -39,24 +39,26 @@ test_that("can create monthly cost for maternity", {
     mar_beddays = c(0, 0, 0, 0, 30)
   )
 
-  dummy_data_cost = dummy_data %>%
+  dummy_data_cost <- dummy_data %>%
     dplyr::mutate(
       apr_cost = c(0, 100, 0, 1000, 3000),
-      may_cost = c(0,   0, 0, 2000, 3100),
-      jun_cost = c(0,   0, 0, 0, 3000),
-      jul_cost = c(0,   0, 0, 0, 3100),
-      aug_cost = c(0,   0, 0, 0, 3100),
-      sep_cost = c(0,   0, 0, 0, 3000),
-      oct_cost = c(0,   0, 0, 0, 3100),
-      nov_cost = c(0,   0, 0, 0, 3000),
-      dec_cost = c(0,   0, 0, 0, 3100),
-      jan_cost = c(0,   0, 0, 0, 3100),
-      feb_cost = c(0,   0, 0, 0, 2800),
-      mar_cost = c(0,   0, 0, 0, 3000)
+      may_cost = c(0, 0, 0, 2000, 3100),
+      jun_cost = c(0, 0, 0, 0, 3000),
+      jul_cost = c(0, 0, 0, 0, 3100),
+      aug_cost = c(0, 0, 0, 0, 3100),
+      sep_cost = c(0, 0, 0, 0, 3000),
+      oct_cost = c(0, 0, 0, 0, 3100),
+      nov_cost = c(0, 0, 0, 0, 3000),
+      dec_cost = c(0, 0, 0, 0, 3100),
+      jan_cost = c(0, 0, 0, 0, 3100),
+      feb_cost = c(0, 0, 0, 0, 2800),
+      mar_cost = c(0, 0, 0, 0, 3000)
     )
 
-  expect_equal(create_monthly_costs(dummy_data),
-               dummy_data_cost)
+  expect_equal(
+    create_monthly_costs(dummy_data),
+    dummy_data_cost
+  )
 })
 
 test_that("can create monthly cost generally", {
@@ -66,38 +68,43 @@ test_that("can create monthly cost generally", {
   # * episodes which span financial years work
   # * episodes in a single month (multiday) work
 
-  dummy_data1 =  dplyr::bind_cols(
+  dummy_data1 <- dplyr::bind_cols(
     year = rep("1718", 4),
     yearstay = c(0, 0, 0, 30),
-    record_keydate1 = c("2017-09-01",
-                        "2017-04-01",
-                        "2017-05-01",
-                        "2017-04-21") %>% lubridate::ymd(),
-    record_keydate2 = c("2017-09-01",
-                        "2017-04-01",
-                        "2017-05-01",
-                        "2017-05-20") %>% lubridate::ymd(),
+    record_keydate1 = c(
+      "2017-09-01",
+      "2017-04-01",
+      "2017-05-01",
+      "2017-04-21"
+    ) %>% lubridate::ymd(),
+    record_keydate2 = c(
+      "2017-09-01",
+      "2017-04-01",
+      "2017-05-01",
+      "2017-05-20"
+    ) %>% lubridate::ymd(),
     cost_per_day = rep(100, 4),
     cost_total_net = c(0, 0, 0, 3000)
   ) %>% create_monthly_beddays("1718",
-                               .data$record_keydate1,
-                               .data$record_keydate2,
-                               count_last = FALSE)
+    .data$record_keydate1,
+    .data$record_keydate2,
+    count_last = FALSE
+  )
 
-  dummy_data1_cost = dummy_data1 %>%
+  dummy_data1_cost <- dummy_data1 %>%
     dplyr::mutate(
-      apr_cost = c(0,   0, 0, 1000),
-      may_cost = c(0,   0, 0, 1900),
-      jun_cost = c(0,   0, 0, 0),
-      jul_cost = c(0,   0, 0, 0),
-      aug_cost = c(0,   0, 0, 0),
-      sep_cost = c(0,   0, 0, 0),
-      oct_cost = c(0,   0, 0, 0),
-      nov_cost = c(0,   0, 0, 0),
-      dec_cost = c(0,   0, 0, 0),
-      jan_cost = c(0,   0, 0, 0),
-      feb_cost = c(0,   0, 0, 0),
-      mar_cost = c(0,   0, 0, 0)
+      apr_cost = c(0, 0, 0, 1000),
+      may_cost = c(0, 0, 0, 1900),
+      jun_cost = c(0, 0, 0, 0),
+      jul_cost = c(0, 0, 0, 0),
+      aug_cost = c(0, 0, 0, 0),
+      sep_cost = c(0, 0, 0, 0),
+      oct_cost = c(0, 0, 0, 0),
+      nov_cost = c(0, 0, 0, 0),
+      dec_cost = c(0, 0, 0, 0),
+      jan_cost = c(0, 0, 0, 0),
+      feb_cost = c(0, 0, 0, 0),
+      mar_cost = c(0, 0, 0, 0)
     )
 
   expect_equal(
