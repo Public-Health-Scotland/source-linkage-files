0e9f0acf2a87123b5dd797aa044795800b1af443
James McMahon <james.mcmahon@phs.scot>
2023-06-02 12:49:11 +0100

    Update the documentation for `last_date_month`

--- R/last_date_month.R
+++ R/last_date_month.R
@@ -1,16 +1,15 @@
 #' Return the end date of the month of the given date
 #'
-#' @description Return the end date of the month of the given date
+#' @param date a date with a date format.
 #'
-#' @param x a date with a date format
+#' @return a vector of dates, giving the last day of the month.
 #'
-#' @return a vector of dates of the end date of the FY year
 #' @export
 #'
 #' @examples
-#' last_date_month(lubridate::as_date("2020-02-05"))
+#' last_date_month(Sys.Date())
 #'
 #' @family date functions
-last_date_month <- function(x) {
-  return(lubridate::ceiling_date(x, "month") - lubridate::days(1))
+last_date_month <- function(date) {
+  return(lubridate::ceiling_date(date, "month") - lubridate::days(1))
 }
