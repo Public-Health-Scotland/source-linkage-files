66e21e61409bf2138f6766c68a924e3061f56827
lizihao-anu <lizihao-anu@users.noreply.github.com>
2023-06-13 11:06:39 +0000

    Style code

--- R/aggregate_by_chi_zihao.R
+++ R/aggregate_by_chi_zihao.R
@@ -12,8 +12,10 @@ aggregate_by_chi_zihao <- function(episode_file) {
 
   episode_file <- episode_file %>%
     dplyr::select(-c(postcode, gpprac)) %>%
-    dplyr::rename("gpprac" = "most_recent_gpprac",
-                  "postcode" = "most_recent_postcode") %>%
+    dplyr::rename(
+      "gpprac" = "most_recent_gpprac",
+      "postcode" = "most_recent_postcode"
+    ) %>%
     dplyr::select(-c(
       dplyr::ends_with("_gpprac"),
       dplyr::ends_with("_postcode"),
@@ -38,123 +40,145 @@ aggregate_by_chi_zihao <- function(episode_file) {
 
   data.table::setnames(
     episode_file,
-    c("ch_chi_cis", "cij_marker", "ooh_case_id"
+    c(
+      "ch_chi_cis", "cij_marker", "ooh_case_id"
       # ,"hh_in_fy"
-      ),
-      c("ch_cis_episodes", "cij_total", "ooh_cases"
-        # ,"hl1_in_fy"
-        )
+    ),
+    c(
+      "ch_cis_episodes", "cij_total", "ooh_cases"
+      # ,"hl1_in_fy"
+    )
+  )
+
+  # Initialize an empty data.table for the aggregated results
+  aggregated_data <- data.table::data.table()
+
+  # Process the data in chunks
+  chunk_size <- min(nrow(episode_file), 5e7)
+  # Adjust the chunk size as per your system's memory capacity
+  n_chunks <- nrow(episode_file) %/% chunk_size
+
+
+  # colums specification
+  # columns to select last
+  cols2 <- vars_end_with(
+    episode_file,
+    c("postcode", "dob", "ggprac")
+  )
+  # columns to select last unique rows
+  cols3 <- c(
+    "ch_cis_episodes",
+    "cij_total",
+    "CIJ_el",
+    "CIJ_non_el",
+    "CIJ_mat",
+    # "cij_delay",
+    "ooh_cases",
+    "preventable_admissions"
+  )
+  # columns to sum up
+  cols4 <- c(
+    vars_end_with(
+      episode_file,
+      c(
+        "episodes",
+        "beddays",
+        "cost",
+        "attendances",
+        "attend",
+        # "contacts",
+        "hours",
+        "alarms",
+        "telecare",
+        "paid_items",
+        "advice",
+        "homev",
+        "time",
+        "assessment",
+        "other",
+        # "dn",
+        "nhs24",
+        "pcc",
+        "_dnas"
       )
+    ),
+    vars_start_with(
+      episode_file,
+      "sds_option"
+    )
+  )
+  cols4 <- cols4[!(cols4 %in% c("ch_cis_episodes"))]
+  # # columns to select maximum
+  # cols5 <- vars_contain(episode_file, "nsu")
+  # columns to select first row
+  cols6 <- c(
+    condition_cols(),
+    # "death_date",
+    # "deceased",
+    "year",
+    vars_end_with(
+      episode_file,
+      c("_cohort", "end_fy", "start_fy")
+    )
+  )
 
-      # Initialize an empty data.table for the aggregated results
-      aggregated_data <- data.table::data.table()
+  for (i in 1:n_chunks) {
+    start <- (i - 1) * chunk_size + 1
+    end <- i * chunk_size
+    # Subset the data to the current chunk
+    chunk <- episode_file[start:end]
 
-      # Process the data in chunks
-      chunk_size <- min(nrow(episode_file), 5e7)
-      # Adjust the chunk size as per your system's memory capacity
-      n_chunks <- nrow(episode_file) %/% chunk_size
+    # compute
+    chunk_cols1 <- chunk[,
+      .(gender = mean(gender)),
+      by = chi
+    ]
+    chunk_cols2 <- chunk[,
+      .SD[.N],
+      .SDcols = cols2,
+      by = chi
+    ]
+    chunk_cols3 <- chunk[,
+      lapply(.SD, function(x) {
+        data.table::uniqueN(x, na.rm = TRUE)
+      }),
+      .SDcols = cols3,
+      by = chi
+    ]
+    chunk_cols4 <- chunk[,
+      lapply(.SD, function(x) {
+        sum(x, na.rm = TRUE)
+      }),
+      .SDcols = cols4,
+      by = chi
+    ]
+    # chunk_cols5 <- chunk[,
+    #                      lapply(.SD, function(x) max(x, na.rm = TRUE)),
+    #                      .SDcols = cols5,
+    #                      by = chi]
+    chunk_cols6 <- chunk[,
+      lapply(.SD, function(x) {
+        x[!is.na(x)][1]
+      }),
+      .SDcols = cols6,
+      by = chi
+    ]
+    chunk_agg <- dplyr::bind_cols(
+      chunk_cols1,
+      chunk_cols2[, chi := NULL],
+      chunk_cols3[, chi := NULL],
+      chunk_cols4[, chi := NULL],
+      # chunk_cols5[, chi := NULL],
+      chunk_cols6[, chi := NULL]
+    )
 
+    # Append the aggregated chunk to the overall result
+    aggregated_data <-
+      data.table::rbindlist(list(aggregated_data, chunk_agg))
+  }
+  aggregated_data <- dplyr::as_tibble(aggregated_data)
 
-      # colums specification
-      # columns to select last
-      cols2 <- vars_end_with(episode_file,
-                             c("postcode", "dob", "ggprac"))
-      # columns to select last unique rows
-      cols3 <- c(
-        "ch_cis_episodes",
-        "cij_total",
-        "CIJ_el",
-        "CIJ_non_el",
-        "CIJ_mat",
-        # "cij_delay",
-        "ooh_cases",
-        "preventable_admissions"
-      )
-      # columns to sum up
-      cols4 <- c(vars_end_with(
-        episode_file,
-        c(
-          "episodes",
-          "beddays",
-          "cost",
-          "attendances",
-          "attend",
-          # "contacts",
-          "hours",
-          "alarms",
-          "telecare",
-          "paid_items",
-          "advice",
-          "homev",
-          "time",
-          "assessment",
-          "other",
-          # "dn",
-          "nhs24",
-          "pcc",
-          "_dnas"
-        )
-      ),
-      vars_start_with(episode_file,
-                      "sds_option"))
-      cols4 <- cols4[!(cols4 %in% c("ch_cis_episodes"))]
-      # # columns to select maximum
-      # cols5 <- vars_contain(episode_file, "nsu")
-      # columns to select first row
-      cols6 <- c(condition_cols(),
-                 # "death_date",
-                 # "deceased",
-                 "year",
-                 vars_end_with(episode_file,
-                               c("_cohort", "end_fy", "start_fy")))
-
-      for (i in 1:n_chunks) {
-        start <- (i - 1) * chunk_size + 1
-        end <- i * chunk_size
-        # Subset the data to the current chunk
-        chunk <- episode_file[start:end]
-
-        # compute
-        chunk_cols1 <- chunk[,
-                             .(gender = mean(gender)),
-                             by = chi]
-        chunk_cols2 <- chunk[,
-                             .SD[.N],
-                             .SDcols = cols2,
-                             by = chi]
-        chunk_cols3 <- chunk[,
-                             lapply(.SD, function(x)
-                               data.table::uniqueN(x, na.rm = TRUE)),
-                             .SDcols = cols3,
-                             by = chi]
-        chunk_cols4 <- chunk[,
-                             lapply(.SD, function(x)
-                               sum(x, na.rm = TRUE)),
-                             .SDcols = cols4,
-                             by = chi]
-        # chunk_cols5 <- chunk[,
-        #                      lapply(.SD, function(x) max(x, na.rm = TRUE)),
-        #                      .SDcols = cols5,
-        #                      by = chi]
-        chunk_cols6 <- chunk[,
-                             lapply(.SD, function(x)
-                               x[!is.na(x)][1]),
-                             .SDcols = cols6,
-                             by = chi]
-        chunk_agg <- dplyr::bind_cols(chunk_cols1,
-                                      chunk_cols2[, chi := NULL],
-                                      chunk_cols3[, chi := NULL],
-                                      chunk_cols4[, chi := NULL],
-                                      # chunk_cols5[, chi := NULL],
-                                      chunk_cols6[, chi := NULL])
-
-        # Append the aggregated chunk to the overall result
-        aggregated_data <-
-          data.table::rbindlist(list(aggregated_data, chunk_agg))
-      }
-      aggregated_data <- dplyr::as_tibble(aggregated_data)
-
-      return(aggregated_data)
+  return(aggregated_data)
 }
 
 
@@ -162,27 +186,36 @@ aggregate_by_chi_zihao <- function(episode_file) {
 #' @describeIn select columns based on patterns
 #'
 vars_end_with <- function(data, vars, ignore_case = FALSE) {
-  names(data)[stringr::str_ends(names(data),
-                                stringr::regex(paste(vars, collapse = "|"),
-                                               ignore_case = ignore_case))]
+  names(data)[stringr::str_ends(
+    names(data),
+    stringr::regex(paste(vars, collapse = "|"),
+      ignore_case = ignore_case
+    )
+  )]
 }
 
 #' select columns starting with some patterns
 #' @describeIn select columns based on patterns
 #'
 vars_start_with <- function(data, vars, ignore_case = FALSE) {
-  names(data)[stringr::str_starts(names(data),
-                                  stringr::regex(paste(vars, collapse = "|"),
-                                                 ignore_case = ignore_case))]
+  names(data)[stringr::str_starts(
+    names(data),
+    stringr::regex(paste(vars, collapse = "|"),
+      ignore_case = ignore_case
+    )
+  )]
 }
 
 #' select columns contains some characters
 #' @describeIn select columns based on patterns
 #'
 vars_contain <- function(data, vars, ignore_case = FALSE) {
-  names(data)[stringr::str_detect(names(data),
-                                  stringr::regex(paste(vars, collapse = "|"),
-                                                 ignore_case = ignore_case))]
+  names(data)[stringr::str_detect(
+    names(data),
+    stringr::regex(paste(vars, collapse = "|"),
+      ignore_case = ignore_case
+    )
+  )]
 }
 
 #' Aggregate CIS episodes
