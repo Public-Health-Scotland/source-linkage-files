311dda967cc9b98944d5ba3a3a6d7ffbebf2402b
James McMahon <james.mcmahon@phs.scot>
2023-06-21 08:54:26 +0100

    Rename the IT CHI deaths file functions

--- R/read_it_chi_deaths.R
+++ R/read_it_chi_deaths.R
@@ -1,30 +1,24 @@
-#' Process the CHI deaths lookup
+#' Read the CHI deaths extract
 #'
-#' @description This will read and process the
-#' CHI deaths lookup, it will return the final data
-#' but also write this out as an rds.
-#'
+#' @description This will read the CHI deaths extract and return the data.
 #' @param file_path Path to CHI Deaths file
 #'
 #' @return the final data as a [tibble][tibble::tibble-package].
 #' @export
 #' @family process extracts
-
-read_lookup_chi_deaths <- function(file_path = get_it_deaths_path()) {
-  # Read data -------------------------------------------------------
-  deaths_data <- read_file(file_path,
+read_it_chi_deaths <- function(file_path = get_it_deaths_path()) {
+  it_chi_deaths <- read_file(file_path,
     col_type = cols(
       "PATIENT_UPI [C]" = col_character(),
       "PATIENT DoD DATE (NRS)" = col_date(format = "%d-%m-%Y"),
       "PATIENT DoD DATE (CHI)" = col_date(format = "%d-%m-%Y")
     )
   ) %>%
-    # rename variables
     dplyr::rename(
       chi = "PATIENT_UPI [C]",
       death_date_nrs = "PATIENT DoD DATE (NRS)",
       death_date_chi = "PATIENT DoD DATE (CHI)"
     )
 
-  return(deaths_data)
+  return(it_chi_deaths)
 }
