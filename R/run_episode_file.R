e1527961310fc57bf4eb57433423a62b042f337d
James McMahon <james.mcmahon@phs.scot>
2023-06-26 17:09:28 +0100

    Fix SPARRA / HHG function

--- R/run_episode_file.R
+++ R/run_episode_file.R
@@ -327,56 +327,64 @@ join_cohort_lookups <- function(ep_file_data, year) {
   return(join_cohort_lookups)
 }
 
-
 #' Join SPARRA and HHG
 #'
-#' @param ep_file_data Episode file data.
-#' @param year financial year, e.g. '1920'
+#' @inheritParams store_ep_file_vars
 #'
 #' @return The data including the SPARRA and HHG variables matched
 #' on to the episode file.
-#'
-join_sparra_hhg <- function(ep_file_data, year) {
-  if (!check_year_valid(year, "SPARRA")) {
-    join_sparra_hhg_file <- ep_file_data %>%
-      dplyr::full_join(
-        read_file(get_sparra_path(year)) %>%
-          dplyr::rename(
-            chi = "upi_number",
-            sparra_start_fy = "sparra_risk_score"
-          ),
-        by = "chi"
-      ) %>%
-      dplyr::full_join(
-        read_file(get_hhg_path(year)) %>%
-          dplyr::rename(
-            chi = "upi_number",
-            hhg_start_fy = "hhg_score"
-          ),
-        by = "chi"
-      )
-
-    return(join_sparra_hhg_file)
+join_sparra_hhg <- function(data, year) {
+  if (check_year_valid(year, "SPARRA")) {
+    data <- dplyr::left_join(
+      data,
+      read_file(get_sparra_path(year)) %>%
+        dplyr::rename(sparra_start_fy = "sparra_risk_score"),
+      by = c("chi", "upi_number"),
+      na_matches = "never",
+      relationship = "many-to-one"
+    )
+  } else {
+    data <- dplyr::mutate(data, sparra_start_fy = NA_real_)
   }
 
-  if (!check_year_valid(year, "HHG")) {
-    join_sparra_hhg_file <- ep_file_data %>%
-      dplyr::full_join(
-        read_file(get_sparra_path(next_fy(year))) %>%
-          dplyr::rename(
-            chi = "upi_number",
-            sparra_end_fy = "sparra_risk_score"
-          ),
-        by = "chi"
-      ) %>%
-      dplyr::full_join(
-        read_file(get_hhg_path(next_fy(year))) %>%
-          dplyr::rename(
-            chi = "upi_number",
-            hhg_end_fy = "hhg_score"
-          ),
-        by = "chi"
-      )
-    return(join_sparra_hhg_file)
+  if (check_year_valid(next_fy(year), "SPARRA")) {
+    data <- dplyr::left_join(
+      data,
+      read_file(get_sparra_path(year)) %>%
+        dplyr::rename(sparra_end_fy = "sparra_risk_score"),
+      by = c("chi", "upi_number"),
+      na_matches = "never",
+      relationship = "many-to-one"
+    )
+  } else {
+    data <- dplyr::mutate(data, sparra_end_fy = NA_real_)
   }
+
+  if (check_year_valid(year, "HHG")) {
+    data <- dplyr::left_join(
+      data,
+      read_file(get_sparra_path(year)) %>%
+        dplyr::rename(hhg_start_fy = "hhg_score"),
+      by = c("chi", "upi_number"),
+      na_matches = "never",
+      relationship = "many-to-one"
+    )
+  } else {
+    data <- dplyr::mutate(data, hhg_start_fy = NA_real_)
+  }
+
+  if (check_year_valid(next_fy(year), "HHG")) {
+    data <- dplyr::left_join(
+      data,
+      read_file(get_sparra_path(year)) %>%
+        dplyr::rename(hhg_end_fy = "hhg_score"),
+      by = c("chi", "upi_number"),
+      na_matches = "never",
+      relationship = "many-to-one"
+    )
+  } else {
+    data <- dplyr::mutate(data, hhg_end_fy = NA_real_)
+  }
+
+  return(data)
 }
