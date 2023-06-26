7a241d3c256e415998ce07b3a014feec7654702f
James McMahon <james.mcmahon@phs.scot>
2023-06-26 16:11:59 +0100

    Remove some redundant code

--- R/run_episode_file.R
+++ R/run_episode_file.R
@@ -382,40 +382,4 @@ join_sparra_hhg <- function(ep_file_data, year) {
       )
     return(join_sparra_hhg_file)
   }
-
-  join_sparra_hhg_file <- ep_file_data %>%
-    dplyr::full_join(
-      read_file(get_sparra_path(fy)) %>%
-        dplyr::rename(
-          chi = "upi_number",
-          sparra_start_fy = "sparra_risk_score"
-        ),
-      by = "chi"
-    ) %>%
-    dplyr::full_join(
-      read_file(get_hhg_path(fy)) %>%
-        dplyr::rename(
-          chi = "upi_number",
-          hhg_start_fy = "hhg_score"
-        ),
-      by = "chi"
-    ) %>%
-    dplyr::full_join(
-      read_file(get_sparra_path(next_fy)) %>%
-        dplyr::rename(
-          chi = "upi_number",
-          sparra_end_fy = "sparra_risk_score"
-        ),
-      by = "chi"
-    ) %>%
-    dplyr::full_join(
-      read_file(get_hhg_path(next_fy)) %>%
-        dplyr::rename(
-          chi = "upi_number",
-          hhg_end_fy = "hhg_score"
-        ),
-      by = "chi"
-    )
-
-  return(join_sparra_hhg_file)
 }
