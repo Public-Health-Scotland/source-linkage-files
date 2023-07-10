8603620a6394f48605c35071b115dd68c5ee18bb
James McMahon <james.mcmahon@phs.scot>
2023-07-10 14:22:23 +0100

    Check the `it_reference` supplied to `get_it_exract_paths`

--- R/get_it_extract_paths.R
+++ R/get_it_extract_paths.R
@@ -18,6 +18,8 @@ get_it_ltc_path <- function(it_reference = NULL, ...) {
       ...
     )
   } else {
+    it_reference <- check_it_reference(it_reference)
+
     it_ltc_path <- get_file_path(
       directory = fs::path(get_slf_dir(), "IT_extracts"),
       file_name = stringr::str_glue("SCTASK{it_reference}_LTCs.csv.gz")
@@ -39,23 +41,24 @@ get_it_ltc_path <- function(it_reference = NULL, ...) {
 #' @export
 #' @family extract file paths
 #' @seealso [get_file_path()] for the generic function.
-get_it_deaths_path <-
-  function(it_reference = NULL, ...) {
-    if (is.null(it_reference)) {
-      it_deaths_path <- get_file_path(
-        directory = fs::path(get_slf_dir(), "IT_extracts"),
-        file_name_regexp = "SCTASK[0-9]{7}_Deaths\\.csv(?:\\.gz)?",
-        ...
-      )
-    } else {
-      it_deaths_path <- get_file_path(
-        directory = fs::path(get_slf_dir(), "IT_extracts"),
-        file_name = stringr::str_glue("SCTASK{it_reference}_Deaths.csv.gz")
-      )
-    }
+get_it_deaths_path <- function(it_reference = NULL, ...) {
+  if (is.null(it_reference)) {
+    it_deaths_path <- get_file_path(
+      directory = fs::path(get_slf_dir(), "IT_extracts"),
+      file_name_regexp = "SCTASK[0-9]{7}_Deaths\\.csv(?:\\.gz)?",
+      ...
+    )
+  } else {
+    it_reference <- check_it_reference(it_reference)
 
-    return(it_deaths_path)
+    it_deaths_path <- get_file_path(
+      directory = fs::path(get_slf_dir(), "IT_extracts"),
+      file_name = stringr::str_glue("SCTASK{it_reference}_Deaths.csv.gz")
+    )
   }
+
+  return(it_deaths_path)
+}
 
 #' IT Prescribing File Path
 #'
@@ -70,23 +73,46 @@ get_it_deaths_path <-
 #' @export
 #' @family extract file paths
 #' @seealso [get_file_path()] for the generic function.
-get_it_prescribing_path <-
-  function(year, it_reference = NULL, ...) {
-    if (is.null(it_reference)) {
-      it_pis_path <- get_file_path(
-        directory = fs::path(get_slf_dir(), "IT_extracts"),
-        file_name_regexp = stringr::str_glue(
-          "SCTASK[0-9]{{7}}_PIS_{convert_fyyear_to_year(year)}.csv(?:\\.gz)?"
-        )
+get_it_prescribing_path <- function(year, it_reference = NULL, ...) {
+  if (is.null(it_reference)) {
+    it_pis_path <- get_file_path(
+      directory = fs::path(get_slf_dir(), "IT_extracts"),
+      file_name_regexp = stringr::str_glue(
+        "SCTASK[0-9]{{7}}_PIS_{convert_fyyear_to_year(year)}.csv(?:\\.gz)?"
       )
-    } else {
-      it_pis_path <- get_file_path(
-        directory = fs::path(get_slf_dir(), "IT_extracts"),
-        file_name = stringr::str_glue(
-          "SCTASK{it_reference}_PIS_{convert_fyyear_to_year(year)}.csv.gz"
-        )
-      )
-    }
+    )
+  } else {
+    it_reference <- check_it_reference(it_reference)
 
-    return(it_pis_path)
+    it_pis_path <- get_file_path(
+      directory = fs::path(get_slf_dir(), "IT_extracts"),
+      file_name = stringr::str_glue(
+        "SCTASK{it_reference}_PIS_{convert_fyyear_to_year(year)}.csv.gz"
+      )
+    )
   }
+
+  return(it_pis_path)
+}
+
+#' Check that an IT reference looks valid
+#'
+#' @param it_reference The IT reference to check
+#'
+#' @return `it_reference` if valid, with the leading "SCTASK" trimmed if
+#' necessary.
+check_it_reference <- function(it_reference) {
+  if (stringr::str_starts(it_reference, stringr::fixed("SCTASK"))) {
+    # If the 'full' reference has been supplied trim to just the number
+    it_reference <- stringr::str_sub(it_reference, start = 7, end = 14)
+  }
+
+  if (stringr::str_detect(it_reference, "[0-9]{7}", negate = TRUE)) {
+    cli::cli_abort(
+      c("x" = "{.arg it_reference} must be exactly 7 numbers."),
+      call = rlang::caller_env()
+    )
+  } else {
+    return(it_reference)
+  }
+}
