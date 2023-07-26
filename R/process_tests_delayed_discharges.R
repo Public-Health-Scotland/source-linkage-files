3a8cc331ec60eb9fd13824111cc720126bc1da5c
Jennit07 <Jennit07@users.noreply.github.com>
2023-07-26 14:36:19 +0000

    Style code

--- R/process_tests_delayed_discharges.R
+++ R/process_tests_delayed_discharges.R
@@ -36,10 +36,13 @@ process_tests_delayed_discharges <- function(data, year) {
 #' @seealso calculate_measures
 produce_source_dd_tests <- function(data) {
   test_flags <- data %>%
-    dplyr::mutate(n_delay_episodes = 1L,
-                  code9_episodes = dplyr::case_when(primary_delay_reason == "9" ~ 1L,
-                                             TRUE ~ 0L)
-                  ) %>%
+    dplyr::mutate(
+      n_delay_episodes = 1L,
+      code9_episodes = dplyr::case_when(
+        primary_delay_reason == "9" ~ 1L,
+        TRUE ~ 0L
+      )
+    ) %>%
     create_hb_test_flags(.data$hbtreatcode) %>%
     # keep variables for comparison
     dplyr::select(c("n_delay_episodes":dplyr::last_col())) %>%
