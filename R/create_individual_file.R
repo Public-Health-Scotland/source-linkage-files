965f05c1692e4b66e2cd90165ef2947dc2a6f1c9
James McMahon <james.mcmahon@phs.scot>
2023-07-31 12:58:34 +0100

    Make sure `year` is added as the first variable

--- R/create_individual_file.R
+++ R/create_individual_file.R
@@ -72,7 +72,7 @@ create_individual_file <- function(
     join_sparra_hhg(year) %>%
     join_slf_lookup_vars() %>%
     join_sc_client(year) %>%
-    dplyr::mutate(year = year)
+    dplyr::mutate(year = year, .before = dplyr::everything())
 
   if (anon_chi_out) {
     individual_file <- individual_file %>%
