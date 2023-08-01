fa8433662096c7537d3ef300d7a1309994ef48eb
Jennifer Thom <jennifer.thom@phs.scot>
2023-08-01 11:06:35 +0100

    Simplify `create_hscp_test_flags`

--- R/create_hscp_test_flags.R
+++ R/create_hscp_test_flags.R
@@ -5,166 +5,42 @@
 #' @param data the data containing a HSCP variable
 #' @param hscp_var HSCP variable e.g. HSCP2019 HSCP2018
 #'
-#' @return a dataframe with flag (1 or 0) for each HSCP
+#' @return a dataframe with flag (TRUE or FALSE) for each HSCP
 #'
 #' @family flag functions
 create_hscp_test_flags <- function(data, hscp_var) {
   data <- data %>%
     dplyr::mutate(
-      Aberdeen_City = dplyr::if_else(
-        {{ hscp_var }} == "S37000001",
-        1L,
-        0L
-      ),
-      Aberdeenshire = dplyr::if_else(
-        {{ hscp_var }} == "S37000002",
-        1L,
-        0L
-      ),
-      Angus = dplyr::if_else(
-        {{ hscp_var }} == "S37000003",
-        1L,
-        0L
-      ),
-      Argyll_and_Bute = dplyr::if_else(
-        {{ hscp_var }} == "S37000004",
-        1L,
-        0L
-      ),
-      Clackmannanshire_and_Stirling = dplyr::if_else(
-        {{ hscp_var }} == "S37000005",
-        1L,
-        0L
-      ),
-      Dumfries_and_Galloway = dplyr::if_else(
-        {{ hscp_var }} == "S37000006",
-        1L,
-        0L
-      ),
-      Dundee_City = dplyr::if_else(
-        {{ hscp_var }} == "S37000007",
-        1L,
-        0L
-      ),
-      East_Ayrshire = dplyr::if_else(
-        {{ hscp_var }} == "S37000008",
-        1L,
-        0L
-      ),
-      East_Dunbartonshire = dplyr::if_else(
-        {{ hscp_var }} == "S37000009",
-        1L,
-        0L
-      ),
-      East_Lothian = dplyr::if_else(
-        {{ hscp_var }} == "S37000010",
-        1L,
-        0L
-      ),
-      East_Renfrewshire = dplyr::if_else(
-        {{ hscp_var }} == "S37000011",
-        1L,
-        0L
-      ),
-      Edinburgh = dplyr::if_else(
-        {{ hscp_var }} == "S37000012",
-        1L,
-        0L
-      ),
-      Falkirk = dplyr::if_else(
-        {{ hscp_var }} == "S37000013",
-        1L,
-        0L
-      ),
-      Highland = dplyr::if_else(
-        {{ hscp_var }} == "S37000016",
-        1L,
-        0L
-      ),
-      Inverclyde = dplyr::if_else(
-        {{ hscp_var }} == "S37000017",
-        1L,
-        0L
-      ),
-      Midlothian = dplyr::if_else(
-        {{ hscp_var }} == "S37000018",
-        1L,
-        0L
-      ),
-      Moray = dplyr::if_else(
-        {{ hscp_var }} == "S37000019",
-        1L,
-        0L
-      ),
-      North_Ayrshire = dplyr::if_else(
-        {{ hscp_var }} == "S37000020",
-        1L,
-        0L
-      ),
-      Orkney_Islands = dplyr::if_else(
-        {{ hscp_var }} == "S37000022",
-        1L,
-        0L
-      ),
-      Renfrewshire = dplyr::if_else(
-        {{ hscp_var }} == "S37000024",
-        1L,
-        0L
-      ),
-      Scottish_Borders = dplyr::if_else(
-        {{ hscp_var }} == "S37000025",
-        1L,
-        0L
-      ),
-      Shetland_Islands = dplyr::if_else(
-        {{ hscp_var }} == "S37000026",
-        1L,
-        0L
-      ),
-      South_Ayrshire = dplyr::if_else(
-        {{ hscp_var }} == "S37000027",
-        1L,
-        0L
-      ),
-      South_Lanarkshire = dplyr::if_else(
-        {{ hscp_var }} == "S37000028",
-        1L,
-        0L
-      ),
-      West_Dunbartonshire = dplyr::if_else(
-        {{ hscp_var }} == "S37000029",
-        1L,
-        0L
-      ),
-      West_Lothian = dplyr::if_else(
-        {{ hscp_var }} == "S37000030",
-        1L,
-        0L
-      ),
-      Western_Isles = dplyr::if_else(
-        {{ hscp_var }} == "S37000031",
-        1L,
-        0L
-      ),
-      Fife = dplyr::if_else(
-        {{ hscp_var }} == "S37000032",
-        1L,
-        0L
-      ),
-      Perth_and_Kinross = dplyr::if_else(
-        {{ hscp_var }} == "S37000033",
-        1L,
-        0L
-      ),
-      Glasgow_City = dplyr::if_else(
-        {{ hscp_var }} %in% c("S37000015", "S37000034"),
-        1L,
-        0L
-      ),
-      North_Lanarkshire = dplyr::if_else(
-        {{ hscp_var }} %in% c("S37000021", "S37000035"),
-        1L,
-        0L
-      )
+      Aberdeen_City = {{ hscp_var }} == "S37000001",
+      Aberdeenshire = {{ hscp_var }} == "S37000002",
+      Angus = {{ hscp_var }} == "S37000003",
+      Argyll_and_Bute = {{ hscp_var }} == "S37000004",
+      Clackmannanshire_and_Stirling = {{ hscp_var }} == "S37000005",
+      Dumfries_and_Galloway = {{ hscp_var }} == "S37000006",
+      Dundee_City = {{ hscp_var }} == "S37000007",
+      East_Ayrshire = {{ hscp_var }} == "S37000008",
+      East_Dunbartonshire = {{ hscp_var }} == "S37000009",
+      East_Lothian = {{ hscp_var }} == "S37000010",
+      East_Renfrewshire = {{ hscp_var }} == "S37000011",
+      Edinburgh = {{ hscp_var }} == "S37000012",
+      Falkirk = {{ hscp_var }} == "S37000013",
+      Highland = {{ hscp_var }} == "S37000016",
+      Inverclyde = {{ hscp_var }} == "S37000017",
+      Midlothian = {{ hscp_var }} == "S37000018",
+      Moray = {{ hscp_var }} == "S37000019",
+      North_Ayrshire = {{ hscp_var }} == "S37000020",
+      Orkney_Islands = {{ hscp_var }} == "S37000022",
+      Renfrewshire = {{ hscp_var }} == "S37000024",
+      Scottish_Borders = {{ hscp_var }} == "S37000025",
+      Shetland_Islands = {{ hscp_var }} == "S37000026",
+      South_Ayrshire = {{ hscp_var }} == "S37000027",
+      South_Lanarkshire = {{ hscp_var }} == "S37000028",
+      West_Dunbartonshire = {{ hscp_var }} == "S37000029",
+      West_Lothian = {{ hscp_var }} == "S37000030",
+      Western_Isles = {{ hscp_var }} == "S37000031",
+      Fife = {{ hscp_var }} == "S37000032",
+      Perth_and_Kinross = {{ hscp_var }} == "S37000033",
+      Glasgow_City = {{ hscp_var }} %in% c("S37000015", "S37000034"),
+      North_Lanarkshire = {{ hscp_var }} %in% c("S37000021", "S37000035"),
     )
 }
