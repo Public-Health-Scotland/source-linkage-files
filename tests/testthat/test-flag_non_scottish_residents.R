61118622516f6a7e29cf30ec76def63ece11ec93
shintoLampgit config --global user.email bateman.mcbride@phs.scotm  git config --global user.name shintoLamp <bateman.mcbride@phs.scot>
2023-08-02 13:32:03 +0100

    Test to check that non-Scottish residents are flagged correctly

---
+++ tests/testthat/test-flag_non_scottish_residents.R
@@ -0,0 +1,26 @@
+test_that("Records are flagged correctly", {
+
+  test_frame <- tibble::tribble(
+    ~postcode, ~gpprac,
+    # Scottish resident
+    "AB1 1AA", 18574,
+    # Dummy postcode and missing gpprac
+    "BF010AA", NA,
+    # Dummy postcode and missing gpprac (2)
+    "ZZ014AA", NA,
+    # Missing postcode and missing gpprac
+    NA, NA,
+    # Not English practice and missing postcode
+    NA, 18574,
+    # Not English practice and dummy postcode
+    "NF1 1AB", 18574,
+    # English postcode and English gpprac
+    "BS4 4RG", 99942
+  )
+
+  test_frame_flagged <- flag_non_scottish_residents(test_frame)
+
+  expect_equal(test_frame_flagged$keep_flag,
+               c(0, 2, 2, 2, 3, 4, 1))
+
+})
