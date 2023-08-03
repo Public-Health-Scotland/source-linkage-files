f6be11c37dafc7c53b9b1b8ec5880d0dd165a9e2
Megan McNicol <43570769+SwiftySalmon@users.noreply.github.com>
2023-08-03 17:32:11 +0100

    Rewrite case when statements (#780)
    * updated code from case_when to case_match as it's a bit easier to read
    
    * Style code
    
    * changed some more `case_when` to `case_match`
    
    * Style code
    
    * [check-spelling] Update metadata
    
    Update for https://github.com/Public-Health-Scotland/source-linkage-files/actions/runs/5752014211/attempts/1
    Accepted in https://github.com/Public-Health-Scotland/source-linkage-files/pull/780#issuecomment-1664201334
    
    Signed-off-by: check-spelling-bot <check-spelling-bot@users.noreply.github.com>
    
    * Add tests for `convert_sending_location_to_lca`
    
    ---------
    
    Signed-off-by: check-spelling-bot <check-spelling-bot@users.noreply.github.com>
    Co-authored-by: marjom02 <megan.mcnicol2@nhs.scot>
    Co-authored-by: SwiftySalmon <SwiftySalmon@users.noreply.github.com>
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>

--- R/get_source_extract_path.R
+++ R/get_source_extract_path.R
@@ -37,25 +37,26 @@ get_source_extract_path <- function(year,
     return(NA)
   }
 
-  file_name <- dplyr::case_when(
-    type == "Acute" ~ "acute_for_source",
-    type == "AE" ~ "a&e_for_source",
-    type == "AT" ~ "Alarms-Telecare-for-source",
-    type == "CH" ~ "care_home_for_source",
-    type == "CMH" ~ "CMH_for_source",
-    type == "Client" ~ "client_for_source",
-    type == "DD" ~ "DD_for_source",
-    type == "Deaths" ~ "deaths_for_source",
-    type == "DN" ~ "DN_for_source",
-    type == "GPOoH" ~ "GP_OOH_for_source",
-    type == "HC" ~ "Home_Care_for_source",
-    type == "Homelessness" ~ "homelessness_for_source",
-    type == "Maternity" ~ "maternity_for_source",
-    type == "MH" ~ "mental_health_for_source",
-    type == "DD" ~ "DD_for_source",
-    type == "Outpatients" ~ "outpatients_for_source",
-    type == "PIS" ~ "prescribing_file_for_source",
-    type == "SDS" ~ "SDS-for-source"
+  file_name <- dplyr::case_match(
+    type,
+    "Acute" ~ "acute_for_source",
+    "AE" ~ "a&e_for_source",
+    "AT" ~ "Alarms-Telecare-for-source",
+    "CH" ~ "care_home_for_source",
+    "CMH" ~ "CMH_for_source",
+    "Client" ~ "client_for_source",
+    "DD" ~ "DD_for_source",
+    "Deaths" ~ "deaths_for_source",
+    "DN" ~ "DN_for_source",
+    "GPOoH" ~ "GP_OOH_for_source",
+    "HC" ~ "Home_Care_for_source",
+    "Homelessness" ~ "homelessness_for_source",
+    "Maternity" ~ "maternity_for_source",
+    "MH" ~ "mental_health_for_source",
+    "DD" ~ "DD_for_source",
+    "Outpatients" ~ "outpatients_for_source",
+    "PIS" ~ "prescribing_file_for_source",
+    "SDS" ~ "SDS-for-source"
   )
 
   source_extract_path <- get_file_path(
