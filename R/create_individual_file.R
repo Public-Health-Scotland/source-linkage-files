215b64e99f118c6b9f0f0efd798f6151c1ac654a
James McMahon <james.mcmahon@phs.scot>
2023-08-09 09:38:24 +0100

    Update R/create_individual_file.R

--- R/create_individual_file.R
+++ R/create_individual_file.R
@@ -745,11 +745,11 @@ join_sc_client <- function(
       by = c("sending_location", "social_care_id"),
       relationship = "one-to-one"
     ) %>%
-    mutate(count_not_known = apply(join_client_demog, 1, function(row) {
+    dplyr::mutate(count_not_known = apply(join_client_demog, 1, function(row) {
       sum(row == "Not Known")
     })) %>%
     dplyr::arrange(chi, count_not_known) %>%
-    distinct(chi, .keep_all = TRUE)
+    dplyr::distinct(chi, .keep_all = TRUE)
 
   # Match on client variables by chi
   individual_file <- individual_file %>%
