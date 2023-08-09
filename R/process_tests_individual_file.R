6f70003e27709d826cd3f98f10301ba1aad3ee63
lizihao-anu <lizihao-anu@users.noreply.github.com>
2023-08-09 16:51:48 +0000

    Style code

--- R/process_tests_individual_file.R
+++ R/process_tests_individual_file.R
@@ -3,16 +3,16 @@
 #' @description check whether individual files have duplicated rows for the same chi
 #'
 #' @return NULL if no duplicated chi, OR rows with duplicated chi.
-process_tests_individual_file <- function(individual_file, anon_chi_in = FALSE){
-  chi_col = dplyr::if_else(anon_chi_in, "anon_chi", "chi")
+process_tests_individual_file <- function(individual_file, anon_chi_in = FALSE) {
+  chi_col <- dplyr::if_else(anon_chi_in, "anon_chi", "chi")
   duplicated_chi <- duplicated(individual_file[[chi_col]])
   dup_num <- sum(duplicated_chi)
-  if(dup_num < 1L){
+  if (dup_num < 1L) {
     print("There is no duplicated CHI")
     return(NULL)
-  }else{
+  } else {
     print("There are duplicated CHIs")
     return(individual_file %>%
-             dplyr::filter(!!sym(chi_col)) %in% duplicated_chi)
+      dplyr::filter(!!sym(chi_col)) %in% duplicated_chi)
   }
 }
