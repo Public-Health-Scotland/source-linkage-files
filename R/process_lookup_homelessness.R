7233ef31f80388d410dfbf96eb3dccdfc70207bf
Megan McNicol <43570769+SwiftySalmon@users.noreply.github.com>
2023-08-16 15:16:55 +0100

    Update R/process_lookup_homelessness.R
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>

--- R/process_lookup_homelessness.R
+++ R/process_lookup_homelessness.R
@@ -24,8 +24,8 @@ create_homelessness_lookup <- function(
     year,
     homelessness_data = read_file(get_source_extract_path(year, "Homelessness"))) {
   homelessness_lookup <- homelessness_data %>%
-    dplyr::distinct(chi, record_keydate1, record_keydate2) %>%
-    tidyr::drop_na(chi) %>%
+    dplyr::distinct(.data$chi, .data$record_keydate1, .data$record_keydate2) %>%
+    tidyr::drop_na(.data$chi) %>%
     dplyr::mutate(hl1_in_fy = 1L)
 
   return(homelessness_lookup)
