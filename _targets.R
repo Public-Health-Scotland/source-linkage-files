b1a9a7bbe0ba083f268deff5167f0407af4cbf92
Megan McNicol <43570769+SwiftySalmon@users.noreply.github.com>
2023-08-22 15:08:44 +0100

    add homelessness flags to the episode file. (#815)
    * still sorting out conflicts
    
    * Update documentation
    
    * Style code
    
    * # Working code for homelessness flags
    
    - removes applications that have a missing end date in line with the homelessness work
    - `group_by` and `summarise(max)` to deal with people that have more than one application to prevent case duplication
    
    * Update documentation
    
    * Update R/process_lookup_homelessness.R
    
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>
    
    * Update R/process_lookup_homelessness.R
    
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>
    
    * Update R/process_lookup_homelessness.R
    
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>
    
    * Update R/process_lookup_homelessness.R
    
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>
    
    * Update documentation
    
    * Update R/process_lookup_homelessness.R
    
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>
    
    * Update R/process_lookup_homelessness.R
    
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>
    
    * Update documentation
    
    * Style code
    
    * Update R/process_lookup_homelessness.R
    
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>
    
    * Update R/process_lookup_homelessness.R
    
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>
    
    * Style code
    
    * Update documentation
    
    * Update R/process_lookup_homelessness.R
    
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>
    
    * Update R/process_lookup_homelessness.R
    
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>
    
    * Update R/process_lookup_homelessness.R
    
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>
    
    * Update documentation
    
    * Style code
    
    * made James' suggested changes and generally tidied up with lintR.
    Replaced `anon_chi` with `chi`
    
    * Style code
    
    * Update documentation
    
    * # Added homelessness flags to episode and individual files, and targets.
    
    finishing off code to add flags to files
    
    * Style code
    
    * Update documentation
    
    * Update R/process_lookup_homelessness.R
    
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>
    
    * Update R/process_lookup_homelessness.R
    
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>
    
    * Update documentation
    
    * Style code
    
    * Update R/process_lookup_homelessness.R
    
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>
    
    * Style code
    
    * Update documentation
    
    * # Updated documentation
    
    added homelessness params to the episode and individual file
    
    ---------
    
    Co-authored-by: marjom02 <megan.mcnicol2@nhs.scot>
    Co-authored-by: SwiftySalmon <SwiftySalmon@users.noreply.github.com>
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>
    Co-authored-by: Jennit07 <67372904+Jennit07@users.noreply.github.com>

--- _targets.R
+++ _targets.R
@@ -339,11 +339,14 @@ list(
         year
       )
     ),
-    tar_target(source_homelessness_extract, process_extract_homelessness(
-      homelessness_data,
-      year,
-      write_to_disk = write_to_disk
-    )),
+    tar_target(
+      source_homelessness_extract,
+      process_extract_homelessness(
+        homelessness_data,
+        year,
+        write_to_disk = write_to_disk
+      )
+    ),
     tar_target(
       tests_source_homelessness_extract,
       process_tests_homelessness(
@@ -544,10 +547,18 @@ list(
       )
     ),
     tar_target(
+      homelessness_lookup,
+      create_homelessness_lookup(
+        year,
+        homelessness_data = source_homelessness_extract
+      )
+    ),
+    tar_target(
       episode_file,
       run_episode_file(
         processed_data_list,
         year,
+        homelessness_lookup = homelessness_lookup,
         write_to_disk
       )
     ),
@@ -563,6 +574,7 @@ list(
       create_individual_file(
         episode_file = episode_file,
         year = year,
+        homelessness_lookup = homelessness_lookup,
         write_to_disk = write_to_disk
       )
     ),
