74d2212186a336388556c527fda66fdf84e37944
Jennifer Thom <jennifer.thom@phs.scot>
2023-09-04 11:16:01 +0100

    Revert "Remove rename of hscp - causes mismatched tests"
    This reverts commit 628da4d6350906c808eabc7bfd4559432c93b6a7.

--- R/get_existing_data_for_tests.R
+++ R/get_existing_data_for_tests.R
@@ -48,6 +48,9 @@ get_existing_data_for_tests <- function(new_data, file_version = "episode") {
       recids = recids,
       col_select = variable_names
     ))
+    if ("hscp2018" %in% variable_names) {
+      slf_data <- dplyr::rename(slf_data, "hscp" = "hscp2018")
+    }
   } else {
     slf_data <- suppressMessages(slfhelper::read_slf_individual(
       year = year,
