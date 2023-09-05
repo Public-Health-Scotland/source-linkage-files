fbb00e10e0e39513af5a9a97669c883648e79d2b
Zihao Li <zihao.li@phs.scot>
2023-09-05 14:51:41 +0100

    Update create_individual_file.R (#824)
    * Update create_individual_file.R
    
    * Update documentation
    
    * Style code
    
    ---------
    
    Co-authored-by: lizihao-anu <lizihao-anu@users.noreply.github.com>

--- R/create_individual_file.R
+++ R/create_individual_file.R
@@ -85,7 +85,7 @@ create_individual_file <- function(
     join_sparra_hhg(year) %>%
     join_slf_lookup_vars() %>%
     join_sc_client(year) %>%
-    add_hri_variables()
+    add_hri_variables() %>%
     dplyr::mutate(year = year, .before = dplyr::everything())
 
   if (anon_chi_out) {
