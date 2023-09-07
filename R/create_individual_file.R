cea985cd85aa6d7c540c25293c1b9453ac6f6aef
Jennifer Thom <jennifer.thom@phs.scot>
2023-09-07 10:46:36 +0100

    Add HRI last

--- R/create_individual_file.R
+++ R/create_individual_file.R
@@ -85,8 +85,9 @@ create_individual_file <- function(
     join_sparra_hhg(year) %>%
     join_slf_lookup_vars() %>%
     join_sc_client(year) %>%
-    add_hri_variables(chi_variable = "chi") %>%
-    dplyr::mutate(year = year, .before = dplyr::everything())
+    dplyr::mutate(year = year, .before = dplyr::everything()) %>%
+    add_hri_variables(chi_variable = "chi")
+
 
 
   if (anon_chi_out) {
