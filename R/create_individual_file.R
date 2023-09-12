0b06fc97eef4b4f26bd2d48d41b9ac9b956e39ae
Jennifer Thom <jennifer.thom@phs.scot>
2023-09-12 09:25:47 +0100

    Remove `dplyr::everything()`

--- R/create_individual_file.R
+++ R/create_individual_file.R
@@ -94,7 +94,7 @@ create_individual_file <- function(
     join_deaths_data(year) %>%
     join_sparra_hhg(year) %>%
     join_slf_lookup_vars() %>%
-    dplyr::mutate(year = year, .before = dplyr::everything()) %>%
+    dplyr::mutate(year = year) %>%
     add_hri_variables(chi_variable = "chi")
 
   if (!check_year_valid(year, type = c("CH", "HC", "AT", "SDS"))) {
