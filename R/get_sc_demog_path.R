978c3f9cd530189b32ff1fae7dae25af8911b693
Jennifer Thom <jennifer.thom@phs.scot>
2023-09-26 15:07:31 +0100

    new function for `client_lookup_path`

--- R/get_sc_demog_path.R
+++ R/get_sc_demog_path.R
@@ -21,3 +21,28 @@ get_sc_demog_lookup_path <- function(update = latest_update(), ...) {
 
   return(sc_demog_lookup_path)
 }
+
+#' Social Care Client Lookup File Path
+#'
+#' @description Get the file path for the Social Care Client lookup file
+#'
+#' @param year Financial year.
+#' @param update The update month to use,
+#' defaults to [latest_update()]
+#'
+#' @param ... additional arguments passed to [get_file_path()]
+#'
+#' @return The path to the social care demographic file
+#' as an [fs::path()]
+#' @export
+#' @family social care lookup file paths
+#' @seealso [get_file_path()] for the generic function.
+get_sc_client_lookup_path <- function(year, update = latest_update(), ...) {
+  sc_client_lookup_path <- get_file_path(
+    directory = fs::path(get_slf_dir(), "Social_care"),
+    file_name = stringr::str_glue("sc_client_lookup_{year}_{update}.parquet"),
+    ...
+  )
+
+  return(sc_client_lookup_path)
+}
