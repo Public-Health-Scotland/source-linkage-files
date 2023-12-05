9b7a20aeb68f80892b0efc3c8cd07cbb6c5de33f
Jennit07 <Jennit07@users.noreply.github.com>
2023-12-05 14:48:47 +0000

    Style code

--- run_slf_process_manually.R
+++ run_slf_process_manually.R
@@ -4,16 +4,16 @@ library(createslf)
 
 ---
 
-## UPDATE: Year you would like to run ##
-year <- "2223"
+  ## UPDATE: Year you would like to run ##
+  year <- "2223"
 
 ## UPDATE: Year on "processed_data_list_XXX" ##
 processed_data_list <- targets::tar_read("processed_data_list_2223")
 
 ---
 
-# Run episode file
-create_episode_file(processed_data_list, year = year) %>%
+  # Run episode file
+  create_episode_file(processed_data_list, year = year) %>%
   process_tests_episode_file(year = year)
 
 # Run individual file
