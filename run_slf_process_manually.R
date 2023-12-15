3ec14c5156e1f316d0605fb2753a186855a51961
Jennifer Thom <jennifer.thom@phs.scot>
2023-12-15 11:20:40 +0000

    Add episode file path again

--- run_slf_process_manually.R
+++ run_slf_process_manually.R
@@ -16,6 +16,8 @@ processed_data_list <- targets::tar_read("processed_data_list_2223")
   create_episode_file(processed_data_list, year = year) %>%
   process_tests_episode_file(year = year)
 
-# Run individual file
-create_individual_file(episode_file, year = year) %>%
+  episode_file <- arrow::read_parquet(get_slf_episode_path(year))
+
+  # Run individual file
+  create_individual_file(episode_file, year = year) %>%
   process_tests_individual_file(year = year)
