42f762cfdebb0933dcb5649d5eefa1978ab7a443
Jennifer Thom <jennifer.thom@phs.scot>
2023-12-19 11:36:21 +0000

    update manual running scripts

--- Run_SLF_Files_manually/run_episode_file_1819.R
+++ Run_SLF_Files_manually/run_episode_file_1819.R
@@ -3,7 +3,8 @@ library(createslf)
 
 year <- "1819"
 
-processed_data_list <- targets::tar_read("processed_data_list_1819")
+processed_data_list <- targets::tar_read("processed_data_list_1819",
+                                         store = fs::path("/conf/sourcedev/Source_Linkage_File_Updates/", "_targets"))
 
 # Run episode file
 create_episode_file(processed_data_list, year = year) %>%
--- Run_SLF_Files_manually/run_episode_file_2122.R
+++ Run_SLF_Files_manually/run_episode_file_2122.R
@@ -3,7 +3,8 @@ library(createslf)
 
 year <- "2122"
 
-processed_data_list <- targets::tar_read("processed_data_list_2122")
+processed_data_list <- targets::tar_read("processed_data_list_2122",
+                                         store = fs::path("/conf/sourcedev/Source_Linkage_File_Updates/", "_targets"))
 
 # Run episode file
 create_episode_file(processed_data_list, year = year) %>%
