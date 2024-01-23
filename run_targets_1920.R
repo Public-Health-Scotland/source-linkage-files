524219109da87911230b43ea76e7cc4c118bae78
Zihao Li <lizihao_anu@outlook.com>
2024-01-23 13:52:48 +0000

    minor changes

--- run_targets_1920.R
+++ run_targets_1920.R
@@ -16,7 +16,5 @@ library(createslf)
 episode_file <- arrow::read_parquet(get_slf_episode_path(year))
 
 # Run individual file
-individual_file = create_individual_file(episode_file, year = year)
-
-individual_file_test = individual_file %>%
+create_individual_file(episode_file, year = year) %>%
   process_tests_individual_file(year = year)
