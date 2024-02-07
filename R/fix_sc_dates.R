744bbc024b5cba09ebe9514cd36ad794b1e24fff
Megan McNicol <43570769+SwiftySalmon@users.noreply.github.com>
2024-02-07 14:28:21 +0000

    SC Demographics and SDS (#900)
    * Style code
    
    * # read in sc demographics
    
    different variables - removed extract date as not accurate, using chi over upi after discussion with social care data management. Added in date of death just for fun.
    
    * social care demographics first draft
    
    removed a lot of the submitted variables and instead using chi variables from chi seeding. Other changes:
    - Fill in missing values,
    - create flag for latest social care id (one from database is not accurate), this makes sure that each chi only has ONE sc id as the latest to stop it creating duplicates
    - change postcode to choose chi over submitted
    
    * Style code
    
    * had a github error? Not sure what happened but commiting first draft of sc demographics
    
    * Style code
    
    * first draft sds.
    No major changes - only how demographics is matched on and how latest social care id is selected
    
    * Update documentation
    
    * demographics - add sending location to group by
    
    * Style code
    
    * Update documentation
    
    * Added ungroup()
    
    * Remove comments
    
    * Remove comments
    
    * Style code
    
    ---------
    
    Co-authored-by: SwiftySalmon <SwiftySalmon@users.noreply.github.com>
    Co-authored-by: marjom02 <megan.mcnicol2@nhs.scot>
    Co-authored-by: Jennit07 <67372904+Jennit07@users.noreply.github.com>
    Co-authored-by: Jennit07 <Jennit07@users.noreply.github.com>
    Co-authored-by: Zihao Li <zihao.li@phs.scot>

--- R/fix_sc_dates.R
+++ R/fix_sc_dates.R
@@ -9,7 +9,7 @@
 #' @return A date vector with replaced end dates
 fix_sc_start_dates <- function(start_date, period_start) {
   # Fix sds_start_date is missing by setting start_date to be the start of
-  # financial year
+  # financial period
   start_date <- dplyr::if_else(
     is.na(start_date),
     period_start,
@@ -30,12 +30,12 @@ fix_sc_start_dates <- function(start_date, period_start) {
 #' @param period Social care latest submission period.
 #'
 #' @return A date vector with replaced end dates
-fix_sc_end_dates <- function(start_date, end_date, period) {
+fix_sc_end_dates <- function(start_date, end_date, period_end_date) {
   # Fix sds_end_date is earlier than sds_start_date by setting end_date to be
   # the end of financial year
   end_date <- dplyr::if_else(
     start_date > end_date,
-    end_fy(year = stringr::str_sub(period, 1L, 4L), "alternate"),
+    period_end_date,
     end_date
   )
 
@@ -57,7 +57,7 @@ fix_sc_end_dates <- function(start_date, end_date, period) {
 #' @return A date vector with replaced end dates
 fix_sc_missing_end_dates <- function(end_date, period_end) {
   # Fix sds_end_date is earlier than sds_start_date by setting end_date to be
-  # the end of financial year
+  # the end of financial period
   end_date <- dplyr::if_else(
     is.na(end_date),
     period_end,
