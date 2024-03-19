7842647819c772ec96d5160b81d3c1f3c218c9f7
Jennit07 <Jennit07@users.noreply.github.com>
2024-03-19 11:55:17 +0000

    Style code

--- R/get_sandpit_extract_path.R
+++ R/get_sandpit_extract_path.R
@@ -11,11 +11,12 @@
 #' @export
 #' @family social care sandpit extract paths
 #' @seealso [get_file_path()] for the generic function.
-get_sandpit_extract_path <- function(type = c("at", "ch", "hc",
-                                      "sds", "client", "demographics"),
+get_sandpit_extract_path <- function(type = c(
+                                       "at", "ch", "hc",
+                                       "sds", "client", "demographics"
+                                     ),
                                      year = NULL,
                                      update = latest_update(), ...) {
-
   dir <- fs::path(get_slf_dir(), "Social_care", "Sandpit_Extracts")
 
   file_name <- dplyr::case_match(
@@ -25,14 +26,14 @@ get_sandpit_extract_path <- function(type = c("at", "ch", "hc",
     "hc" ~ "sandpit_hc_extract",
     "sds" ~ "sandpit_sds_extract",
     "client" ~ "sandpit_sc_client_extract",
-    "demographics" ~ "sandpit_sc_demographics_extract")
+    "demographics" ~ "sandpit_sc_demographics_extract"
+  )
 
   if (type == "client") {
-  sandpit_extract_path <- fs::path(dir, stringr::str_glue("{file_name}_{year}_{update}.parquet"))
-  }else{
-  sandpit_extract_path <- fs::path(dir, stringr::str_glue("{file_name}_{update}.parquet"))
+    sandpit_extract_path <- fs::path(dir, stringr::str_glue("{file_name}_{year}_{update}.parquet"))
+  } else {
+    sandpit_extract_path <- fs::path(dir, stringr::str_glue("{file_name}_{update}.parquet"))
   }
 
   return(sandpit_extract_path)
 }
-
