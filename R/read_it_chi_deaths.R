47c17ab12969b4e076d9fde0f478c4f694b25c8a
Jennifer Thom <jennifer.thom@phs.scot>
2024-05-17 16:03:18 +0100

    Change `read` functions to read anon_chi

--- R/read_it_chi_deaths.R
+++ R/read_it_chi_deaths.R
@@ -9,16 +9,17 @@
 read_it_chi_deaths <- function(file_path = get_it_deaths_path()) {
   it_chi_deaths <- read_file(file_path,
     col_type = readr::cols(
-      "PATIENT_UPI [C]" = readr::col_character(),
+      "anon_chi" = readr::col_character(),
       "PATIENT DoD DATE (NRS)" = readr::col_date(format = "%d-%m-%Y"),
       "PATIENT DoD DATE (CHI)" = readr::col_date(format = "%d-%m-%Y")
     )
   ) %>%
     dplyr::rename(
-      chi = "PATIENT_UPI [C]",
+      anon_chi = "anon_chi",
       death_date_nrs = "PATIENT DoD DATE (NRS)",
       death_date_chi = "PATIENT DoD DATE (CHI)"
-    )
+    ) %>%
+    slfhelper::get_chi()
 
   return(it_chi_deaths)
 }
