d9a70cc19959b6b934dbd184bfabcef3edad8754
Jennifer Thom <jennifer.thom@phs.scot>
2024-05-21 09:03:23 +0100

    switch between chi - ooh and dd

--- R/read_extract_delayed_discharges.R
+++ R/read_extract_delayed_discharges.R
@@ -8,12 +8,12 @@
 read_extract_delayed_discharges <- function(file_path = get_dd_path()) {
   extract_delayed_discharges <- read_file(file_path) %>%
     janitor::clean_names() %>%
-    slfhelper::get_chi() %>%
     dplyr::mutate(
       monthflag = lubridate::my(.data[["monthflag"]]),
       delay_end_reason = as.integer(.data[["delay_end_reason"]])
     ) %>%
-    dplyr::select(-.data[["cennum"]])
+    dplyr::select(-.data[["cennum"]]) %>%
+    slfhelper::get_chi()
 
   return(extract_delayed_discharges)
 }
