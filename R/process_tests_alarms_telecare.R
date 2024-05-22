fd8827bd9338ff5d71752cbc46745367cdf8c19f
Jennifer Thom <jennifer.thom@phs.scot>
2024-05-22 16:30:13 +0100

    update tests to use `chi`

--- R/process_tests_alarms_telecare.R
+++ R/process_tests_alarms_telecare.R
@@ -8,6 +8,10 @@
 #'
 #' @export
 process_tests_alarms_telecare <- function(data, year) {
+
+  data <- data %>%
+    slfhelper::get_chi()
+
   old_data <- get_existing_data_for_tests(data)
 
   data <- rename_hscp(data)
