6852db76bd2da69bf9c6f48f27303a2065067726
Jennifer Thom <jennifer.thom@phs.scot>
2024-05-27 14:25:22 +0100

    Update IT extracts to maintain chi

--- R/read_extract_prescribing.R
+++ R/read_extract_prescribing.R
@@ -6,7 +6,7 @@
 read_extract_prescribing <- function(year, file_path = get_it_prescribing_path(year)) {
   pis_file <- read_file(file_path,
     col_type = readr::cols_only(
-      "anon_chi" = readr::col_character(),
+      "Pat UPI [C]" = readr::col_character(),
       "Pat DoB [C]" = readr::col_date(format = "%d-%m-%Y"),
       "Pat Gender" = readr::col_double(),
       "Pat Postcode [C]" = readr::col_character(),
@@ -17,15 +17,14 @@ read_extract_prescribing <- function(year, file_path = get_it_prescribing_path(y
   ) %>%
     # Rename variables
     dplyr::rename(
-      anon_chi = "anon_chi",
+      chi = "Pat UPI [C]",
       dob = "Pat DoB [C]",
       gender = "Pat Gender",
       postcode = "Pat Postcode [C]",
       gpprac = "Practice Code",
       no_paid_items = "Number of Paid Items",
       cost_total_net = "PD Paid GIC excl. BB"
-    ) %>%
-    slfhelper::get_chi()
+    )
 
   return(pis_file)
 }
--- R/read_it_chi_deaths.R
+++ R/read_it_chi_deaths.R
@@ -9,17 +9,16 @@
 read_it_chi_deaths <- function(file_path = get_it_deaths_path()) {
   it_chi_deaths <- read_file(file_path,
     col_type = readr::cols(
-      "anon_chi" = readr::col_character(),
+      "PATIENT_UPI [C]" = readr::col_character(),
       "PATIENT DoD DATE (NRS)" = readr::col_date(format = "%d-%m-%Y"),
       "PATIENT DoD DATE (CHI)" = readr::col_date(format = "%d-%m-%Y")
     )
   ) %>%
     dplyr::rename(
-      anon_chi = "anon_chi",
+      chi = "PATIENT_UPI [C]",
       death_date_nrs = "PATIENT DoD DATE (NRS)",
       death_date_chi = "PATIENT DoD DATE (CHI)"
-    ) %>%
-    slfhelper::get_chi()
+    )
 
   return(it_chi_deaths)
 }
