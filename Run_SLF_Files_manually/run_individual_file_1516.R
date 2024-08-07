298cfe75125df86be39c0de73e591a448ee3c543
Jennifer Thom <jennifer.thom@phs.scot>
2024-08-07 10:06:45 +0100

    Add Running SLF files manually scripts

---
+++ Run_SLF_Files_manually/run_individual_file_1516.R
@@ -0,0 +1,9 @@
+library(createslf)
+
+year <- "1516"
+
+episode_file <- arrow::read_parquet(get_slf_episode_path(year))
+
+# Run individual file
+create_individual_file(episode_file, year = year)# %>%
+  #process_tests_individual_file(year = year)
