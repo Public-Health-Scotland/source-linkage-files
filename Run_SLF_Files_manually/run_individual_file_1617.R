1dd71830cb69fc98f5507cc69980d7c81a806396
Jennit07 <Jennit07@users.noreply.github.com>
2024-09-16 15:45:08 +0000

    Style code

--- Run_SLF_Files_manually/run_individual_file_1617.R
+++ Run_SLF_Files_manually/run_individual_file_1617.R
@@ -6,4 +6,4 @@ episode_file <- arrow::read_parquet(get_slf_episode_path(year))
 
 # Run individual file
 create_individual_file(episode_file, year = year) %>%
- process_tests_individual_file(year = year)
+  process_tests_individual_file(year = year)
