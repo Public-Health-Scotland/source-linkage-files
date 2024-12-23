d9c41c381644441850a0be015a9cf490711a8e71
lizihao-anu <lizihao-anu@users.noreply.github.com>
2024-12-23 11:50:34 +0000

    Style code

--- R/run_extract_tests.R
+++ R/run_extract_tests.R
@@ -1,14 +1,15 @@
-run_all_extract_tests = function(year_list) {
+run_all_extract_tests <- function(year_list) {
   # one-off tests
   run_lookup_tests()
   # year specific tests
   parallel::mclapply(year_list,
-                     run_year_extract_tests,
-                     mc.cores = length(year_list))
+    run_year_extract_tests,
+    mc.cores = length(year_list)
+  )
 }
 
 
-run_lookup_tests = function() {
+run_lookup_tests <- function() {
   # Tests, LOOKUP series
   ## sc_demog_lookup
   process_tests_sc_demographics(read_file(get_sc_demog_lookup_path()))
@@ -28,60 +29,96 @@ run_lookup_tests = function() {
   process_tests_sc_all_sds_episodes(read_file(get_sc_sds_episodes_path()))
 }
 
-run_year_extract_tests = function(year) {
+run_year_extract_tests <- function(year) {
   # Tests, EXTRACT series, year specific
   ## acute
-  process_tests_acute(read_file(get_source_extract_path(year, "acute")),
-                      year)
+  process_tests_acute(
+    read_file(get_source_extract_path(year, "acute")),
+    year
+  )
   ## ae
-  process_tests_ae(read_file(get_source_extract_path(year, "ae")),
-                   year)
+  process_tests_ae(
+    read_file(get_source_extract_path(year, "ae")),
+    year
+  )
   ## cmh
-  process_tests_cmh(read_file(get_source_extract_path(year, "cmh")),
-                    year)
+  process_tests_cmh(
+    read_file(get_source_extract_path(year, "cmh")),
+    year
+  )
   ## dd
-  process_tests_delayed_discharges(read_file(get_source_extract_path(year, "dd")),
-                                   year)
+  process_tests_delayed_discharges(
+    read_file(get_source_extract_path(year, "dd")),
+    year
+  )
   ## dn
-  process_tests_district_nursing(read_file(get_source_extract_path(year, "dn")),
-                                 year)
+  process_tests_district_nursing(
+    read_file(get_source_extract_path(year, "dn")),
+    year
+  )
   ## homelessness
-  process_tests_homelessness(read_file(get_source_extract_path(year, "homelessness")),
-                             year)
+  process_tests_homelessness(
+    read_file(get_source_extract_path(year, "homelessness")),
+    year
+  )
   ## ltc
-  process_tests_ltcs(read_file(get_ltcs_path(year)),
-                     year)
+  process_tests_ltcs(
+    read_file(get_ltcs_path(year)),
+    year
+  )
   ## maternity
-  process_tests_maternity(read_file(get_source_extract_path(year, "maternity")),
-                          year)
+  process_tests_maternity(
+    read_file(get_source_extract_path(year, "maternity")),
+    year
+  )
   ## mental_health
-  process_tests_mental_health(read_file(get_source_extract_path(year, "mh")),
-                              year)
+  process_tests_mental_health(
+    read_file(get_source_extract_path(year, "mh")),
+    year
+  )
   ## nrs_death
-  process_tests_nrs_deaths(read_file(get_source_extract_path(year, "deaths")),
-                           year)
+  process_tests_nrs_deaths(
+    read_file(get_source_extract_path(year, "deaths")),
+    year
+  )
   ## ooh
-  process_tests_gp_ooh(read_file(get_source_extract_path(year, "gp_ooh")),
-                       year)
+  process_tests_gp_ooh(
+    read_file(get_source_extract_path(year, "gp_ooh")),
+    year
+  )
   ## outpatients
-  process_tests_outpatients(read_file(get_source_extract_path(year, "outpatients")),
-                            year)
+  process_tests_outpatients(
+    read_file(get_source_extract_path(year, "outpatients")),
+    year
+  )
   ## prescribing
-  process_tests_prescribing(read_file(get_source_extract_path(year, "pis")),
-                            year)
+  process_tests_prescribing(
+    read_file(get_source_extract_path(year, "pis")),
+    year
+  )
   ## sc_client
-  process_tests_sc_client_lookup(read_file(get_sc_client_lookup_path(year)),
-                                 year)
+  process_tests_sc_client_lookup(
+    read_file(get_sc_client_lookup_path(year)),
+    year
+  )
   ## alarm_telecare
-  process_tests_alarms_telecare(read_file(get_source_extract_path(year, type = "at")),
-                                year)
+  process_tests_alarms_telecare(
+    read_file(get_source_extract_path(year, type = "at")),
+    year
+  )
   ## care_home
-  process_tests_care_home(read_file(get_source_extract_path(year, type = "ch")),
-                          year)
+  process_tests_care_home(
+    read_file(get_source_extract_path(year, type = "ch")),
+    year
+  )
   ## home care
-  process_tests_home_care(read_file(get_source_extract_path(year, type = "hc")),
-                          year)
+  process_tests_home_care(
+    read_file(get_source_extract_path(year, type = "hc")),
+    year
+  )
   ## sds
-  process_tests_sds(read_file(get_source_extract_path(year, type = "sds")),
-                    year)
+  process_tests_sds(
+    read_file(get_source_extract_path(year, type = "sds")),
+    year
+  )
 }
