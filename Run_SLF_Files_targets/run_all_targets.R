7c2072806be6ba3894f0d3435b0755f171665e4a
Jennifer Thom <jennifer.thom@phs.scot>
2025-01-27 09:01:14 +0000

    Use `tar_make` instead of `tar_make_future`

--- Run_SLF_Files_targets/run_all_targets.R
+++ Run_SLF_Files_targets/run_all_targets.R
@@ -3,6 +3,6 @@ library(createslf)
 
 # use tar_make_future() to run targets for all years
 # This will run everything needed for creating the episode file.
-tar_make_future()
+tar_make()
 
 createslf::combine_tests()
