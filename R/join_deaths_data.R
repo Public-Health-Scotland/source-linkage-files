c2402b85b5d3486ad66f917d6862d1d0252b1fcd
Jennifer Thom <jennifer.thom@phs.scot>
2025-02-05 12:34:31 +0000

    Remove `slfhelper::get_chi`

--- R/join_deaths_data.R
+++ R/join_deaths_data.R
@@ -9,7 +9,7 @@
 join_deaths_data <- function(
     data,
     year,
-    slf_deaths_lookup = read_file(get_slf_deaths_lookup_path(year)) %>% slfhelper::get_chi()) {
+    slf_deaths_lookup = read_file(get_slf_deaths_lookup_path(year))) {
   data <- data %>%
     dplyr::left_join(
       slf_deaths_lookup %>%
