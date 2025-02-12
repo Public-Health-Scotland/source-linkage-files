ccfda5229bc9a40769d448cdcdf1f5ce775528bc
lizihao-anu <97514488+lizihao-anu@users.noreply.github.com>
2025-02-12 12:01:22 +0000

    Style code

--- Pre_processing_scripts/01_Sort_IT_Extracts.R
+++ Pre_processing_scripts/01_Sort_IT_Extracts.R
@@ -1,10 +1,14 @@
 rm(list = ls())
 devtools::load_all()
 
-it_extract_path = file.path(get_slf_dir(),
-                            "IT_extracts")
-it_extract_anon_path = file.path(it_extract_path,
-                                 "anon-chi-IT")
+it_extract_path <- file.path(
+  get_slf_dir(),
+  "IT_extracts"
+)
+it_extract_anon_path <- file.path(
+  it_extract_path,
+  "anon-chi-IT"
+)
 
 is_chi_in_file <- function(filename) {
   data <- read.csv(filename, nrow = 1)
@@ -13,56 +17,61 @@ is_chi_in_file <- function(filename) {
 
 # detect task number and tidy up parquet files from last request ---------
 # One can manually delete files and comment this part out.
-get_previous_it_task_num = function() {
-  existing_parquet_list = list.files(it_extract_anon_path)
+get_previous_it_task_num <- function() {
+  existing_parquet_list <- list.files(it_extract_anon_path)
   ltcs_file <-
     existing_parquet_list[stringr::str_detect(existing_parquet_list, "LTCs\\.parquet$")]
-  task_num = stringr::str_extract(ltcs_file, "SCTASK\\d+")
+  task_num <- stringr::str_extract(ltcs_file, "SCTASK\\d+")
   if (length(task_num) == 1L) {
     return(task_num)
-  } else{
+  } else {
     cli::cli_alert_danger("Detected 0 or more than 1 task numbers! Please check!")
     return(NULL)
   }
 }
 
-get_current_it_task_num = function() {
+get_current_it_task_num <- function() {
   new_csv_files <- list.files(it_extract_path)
   ltcs_file <-
     new_csv_files[stringr::str_detect(new_csv_files, "LTCs\\.csv$")]
-  task_num = stringr::str_extract(ltcs_file, "SCTASK\\d+")
+  task_num <- stringr::str_extract(ltcs_file, "SCTASK\\d+")
   if (length(task_num) == 1L) {
     return(task_num)
-  } else{
+  } else {
     cli::cli_abort("Detected 0 or more than 1 task numbers! Please check!")
   }
 }
 
-previous_task_num = get_previous_it_task_num()
-current_task_num = get_current_it_task_num()
+previous_task_num <- get_previous_it_task_num()
+current_task_num <- get_current_it_task_num()
 
-existing_parquet_files = list.files(it_extract_anon_path, full.names = TRUE)
+existing_parquet_files <- list.files(it_extract_anon_path, full.names = TRUE)
 if (is.null(previous_task_num)) {
   cli::cli_alert_info("No IT Extracts from last update to be removed.")
-} else{
+} else {
   file.remove(existing_parquet_files[grepl(previous_task_num, existing_parquet_files)])
 }
 
 
 # convert csv to anon parquet ---------------------------------------------
-csv_files = list.files(it_extract_path,
-                       pattern = "SCTASK[0-9]{7}_(PIS_20[0-9]{2}|Deaths|LTCs)\\.csv(\\.gz)?",
-                       full.names = TRUE)
+csv_files <- list.files(it_extract_path,
+  pattern = "SCTASK[0-9]{7}_(PIS_20[0-9]{2}|Deaths|LTCs)\\.csv(\\.gz)?",
+  full.names = TRUE
+)
 
-convert_it_csv_to_parquet = function(csv_file) {
-  parequet_file = gsub("\\.csv(\\.gz)?$", ".parquet", csv_file)
-  data = read_file(csv_file)
-  new_file = file.path(it_extract_anon_path,
-                       paste0("anon-", basename(parequet_file)))
+convert_it_csv_to_parquet <- function(csv_file) {
+  parequet_file <- gsub("\\.csv(\\.gz)?$", ".parquet", csv_file)
+  data <- read_file(csv_file)
+  new_file <- file.path(
+    it_extract_anon_path,
+    paste0("anon-", basename(parequet_file))
+  )
   if (is_chi_in_file(csv_file)) {
-    data = data %>%
-      dplyr::rename_with( ~ paste0("chi"),
-                          tidyselect::contains("UPI", ignore.case = FALSE)) %>%
+    data <- data %>%
+      dplyr::rename_with(
+        ~ paste0("chi"),
+        tidyselect::contains("UPI", ignore.case = FALSE)
+      ) %>%
       slfhelper::get_anon_chi()
   }
   write_file(data, new_file)
@@ -71,7 +80,7 @@ convert_it_csv_to_parquet = function(csv_file) {
 }
 
 # check disk available size before moving files
-check_hscdiip_available_size = function(){
+check_hscdiip_available_size <- function() {
   # Get the disk space info for `/conf/hscdiip`
   disk_info <- system("df -h /conf/hscdiip", intern = TRUE)
 
@@ -81,9 +90,11 @@ check_hscdiip_available_size = function(){
   # Extract available space (typically the 4th column)
   available_space <- disk_values[4]
 
-  csv_files_size = paste0(round(file.size(csv_files) %>% sum() / 1024^3, 1),
-                          "G")
-  message = stringr::str_glue(
+  csv_files_size <- paste0(
+    round(file.size(csv_files) %>% sum() / 1024^3, 1),
+    "G"
+  )
+  message <- stringr::str_glue(
     "There are {available_space} avaialable in hscdiip, and the rest of operation requires around {csv_files_size}."
   )
   cli::cli_alert_warning(message)
@@ -97,10 +108,10 @@ lapply(csv_files, convert_it_csv_to_parquet)
 
 
 # zip and archive current new parquet files ----------------------------
-parquet_new_list = list.files(it_extract_anon_path, full.names = TRUE)
-parquet_new_list = parquet_new_list[grepl(current_task_num, parquet_new_list)]
+parquet_new_list <- list.files(it_extract_anon_path, full.names = TRUE)
+parquet_new_list <- parquet_new_list[grepl(current_task_num, parquet_new_list)]
 
-zip_file_path = zip::zip(
+zip_file_path <- zip::zip(
   zipfile = file.path(
     it_extract_path,
     stringr::str_glue("archive/{current_task_num}_{latest_update()}.zip")
@@ -111,7 +122,7 @@ zip_file_path = zip::zip(
 )
 
 # delete csv files in this update
-if(file.exists(zip_file_path)){
+if (file.exists(zip_file_path)) {
   file.remove(csv_files)
 }
 
