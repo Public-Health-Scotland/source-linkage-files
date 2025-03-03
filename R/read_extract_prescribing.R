192af733d5623d35745e5175a31596e25777e796
Zihao Li <lizihao_anu@outlook.com>
2025-03-03 16:30:02 +0000

    improve reading IT parquet files

--- R/read_extract_prescribing.R
+++ R/read_extract_prescribing.R
@@ -4,19 +4,9 @@
 #'
 #' @export
 read_extract_prescribing <- function(year, file_path = get_it_prescribing_path(year)) {
-  pis_file <- read_file(file_path,
-    col_type = readr::cols_only(
-      "anon_chi" = readr::col_character(),
-      "Pat DoB [C]" = readr::col_date(format = "%d-%m-%Y"),
-      "Pat Gender" = readr::col_double(),
-      "Pat Postcode [C]" = readr::col_character(),
-      "Practice Code" = readr::col_character(),
-      "Number of Paid Items" = readr::col_double(),
-      "PD Paid GIC excl. BB" = readr::col_double()
-    )
-  ) %>%
+  pis_file <- read_file(file_path) %>%
     # Rename variables
-    dplyr::rename(
+    dplyr::select(
       anon_chi = "anon_chi",
       dob = "Pat DoB [C]",
       gender = "Pat Gender",
--- R/read_it_chi_deaths.R
+++ R/read_it_chi_deaths.R
@@ -7,17 +7,15 @@
 #' @export
 #' @family process extracts
 read_it_chi_deaths <- function(file_path = get_it_deaths_path()) {
-  it_chi_deaths <- read_file(file_path,
-    col_type = readr::cols(
-      "anon_chi" = readr::col_character(),
-      "PATIENT DoD DATE (NRS)" = readr::col_date(format = "%d-%m-%Y"),
-      "PATIENT DoD DATE (CHI)" = readr::col_date(format = "%d-%m-%Y")
-    )
-  ) %>%
-    dplyr::rename(
+  it_chi_deaths <- read_file(file_path) %>%
+    dplyr::select(
       anon_chi = "anon_chi",
       death_date_nrs = "PATIENT DoD DATE (NRS)",
       death_date_chi = "PATIENT DoD DATE (CHI)"
+    ) %>%
+    dplyr::mutate(
+      death_date_nrs = lubridate::ymd(as.Date(death_date_nrs)),
+      death_date_chi = lubridate::ymd(as.Date(death_date_chi))
     )
 
   return(it_chi_deaths)
