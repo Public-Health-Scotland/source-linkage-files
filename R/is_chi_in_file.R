707493c85894fb63b21bef2d9b3de3de4771e834
lizihao-anu <97514488+lizihao-anu@users.noreply.github.com>
2025-05-07 13:51:26 +0000

    Style code

--- R/is_chi_in_file.R
+++ R/is_chi_in_file.R
@@ -6,13 +6,13 @@
 #' @return TRUE or FALSE
 is_chi_in_file <- function(filename) {
   file_type <- tools::file_ext(filename)
-  if(file_type == "csv"){
+  if (file_type == "csv") {
     data <- read.csv(filename, nrow = 1)
     return(grepl("UPI", names(data)) %>% any())
-  }else if(file_type == "parquet"){
+  } else if (file_type == "parquet") {
     ds <- arrow::open_dataset(filename)
     return(grepl("UPI", names(ds$schema$names)) %>% any())
-  }else{
+  } else {
     cli::cli_abort(stringr::str_glue("Unknown type: .{file_type}"))
   }
 }
