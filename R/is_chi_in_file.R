44e478c3c8440021968e773aafbff71a10ee09c0
Zihao Li <zihao.li@phs.scot>
2025-05-07 13:48:12 +0000

    update is_chi_in_file to allow for parquet file

--- R/is_chi_in_file.R
+++ R/is_chi_in_file.R
@@ -5,6 +5,14 @@
 #'
 #' @return TRUE or FALSE
 is_chi_in_file <- function(filename) {
-  data <- read.csv(filename, nrow = 1)
-  return(grepl("UPI", names(data)) %>% any())
+  file_type <- tools::file_ext(filename)
+  if(file_type == "csv"){
+    data <- read.csv(filename, nrow = 1)
+    return(grepl("UPI", names(data)) %>% any())
+  }else if(file_type == "parquet"){
+    ds <- arrow::open_dataset(filename)
+    return(grepl("UPI", names(ds$schema$names)) %>% any())
+  }else{
+    cli::cli_abort(stringr::str_glue("Unknown type: .{file_type}"))
+  }
 }
