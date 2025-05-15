aaa496170e87b50d3d6cb839abe6a8b2786e542d
jennit07 <Jennifer.Thom@phs.scot>
2025-05-15 13:31:24 +0000

    restructure pre-processing scripts

--- Pre_processing_scripts/00_Sort_BI_Extracts.R
+++ Pre_processing_scripts/00_Sort_BI_Extracts.R
@@ -1,5 +1,28 @@
-# Create a function to read variable names
-# load is_chi_in_file() function
+################################################################################
+# Name of file -  00_Sort_BI_Extracts.R
+#
+# Original Authors - Jennifer Thom, Zihao Li
+# Original Date - July 2024
+# Written/run on - R Posit
+# Version of R - 4.1.2
+#
+# Description:
+#     Run this script in stages to check and sort BI extracts into each year
+#     specific folder with anon_chi attached. The script will look for extracts
+#     in:
+#             /conf/sourcedev/Source_Linkage_File_Updates/Extracts Temp
+#     This will then extract the financial year from each extract and set up to
+#     move files to each respective year/extracts folder e.g:
+#             /conf/sourcedev/Source_Linkage_File_Updates/1920/Extracts
+#     Once the extract has moved this will now have the anon_chi and the file
+#     will be renamed e.g: "anon-Acute-episode-level-extract-201920.csv.gz" This
+#     will also use compression to save space in sourcedev.
+#
+################################################################################
+
+## Stage 1 - Setup environment
+#-------------------------------------------------------------------------------
+# load package
 devtools::load_all()
 
 # Define the source directory and financial year pattern
@@ -13,6 +36,11 @@ cat(stringr::str_glue("Looking in '{source_dir}' for csv files."))
 csv_files <- list.files(source_dir, pattern = ".csv", full.names = TRUE)
 print(stringr::str_glue("Found {length(csv_files)} csv files to process."))
 
+
+## Stage 2 - Setup functions
+#-------------------------------------------------------------------------------
+
+# Function 1--
 # Create a function to extract the financial year from a filename
 extract_financial_year <- function(filename) {
   match <- regexpr(pattern, basename(filename))
@@ -24,7 +52,8 @@ extract_financial_year <- function(filename) {
   }
 }
 
-# function to move files
+# Function 2--
+# Create a function to move files to the correct location
 move_temps_to_year_extract <- function(csv_file, compress_files = TRUE) {
   financial_year <- extract_financial_year(csv_file)
   # check if year directory exists
@@ -35,6 +64,7 @@ move_temps_to_year_extract <- function(csv_file, compress_files = TRUE) {
       dir.create(financial_year_dir)
     }
 
+    # Set up the new file path with the "anon-" prefix
     new_file_path <- file.path(financial_year_dir, paste0("anon-", basename(csv_file)))
 
     # set up new file path location to move each file to their destination.
@@ -63,4 +93,11 @@ move_temps_to_year_extract <- function(csv_file, compress_files = TRUE) {
   }
 }
 
+## Stage 3 - Use lapply to move the list of files to the correct folders.
+#            lapply works by applying the functions to each file.
+#-------------------------------------------------------------------------------
 lapply(csv_files, move_temps_to_year_extract, compress_files = compress_files)
+
+#-------------------------------------------------------------------------------
+
+## End of Script ##
