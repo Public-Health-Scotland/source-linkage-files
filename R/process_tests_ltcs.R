18f3ee9773d9bb6b85d2ce0d01bb2f7cce841993
Zihao Li <zihao.li@phs.scot>
2025-08-04 16:04:04 +0000

    minor changes to process_tests_ltcs

--- R/process_tests_ltcs.R
+++ R/process_tests_ltcs.R
@@ -40,10 +40,11 @@ produce_source_ltc_tests <- function(data) {
       unique_chi = dplyr::n_distinct(.data$anon_chi),
       # Sum each LTC variable
       dplyr::across(dplyr::all_of(ltc_vars), ~ sum(.x, na.rm = TRUE)),
-    tidyr::pivot_longer(
-      cols = dplyr::everything(),
-      names_to = "measure",
-      values_to = "value"
+      tidyr::pivot_longer(
+        cols = dplyr::everything(),
+        names_to = "measure",
+        values_to = "value"
+      )
     )
 
   return(join_output)
