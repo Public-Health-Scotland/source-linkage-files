74ada41229448fc29d5d4cec588eb70f705e5d8d
Oluwatobi <oluwatobi.eoni@gmail.com>
2025-08-14 12:04:28 +0000

    update check_year_valid; sparra 2526 available

--- R/00-update_refs.R
+++ R/00-update_refs.R
@@ -105,7 +105,9 @@ check_year_valid <- function(
   } else if (year >= "2425" && all(type %in% c("sds"))) {
     return(FALSE)
     ## CHECK - what data do we have available for Social Care and SPARRA?
-  } else if (year >= "2526" && all(type %in% c("nsu", "ch", "hc", "sds", "at", "sparra"))) {
+  } else if (year >= "2526" && all(type %in% c("nsu", "ch", "hc", "sds", "at"))) {
+    return(FALSE)
+  } else if (year >= "2627" && all(type %in% c("sparra"))) {
     return(FALSE)
   }
 
