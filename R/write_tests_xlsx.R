0ccdaf63b397137a257d59885f79fae706c48bb5
jennit07 <Jennifer.Thom@phs.scot>
2025-08-27 09:31:36 +0000

    Update correct permissions

--- R/write_tests_xlsx.R
+++ R/write_tests_xlsx.R
@@ -68,7 +68,15 @@ setup_tests_file_name <- function(sheet_name,
     tests_to_combine
   )
   if (!exists(folder_path)) {
-    fs::dir_create(folder_path)
+    fs::dir_create(folder_path, mode = "u=rwx,go=rwx")
+  }
+
+  if (fs::file_info(path = folder_path)$user == Sys.getenv("USER")) {
+    # Set the correct permissions (read, write, execute)
+    fs::file_chmod(path = folder_path, mode = "770")
+    # change the owner so that hscdiip is the group owner.
+    # use fs::group_ids() for checking
+    fs::file_chown(path = folder_path, group_id = 3206)
   }
 
   tests_workbook_path <- fs::path(folder_path,
