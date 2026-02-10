ef786d5ae2b8ad1d5c55b149f85347fc980e5ab7
Zihao Li <zihao.li@phs.scot>
2026-02-10 12:28:30 +0000

    automate UAT testing

---
+++ UAT_test_scripts/02_uat_fn_script.R
@@ -0,0 +1,34 @@
+
+# Read in Test data
+sdl_data <- as_tibble(dbGetQuery(
+  denodo_connect,
+  glue::glue("select * from sdl.{sdl_name} LIMIT 100")
+))
+
+# Read boxi data
+boxi_data <- get(fn_name)(year = "1920")
+
+# Read denodo variables for renaming SLF variables
+denodo_vars <- readxl::read_excel(get_slf_variable_lookup(),
+                                  sheet = dataset_name
+)
+
+
+#-------------------------------------------------------------------------------
+
+## Create Output --------
+dataset_output <- create_uat_output(
+  dataset_name = dataset_name,
+  boxi_data = boxi_data,
+  sdl_data = sdl_data,
+  denodo_vars = denodo_vars
+)
+
+## Write to Excel workbook
+dataset_output %>%
+  write_uat_tests(
+    sheet_name = dataset_name,
+    analyst = analyst
+  )
+
+# End of Script #
---
+++ UAT_test_scripts/uat_all.R
@@ -0,0 +1,60 @@
+## Setup environment ---
+rm(list = ls())
+
+# Load createslf package
+devtools::load_all(".")
+
+# Read in the package libraries
+library(DBI)
+library(odbc)
+library(dplyr)
+library(tibble)
+library(purrr)
+library("writexl")
+library("openxlsx")
+library("readxl")
+
+# Open a connection to DVPREPROD (test environment)
+# or DVPROD (production environment)
+denodo_connect <- suppressWarnings(
+  dbConnect(odbc(),
+            dsn = "DVPREPROD", # or DVPROD
+            uid = .rs.askForPassword("Username:"),
+            pwd = .rs.askForPassword("Enter your test environment password")
+  )
+)
+
+# Source functions
+source(here::here("UAT_test_scripts/01_uat_functions.R"))
+
+#-------------------------------------------------------------------------------
+
+## Setup data --------------
+
+# Analyst name for folder structure
+analyst <- "Zihao"
+
+name_list <- openxlsx::read.xlsx(
+  file.path(
+    get_dev_dir(),
+    "uat_testing",
+    "1_source_data_views",
+    "Lookups",
+    "uat_names.xlsx"
+  )
+)
+
+datasets = c("maternity", "ae")
+name_list <- name_list %>%
+  dplyr::filter(dataset_list %in% datasets)
+
+
+for(ii in 1:nrow(name_list)){
+  dataset_name = name_list$dataset_list[ii]
+  fn_name = name_list$fn_list[ii]
+  sdl_name = name_list$sdl_list[ii]
+
+  source(here::here("UAT_test_scripts/02_uat_fn_script.R"))
+}
+
+DBI::dbDisconnect(denodo_connect)
