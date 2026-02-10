f746f5a9291735e6e151ce0c96bb97618fbbe019
Oluwatobi <oluwatobi.eoni@gmail.com>
2026-02-10 11:17:00 +0000

    add code for descriptive statistics

---
+++ UAT_test_scripts/uat_descriptive_stats.R
@@ -0,0 +1,60 @@
+################################################################################
+# Name of file - uat_descriptive_stats.R
+#
+# Original Authors - Oluwatobi Oni
+# Original Date - January 2026
+# Written/run on - R Posit
+# Version of R - 4.4.2
+#
+# Description: Functions used to support the BYOC UAT tests.
+#
+################################################################################
+
+library(openxlsx)
+library(dplyr)
+
+devtools::load_all()
+
+# 1. Define the lists to loop through
+years <- c("1415", "1516", "1617", "1718", "1819", "1920", "2021", "2122", "2223", "2324", "2425", "2526")
+types <- c("acute", "ae", "at", "ch", "client", "cmh", "dd", "deaths", "dn", "gp_ooh", "hc", "homelessness", "maternity", "mh", "outpatients", "pis", "sds")
+
+# 2. Create an empty list to hold the results
+results_list <- list()
+
+# 3. Start the loop
+for (t in types) {
+  for (y in years) {
+
+    message("Currently processing: ", t, " for fy ", y)
+
+    # Get the file path and read the data
+    file_path <- get_source_extract_path(y, type = t)
+    data <- read_file(file_path)
+
+    # Descriptive Statistics Calculations
+    rows <- nrow(data)
+    cols <- ncol(data)
+    nas  <- sum(is.na(data))
+    prop <- nas / (rows * cols)
+
+    # Store the results
+    results_list[[paste(t, y)]] <- data.frame(
+      Dataset = t,
+      FY = y,
+      Number_of_Rows = rows,
+      Number_of_Columns = cols,
+      Proportion_of_NA = prop
+    )
+
+    # Remove the big data object and clean memory before the next loop to save memory
+    rm(data)
+    gc()
+  }
+}
+
+# 4. Combine all the small results into one big table
+final_table <- do.call(rbind, results_list)
+
+# 5. Save to Excel
+write.xlsx(final_table, "/conf/sourcedev/Source_Linkage_File_Updates/uat_testing/4_dataset_testing/UAT_support.xlsx", sheetName = "UAT support")
