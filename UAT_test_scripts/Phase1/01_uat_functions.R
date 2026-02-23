409704f4791620a76798800556fb5eb86ceb50c5
jennit07 <Jennifer.Thom@phs.scot>
2026-02-23 11:02:46 +0000

    Move scripts from create_uat_test_scripts to this branch

---
+++ UAT_test_scripts/Phase1/01_uat_functions.R
@@ -0,0 +1,184 @@
+################################################################################
+# Name of file - 01_uat_functions.R
+#
+# Original Authors - Jennifer Thom
+# Original Date - January 2026
+# Written/run on - R Posit
+# Version of R - 4.4.2
+#
+# Description: Functions used to support the UAT test scrips.
+#
+################################################################################
+
+# function for file path
+get_slf_variable_lookup <- function() {
+  uat_dir <- fs::path("/", "conf", "sourcedev", "Source_Linkage_File_Updates", "uat_testing", "1_source_data_views", "Lookups")
+  file_name <- "SLF_variable_lookup.xlsx"
+  path <- stringr::str_glue("{uat_dir}/{file_name}")
+
+  return(path)
+}
+
+
+get_name_list_lookup <- function() {
+  uat_dir <- fs::path("/", "conf", "sourcedev", "Source_Linkage_File_Updates", "uat_testing", "1_source_data_views", "Lookups")
+  file_name <- "uat_names.xlsx"
+  path <- stringr::str_glue("{uat_dir}/{file_name}")
+
+  return(path)
+}
+
+
+## Main function to create test output.
+create_uat_output <- function(dataset_name, boxi_data, sdl_data, denodo_vars) {
+  # Create a tibble containing info on boxi cols
+  boxi_cols <- tibble(
+    date = Sys.Date(),
+    dataset_name,
+    cols = colnames(boxi_data),
+    origin_boxi = "boxi",
+    boxi_type = sapply(boxi_data, function(z) class(z)[1]),
+    total_cols_boxi = ncol(boxi_data)
+  )
+
+  check_vars <- full_join(boxi_cols, denodo_vars, by = "cols") %>%
+    # Create a flag to check if there are unused variables in the boxi extract
+    mutate(
+      not_used_boxi_extract = if_else((!is.na(cols) & is.na(denodo_name)), 1, 0),
+      # if no denodo name available, use boxi name - dont want to drop variables here
+      # should be the unused variables flagged above
+      denodo_name = if_else(is.na(denodo_name), cols, denodo_name),
+      # count number of boxi cols
+      count = 1L,
+      total_cols_boxi = sum(count),
+      # count number of boxi cols not used
+      total_unused_cols_boxi = sum(not_used_boxi_extract)
+    ) %>%
+    select(c("date", "dataset_name", "denodo_name", "origin_boxi", "boxi_type", "total_cols_boxi", "not_used_boxi_extract")) %>%
+    rename(cols = "denodo_name")
+
+  boxi_cols <- check_vars
+
+  # Create a tibble containing info on sdl cols
+  sdl_cols <- tibble(
+    date = Sys.Date(),
+    dataset_name,
+    cols = colnames(sdl_data),
+    origin_sdl = "sdl",
+    sdl_type = sapply(sdl_data, function(z) class(z)[1]),
+    total_cols_sdl = ncol(sdl_data)
+  )
+
+  # Do a full join of both dataframes
+  test_output <- full_join(boxi_cols, sdl_cols, by = c("date", "dataset_name", "cols")) %>%
+    mutate(
+      # Check - do the data types match?
+      type_match = boxi_type == sdl_type,
+      # Check - are the boxi cols present in the sdl view?
+      is_boxi_in_sdl = case_when(
+        origin_boxi != origin_sdl ~ "Yes",
+        (origin_boxi == "boxi" & is.na(origin_sdl)) ~ "No - boxi not in sdl",
+        (is.na(origin_boxi) & origin_sdl == "sdl") ~ "No - sdl not in boxi",
+        TRUE ~ "No"
+      ),
+      # Check if names match boxi/sdl
+      names_expected_in_sdl = if_else((origin_boxi == "boxi" & origin_sdl == "sdl"), "Yes", "No"),
+      # Flag mismatching variables
+      mismatched_col = if_else((is_boxi_in_sdl == "No - boxi not in sdl" |
+                                  is_boxi_in_sdl == "No - sdl not in boxi"),
+                               1L, 0L
+      ),
+      # Check status
+      test_status = case_when(
+        (is_boxi_in_sdl == "No - boxi not in sdl" & is.na(origin_sdl) & origin_boxi == "boxi") ~ "FAIL: Missing in sdl view",
+        (is_boxi_in_sdl == "No - sdl not in boxi" & is.na(origin_boxi) & origin_sdl == "sdl") ~ "FAIL: Missing in boxi view",
+        names_expected_in_sdl == "No" ~ "FAIL: SDL names not as expected",
+        !type_match ~ "FAIL: Data Type does not match",
+        TRUE ~ "PASS"
+      )
+    )
+
+  return(test_output)
+}
+
+
+#### Function for writing uat tests to disk
+write_uat_tests <- function(uat_data, sheet_name, analyst) {
+  tests_workbook_path <-
+    stringr::str_glue("/conf/sourcedev/Source_Linkage_File_Updates/uat_testing/1_source_data_views/{analyst}/uat_tests.xlsx")
+
+  if (fs::file_exists(tests_workbook_path)) {
+    # Load the data from the existing workbook
+    wb <- openxlsx::loadWorkbook(tests_workbook_path)
+  } else {
+    # Create a blank workbook object
+    wb <- openxlsx::createWorkbook()
+  }
+
+  # add a new sheet for tests
+  date_today <- format(Sys.Date(), "%d_%b")
+  date_today <- stringr::str_to_lower(date_today)
+  sheet_name_dated <- stringr::str_glue("{sheet_name}_{date_today}")
+
+  # If there has already been a sheet created today, append the time
+  if (sheet_name_dated %in% names(wb)) {
+    sheet_name_dated <-
+      paste0(sheet_name_dated, format(Sys.time(), "_%H%M"))
+  }
+
+  # Add new sheet
+  openxlsx::addWorksheet(wb, sheet_name_dated)
+
+  openxlsx::setColWidths(
+    wb = wb,
+    sheet = sheet_name_dated,
+    cols = 2L:ncol(uat_data),
+    widths = "auto"
+  )
+
+  status_col <- which(names(uat_data) == "test_status")
+
+  # Apply conditional formatting to highlight issues
+  openxlsx::conditionalFormatting(
+    wb = wb,
+    sheet = sheet_name_dated,
+    cols = status_col,
+    rows = 2L:(nrow(uat_data) + 1L),
+    rule = "FAIL",
+    type = "contains"
+  )
+
+  # write uat output to the new sheet
+  openxlsx::writeDataTable(
+    wb = wb,
+    sheet = sheet_name_dated,
+    x = uat_data,
+    tableStyle = "TableStyleMedium19",
+    withFilter = FALSE
+  )
+
+  # Reorder the sheets alphabetically
+  sheet_names <- wb$sheet_names
+  names(sheet_names) <- wb$sheetOrder
+
+  openxlsx::worksheetOrder(wb) <- names(sort(sheet_names))
+
+  # Write the data to the workbook on disk
+  openxlsx::saveWorkbook(wb,
+                         tests_workbook_path,
+                         overwrite = TRUE
+  )
+
+  log_info(
+    "UAT {dataset_name} written to {tests_workbook_path}"
+  )
+
+  if (fs::file_info(path = tests_workbook_path)$user == Sys.getenv("USER")) {
+    # Set the correct permissions (read, write, execute)
+    fs::file_chmod(path = tests_workbook_path, mode = "770")
+    # change the owner so that hscdiip is the group owner.
+    # use fs::group_ids() for checking
+    fs::file_chown(path = tests_workbook_path, group_id = 3206)
+  }
+}
+
---
+++ UAT_test_scripts/Phase1/uat_all.R
@@ -0,0 +1,94 @@
+################################################################################
+# Name of file - uat_all.R
+#
+# Original Authors - Jennifer Thom
+# Original Date - January 2026
+# Written/run on - R Posit
+# Version of R - 4.4.2
+#
+# Description: Functions used to support the UAT test scrips.
+#
+#################################################################################
+
+# Setup environment ---
+rm(list = ls())
+
+# Load createslf package
+devtools::load_all(".")
+
+# Read in the package libraries
+library(DBI)
+library(odbc)
+library(dplyr)
+library(tibble)
+library(purrr)
+library("writexl")
+library("openxlsx")
+library("readxl")
+library(logger)
+
+# Open a connection to DVPREPROD (test environment)
+# or DVPROD (production environment)
+denodo_connect <- suppressWarnings(
+  dbConnect(odbc(),
+            dsn = "DVPREPROD", # or DVPROD
+            uid = .rs.askForPassword("Username:"),
+            pwd = .rs.askForPassword("Enter your test environment password")
+  )
+)
+
+# Source functions
+source(here::here("UAT_test_scripts/01_uat_functions.R"))
+
+#-------------------------------------------------------------------------------
+
+## Setup data --------------
+
+# Analyst name for folder structure
+analyst <- "Jen"
+
+datasets <- c("acute", "nrs_deaths", "gp_ooh_consultations")
+
+# Read name list for matching each dataset function/sdl name
+name_list <- readxl::read_excel(get_name_list_lookup()) %>%
+  dplyr::filter(dataset_list %in% datasets) %>%
+  dplyr::arrange(dataset_list)
+
+
+for (ii in 1:nrow(name_list)) {
+  dataset_name <- name_list$dataset_list[ii]
+  fn_name <- name_list$fn_list[ii]
+  sdl_name <- name_list$sdl_list[ii]
+
+  log_info(
+    "UAT run {ii}/{nrow(name_list)} | dataset = {dataset_name}, function = {fn_name}, sdl_name = {sdl_name}"
+  )
+
+  source(here::here("UAT_test_scripts/02_uat_fn_script.R"))
+}
+
+
+################################################################################
+## DEBUGGING
+
+# If you need to run one dataset within this loop, assign ii to the row number
+# and run the following code. This will put everything you need into the
+# environment then you can run through 02_uat_fn_script and 01_uat_functions
+# to see where the problem is
+
+# Assign row number (returns the dataset you want to look at in names_list)
+# ii <- 3
+
+# After Assigning ii as the row number you want to investigate, run the
+# following code:
+# dataset_name <- name_list$dataset_list[ii]
+# fn_name <- name_list$fn_list[ii]
+# sdl_name <- name_list$sdl_list[ii]
+
+# Now run lines 1-13 in 02_uat_fn_script.r and line by line in
+# 01_uat_functions.r to debug
+
+#################################################################################
+
+DBI::dbDisconnect(denodo_connect)
+
