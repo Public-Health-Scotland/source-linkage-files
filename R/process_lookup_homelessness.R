e81a9a58f71e3c6f62ae172435e7a5cb611c36e0
LucyEmma22 <lucyemma02@gmail.com>
2026-03-11 17:55:35 +0000

    Fix depreciation warning for create_homelessness_lookup in process_lookup_homelessness

--- R/process_lookup_homelessness.R
+++ R/process_lookup_homelessness.R
@@ -20,7 +20,7 @@ create_homelessness_lookup <- function(
   }
   homelessness_lookup <- homelessness_data %>%
     dplyr::distinct(.data$anon_chi, .data$record_keydate1, .data$record_keydate2) %>%
-    tidyr::drop_na(.data$anon_chi) %>%
+    tidyr::drop_na(anon_chi) %>%
     dplyr::mutate(hl1_in_fy = 1L)
 
   cli::cli_alert_info("Create homelessness lookup function finished at {Sys.time()}")
