d7925674a44a53f8e9d1e9597b90662854e69f89
LucyEmma22 <lucyemma02@gmail.com>
2026-03-12 18:31:46 +0000

    Fix depreciation warnings for produce_slf_postcode_tests in process_tests_lookup_pc

--- R/process_tests_lookup_pc.R
+++ R/process_tests_lookup_pc.R
@@ -43,7 +43,7 @@ produce_slf_postcode_tests <- function(data) {
     # create other test flags
     dplyr::mutate(n_postcode = 1L) %>%
     # remove variables that are not test flags
-    dplyr::select(c(.data$NHS_Ayrshire_and_Arran:.data$n_postcode)) %>%
+    dplyr::select(c(NHS_Ayrshire_and_Arran:n_postcode)) %>%
     # use function to sum new test flags
     calculate_measures(measure = "sum")
 }
