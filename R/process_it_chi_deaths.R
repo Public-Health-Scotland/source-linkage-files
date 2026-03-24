65a68b352fe36bf4abadbdc2ed661f09591c581f
LucyEmma22 <lucyemma02@gmail.com>
2026-03-24 17:47:34 +0000

    Draft refactoring of process_it_chi_deaths

--- R/process_it_chi_deaths.R
+++ R/process_it_chi_deaths.R
@@ -10,7 +10,15 @@
 #' @return the final data as a [tibble][tibble::tibble-package].
 #' @export
 #' @family process extracts
-process_it_chi_deaths <- function(data, write_to_disk = TRUE) {
+process_it_chi_deaths <- function(data,
+                                  write_to_disk = TRUE,
+                                  BYOC_MODE = FALSE,
+                                  run_id = NA,
+                                  run_date_time = NA
+                                  ) {
+
+  log_slf_event(stage = "process", status = "start", type = "it_chi_deaths", year = "all")
+
   it_chi_deaths_clean <- data %>%
     dplyr::arrange(
       dplyr::desc(.data$death_date_nrs),
@@ -19,17 +27,31 @@ process_it_chi_deaths <- function(data, write_to_disk = TRUE) {
     dplyr::distinct(.data$anon_chi, .keep_all = TRUE) %>%
     # remove death_date_nrs as this is the nrs weekly unvalidated data and we should not use this.
     # the boxi nrs death date is more reliable as this is provided monthly and is validated.
-    dplyr::select(.data$anon_chi, .data$death_date_chi) %>%
     dplyr::mutate(
-      death_date_chi = lubridate::ymd(.data$death_date_chi)
-    )
+      death_date_chi = lubridate::ymd(.data$death_date_chi),
+      run_id = run_id,
+      run_date_time = run_date_time
+    ) %>%
+    dplyr::select(
+      "run_id",
+      "run_date_time",
+      "anon_chi",
+      "death_date_chi"
+      )
 
   if (write_to_disk) {
-    it_chi_deaths_clean %>%
-      write_file(get_slf_chi_deaths_path(check_mode = "write"),
-        group_id = 3206 # hscdiip owner
-      )
+    write_file(
+      it_chi_deaths_clean,
+      get_slf_chi_deaths_path(
+        BYOC_MODE,
+        check_mode = "write"
+      ),
+      BYOC_MODE = BYOC_MODE,
+      group_id = 3206 # hscdiip owner
+    )
   }
+
+  log_slf_event(stage = "process", status = "complete", type = "it_chi_deaths", year = "all")
 
   return(it_chi_deaths_clean)
 }
