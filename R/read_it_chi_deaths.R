033a3c6182366f4a3e3f8be0c9844158ccee50a9
LucyEmma22 <lucyemma02@gmail.com>
2026-03-24 17:47:11 +0000

    Draft refactoring of read_it_chi_deaths

--- R/read_it_chi_deaths.R
+++ R/read_it_chi_deaths.R
@@ -6,17 +6,31 @@
 #' @return the final data as a [tibble][tibble::tibble-package].
 #' @export
 #' @family process extracts
-read_it_chi_deaths <- function(file_path = get_it_deaths_path()) {
-  it_chi_deaths <- read_file(file_path) %>%
+read_it_chi_deaths <- function(denodo_connect = get_denodo_connection(BYOC_MODE = BYOC_MODE),
+                               file_path = get_it_deaths_path(BYOC_MODE = BYOC_MODE), ## CHECK + Is this needed?
+                               BYOC_MODE ## Is this needed?
+                               ) {
+
+  log_slf_event(stage = "read", status = "start", type = "it_chi_deaths", year = "all")
+
+  on.exit(try(DBI::dbDisconnect(denodo_connect), silent = TRUE), add = TRUE)
+
+  it_chi_deaths <- dplyr::tbl(
+    denodo_connect,
+    dbplyr::in_schema("sdl", "sdl_XXX")) %>% ## PLACEHOLDER
     dplyr::select(
-      anon_chi = "anon_chi",
-      death_date_nrs = "PATIENT DoD DATE (NRS)",
-      death_date_chi = "PATIENT DoD DATE (CHI)"
-    ) %>%
+      anon_chi = "patient_upi", ## CHECK
+      death_date_nrs = "patient_dod_nrs", ## CHECK
+      death_date_chi = "patient_dod_chi" ## CHECK
+      ) %>%
     dplyr::mutate(
       death_date_nrs = lubridate::dmy(.data$death_date_nrs),
       death_date_chi = lubridate::dmy(.data$death_date_chi)
-    )
+      ) %>%
+    dplyr::collect() %>%
+    slfhelper::get_anon_chi("anon_chi") ## CHECK
+
+  log_slf_event(stage = "read", status = "complete", type = "it_chi_deaths", year = "all")
 
   return(it_chi_deaths)
 }
