5f4266705ab54c62d1f912082214b111ecf32675
OluwatobiOni <108776112+OluwatobiOni@users.noreply.github.com>
2026-04-07 17:43:57 +0000

    Style code

--- R/read_extract_gp_ooh.R
+++ R/read_extract_gp_ooh.R
@@ -13,32 +13,36 @@
 #' @export
 #' @family process extracts
 read_extract_gp_ooh <- function(
-    year,
-    denodo_connect = get_denodo_connection(BYOC_MODE = BYOC_MODE),
-    diagnosis_path = get_boxi_extract_path(year = year, type = "gp_ooh-d", BYOC_MODE = BYOC_MODE),
-    outcomes_path = get_boxi_extract_path(year = year, type = "gp_ooh-o", BYOC_MODE = BYOC_MODE),
-    consultations_path = get_boxi_extract_path(year = year, type = "gp_ooh-c", BYOC_MODE = BYOC_MODE),
-    BYOC_MODE
+  year,
+  denodo_connect = get_denodo_connection(BYOC_MODE = BYOC_MODE),
+  diagnosis_path = get_boxi_extract_path(year = year, type = "gp_ooh-d", BYOC_MODE = BYOC_MODE),
+  outcomes_path = get_boxi_extract_path(year = year, type = "gp_ooh-o", BYOC_MODE = BYOC_MODE),
+  consultations_path = get_boxi_extract_path(year = year, type = "gp_ooh-c", BYOC_MODE = BYOC_MODE),
+  BYOC_MODE
 ) {
   log_slf_event(stage = "read", status = "start", type = "gpooh", year = year)
 
   on.exit(try(DBI::dbDisconnect(denodo_connect), silent = TRUE), add = TRUE)
 
   ooh_extracts <- list(
-    "diagnosis"     = read_extract_ooh_diagnosis(year = year,
-                                                 denodo_connect = denodo_connect,
-                                                 file_path = diagnosis_path,
-                                                 BYOC_MODE = BYOC_MODE),
-
-    "outcomes"      = read_extract_ooh_outcomes(year = year,
-                                                denodo_connect = denodo_connect,
-                                                file_path = outcomes_path,
-                                                BYOC_MODE = BYOC_MODE),
-
-    "consultations" = read_extract_ooh_consultations(year = year,
-                                                     denodo_connect = denodo_connect,
-                                                     file_path = consultations_path,
-                                                     BYOC_MODE = BYOC_MODE)
+    "diagnosis" = read_extract_ooh_diagnosis(
+      year = year,
+      denodo_connect = denodo_connect,
+      file_path = diagnosis_path,
+      BYOC_MODE = BYOC_MODE
+    ),
+    "outcomes" = read_extract_ooh_outcomes(
+      year = year,
+      denodo_connect = denodo_connect,
+      file_path = outcomes_path,
+      BYOC_MODE = BYOC_MODE
+    ),
+    "consultations" = read_extract_ooh_consultations(
+      year = year,
+      denodo_connect = denodo_connect,
+      file_path = consultations_path,
+      BYOC_MODE = BYOC_MODE
+    )
   )
 
   log_slf_event(stage = "read", status = "complete", type = "gpooh", year = year)
