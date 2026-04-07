72b13bafeda70b7c170bcda77c642b7503cf9db7
Zihao Li <zihao.li@phs.scot>
2026-04-07 11:46:19 +0000

    refactor read_extract_nrs_deaths

--- R/read_extract_nrs_deaths.R
+++ R/read_extract_nrs_deaths.R
@@ -4,69 +4,69 @@
 #'
 #' @export
 read_extract_nrs_deaths <- function(
-  year,
-  file_path = get_boxi_extract_path(year = year, type = "deaths")
+    year,
+    denodo_connect = get_denodo_connect(BYOC_MODE = BYOC_MODE),
+    file_path = get_boxi_extract_path(year, type = "deaths", BYOC_MODE = BYOC_MODE),
+    BYOC_MODE
 ) {
-  log_slf_event(stage = "read", status = "start", type = "deaths", year = year)
+  year = check_year_format(year, "fyyear")
+  c_year = convert_fyyear_to_year(year)
 
-  extract_nrs_deaths <- read_file(file_path,
-    col_types = readr::cols_only(
-      "Death Location Code" = readr::col_character(),
-      "Geo Council Area Code" = readr::col_character(),
-      "Geo Postcode [C]" = readr::col_character(),
-      "Geo HSCP of Residence Code - current" = readr::col_character(),
-      "NHS Board of Occurrence Code - current" = readr::col_character(),
-      "NHS Board of Residence Code - current" = readr::col_character(),
-      "Pat Date Of Birth [C]" = readr::col_date(format = "%Y/%m/%d %T"),
-      "Date of Death(99)" = readr::col_date(format = "%Y/%m/%d %T"),
-      "Pat Gender Code" = readr::col_double(),
-      "anon_chi" = readr::col_character(),
-      "Place Death Occurred Code" = readr::col_character(),
-      "Post Mortem Code" = readr::col_character(),
-      "Prim Cause of Death Code (6 char)" = readr::col_character(),
-      "Sec Cause of Death 0 Code (6 char)" = readr::col_character(),
-      "Sec Cause of Death 1 Code (6 char)" = readr::col_character(),
-      "Sec Cause of Death 2 Code (6 char)" = readr::col_character(),
-      "Sec Cause of Death 3 Code (6 char)" = readr::col_character(),
-      "Sec Cause of Death 4 Code (6 char)" = readr::col_character(),
-      "Sec Cause of Death 5 Code (6 char)" = readr::col_character(),
-      "Sec Cause of Death 6 Code (6 char)" = readr::col_character(),
-      "Sec Cause of Death 7 Code (6 char)" = readr::col_character(),
-      "Sec Cause of Death 8 Code (6 char)" = readr::col_character(),
-      "Sec Cause of Death 9 Code (6 char)" = readr::col_character(),
-      "Unique Record Identifier" = readr::col_character(),
-      "GP practice code(99)" = readr::col_character()
-    )
+  log_slf_event(
+    stage = "read",
+    status = "start",
+    type = "deaths",
+    year = year
+  )
+
+  on.exit(try(DBI::dbDisconnect(denodo_connect), silent = TRUE), add = TRUE)
+
+  extract_nrs_deaths <- dplyr::tbl(
+    denodo_connect,
+    # TODO: check table name
+    dbplyr::in_schema("sdl", "sdl_nrs_deaths_episodes")
   ) %>%
-    dplyr::rename(
-      death_location_code = "Death Location Code",
-      lca = "Geo Council Area Code",
-      postcode = "Geo Postcode [C]",
-      hscp = "Geo HSCP of Residence Code - current",
-      death_board_occurrence = "NHS Board of Occurrence Code - current",
-      hbrescode = "NHS Board of Residence Code - current",
-      dob = "Pat Date Of Birth [C]",
-      record_keydate1 = "Date of Death(99)",
-      gender = "Pat Gender Code",
-      anon_chi = "anon_chi",
-      place_death_occurred = "Place Death Occurred Code",
-      post_mortem = "Post Mortem Code",
-      deathdiag1 = "Prim Cause of Death Code (6 char)",
-      deathdiag2 = "Sec Cause of Death 0 Code (6 char)",
-      deathdiag3 = "Sec Cause of Death 1 Code (6 char)",
-      deathdiag4 = "Sec Cause of Death 2 Code (6 char)",
-      deathdiag5 = "Sec Cause of Death 3 Code (6 char)",
-      deathdiag6 = "Sec Cause of Death 4 Code (6 char)",
-      deathdiag7 = "Sec Cause of Death 5 Code (6 char)",
-      deathdiag8 = "Sec Cause of Death 6 Code (6 char)",
-      deathdiag9 = "Sec Cause of Death 7 Code (6 char)",
-      deathdiag10 = "Sec Cause of Death 8 Code (6 char)",
-      deathdiag11 = "Sec Cause of Death 9 Code (6 char)",
-      uri = "Unique Record Identifier",
-      gpprac = "GP practice code(99)"
-    )
+    dplyr::select(
+      death_location_code = "death_location_code",
+      lca = "geo_council_area_code",
+      postcode = "geo_postcode",
+      hscp = "geo_hscp_of_residence_code_curr",
+      death_board_occurrence = "nhs_board_of_occurrence_code_curr",
+      hbrescode = "nhs_board_of_residence_code_curr",
+      dob = "patient_dob",
+      record_keydate1 = "patient_dod",
+      gender = "pat_gender_code",
+      chi = "patient_chi",
+      place_death_occurred = "place_death_occurred_code",
+      post_mortem = "post_mortem_code",
+      deathdiag1 = "primary_cause_of_death_code",
+      deathdiag2 = "secondary_cause_of_death_0_code",
+      deathdiag3 = "secondary_cause_of_death_1_code",
+      deathdiag4 = "secondary_cause_of_death_2_code",
+      deathdiag5 = "secondary_cause_of_death_3_code",
+      deathdiag6 = "secondary_cause_of_death_4_code",
+      deathdiag7 = "secondary_cause_of_death_5_code",
+      deathdiag8 = "secondary_cause_of_death_6_code",
+      deathdiag9 = "secondary_cause_of_death_7_code",
+      deathdiag10 = "secondary_cause_of_death_8_code",
+      deathdiag11 = "secondary_cause_of_death_9_code",
+      uri = "unique_record_identifier",
+      gpprac = "gp_practice_code",
+      date_of_death_financial_year = "date_of_death_financial_year"
+    ) %>%
+    dplyr::filter(
+      date_of_death_financial_year == c_year
+    ) %>%
+    dplyr::select(-"date_of_death_financial_year") %>%
+    dplyr::collect() %>%
+    slfhelper::get_anon_chi()
 
-  log_slf_event(stage = "read", status = "complete", type = "deaths", year = year)
+  log_slf_event(
+    stage = "read",
+    status = "complete",
+    type = "deaths",
+    year = year
+  )
 
   return(extract_nrs_deaths)
 }
