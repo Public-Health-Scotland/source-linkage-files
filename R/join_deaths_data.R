de5da16b20519bf37980c808ef969591ef9acb32
Zihao Li <zihao.li@phs.scot>
2026-04-08 11:18:30 +0000

    refactor process_lookup_deaths delete process_lookup_deaths and move functionality to join_deaths_data in create_episode_file

--- R/join_deaths_data.R
+++ R/join_deaths_data.R
@@ -2,15 +2,23 @@
 #'
 #' @param data Episode file data
 #' @param year financial year, e.g. '1920'
-#' @param slf_deaths_lookup The SLF deaths lookup.
+#' @param BYOC_MODE BYOC mode
 #'
 #' @return The data including the deaths lookup matched
 #'         on to the episode file.
 join_deaths_data <- function(
   data,
   year,
-  slf_deaths_lookup = read_file(get_slf_deaths_lookup_path(year))
+  BYOC_MODE = FALSE
 ) {
+  slf_deaths_lookup = read_file(get_combined_slf_deaths_lookup_path(BYOC_MODE = BYOC_MODE)) %>%
+    # Filter the chi death dates to the FY as the lookup is by FY
+    dplyr::filter(fy == year) %>%
+    # use the BOXI NRS death date by default, but if it's missing, use the chi death date.
+    dplyr::mutate(
+      deceased = TRUE
+    )
+
   data <- data %>%
     dplyr::left_join(
       slf_deaths_lookup %>%
