84147123a480bafb4d207ff0a022de7d06dabceb
Zihao Li <zihao.li@phs.scot>
2026-04-22 14:18:02 +0000

    dummy targets and run_sdl

--- SDL_process/run_sdl.r
+++ SDL_process/run_sdl.r
@@ -91,7 +91,7 @@ year <- "1920"
 ## Build BYOC Output File Paths ----
 byoc_output_files <- get_byoc_output_files(
   year,
-  types = c("maternity", "mh")
+  types = c("ltcs")
 )
 # using homelessness for test purpose. When development is complete,
 # we change to "types = "byoc_input_files""
