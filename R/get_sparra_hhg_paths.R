7e6a713d8a51297f3177bdda759f8e3d1d2f91eb
jennit07 <Jennifer.Thom@phs.scot>
2026-04-30 15:38:20 +0000

    New fn `get_sparra_data`

--- R/get_sparra_hhg_paths.R
+++ R/get_sparra_hhg_paths.R
@@ -47,3 +47,35 @@ get_sparra_path <- function(year, ...) {
 
   return(sparra_file_path)
 }
+
+
+#' SPARRA Extract File data
+#'
+#' @description Get the path to the SPARRA extract
+#'
+#' @param year Year of extract
+#' @param denodo_connect connection to denodo
+#' @param BYOC_MODE BYOC MODE
+#'
+#' @return The path to the SPARRA extract as an [fs::path()]
+#' @export
+#' @family extract file paths
+#' @seealso [get_file_path()] for the generic function.
+get_sparra_data <- function(year,
+                            denodo_connect = get_denodo_connection(BYOC_MODE = BYOC_MODE),
+                            BYOC_MODE) {
+
+
+  if (isTRUE(BYOC_MODE)) {
+    extract_sparra <- dplyr::tbl(
+      denodo_connect,
+      dbplyr::in_schema("sdl", "sdl_sparra_source")
+    ) %>%
+      ## TODO: Check filters/sparra risk year ##
+      collect()
+  } else {
+    extract_sparra <- read_file(get_sparra_path(year))
+  }
+  return(extract_sparra)
+}
+
