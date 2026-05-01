2d11bbbd664b157d2daccf62123d44c7d6cb286a
Oluwatobi <oluwatobi.eoni@gmail.com>
2026-05-01 15:48:11 +0000

    refactor targets for AT

--- _targets.R
+++ _targets.R
@@ -342,7 +342,10 @@ list(
     # Target name
     all_at_extract,
     # Function
-    read_sc_all_alarms_telecare(),
+    read_sc_all_alarms_telecare(
+      denodo_connect = get_denodo_connection(BYOC_MODE = BYOC_MODE),
+      BYOC_MODE = BYOC_MODE
+    ),
     cue = tar_cue_age(
       name = all_at_extract,
       age = as.difftime(28.0, units = "days")
@@ -356,6 +359,9 @@ list(
     process_sc_all_alarms_telecare(
       all_at_extract,
       sc_demog_lookup = sc_demog_lookup,
+      BYOC_MODE = BYOC_MODE,
+      run_id = run_id,
+      run_date_time = run_date_time,
       write_to_disk = write_to_disk
     ),
     priority = 0.5
@@ -1023,6 +1029,9 @@ list(
       process_extract_alarms_telecare(
         data = all_at,
         year = year,
+        BYOC_MODE = BYOC_MODE,
+        run_id = run_id,
+        run_date_time = run_date_time,
         write_to_disk = write_to_disk
       )
     ),
