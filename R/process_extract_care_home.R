0dc12b84a2df420dae6b39c67808077de5797e00
Jennit07 <67372904+Jennit07@users.noreply.github.com>
2026-05-07 14:36:01 +0000

    Style code

--- R/process_extract_care_home.R
+++ R/process_extract_care_home.R
@@ -18,13 +18,14 @@
 #' @export
 #' @family process extracts
 process_extract_care_home <- function(
-    data,
-    year,
-    ch_costs = read_file(get_ch_costs_path(BYOC_MODE)),
-    BYOC_MODE = FALSE,
-    run_id = NA,
-    run_date_time = NA,
-    write_to_disk = TRUE) {
+  data,
+  year,
+  ch_costs = read_file(get_ch_costs_path(BYOC_MODE)),
+  BYOC_MODE = FALSE,
+  run_id = NA,
+  run_date_time = NA,
+  write_to_disk = TRUE
+) {
   log_slf_event(stage = "process", status = "start", type = "ch", year = year)
 
   # Only run for a single year
