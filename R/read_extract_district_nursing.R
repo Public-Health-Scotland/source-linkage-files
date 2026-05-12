41a87742bd0144efa77251b7f447793eba3bcbb0
LucyEmma22 <lucyemma02@gmail.com>
2026-05-12 17:05:21 +0000

    Refactor read_extract_district_nursing

--- R/read_extract_district_nursing.R
+++ R/read_extract_district_nursing.R
@@ -3,59 +3,45 @@
 #' @inherit read_extract_acute
 #'
 #' @export
-read_extract_district_nursing <- function(
-  year,
-  file_path = get_boxi_extract_path(year = year, type = "dn")
-) {
+read_extract_district_nursing <- function(year,
+                                          denodo_connect = get_denodo_connection(BYOC_MODE = BYOC_MODE),
+                                          file_path = get_boxi_extract_path(year = year, type = "dn", BYOC_MODE),
+                                          BYOC_MODE) {
   log_slf_event(stage = "read", status = "start", type = "dn", year = year)
 
-  if (file_path == get_dummy_boxi_extract_path()) {
+  if (file_path == get_dummy_boxi_extract_path(BYOC_MODE = BYOC_MODE)) {
     return(tibble::tibble())
   }
 
+  on.exit(try(DBI::dbDisconnect(denodo_connect), silent = TRUE), add = TRUE)
+
   # Read BOXI extract
-  extract_district_nursing <- read_file(file_path,
-    col_types = readr::cols_only(
-      `Treatment NHS Board Code 9` = readr::col_character(),
-      `Age at Contact Date` = readr::col_integer(),
-      `Contact Date` = readr::col_date(format = "%Y/%m/%d %T"),
-      `Primary Intervention Category` = readr::col_character(),
-      `Other Intervention Category (1)` = readr::col_character(),
-      `Other Intervention Category (2)` = readr::col_character(),
-      `anon_chi` = readr::col_character(),
-      `Patient DoB Date [C]` = readr::col_date(format = "%Y/%m/%d %T"),
-      `Patient Postcode [C] (Contact)` = readr::col_character(),
-      `Duration of Contact (measure)` = readr::col_double(),
-      Gender = readr::col_double(),
-      `Location of Contact` = readr::col_character(),
-      `Practice NHS Board Code 9 (Contact)` = readr::col_character(),
-      `Patient Council Area Code (Contact)` = readr::col_character(),
-      `Practice Code (Contact)` = readr::col_character(),
-      `NHS Board of Residence Code 9 (Contact)` = readr::col_character(),
-      `HSCP of Residence Code (Contact)` = readr::col_character(),
-      `Patient Data Zone 2011 (Contact)` = readr::col_character()
-    )
-  ) %>%
-    # rename
-    dplyr::rename(
-      age = "Age at Contact Date",
-      dob = "Patient DoB Date [C]",
-      gender = "Gender",
-      hscp = "HSCP of Residence Code (Contact)",
-      hbrescode = "NHS Board of Residence Code 9 (Contact)",
-      lca = "Patient Council Area Code (Contact)",
-      postcode = "Patient Postcode [C] (Contact)",
-      gpprac = "Practice Code (Contact)",
-      hbpraccode = "Practice NHS Board Code 9 (Contact)",
-      hbtreatcode = "Treatment NHS Board Code 9",
-      anon_chi = "anon_chi",
-      record_keydate1 = "Contact Date",
-      primary_intervention = "Primary Intervention Category",
-      intervention_1 = "Other Intervention Category (1)",
-      intervention_2 = "Other Intervention Category (2)",
-      duration_contact = "Duration of Contact (measure)",
-      location_contact = "Location of Contact"
-    )
+  extract_district_nursing <- dplyr::tbl(
+    denodo_connect,
+    dbplyr::in_schema("sdl", "sdl_district_nursing_source")) %>% # TODO: Check table name.
+    # TODO: Check whether to filter by year.
+    dplyr::select(
+      age = "age_at_contact_date",
+      dob = "patient_dob_date",
+      gender = "gender",
+      hscp = "hscp_of_residence_code",
+      hbrescode = "nhs_board_of_residence_9",
+      lca = "patient_council_area",
+      postcode = "patient_postcode",
+      gpprac = "practice_code",
+      hbpraccode = "practice_nhs_board_code_9",
+      hbtreatcode = "treatment_nhs_board_code_9",
+      chi = "patient_chi",
+      record_keydate1 = "contact_date",
+      primary_intervention = "primary_intervention_category",
+      intervention_1 = "other_intervention_category_1",
+      intervention_2 = "other_intervention_category_2",
+      duration_contact = "duration_of_contact",
+      location_contact = "location_of_contact",
+      `Patient Data Zone 2011 (Contact)` = "patient_data_zone_2011"
+    ) %>%
+    dplyr::collect() %>%
+    slfhelper::get_anon_chi("chi")
 
   log_slf_event(stage = "read", status = "complete", type = "dn", year = year)
 
