1fedfdad3f5ef7a85bdf6ee8ddf94292b240b0fb
Zihao Li <zihao.li@phs.scot>
2026-06-04 14:06:25 +0000

    using arrow read parquet

--- SDL_process/dummy_targets.R
+++ SDL_process/dummy_targets.R
@@ -200,7 +200,7 @@ list(
       # use this anonymous function with redundant but necessary refined_death
       # to make sure reading year-specific NRS deaths extracts after it is produced
       (\(year, refined_death_data) {
-        createslf::read_file(get_source_extract_path(year, "nrs_deaths", BYOC_MODE = BYOC_MODE)) %>%
+        arrow::read_parquet(get_source_extract_path(year, "nrs_deaths", BYOC_MODE = BYOC_MODE)) %>%
           as.data.frame()
       })(year, refined_death_data)
     )
