35c2547776e6bbf391aa8a25e55cc938463996e7
jennit07 <Jennifer.Thom@phs.scot>
2026-06-30 12:42:12 +0000

    Initial commit - NSU/HHG

---
+++ SDL_automated_scripts/Combine_hhg.R
@@ -0,0 +1,46 @@
+##########################
+# Combine HHG files
+#
+# Original Author - Jennifer Thom
+# Original Date - June 2026
+# Written/run on - R Posit
+# Version of R - 4.2.2
+##########################
+
+
+## Stage 1 - Setup environment
+#-------------------------------------------------------------------------------
+# load package
+devtools::load_all()
+
+# Define the source directory and financial year pattern
+hhg_dir <- "/conf/hscdiip/SLF_Extracts/HHG"
+pattern <- "anon-HHG-20(\\d{4})\\.parquet"
+output_dir <- "/conf/hscdiip/SDL_submission/hhg"
+
+# List all the CSV files in the source directory
+cat(stringr::str_glue("Looking in '{hhg_dir}' for parquet files."))
+parquet_files <- list.files(hhg_dir, pattern = pattern, full.names = TRUE)
+print(stringr::str_glue("Found {length(parquet_files)} parquet files to process."))
+
+
+## Stage 2 - Read and clean columns before binding
+#-------------------------------------------------------------------------------
+hhg_files <- purrr::map(parquet_files,
+                        function(hhg_files){
+
+                          year <- stringr::str_extract(basename(hhg_files), "(\\d{6})")
+                          year <- stringr::str_sub(year, 3, 6)
+                          df <- read_file(hhg_files) %>%
+                            dplyr::mutate(year = year)
+                          current_names <- names(df)
+                          return(df)
+                        }
+)
+
+## Stage 3 - Bind files together and save to disk
+#-------------------------------------------------------------------------------
+hhg_combined_output <- dplyr::bind_rows(hhg_files)
+
+hhg_combined_output %>%
+  write_file(glue::glue("{output_dir}/SDL_HHG_20260617_1.parquet"))
---
+++ SDL_automated_scripts/Combine_nsu.R
@@ -0,0 +1,46 @@
+##########################
+# Combine NSU files
+#
+# Original Author - Jennifer Thom
+# Original Date - June 2026
+# Written/run on - R Posit
+# Version of R - 4.2.2
+##########################
+
+
+## Stage 1 - Setup environment
+#-------------------------------------------------------------------------------
+# load package
+devtools::load_all()
+
+# Define the source directory and financial year pattern
+nsu_dir <- "/conf/hscdiip/SLF_Extracts/NSU"
+pattern <- "anon-All_CHIs_20(\\d{4})\\.parquet"
+output_dir <- "/conf/hscdiip/SDL_submission/nsu"
+
+# List all the CSV files in the source directory
+cat(stringr::str_glue("Looking in '{nsu_dir}' for parquet files."))
+parquet_files <- list.files(nsu_dir, pattern = pattern, full.names = TRUE)
+print(stringr::str_glue("Found {length(parquet_files)} parquet files to process."))
+
+
+## Stage 2 - Read and clean columns before binding
+#-------------------------------------------------------------------------------
+nsu_files <- purrr::map(parquet_files,
+                 function(nsu_files){
+                   df <- read_file(nsu_files) %>%
+                     dplyr::mutate(gpprac = as.character(gpprac))
+                   current_names <- names(df)
+                    return(df)
+                 }
+)
+
+## Stage 3 - Bind files together and save to disk
+#-------------------------------------------------------------------------------
+nsu_combined_output <- dplyr::bind_rows(nsu_files)
+
+  nsu_combined_output %>%
+  write_file(glue::glue("{output_dir}/SDL_NSU_20260617_1.parquet"))
+
+
+## END OF SCRIPT ##
