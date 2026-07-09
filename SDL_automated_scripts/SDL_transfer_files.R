957192c8775deafd342659dc3ace116d39e3b3ba
Zihao Li <zihao.li@phs.scot>
2026-07-09 09:33:16 +0000

    add 6 csv files

--- SDL_automated_scripts/SDL_transfer_files.R
+++ SDL_automated_scripts/SDL_transfer_files.R
@@ -13,13 +13,26 @@
 devtools::load_all()
 
 # Define Submission date - ### UPDATE ###
-submission_date <- "20260630"
+submission_date <- "20260708"
 
 # Define version of file - ### UPDATE ###
 n <- "1"
 
 # Define list of files - ### UPDATE ###
-file_list <- c("dd", "hhg", "nsu", "spd", "simd")
+# file_list <- c(
+#   "dd",
+#   "dn",
+#   "dn_contacts",
+#   "hhg",
+#   "nsu",
+#   "spd",
+#   "simd",
+#   "cmh",
+#   "hc_cost_lookup",
+#   "dn_cost_lookup",
+#   "gpprac_lookup"
+# )
+file_list <- c("hc_cost_lookup", "dn_cost_lookup", "gpprac_lookup")
 
 #-------------------------------------------------------------------------------
 # Stage 2 - Set up functions
@@ -30,9 +43,15 @@ get_sdl_file_paths <- function(file_list) {
   # Match dataset to current file path
   # NOTE: all datasets should be fully combined at this stage.
   # e.g no year specific files
-  file_path <- dplyr::case_match(
+  file_path <- dplyr::recode_values(
     file_list,
+    "cmh" ~ get_combined_cmh_path(),
     "dd" ~ get_dd_path(),
+    "dn" ~ get_combined_dn_path(),
+    "dn_contacts" ~ get_sdl_dn_contacts_path(),
+    "dn_cost_lookup" ~ get_sdl_dn_costs_path(),
+    "gpprac_lookup" ~ get_sdl_gpprac_lookup_path(),
+    "hc_cost_lookup" ~ get_sdl_hc_costs_path(),
     "hhg" ~ get_combined_hhg_path(),
     "nsu" ~ get_combined_nsu_path(),
     "spd" ~ get_spd_path(),
@@ -51,9 +70,16 @@ get_sdl_output_path <- function(file_list) {
   output_dir <- stringr::str_glue("{top_dir}{file_list}/")
 
   # Match file to the new naming convention
-  output_name <- dplyr::case_match(
+  output_name <- dplyr::recode_values(
     file_list,
+    # for csv files, no need for `{n}`.
+    "cmh" ~ stringr::str_glue("SDL_cmh_{submission_date}.csv"),
     "dd" ~ stringr::str_glue("SDL_dd_{submission_date}_{n}.parquet"),
+    "dn" ~ stringr::str_glue("SDL_dn_{submission_date}.csv"),
+    "dn_contacts" ~ stringr::str_glue("SDL_dn_contacts_{submission_date}.csv"),
+    "dn_cost_lookup" ~ stringr::str_glue("SDL_dn_cost_lookup_{submission_date}.csv"),
+    "gpprac_lookup" ~ stringr::str_glue("SDL_gpprac_lookup_{submission_date}.csv"),
+    "hc_cost_lookup" ~ stringr::str_glue("SDL_hc_cost_lookup_{submission_date}.csv"),
     "hhg" ~ stringr::str_glue("SDL_hhg_{submission_date}_{n}.parquet"),
     "nsu" ~ stringr::str_glue("SDL_nsu_{submission_date}_{n}.parquet"),
     "spd" ~ stringr::str_glue("SDL_spd_{submission_date}_{n}.parquet"),
@@ -72,9 +98,7 @@ move_sdl_files <- function(sdl_files, file_list) {
 
   sdl_file <- read_file(sdl_files) %>%
     # Create a column containing the old file name
-    dplyr::mutate(
-      file_name = file_name
-    ) %>%
+    dplyr::mutate(file_name = file_name) %>%
     # write to disk with new name
     # format SDL_[DATASET]_[YYYYMMDD]_[N].parquet
     write_file(get_sdl_output_path(file_list))
