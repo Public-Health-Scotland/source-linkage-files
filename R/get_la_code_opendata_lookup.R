e8fa9e0637de3ef5b6a92d3f77ac360f59b28749
LucyEmma22 <lucyemma02@gmail.com>
2026-07-14 18:41:36 +0000

    Change get_la_code_opendata_lookup to read from denodo

--- R/get_la_code_opendata_lookup.R
+++ R/get_la_code_opendata_lookup.R
@@ -1,18 +1,37 @@
-#' Download the LA code lookup
+#' LA Code Lookup
 #'
-#' @description Download and process the Local Authority lookup from the Open
-#' Data platform
+#' @description Return the data for Local Authority lookup.
+#'
+#' @param denodo_connect Connection to denodo
+#' @param BYOC_MODE BYOC MODE
 #'
 #' @return a [tibble][tibble::tibble-package] with the Local Authority names
 #' and codes.
 #' @export
-get_la_code_opendata_lookup <- function() {
-  res_id <- "967937c4-8d67-4f39-974f-fd58c4acfda5"
-  la_code_lookup <- phsopendata::get_resource(
-    res_id = res_id,
-    col_select = c("CA", "CAName")
+#'
+#' @family lookup files
+get_la_code_opendata_lookup <- function(
+    denodo_connect = get_denodo_connection(BYOC_MODE = BYOC_MODE),
+    BYOC_MODE = BYOC_MODE
+) {
+  log_slf_event(stage = "read", status = "start", type = "la_code", year = "all") # TODO: Check type
+
+  # Denodo disconnect
+  on.exit(try(DBI::dbDisconnect(denodo_connect), silent = TRUE), add = TRUE)
+
+  # Read data
+  la_code_lookup <- dplyr::tbl(
+    denodo_connect,
+    dbplyr::in_schema("sdl", "sdl_la_code_source") # TODO: Check name
   ) %>%
+    # Rename variables
+    select(
+      CA = "CA",
+      CAName = "CAName"
+    ) %>%
     dplyr::distinct() %>%
+    # Collect
+    dplyr::collect() %>%
     dplyr::mutate(
       sending_local_authority_name = dplyr::recode_values(
         .data$CAName,
@@ -22,6 +41,8 @@ get_la_code_opendata_lookup <- function() {
       ) %>%
         stringr::str_replace("\\sand\\s", " \\& ")
     )
+
+  log_slf_event(stage = "read", status = "complete", type = "la_code", year = "all")
 
   return(la_code_lookup)
 }
