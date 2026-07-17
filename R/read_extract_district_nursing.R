b146c3ed41f15408f2c1aeb162264c024b8b342a
LucyEmma22 <lucyemma02@gmail.com>
2026-07-17 14:55:37 +0000

    Remove file path from read_extract_district_nursing

--- R/read_extract_district_nursing.R
+++ R/read_extract_district_nursing.R
@@ -3,27 +3,35 @@
 #' @inherit read_extract_acute
 #'
 #' @export
-read_extract_district_nursing <- function(year,
-                                          denodo_connect = get_denodo_connection(BYOC_MODE = BYOC_MODE),
-                                          file_path = get_boxi_extract_path(year = year, type = "dn", BYOC_MODE),
-                                          BYOC_MODE) {
+read_extract_district_nursing <- function(
+    year,
+    denodo_connect = get_denodo_connection(BYOC_MODE = BYOC_MODE),
+    BYOC_MODE
+) {
   log_slf_event(stage = "read", status = "start", type = "dn", year = year)
 
-  if (file_path == get_dummy_boxi_extract_path(BYOC_MODE = BYOC_MODE)) {
-    return(tibble::tibble())
-  }
-
-  on.exit(try(DBI::dbDisconnect(denodo_connect), silent = TRUE), add = TRUE)
-
+  # Check and convert to calendar year
   year <- check_year_format(year, format = "fyyear")
   c_year <- convert_fyyear_to_year(year)
 
-  # Read BOXI extract
+  # Specify years available for running
+  if (!check_year_valid(year, type = "dn")) {
+    return(tibble::tibble())
+  }
+
+  # Denodo disconnect
+  on.exit(try(DBI::dbDisconnect(denodo_connect), silent = TRUE), add = TRUE)
+
+  # Read extract
   extract_district_nursing <- dplyr::tbl(
     denodo_connect,
-    dbplyr::in_schema("sdl", "sdl_district_nursing_source")
-  ) %>% # TODO: Check table name.
-    dplyr::filter(year == c_year) %>% # TODO: Check year column name.
+    dbplyr::in_schema("sdl", "sdl_dn_source")
+  ) %>%
+    # Filter by calendar year
+    dplyr::filter(
+      year == c_year # TODO: Data has no year column to filter on. Closest is contact_date
+    ) %>%
+    # Rename variables
     dplyr::select(
       age = "age_at_contact_date",
       dob = "patient_dob_date",
@@ -35,7 +43,7 @@ read_extract_district_nursing <- function(year,
       gpprac = "practice_code",
       hbpraccode = "practice_nhs_board_code_9",
       hbtreatcode = "treatment_nhs_board_code_9",
-      chi = "patient_chi",
+      anon_chi = "patient_chi",
       record_keydate1 = "contact_date",
       primary_intervention = "primary_intervention_category",
       intervention_1 = "other_intervention_category_1",
@@ -44,8 +52,8 @@ read_extract_district_nursing <- function(year,
       location_contact = "location_of_contact",
       `Patient Data Zone 2011 (Contact)` = "patient_data_zone_2011"
     ) %>%
-    dplyr::collect() %>%
-    slfhelper::get_anon_chi("chi")
+    # Collect
+    dplyr::collect()
 
   log_slf_event(stage = "read", status = "complete", type = "dn", year = year)
 
