e1e7e9e4a7b5994f64c8c1677ed8fb2aa16d0671
LucyEmma22 <lucyemma02@gmail.com>
2026-07-30 12:21:00 +0000

    Move get functions to seperate scripts

--- R/get_costs_paths.R
+++ R/get_costs_paths.R
@@ -69,51 +69,6 @@ get_dn_raw_costs_path <- function(...) {
   return(dn_raw_costs_path)
 }
 
-#' Raw District Nursing Costs Data
-#'
-#' @description Return the data for District Nursing raw costs.
-#'
-#' @param denodo_connect Connection to denodo
-#' @param BYOC_MODE BYOC MODE
-#'
-#' @return a [tibble][tibble::tibble-package].
-#' @export
-#'
-#' @family lookup files
-get_dn_raw_costs_data <- function(
-    denodo_connect = get_denodo_connection(BYOC_MODE = BYOC_MODE),
-    BYOC_MODE
-) {
-  log_slf_event(stage = "read", status = "start", type = "dn_cost_lookup", year = "all")
-
-  # Denodo disconnect
-  on.exit(try(DBI::dbDisconnect(denodo_connect), silent = TRUE), add = TRUE)
-
-  # Read data
-  dn_raw_costs_data <- dplyr::tbl(
-    denodo_connect,
-    dbplyr::in_schema("sdl", "sdl_dn_cost_lookup_source")
-  ) %>%
-    # Rename variables
-    dplyr::select(
-      hb2019 = "hb2019",
-      board_name = "board_name",
-      board_cypher = "board_cypher",
-      year = "year",
-      cost = "cost"
-    ) %>%
-    # Collect
-    dplyr::collect() %>%
-    # Data type modification
-    dplyr::mutate(
-      year = check_year_format(.data$year)
-    )
-
-  log_slf_event(stage = "read", status = "complete", type = "dn_cost_lookup", year = "all")
-
-  return(dn_raw_costs_data)
-}
-
 #' District Nursing Contacts File Path - LOCAL ONLY
 #'
 #' @description Get the District Nursing contacts path - LOCAL ONLY
@@ -132,46 +87,6 @@ get_dn_contacts_path <- function(...) {
   )
 
   return(dn_contacts_path)
-}
-
-#' District Nursing Contacts Data
-#'
-#' @description Return the data for District Nursing contacts.
-#'
-#' @param denodo_connect Connection to denodo
-#' @param BYOC_MODE BYOC MODE
-#'
-#' @return a [tibble][tibble::tibble-package].
-#' @export
-#'
-#' @family lookup files
-get_dn_contacts_data <- function(
-    denodo_connect = get_denodo_connection(BYOC_MODE = BYOC_MODE),
-    BYOC_MODE
-) {
-  log_slf_event(stage = "read", status = "start", type = "dn_contact_lookup", year = "all")
-
-  # Denodo disconnect
-  on.exit(try(DBI::dbDisconnect(denodo_connect), silent = TRUE), add = TRUE)
-
-  # Read data
-  dn_contacts_data <- dplyr::tbl(
-    denodo_connect,
-    dbplyr::in_schema("sdl", "sdl_dn_contacts_source")
-  ) %>%
-    # Rename variables
-    dplyr::select(
-      contact_financial_year = "contact_financial_year",
-      hb2019 = "treatment_nhs_board_code_9",
-      treatment_nhs_board_name = "treatment_nhs_board_name",
-      number_of_contacts = "number_of_contacts"
-    ) %>%
-    # Collect
-    dplyr::collect()
-
-  log_slf_event(stage = "read", status = "complete", type = "dn_contact_lookup", year = "all")
-
-  return(dn_contacts_data)
 }
 
 #' GP Out of Hours Costs File Path
---
+++ R/get_dn_raw_costs_data.R
@@ -0,0 +1,44 @@
+#' Raw District Nursing Costs Data
+#'
+#' @description Return the data for District Nursing raw costs.
+#'
+#' @param denodo_connect Connection to denodo
+#' @param BYOC_MODE BYOC MODE
+#'
+#' @return a [tibble][tibble::tibble-package].
+#' @export
+#'
+#' @family lookup files
+get_dn_raw_costs_data <- function(
+    denodo_connect = get_denodo_connection(BYOC_MODE = BYOC_MODE),
+    BYOC_MODE
+) {
+  log_slf_event(stage = "read", status = "start", type = "dn_cost_lookup", year = "all")
+
+  # Denodo disconnect
+  on.exit(try(DBI::dbDisconnect(denodo_connect), silent = TRUE), add = TRUE)
+
+  # Read data
+  dn_raw_costs_data <- dplyr::tbl(
+    denodo_connect,
+    dbplyr::in_schema("sdl", "sdl_dn_cost_lookup_source")
+  ) %>%
+    # Rename variables
+    dplyr::select(
+      hb2019 = "hb2019",
+      board_name = "board_name",
+      board_cypher = "board_cypher",
+      year = "year",
+      cost = "cost"
+    ) %>%
+    # Collect
+    dplyr::collect() %>%
+    # Data type modification
+    dplyr::mutate(
+      year = check_year_format(.data$year)
+    )
+
+  log_slf_event(stage = "read", status = "complete", type = "dn_cost_lookup", year = "all")
+
+  return(dn_raw_costs_data)
+}
