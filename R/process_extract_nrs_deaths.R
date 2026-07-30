0f2592aa8451edbf03968147c6027a384477619a
Lucy Binsted <78858900+LucyEmma22@users.noreply.github.com>
2026-07-30 10:33:12 +0100

    Minor formatting

--- R/process_extract_nrs_deaths.R
+++ R/process_extract_nrs_deaths.R
@@ -26,7 +26,12 @@ process_extract_nrs_deaths <- function(data,
   stopifnot(length(year) == 1L)
 
   # Check that the supplied year is in the correct format
-  year <- check_year_format(year)
+  year <- check_year_format(year, format = "fyyear")
+
+  # If no data is available in the FY then return immediately.
+  if (identical(data, tibble::tibble())) {
+    return(data)
+  }
 
   # Data Cleaning  ---------------------------------------
 
