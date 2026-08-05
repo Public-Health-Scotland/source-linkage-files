33822fd67251c606c6520ac6f0c91115fb71cab3
LucyEmma22 <lucyemma02@gmail.com>
2026-08-05 15:58:34 +0000

    Remove get sg_pub_data from produce_homelessness_completeness.R

--- R/produce_homelessness_completeness.R
+++ R/produce_homelessness_completeness.R
@@ -150,40 +150,6 @@ get_sg_homelessness_pub_path <- function(...) {
 }
 
 
-#' Homelessness Completeness SG publication figures
-#'
-#' @description Return the data for Homelessness Completeness lookup.
-#'
-#' @param denodo_connect Connection to denodo
-#' @param BYOC_MODE BYOC MODE
-#'
-#' @return a [tibble][tibble::tibble-package].
-#' @export
-#'
-#' @family lookup files
-get_sg_pub_data <- function(
-  denodo_connect = get_denodo_connection(BYOC_MODE = BYOC_MODE),
-  BYOC_MODE = BYOC_MODE
-) {
-  log_slf_event(stage = "read", status = "start", type = "homelessness_completeness", year = "all") # TODO: Check type
-
-  # Denodo disconnect
-  on.exit(try(DBI::dbDisconnect(denodo_connect), silent = TRUE), add = TRUE)
-
-  # Read data
-  sg_pub_data <- dplyr::tbl(
-    denodo_connect,
-    dbplyr::in_schema("sdl", "sdl_homelessness_completeness_source")
-  ) %>%
-    # Collect
-    dplyr::collect()
-
-  log_slf_event(stage = "read", status = "complete", type = "homelessness_completeness", year = "all")
-
-  return(get_sg_pub_data)
-}
-
-
 #' Homelessness Completeness lookup path
 #'
 #' @description Get the path to the Homelessness Completeness lookup. This file
@@ -206,7 +172,7 @@ get_homelessness_completeness_path <- function(
 ) {
   if (BYOC_MODE) {
     completeness_file_path <- fs::path(
-      directory = denodo_output_path(), # todo: waiting to be finalised
+      directory = denodo_output_path(),
       file_name = stringr::str_glue(
         "homelessness_completeness_{year}_{update}.parquet"
       )
