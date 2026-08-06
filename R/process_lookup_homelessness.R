3e1fe9aaf97dc5e5666f3d4aec9423305d52cca6
LucyEmma22 <78858900+LucyEmma22@users.noreply.github.com>
2026-08-06 14:48:53 +0000

    Style code

--- R/process_lookup_homelessness.R
+++ R/process_lookup_homelessness.R
@@ -13,7 +13,8 @@
 create_homelessness_lookup <- function(
   year,
   homelessness_data = read_file(
-    get_source_extract_path(year = year, type = "homelessness", BYOC_MODE = BYOC_MODE))
+    get_source_extract_path(year = year, type = "homelessness", BYOC_MODE = BYOC_MODE)
+  )
 ) {
   log_slf_event(stage = "process", status = "start", type = "homelessness_lookup", year = year)
 
