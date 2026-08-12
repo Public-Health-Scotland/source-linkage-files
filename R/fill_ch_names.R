211f65a5ab44649bffb2e3f5be39abde4aa75d82
jennit07 <Jennifer.Thom@phs.scot>
2026-08-12 14:52:10 +0000

    Update fill_ch_names

--- R/fill_ch_names.R
+++ R/fill_ch_names.R
@@ -5,15 +5,18 @@
 #'
 #' @return the same data with improved accuracy and completeness of the Care
 #' Home names and postcodes, as a [tibble][tibble::tibble-package].
-fill_ch_names <- function(ch_data) {
+fill_ch_names <- function(ch_data,
+                          spd_data = get_spd_data(BYOC_MODE = BYOC_MODE),
+                          uk_pc_list = get_uk_postcode_data(BYOC_MODE = BYOC_MODE),
+                          ch_name_lookup = get_slf_ch_name_lookup_data(BYOC_MODE = BYOC_MODE)) {
   # fix the issue "no visible binding for global variable x, y"
   x <- y <- NULL
 
-  spd_list <- get_spd_data(BYOC_MODE) %>%
+  spd_list <- spd_data %>%
     dplyr::pull(.data$pc7)
 
-  uk_pc_list <- get_uk_postcode_data(BYOC_MODE) %>%
-    dplyr::pull()
+  uk_pc_list <- uk_pc_list %>%
+    dplyr::pull("pcd")
 
 
   ch_data <- ch_data %>%
@@ -48,7 +51,7 @@ fill_ch_names <- function(ch_data) {
 
   # Contact: IntelligenceTeam@careinspectorate.gov.scot
   # for an updated lookup list
-  ch_name_lookup <- get_slf_ch_name_lookup_data(BYOC_MODE) %>%
+  ch_name_lookup <- ch_name_lookup %>%
     # Drop any Care Homes that were closed before 2017/18
     dplyr::select(
       ch_postcode = "AccomPostCodeNo",
