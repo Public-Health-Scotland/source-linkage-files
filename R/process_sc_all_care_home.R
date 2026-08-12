95b8d4d2c5440683281f143edbc4e5d8a338de9d
jennit07 <Jennifer.Thom@phs.scot>
2026-08-12 14:54:10 +0000

    Update process ALL CHs

--- R/process_sc_all_care_home.R
+++ R/process_sc_all_care_home.R
@@ -10,8 +10,8 @@
 #' @param refined_death The processed lookup of deaths from IT produced
 #' with [process_refined_death()].
 #' @param uk_pc_directory UK postcode directory
-#' @param ch_name_lookup_path Care home name lookup
-#' @param spd_path Scottish postcode directory
+#' @param ch_name_lookup Care home name lookup
+#' @param spd_dataScottish postcode directory
 #' @param BYOC_MODE BYOC_MODE
 #' @param run_id Denodo identifier
 #' @param run_date_time Denodo identifier
@@ -24,11 +24,11 @@
 #' @export
 process_sc_all_care_home <- function(
   data,
-  sc_demog_lookup = read_file(get_sc_demog_lookup_path(BYOC_MODE)), ### TODO - SDL/Denodo name??
-  refined_death = read_file(get_combined_slf_deaths_lookup_path(BYOC_MODE)), ### TODO - SDL/Denodo name??
-  uk_pc_directory = get_uk_postcode_data(BYOC_MODE), ### TODO - SDL/Denodo name??
-  ch_name_lookup_path = get_slf_ch_name_lookup_data(BYOC_MODE), ### TODO - SDL/Denodo name??
-  spd_path = get_spd_data(BYOC_MODE), ### TODO - SDL/DENODO NAME??
+  sc_demog_lookup = read_file(get_sc_demog_lookup_path(BYOC_MODE = BYOC_MODE)), ### TODO - SDL/Denodo name??
+  refined_death = read_file(get_combined_slf_deaths_lookup_path(BYOC_MODE = BYOC_MODE)), ### TODO - SDL/Denodo name??
+  uk_pc_directory = get_uk_postcode_data(BYOC_MODE = BYOC_MODE),
+  ch_name_lookup = get_slf_ch_name_lookup_data(BYOC_MODE = BYOC_MODE),
+  spd_data = get_spd_data(BYOC_MODE = BYOC_MODE),
   BYOC_MODE = FALSE,
   run_id = NA,
   run_date_time = NA,
@@ -93,8 +93,9 @@ process_sc_all_care_home <- function(
   # cleaning and matching care home names
   name_postcode_clean <- fill_ch_names(
     ch_data = data,
-    ch_name_lookup_path = ch_name_lookup_path,
-    spd_path = spd_path
+    spd_data = spd_data,
+    uk_pc_list = uk_pc_directory,
+    ch_name_lookup = get_slf_ch_name_lookup_data(BYOC_MODE = BYOC_MODE)
   )
 
   fixed_ch_provider <- name_postcode_clean %>%
