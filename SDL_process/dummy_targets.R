57ee71835b5c9a02264d819e089b448778acac1b
LucyEmma22 <lucyemma02@gmail.com>
2026-08-25 15:58:50 +0000

    Comment out unused targets

--- SDL_process/dummy_targets.R
+++ SDL_process/dummy_targets.R
@@ -206,55 +206,55 @@ list(
   # ),
 
   ### GP Out of Hours costs------
-  tar_target(
-    # Target name
-    gp_ooh_cost_lookup,
-    # Function
-    process_costs_gp_ooh(BYOC_MODE = BYOC_MODE)
-  ),
+  # tar_target(
+  #   # Target name
+  #   gp_ooh_cost_lookup,
+  #   # Function
+  #   process_costs_gp_ooh(BYOC_MODE = BYOC_MODE)
+  # ),
 
   ## Stage 2.2 year specific targets ------
   tar_map(
     list(year = years_to_run),
 
     ### Accident & Emergency (AE2) activity --------------
-    # READ - A&E
-    tar_target(
-      # Target name
-      ae_data,
-      # Function
-      read_extract_ae(
-        year = year,
-        denodo_connect = get_denodo_connection(BYOC_MODE = BYOC_MODE),
-        BYOC_MODE = BYOC_MODE
-      )
-    ),
-    # READ - A&E CUP
-    tar_target(
-      # Target name
-      ae_cup_file,
-      # Function
-      read_extract_ae_cup(
-        year = year,
-        denodo_connect = get_denodo_connection(BYOC_MODE = BYOC_MODE),
-        BYOC_MODE = BYOC_MODE
-      )
-    ),
-    # PROCESS - A&E
-    tar_target(
-      # Target name
-      source_ae_extract,
-      # Function
-      process_extract_ae(
-        data = ae_data,
-        year = year,
-        ae_cup_file = ae_cup_file,
-        write_to_disk = write_to_disk,
-        BYOC_MODE = BYOC_MODE,
-        run_id = run_id,
-        run_date_time = run_date_time
-      )
-    ),
+    # # READ - A&E
+    # tar_target(
+    #   # Target name
+    #   ae_data,
+    #   # Function
+    #   read_extract_ae(
+    #     year = year,
+    #     denodo_connect = get_denodo_connection(BYOC_MODE = BYOC_MODE),
+    #     BYOC_MODE = BYOC_MODE
+    #   )
+    # ),
+    # # READ - A&E CUP
+    # tar_target(
+    #   # Target name
+    #   ae_cup_file,
+    #   # Function
+    #   read_extract_ae_cup(
+    #     year = year,
+    #     denodo_connect = get_denodo_connection(BYOC_MODE = BYOC_MODE),
+    #     BYOC_MODE = BYOC_MODE
+    #   )
+    # ),
+    # # PROCESS - A&E
+    # tar_target(
+    #   # Target name
+    #   source_ae_extract,
+    #   # Function
+    #   process_extract_ae(
+    #     data = ae_data,
+    #     year = year,
+    #     ae_cup_file = ae_cup_file,
+    #     write_to_disk = write_to_disk,
+    #     BYOC_MODE = BYOC_MODE,
+    #     run_id = run_id,
+    #     run_date_time = run_date_time
+    #   )
+    # ),
 
     ### Maternity (SMR02) Acitivity----
     # # READ - Maternity
