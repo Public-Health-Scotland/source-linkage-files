cbf54c3e936cbe8882082a32b874a5c9941501fc
Zihao Li <zihao.li@phs.scot>
2026-09-07 15:24:32 +0000

    update compare_variable_type

--- SDL_process/compare_variable_type.R
+++ SDL_process/compare_variable_type.R
@@ -127,9 +127,15 @@ normalise_type <- function(x) {
 # Running Loop ----
 for (ii in 1:length(datasets)) {
   logger::log_info(paste0("start ", ii, ", ", datasets[ii]))
-  file_path <- mapping$file_name[ii]
-  dataset_id <- mapping$dataset_id[ii]
-  spec_sheetname <- mapping$spec_sheetname[ii]
+  file_path <- mapping %>% filter(
+    dataset_id == datasets[ii]
+  ) %>%
+    pull(file_name)
+  dataset_id <- datasets[ii]
+  spec_sheetname <- mapping %>% filter(
+    dataset_id == datasets[ii]
+  ) %>%
+    pull(spec_sheetname)
 
   # Example dataframe
   df <- createslf::read_file(file_path)
