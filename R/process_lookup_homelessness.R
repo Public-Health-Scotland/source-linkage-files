d1976817f44b16720e9cd0169cc5102c4726f5a7
Zihao Li <zihao.li@phs.scot>
2026-09-09 10:55:38 +0000

    reading intermediate files during updating/targets

--- R/process_lookup_homelessness.R
+++ R/process_lookup_homelessness.R
@@ -15,7 +15,6 @@ create_homelessness_lookup <- function(
   homelessness_data = read_file(
     get_source_extract_path(year = year, type = "homelessness", BYOC_MODE = BYOC_MODE)
   )
-  # TODO: homelessness_data = get_sdl_processed_data(type = "homelessness", year = year, BYOC_MODE = BYOC_MODE)
 ) {
   log_slf_event(stage = "process", status = "start", type = "homelessness_lookup", year = year)
 
