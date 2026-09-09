0399ec7e4509433e63b4ee6230bb0733e8a35c9c
LucyEmma22 <lucyemma02@gmail.com>
2026-09-09 12:33:25 +0000

    Change year filtering

--- R/read_extract_district_nursing.R
+++ R/read_extract_district_nursing.R
@@ -10,14 +10,8 @@ read_extract_district_nursing <- function(
 ) {
   log_slf_event(stage = "read", status = "start", type = "dn", year = year)
 
-  # Check and convert to calendar year
+  # Check year
   year <- check_year_format(year, format = "fyyear")
-  c_year <- convert_fyyear_to_year(year)
-
-  # Create dates to filter by calendar year
-  # TODO: Check logic and whether this is the correct year column.
-  start_date <- as.Date(paste0(substr(c_year, 1, 4), "-03-31"))
-  end_date <- as.Date(paste0(as.integer(substr(c_year, 1, 4)) + 1, "-04-01"))
 
   # Specify years available for running
   if (!check_year_valid(year, type = "dn")) {
@@ -33,10 +27,8 @@ read_extract_district_nursing <- function(
     dbplyr::in_schema("sdl", "sdl_dn_source")
   ) %>%
     # Filter by calendar year
-    # TODO: Check logic and whether this is the correct year column.
     dplyr::filter(
-      .data$contact_date > start_date,
-      .data$contact_date < end_date
+      .data$year == year
     ) %>%
     # Rename variables
     dplyr::select(
