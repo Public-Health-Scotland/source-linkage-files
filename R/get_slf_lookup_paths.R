773f5446cf82b8a2343258a329816e79bb666b94
jennit07 <Jennifer.Thom@phs.scot>
2026-09-15 14:22:58 +0000

    Split function to own script

--- R/get_slf_lookup_paths.R
+++ R/get_slf_lookup_paths.R
@@ -32,30 +32,6 @@ get_uk_postcode_path <- function(...) {
 }
 
 
-#' get uk postcode list file data
-#' @description get uk postcode list file
-#' @param denodo_connect connection to denodo
-#' @param BYOC_MODE BYOC_MODE
-#' @family lookup file paths
-get_uk_postcode_data <- function(denodo_connect = get_denodo_connection(BYOC_MODE = BYOC_MODE),
-                                 BYOC_MODE) {
-  log_slf_event(stage = "read", status = "start", type = "uk_postcode", year = "all")
-
-  # Disconnect from Denodo
-  on.exit(try(DBI::dbDisconnect(denodo_connect), silent = TRUE), add = TRUE)
-
-  extract_uk_pc <- dplyr::tbl(
-    denodo_connect,
-    dbplyr::in_schema("sdl", "sdl_uk_postcode_list_source")
-  ) %>%
-    dplyr::collect()
-
-  log_slf_event(stage = "read", status = "complete", type = "uk_postcode", year = "all")
-
-  return(extract_uk_pc)
-}
-
-
 #' SLF GP Lookup File Path
 #'
 #' @description Get the full path to the SLF GP practice lookup
